cdmihai/cli

Allow false positive rebuilds when timestamp collision occurs
Fixes #965

src/Microsoft.DotNet.Tools.Builder/CompileContext.cs
@@ -121,7 +121,7 @@ namespace Microsoft.DotNet.Tools.Build
             }
 
             // find inputs that are older than the earliest output
-            var newInputs = compilerIO.Inputs.FindAll(p => File.GetLastWriteTime(p) > minDate);
+            var newInputs = compilerIO.Inputs.FindAll(p => File.GetLastWriteTime(p) >= minDate);
 
             if (!newInputs.Any())
             {

dotnet/sdk

src/dotnet/commands/dotnet-build/CompileContext.cs
@@ -130,7 +130,7 @@ namespace Microsoft.DotNet.Tools.Build
             }
 
             // find inputs that are older than the earliest output
-            var newInputs = compilerIO.Inputs.FindAll(p => File.GetLastWriteTimeUtc(p) > minDateUtc);
+            var newInputs = compilerIO.Inputs.FindAll(p => File.GetLastWriteTimeUtc(p) >= minDateUtc);
 
             if (!newInputs.Any())
             {