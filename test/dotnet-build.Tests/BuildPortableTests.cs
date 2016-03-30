dotnet/cli

Adding a runtimeconfig.dev.json containing the additionalProbePaths when doing dotnet build.

test/dotnet-build.Tests/BuildPortableTests.cs
@@ -6,8 +6,9 @@ namespace Microsoft.DotNet.Tools.Builder.Tests
 {
     public class BuildPortableTests : TestBase
     {
-        [Fact]
-        public void BuildingAPortableProjectProducesDepsFile()
+        private readonly DirectoryInfo _netstandardappOutput;
+        
+        public BuildPortableTests()
         {
             var testInstance = TestAssetsManager.CreateTestInstance("PortableTests")
                 .WithLockFiles();
@@ -20,17 +21,43 @@ namespace Microsoft.DotNet.Tools.Builder.Tests
 
             var outputBase = new DirectoryInfo(Path.Combine(testInstance.TestRoot, "PortableApp", "bin", "Debug"));
 
-            var netstandardappOutput = outputBase.Sub("netstandard1.5");
-
-            netstandardappOutput.Should()
-                .Exist().And
-                .HaveFiles(new[]
-                {
-                    "PortableApp.deps",
-                    "PortableApp.deps.json",
-                    "PortableApp.dll",
-                    "PortableApp.pdb"
-                });
+            _netstandardappOutput = outputBase.Sub("netstandard1.5");
+        }
+        
+        [Fact]
+        public void BuildingAPortableProjectProducesDepsFile()
+        {
+            _netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.deps");
+        }
+        
+        [Fact]
+        public void BuildingAPortableProjectProducesDepsJsonFile()
+        {
+            _netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.deps.json");
+        }
+        
+        [Fact]
+        public void BuildingAPortableProjectProducesADllFile()
+        {
+            _netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.dll");
+        }
+        
+        [Fact]
+        public void BuildingAPortableProjectProducesAPdbFile()
+        {
+            _netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.pdb");
+        }
+        
+        [Fact]
+        public void BuildingAPortableProjectProducesARuntimeConfigJsonFile()
+        {
+            _netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.runtimeconfig.json");
+        }
+        
+        [Fact]
+        public void BuildingAPortableProjectProducesARuntimeConfigDevJsonFile()
+        {
+            _netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.runtimeconfig.dev.json");
         }
     }
 }

dotnet/sdk

test/dotnet-build.Tests/BuildPortableTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Microsoft.DotNet.Tools.Test.Utilities;
 using Xunit;
+using Microsoft.DotNet.TestFramework;
 
 namespace Microsoft.DotNet.Tools.Builder.Tests
 {
@@ -9,9 +10,65 @@ namespace Microsoft.DotNet.Tools.Builder.Tests
         [Fact]
         public void BuildingAPortableProjectProducesDepsFile()
         {
-            var testInstance = TestAssetsManager.CreateTestInstance("PortableTests")
-                .WithLockFiles();
+            var testInstance = TestAssetsManager.CreateTestInstance("PortableTests").WithLockFiles();
 
+            var netstandardappOutput = Build(testInstance);
+
+            netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.deps");
+        }
+
+        [Fact]
+        public void BuildingAPortableProjectProducesDepsJsonFile()
+        {
+            var testInstance = TestAssetsManager.CreateTestInstance("PortableTests").WithLockFiles();
+
+            var netstandardappOutput = Build(testInstance);
+
+            netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.deps.json");
+        }
+
+        [Fact]
+        public void BuildingAPortableProjectProducesADllFile()
+        {
+            var testInstance = TestAssetsManager.CreateTestInstance("PortableTests").WithLockFiles();
+
+            var netstandardappOutput = Build(testInstance);
+
+            netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.dll");
+        }
+
+        [Fact]
+        public void BuildingAPortableProjectProducesAPdbFile()
+        {
+            var testInstance = TestAssetsManager.CreateTestInstance("PortableTests").WithLockFiles();
+
+            var netstandardappOutput = Build(testInstance);
+
+            netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.pdb");
+        }
+
+        [Fact]
+        public void BuildingAPortableProjectProducesARuntimeConfigJsonFile()
+        {
+            var testInstance = TestAssetsManager.CreateTestInstance("PortableTests").WithLockFiles();
+
+            var netstandardappOutput = Build(testInstance);
+
+            netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.runtimeconfig.json");
+        }
+
+        [Fact]
+        public void BuildingAPortableProjectProducesARuntimeConfigDevJsonFile()
+        {
+            var testInstance = TestAssetsManager.CreateTestInstance("PortableTests").WithLockFiles();
+
+            var netstandardappOutput = Build(testInstance);
+
+            netstandardappOutput.Should().Exist().And.HaveFile("PortableApp.runtimeconfig.dev.json");
+        }
+
+        private DirectoryInfo Build(TestInstance testInstance)
+        {
             var result = new BuildCommand(
                 projectPath: Path.Combine(testInstance.TestRoot, "PortableApp"))
                 .ExecuteWithCapturedOutput();
@@ -20,17 +77,7 @@ namespace Microsoft.DotNet.Tools.Builder.Tests
 
             var outputBase = new DirectoryInfo(Path.Combine(testInstance.TestRoot, "PortableApp", "bin", "Debug"));
 
-            var netstandardappOutput = outputBase.Sub("netstandard1.5");
-
-            netstandardappOutput.Should()
-                .Exist().And
-                .HaveFiles(new[]
-                {
-                    "PortableApp.deps",
-                    "PortableApp.deps.json",
-                    "PortableApp.dll",
-                    "PortableApp.pdb"
-                });
+            return outputBase.Sub("netstandard1.5");
         }
     }
 }