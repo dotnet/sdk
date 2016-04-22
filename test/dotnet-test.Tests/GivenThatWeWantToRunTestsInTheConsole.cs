dotnet/cli

Modify dotnet-test to run testRunner for all tfms Fixes #2506

test/dotnet-test.Tests/GivenThatWeWantToRunTestsInTheConsole.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Dotnet.Tools.Test.Tests
         {
             var testAssetManager = new TestAssetsManager(Path.Combine(RepoRoot, "TestAssets"));
             var testInstance =
-                testAssetManager.CreateTestInstance("ProjectWithTests", identifier: "ConsoleTests");
+                testAssetManager.CreateTestInstance(Path.Combine("ProjectsWithTests", "NetCoreAppOnlyProject"), identifier: "ConsoleTests");
 
             _projectFilePath = Path.Combine(testInstance.TestRoot, "project.json");
             var contexts = ProjectContext.CreateContextForEachFramework(

test/dotnet-test.Tests/GivenThatWeWantToUseDotnetTestE2EInDesignTime.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Dotnet.Tools.Test.Tests
         public GivenThatWeWantToUseDotnetTestE2EInDesignTime()
         {
             var testAssetManager = new TestAssetsManager(Path.Combine(RepoRoot, "TestAssets"));
-            var testInstance = testAssetManager.CreateTestInstance("ProjectWithTests");
+            var testInstance = testAssetManager.CreateTestInstance(Path.Combine("ProjectsWithTests", "NetCoreAppOnlyProject"));
 
             _projectFilePath = Path.Combine(testInstance.TestRoot, "project.json");
             var contexts = ProjectContext.CreateContextForEachFramework(

dotnet/sdk

test/dotnet-test.Tests/GivenThatWeWantToRunTestsInTheConsole.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Dotnet.Tools.Test.Tests
         public GivenThatWeWantToRunTestsInTheConsole()
         {
             var testInstance =
-                TestAssetsManager.CreateTestInstance("ProjectWithTests", identifier: "ConsoleTests");
+                TestAssetsManager.CreateTestInstance(Path.Combine("ProjectsWithTests", "NetCoreAppOnlyProject"), identifier: "ConsoleTests");
 
             _projectFilePath = Path.Combine(testInstance.TestRoot, "project.json");
             var contexts = ProjectContext.CreateContextForEachFramework(

test/dotnet-test.Tests/GivenThatWeWantToUseDotnetTestE2EInDesignTime.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Dotnet.Tools.Test.Tests
 
         public GivenThatWeWantToUseDotnetTestE2EInDesignTime()
         {
-            var testInstance = TestAssetsManager.CreateTestInstance("ProjectWithTests");
+            var testInstance = TestAssetsManager.CreateTestInstance(Path.Combine("ProjectsWithTests", "NetCoreAppOnlyProject"));
 
             _projectFilePath = Path.Combine(testInstance.TestRoot, "project.json");
             var contexts = ProjectContext.CreateContextForEachFramework(