6d2fae75b6543793fc226bc832ea4c004d409302
Mike Lorbetske <mike.lorbetske@n3-p.com>
2016-05-18 20:23:39 -0700

    Last few name updates
    Commit migrated from https://github.com/dotnet/templating/commit/8f0ff4434861936eda32bfa6b215007aec37de38

--- src/TemplateEngine/Microsoft.TemplateEngine.Core/Properties/AssemblyInfo.cs
+++ src/TemplateEngine/Microsoft.TemplateEngine.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mutant Chicken")]
+[assembly: AssemblyProduct("Microsoft Template Engine")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
