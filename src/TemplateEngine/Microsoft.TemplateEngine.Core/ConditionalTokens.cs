c5ae2288782fa515d45f8c82d3e803c1a7c6d925
seancpeters <sepeters@microsoft.com>
2016-08-22 16:34:23 -0700

    Holds the token params for Conditional constructor
    Holds the token params for Conditional constructor
    
    Commit migrated from https://github.com/dotnet/templating/commit/cea6f86531818c96b1a4c23ecc1dbfded25df732

---
+++ src/TemplateEngine/Microsoft.TemplateEngine.Core/ConditionalTokens.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.TemplateEngine.Core
+{
+    public class ConditionalTokens
+    {
+        public IList<string> IfTokens = new List<string>();
+        public IList<string> ElseTokens = new List<string>();
+        public IList<string> ElseIfTokens = new List<string>();
+        public IList<string> EndIfTokens = new List<string>();
+        public IList<string> ActionableIfTokens = new List<string>();
+        public IList<string> ActionableElseTokens = new List<string>();
+        public IList<string> ActionableElseIfTokens = new List<string>();
+        public IList<string> ActionableOperations = new List<string>();
+    }
+}
