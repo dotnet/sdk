a2aecec9565ed0c582c3f06ad36a64b85fe899e6
mlorbetske <mike.lorbetske@n3-p.com>
2016-09-17 01:03:37 -0700

    Removed comment about failing test
    Commit migrated from https://github.com/dotnet/templating/commit/ac319238436641eee716948664ebe0074b3525b6

--- test/TemplateEngine/Microsoft.TemplateEngine.Core.UnitTests/ConditionalTests.cs
+++ test/TemplateEngine/Microsoft.TemplateEngine.Core.UnitTests/ConditionalTests.cs
@@ -2184,7 +2184,6 @@ Past endif
 // dont uncomment";
             testCases.Add(originalValue);
 
-//For some reason this test fails on Linux with the else in the ////otherwise not being uncommented
             originalValue = @"Hello
 ////#check (VALUE_IF)
     //if value
