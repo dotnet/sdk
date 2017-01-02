3fce621f81b787a8cd9f0ca423f3eb5876868791
Mike Lorbetske <mlorbe@microsoft.com>
2017-01-02 11:54:09 -0800

    Only change the behavior for parameter symbols
    Commit migrated from https://github.com/dotnet/templating/commit/a92d00419746760353bfb65c79007ddeef52c26f

--- src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/SimpleConfigModel.cs
+++ src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/SimpleConfigModel.cs
@@ -418,7 +418,18 @@ namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects
                 {
                     if (symbol.Value.Replaces != null)
                     {
-                        macroGeneratedReplacements.Add(new ReplacementTokens(symbol.Key, symbol.Value.Replaces));
+                        //If "replaces" is on a parameter, replace the literal value in the "replaces" property
+                        //  with the value of the parameter
+                        if (string.Equals(symbol.Value.Type, "parameter", StringComparison.OrdinalIgnoreCase))
+                        {
+                            macroGeneratedReplacements.Add(new ReplacementTokens(symbol.Key, symbol.Value.Replaces));
+                        }
+                        //Otherwise, replace the literal value (the symbol name) with the value held by the symbol
+                        //  with the name specified in the "replaces" property
+                        else
+                        {
+                            macroGeneratedReplacements.Add(new ReplacementTokens(symbol.Value.Replaces, symbol.Key));
+                        }
                     }
                 }
             }
