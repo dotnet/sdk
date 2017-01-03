10d1654ccef60d6d2133ae4e878f3379c9b9b8af
seancpeters <sepeters@microsoft.com>
2017-01-03 15:51:41 -0800

    added test for the switch macro
    Commit migrated from https://github.com/dotnet/templating/commit/13a6dd8472af72612e75b77dc2d4be7679988506

---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/MacroTests/SwtichMacroTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Core;
+using Microsoft.TemplateEngine.Core.Contracts;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Macros;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Macros.Config;
+using Microsoft.TemplateEngine.TestHelper;
+using static Microsoft.TemplateEngine.Orchestrator.RunnableProjects.RunnableProjectGenerator;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests.MacroTests
+{
+    public class SwtichMacroTests : TestBase
+    {
+        [Fact(DisplayName = nameof(TestSwitchConfig))]
+        public void TestSwitchConfig()
+        {
+            string variableName = "mySwitchVar";
+            string evaluator = "C++";
+            string dataType = "string";
+            string expectedValue = "this one";
+            IList<KeyValuePair<string, string>> switches = new List<KeyValuePair<string, string>>();
+            switches.Add(new KeyValuePair<string, string>("(3 > 10)", "three greater than ten - false"));
+            switches.Add(new KeyValuePair<string, string>("(false)", "false value"));
+            switches.Add(new KeyValuePair<string, string>("(10 > 0)", expectedValue));
+            switches.Add(new KeyValuePair<string, string>("(5 > 4)", "not this one"));
+            SwitchMacroConfig macroConfig = new SwitchMacroConfig(variableName, evaluator, dataType, switches);
+
+            IVariableCollection variables = new VariableCollection();
+            IRunnableProjectConfig config = new SimpleConfigModel();
+            IParameterSet parameters = new ParameterSet(config);
+            ParameterSetter setter = MacroTestHelpers.TestParameterSetter(parameters);
+
+            SwitchMacro macro = new SwitchMacro();
+            macro.EvaluateConfig(variables, macroConfig, parameters, setter);
+            ITemplateParameter resultParam;
+            Assert.True(parameters.TryGetParameterDefinition(variableName, out resultParam));
+            string resultValue = (string)parameters.ResolvedValues[resultParam];
+            Assert.Equal(resultValue, expectedValue);
+        }
+
+        [Fact(DisplayName = nameof(TestSwitchDeferredConfig))]
+        public void TestSwitchDeferredConfig()
+        {
+            string variableName = "mySwitchVar";
+            string evaluator = "C++";
+            string dataType = "string";
+            string expectedValue = "this one";
+            string switchCases = @"[
+                {
+                    'condition': '(3 > 10)',
+                    'value': 'three greater than ten'
+                },
+                {
+                    'condition': '(false)',
+                    'value': 'false value'
+                },
+                {
+                    'condition': '(10 > 0)',
+                    'value': '" + expectedValue + @"'
+                },
+                {
+                    'condition': '(5 > 4)',
+                    'value': 'not this one'
+                }
+            ]";
+
+            Dictionary<string, JToken> jsonParameters = new Dictionary<string, JToken>();
+            jsonParameters.Add("evaluator", evaluator);
+            jsonParameters.Add("datatype", dataType);
+            jsonParameters.Add("cases", JArray.Parse(switchCases));
+
+            GeneratedSymbolDeferredMacroConfig deferredConfig = new GeneratedSymbolDeferredMacroConfig("SwitchMacro", variableName, jsonParameters);
+
+            IVariableCollection variables = new VariableCollection();
+            IRunnableProjectConfig config = new SimpleConfigModel();
+            IParameterSet parameters = new ParameterSet(config);
+            ParameterSetter setter = MacroTestHelpers.TestParameterSetter(parameters);
+
+            SwitchMacro macro = new SwitchMacro();
+            macro.EvaluateDeferredConfig(variables, deferredConfig, parameters, setter);
+            ITemplateParameter resultParam;
+            Assert.True(parameters.TryGetParameterDefinition(variableName, out resultParam));
+            string resultValue = (string)parameters.ResolvedValues[resultParam];
+            Assert.Equal(resultValue, expectedValue);
+        }
+    }
+}
