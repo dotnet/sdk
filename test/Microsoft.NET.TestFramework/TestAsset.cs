09bbd3ba3adbd5727458af437fc2088201318dd5
Nate McMaster <nate.mcmaster@microsoft.com>
2017-01-03 19:44:04 -0800

    Support well-known publish target extensions

--- test/Microsoft.NET.TestFramework/TestAsset.cs
+++ test/Microsoft.NET.TestFramework/TestAsset.cs
@@ -22,6 +22,8 @@ namespace Microsoft.NET.TestFramework
 
         public string TestRoot => Path;
 
+        public string SourceRoot => _testAssetRoot;
+
         internal TestAsset(string testDestination, string buildVersion) : base(testDestination)
         {
             BuildVersion = buildVersion;
@@ -117,7 +119,7 @@ namespace Microsoft.NET.TestFramework
                 }
             }
             return this;
-            
+
         }
 
         public void SetSdkVersion(XDocument project)
