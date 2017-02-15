facf13417cf42385a9b92d10a65f67ecbde4534f
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-02-14 21:40:50 -0800

    Boostrapper project and initial attempt at a VS wizard
    Commit migrated from https://github.com/dotnet/templating/commit/a5a29565377deb31d2584a6bfe7d6d7f17106f50

---
+++ src/TemplateEngine/Microsoft.TemplateEngine.IDE/Bootstrapper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Edge;
+using Microsoft.TemplateEngine.Edge.Settings;
+using Microsoft.TemplateEngine.Edge.Template;
+using Microsoft.TemplateEngine.Utils;
+
+namespace Microsoft.TemplateEngine.IDE
+{
+    public class Bootstrapper
+    {
+        private readonly ITemplateEngineHost _host;
+        private readonly Action<IEngineEnvironmentSettings, IInstaller> _onFirstRun;
+        private readonly Paths _paths;
+        private readonly TemplateCreator _templateCreator;
+        private readonly TemplateCache _templateCache;
+
+        private EngineEnvironmentSettings EnvironmentSettings { get; }
+
+        private IInstaller Installer { get; }
+
+        public Bootstrapper(ITemplateEngineHost host, Action<IEngineEnvironmentSettings, IInstaller> onFirstRun, bool virtualizeConfiguration)
+        {
+            _host = host;
+            EnvironmentSettings = new EngineEnvironmentSettings(host, x => new SettingsLoader(x));
+            Installer = new Installer(EnvironmentSettings);
+            _onFirstRun = onFirstRun;
+            _paths = new Paths(EnvironmentSettings);
+            _templateCreator = new TemplateCreator(EnvironmentSettings);
+            _templateCache = new TemplateCache(EnvironmentSettings);
+
+            if (virtualizeConfiguration)
+            {
+                EnvironmentSettings.Host.VirtualizeDirectory(_paths.User.BaseDir);
+            }
+        }
+
+        private void EnsureInitialized()
+        {
+            if (!_paths.Exists(_paths.User.BaseDir) || !_paths.Exists(_paths.User.FirstRunCookie))
+            {
+                _onFirstRun?.Invoke(EnvironmentSettings, Installer);
+                _paths.WriteAllText(_paths.User.FirstRunCookie, "");
+            }
+        }
+
+        public void Register(Type type)
+        {
+            EnsureInitialized();
+            EnvironmentSettings.SettingsLoader.Components.Register(type);
+        }
+
+        public void Register(Assembly assembly)
+        {
+            EnsureInitialized();
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                EnvironmentSettings.SettingsLoader.Components.Register(type);
+            }
+        }
+
+        public void Install(string path)
+        {
+            EnsureInitialized();
+            Installer.InstallPackages(new[] { path });
+        }
+
+        public void Install(params string[] paths)
+        {
+            EnsureInitialized();
+            Installer.InstallPackages(paths);
+        }
+
+        public void Install(IEnumerable<string> paths)
+        {
+            EnsureInitialized();
+            Installer.InstallPackages(paths);
+        }
+
+        public IReadOnlyCollection<IFilteredTemplateInfo> ListTemplates(bool exactMatchesOnly, params Func<ITemplateInfo, string, MatchInfo?>[] filters)
+        {
+            return _templateCreator.List(exactMatchesOnly, filters);
+        }
+
+        public async Task<ICreationResult> CreateAsync(ITemplateInfo info, string name, string outputPath, IReadOnlyDictionary<string, string> parameters, bool skipUpdateCheck)
+        {
+            TemplateCreationResult instantiateResult = await _templateCreator.InstantiateAsync(info, name, name, outputPath, parameters, skipUpdateCheck).ConfigureAwait(false);
+            return instantiateResult.ResultInfo;
+        }
+    }
+}
---
+++ src/TemplateEngine/Microsoft.TemplateEngine.IDE/Installer.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Edge;
+using Microsoft.TemplateEngine.Edge.Settings;
+
+namespace Microsoft.TemplateEngine.IDE
+{
+    internal class Installer : IInstaller
+    {
+        private readonly IEngineEnvironmentSettings _environmentSettings;
+        private readonly Paths _paths;
+        private readonly TemplateCache _templateCache;
+
+        public Installer(IEngineEnvironmentSettings environmentSettings)
+        {
+            _environmentSettings = environmentSettings;
+            _paths = new Paths(environmentSettings);
+            _templateCache = new TemplateCache(_environmentSettings);
+        }
+
+        public void InstallPackages(IEnumerable<string> installationRequests)
+        {
+            List<string> localSources = new List<string>();
+            InstallLocalPackages(installationRequests.Where(x => x != null));
+        }
+
+        private void InstallLocalPackages(IEnumerable<string> packageNames)
+        {
+            List<string> toInstall = new List<string>();
+
+            foreach (string package in packageNames)
+            {
+                string pkg = package.Trim();
+                pkg = _environmentSettings.Environment.ExpandEnvironmentVariables(pkg);
+                string pattern = null;
+
+                int wildcardIndex = pkg.IndexOfAny(new[] { '*', '?' });
+
+                if (wildcardIndex > -1)
+                {
+                    int lastSlashBeforeWildcard = pkg.LastIndexOfAny(new[] { '\\', '/' });
+                    pattern = pkg.Substring(lastSlashBeforeWildcard + 1);
+                    pkg = pkg.Substring(0, lastSlashBeforeWildcard);
+                }
+
+                try
+                {
+                    if (pattern != null)
+                    {
+                        string fullDirectory = new DirectoryInfo(pkg).FullName;
+                        string fullPathGlob = Path.Combine(fullDirectory, pattern);
+                        _templateCache.Scan(fullPathGlob);
+                    }
+                    else if (_environmentSettings.Host.FileSystem.DirectoryExists(pkg) || _environmentSettings.Host.FileSystem.FileExists(pkg))
+                    {
+                        string packageLocation = new DirectoryInfo(pkg).FullName;
+                        _templateCache.Scan(packageLocation);
+                    }
+                    else
+                    {
+                        _environmentSettings.Host.OnNonCriticalError("InvalidPackageSpecification", pkg, null, 0);
+                    }
+                }
+                catch
+                {
+                    _environmentSettings.Host.OnNonCriticalError("InvalidPackageSpecification", pkg, null, 0);
+                }
+            }
+
+            _templateCache.WriteTemplateCaches();
+        }
+    }
+}
