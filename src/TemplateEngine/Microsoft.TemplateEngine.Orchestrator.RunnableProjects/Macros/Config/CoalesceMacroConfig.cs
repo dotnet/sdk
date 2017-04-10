0a47ce98187d061ae696f9b843b284a3516c515c
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-04-10 10:20:27 -0700

    Add coalescing macro
    Commit migrated from https://github.com/dotnet/templating/commit/dc5e0a9f8f9776af406c494ad1b41319cce2eb1f

---
+++ src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/Macros/Config/CoalesceMacroConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.TemplateEngine.Core.Contracts;
+
+namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Macros.Config
+{
+    public class CoalesceMacroConfig : IMacroConfig
+    {
+        public string VariableName { get; }
+
+        public string SourceVariableName { get; }
+
+        public string DefaultValue { get; }
+
+        public string FallbackVariableName { get; }
+
+        public string Type => "coalesce";
+
+        public CoalesceMacroConfig(string variableName, string sourceVariableName, string defaultValue, string fallbackVariableName)
+        {
+            VariableName = variableName;
+            SourceVariableName = sourceVariableName;
+            DefaultValue = defaultValue;
+            FallbackVariableName = fallbackVariableName;
+        }
+    }
+}
