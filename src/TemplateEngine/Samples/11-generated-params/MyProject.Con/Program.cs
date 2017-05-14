22aeb5e8623a4775bd28b0814cd59ca6efd5254c
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-05-14 11:07:56 -0700

    adding samples with generated params
    Commit migrated from https://github.com/dotnet/templating/commit/8a3899a60d17aa3aefa8f13c05eebc9a9a97fd2c

---
+++ src/TemplateEngine/Samples/11-generated-params/MyProject.Con/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyProject.Con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+From const: 1234
+Name:       John Smith (a)
+Name upper: John Smith (U)
+Name lower: John Smith (l)
+From eval: 4321
+");
+        }
+    }
+}
