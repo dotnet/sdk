06343789b1127f411793e3bba909899d1b94a7cc
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-06-04 00:44:26 -0700

    Fix parameter hiding Add mssing endif for webapi Update NuGet feeds to point at release Install templates from the feed into the runner Add tests for mvc with individual auth
    Commit migrated from https://github.com/dotnet/templating/commit/28afea9050dea41fcf965bf37ec7e5ac71da741b

--- src/TemplateEngine/Tools/ProjectTestRunner/Battery.cs
+++ src/TemplateEngine/Tools/ProjectTestRunner/Battery.cs
@@ -47,6 +47,9 @@ namespace ProjectTestRunner
 
             Proc.Run("dotnet", $"{Creator} --debug:reinit").WaitForExit();
             Proc.Run("dotnet", $"{Creator}").WaitForExit();
+
+            string templateFeedDirectory = FindTemplateFeedDirectory(BasePath);
+            Proc.Run("dotnet", $"{Creator} -i \"{templateFeedDirectory}\"").WaitForExit();            
         }
 
         public Battery(ITestOutputHelper outputHelper)
@@ -176,6 +179,20 @@ namespace ProjectTestRunner
             {
                 watch.Stop();
             }
+        }
+
+        private static string FindTemplateFeedDirectory(string batteryDirectory)
+        {
+            DirectoryInfo currentDirectory = new DirectoryInfo(batteryDirectory);
+            string templateFeed = Path.Combine(currentDirectory.FullName, "template_feed");
+
+            while (!Directory.Exists(templateFeed))
+            {
+                currentDirectory = currentDirectory.Parent;
+                templateFeed = Path.Combine(currentDirectory.FullName, "template_feed");
+            }
+
+            return templateFeed;
         }
 
         private static IHandlerResult Create(string creator, string installPackage, string command, string targetPath)
