5767f1a62f92543f79a5eb39e815bfb3033f0a90
William Li <wul@microsoft.com>
2017-07-06 13:58:33 -0700

    Adjust prop to hard code number
    And add more tests
    There is no BundledNETStandardTargetFrameworkVersion and BundledNETCoreAppTargetFrameworkVersion
    available for 1.1 repo. And it actually carry several version
    
    When compare version we use 1.2, since we want netcore 1.1.1 to work. And 1.2 > 1.1.1
    However, in the error message, we use "1.1" since 1.2 doesn't exist

--- src/Tasks/Microsoft.NET.Build.Tasks/Resources/Strings.Designer.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/Resources/Strings.Designer.cs
@@ -10,7 +10,7 @@
 
 namespace Microsoft.NET.Build.Tasks {
     using System;
-    
+    using System.Reflection;
     
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -39,7 +39,7 @@ namespace Microsoft.NET.Build.Tasks {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.NET.Build.Tasks.Resources.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.NET.Build.Tasks.Resources.Strings",  typeof(Strings).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
