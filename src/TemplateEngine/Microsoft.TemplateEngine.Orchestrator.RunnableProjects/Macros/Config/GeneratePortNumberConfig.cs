b94dc28ff818ced64b27128636065ff8ebce45c5
seancpeters <sepeters@microsoft.com>
2017-07-13 16:11:53 -0700

    allowing port generation to specify ranges
    Commit migrated from https://github.com/dotnet/templating/commit/ff4876750fb3621e4af154bbcd9c7e4e029b0d53

--- src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/Macros/Config/GeneratePortNumberConfig.cs
+++ src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/Macros/Config/GeneratePortNumberConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using Microsoft.TemplateEngine.Core.Contracts;
@@ -15,35 +16,95 @@ namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Macros.Config
 
         public int Port { get; }
 
-        public GeneratePortNumberConfig(string variableName, int fallback)
+        public int Low { get; }
+
+        public int High { get; }
+
+        private static readonly HashSet<int> UnsafePorts = new HashSet<int>() {
+                    2049, // nfs
+                    3659, // apple-sasl / PasswordServer
+                    4045, // lockd
+                    6000, // X11
+                    6665, // Alternate IRC [Apple addition]
+                    6666, // Alternate IRC [Apple addition]
+                    6667, // Standard IRC [Apple addition]
+                    6668, // Alternate IRC [Apple addition]
+                    6669, // Alternate IRC [Apple addition]
+        };
+
+        public GeneratePortNumberConfig(string variableName, int fallback, int low, int high)
         {
             VariableName = variableName;
+            Random rand = new Random();
+            int startPort = rand.Next(low, high);
+
+            for (int testPort = startPort; testPort <= high; testPort++)
+            {
+                if (TryAllocatePort(testPort, out Socket testSocket))
+                {
+                    Socket = testSocket;
+                    Port = ((IPEndPoint)Socket.LocalEndPoint).Port;
+                    return;
+                }
+            }
+
+            for (int testPort = low; testPort < startPort; testPort++)
+            {
+                if (TryAllocatePort(testPort, out Socket testSocket))
+                {
+                    Socket = testSocket;
+                    Port = ((IPEndPoint)Socket.LocalEndPoint).Port;
+                    return;
+                }
+            }
+
+            Port = fallback;
+        }
+
+        private bool TryAllocatePort(int testPort, out Socket testSocket)
+        {
+            testSocket = null;
+
+            if (UnsafePorts.Contains(testPort))
+            {
+                return false;
+            }
 
             try
             {
                 if (Socket.OSSupportsIPv4)
                 {
-                    Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    testSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 }
                 else if (Socket.OSSupportsIPv6)
                 {
-                    Socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+                    testSocket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
                 }
             }
             catch
             {
+                testSocket?.Dispose();
+                return false;
             }
 
-            if (Socket != null)
+            if (testSocket != null)
             {
-                IPEndPoint endPoint = new IPEndPoint(Socket.AddressFamily == AddressFamily.InterNetworkV6 ? IPAddress.IPv6Any : IPAddress.Any, 0);
-                Socket.Bind(endPoint);
-                Port = ((IPEndPoint)Socket.LocalEndPoint).Port;
+                IPEndPoint endPoint = new IPEndPoint(testSocket.AddressFamily == AddressFamily.InterNetworkV6 ? IPAddress.IPv6Any : IPAddress.Any, testPort);
+
+                try
+                {
+                    testSocket.Bind(endPoint);
+                    return true;
+                }
+                catch
+                {
+                    testSocket?.Dispose();
+                    return false;
+                }
             }
-            else
-            {
-                Port = fallback;
-            }
+
+            testSocket = null;
+            return false;
         }
     }
 }
