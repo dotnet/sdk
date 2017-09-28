9aa4c721f501e777a122c840b85d0e880db2dc02
seancpeters <sepeters@microsoft.com>
2017-09-27 19:35:11 -0700

    standardizing choice values for telemetry
    Commit migrated from https://github.com/dotnet/templating/commit/1f308daf493299134f1a89d8281b377f0c942f68

---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Mocks/MockTemplate.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.TemplateEngine.Abstractions;
+
+namespace Microsoft.TemplateEngine.Mocks
+{
+    public class MockTemplate : ITemplateInfo
+    {
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public IReadOnlyList<string> Classifications { get; set; }
+
+        public string DefaultName { get; set; }
+
+        public string Identity { get; set; }
+
+        public Guid GeneratorId { get; set; }
+
+        public string GroupIdentity { get; set; }
+
+        public int Precedence { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public IReadOnlyDictionary<string, ICacheTag> Tags { get; set; }
+
+        public IReadOnlyDictionary<string, ICacheParameter> CacheParameters { get; set; }
+
+        public IReadOnlyList<ITemplateParameter> Parameters { get; set; }
+
+        public Guid ConfigMountPointId { get; set; }
+
+        public string ConfigPlace { get; set; }
+
+        public Guid LocaleConfigMountPointId { get; set; }
+
+        public string LocaleConfigPlace { get; set; }
+
+        public Guid HostConfigMountPointId { get; set; }
+
+        public string HostConfigPlace { get; set; }
+
+        public string ThirdPartyNotices { get; set; }
+
+        public IReadOnlyDictionary<string, IBaselineInfo> BaselineInfo { get; set; }
+    }
+}
