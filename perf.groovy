a5f31d7e06f3375ec9c48a70ee11da1eb1297a9c
jbeisner <jbeisner@microsoft.com>
2018-02-22 21:09:41 +0000

    Move the upload tasks into a batch file.

--- perf.groovy
+++ perf.groovy
@@ -39,53 +39,19 @@ def static getBuildJobName(def configuration, def os) {
 
                 def configuration = 'Release'
                 def runType = isPR ? 'private' : 'rolling'
-                def benchViewName = isPR ? 'SDK private %BenchviewCommitName%' : 'SDK rolling %GIT_BRANCH_WITHOUT_ORIGIN% %GIT_COMMIT%'
-                def uploadString = '-uploadToBenchview'
                 def perfWorkingDirectory = '%WORKSPACE%\\artifacts\\$configuration\\TestResults\\Performance'
 
                 steps {
                    // Build solution and run the performance tests
-                   batchFile("\"%WORKSPACE%\\build.cmd\" -sign -ci -perf /p:PerfIterations=10 /p:PerfOutputDirectory=\"${perfWorkingDirectory}\" /p:PerfCollectionType=stopwatch")
+                   batchFile("\"%WORKSPACE%\\build.cmd\" -configuration ${configuration} -sign -ci -perf /p:PerfIterations=10 /p:PerfOutputDirectory=\"${perfWorkingDirectory}\" /p:PerfCollectionType=stopwatch")
 
-                   // Download BenchView Python tools
-                   batchFile("powershell -NoProfile wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile \"${perfWorkingDirectory}\\nuget.exe\"")
-                   batchFile("if exist \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\" rmdir /s /q \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\"")
-                   batchFile("\"${perfWorkingDirectory}\\nuget.exe\" install Microsoft.BenchView.JSONFormat -Source http://benchviewtestfeed.azurewebsites.net/nuget -OutputDirectory \"${perfWorkingDirectory}\" -Prerelease -ExcludeVersion")
-
-                   // Do this here to remove the origin but at the front of the branch name as this is a problem for BenchView
-                   batchFile("if \"%GIT_BRANCH:~0,7%\" == \"origin/\" (set \"GIT_BRANCH_WITHOUT_ORIGIN=%GIT_BRANCH:origin/=%\") else (set \"GIT_BRANCH_WITHOUT_ORIGIN=%GIT_BRANCH%\")\n" +
-                   "set \"BENCHVIEWNAME=${benchViewName}\"\n" +
-                   "set \"BENCHVIEWNAME=%BENCHVIEWNAME:\"=\"\"%\"\n" +
-                   "echo Creating: \"${perfWorkingDirectory}\\submission-metadata.json\"\n" +
-                   "py \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\\tools\\submission-metadata.py\" --name \"%BENCHVIEWNAME%\" --user-email \"dotnet-bot@microsoft.com\" -o \"${perfWorkingDirectory}\\submission-metadata.json\"\n" +
-                   "echo Creating: \"${perfWorkingDirectory}\\build.json\"\n" +
-                   "py \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\\tools\\build.py\" git --branch %GIT_BRANCH_WITHOUT_ORIGIN% --type ${runType} -o \"${perfWorkingDirectory}\\build.json\"")
-
-                   batchFile("echo Creating: \"${perfWorkingDirectory}\\machinedata.json\"\n" +
-                   "py \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\\tools\\machinedata.py\" -o \"${perfWorkingDirectory}\\machinedata.json\"")
-
-                   batchFile("echo Creating: \"${perfWorkingDirectory}\\measurement.json\"\n" +
-                   "pushd \"${perfWorkingDirectory}\"\n" +
-                   "for /f \"tokens=*\" %%a in ('dir /b/a-d *.xml') do (\n" +
-                   "echo Processing: %%a\n" +
-                   "py \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\\tools\\measurement.py\" xunitscenario \"%%a\" --better desc --append -o \"${perfWorkingDirectory}\\measurement.json\")\n" +
-                   "popd")
-
-                   batchFile("echo Creating: \"${perfWorkingDirectory}\\submission.json\"\n" +
-                   "py \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\\tools\\submission.py\" \"${perfWorkingDirectory}\\measurement.json\"" +
-                   " --build \"${perfWorkingDirectory}\\build.json\"" +
-                   " --machine-data \"${perfWorkingDirectory}\\machinedata.json\"" +
-                   " --metadata \"${perfWorkingDirectory}\\submission-metadata.json\"" +
-                   " --group \"SDK Perf Tests\"" +
-                   " --type \"${runType}\"" +
-                   " --config-name \"${configuration}\"" +
-                   " --config Configuration \"${configuration}\"" +
-                   " --architecture \"${arch}\"" +
-                   " --machinepool \"perfsnake\"" +
-                   " -o \"${perfWorkingDirectory}\\submission.json\"")
-
-                   batchFile("echo Uploading: \"${perfWorkingDirectory}\\submission.json\"\n" +
-                   "py \"${perfWorkingDirectory}\\Microsoft.BenchView.JSONFormat\\tools\\upload.py\" \"${perfWorkingDirectory}\\submission.json\" --container coreclr")
+                   // Upload perf results to BenchView
+                   batchFile("set perfWorkingDirectory=${perfWorkingDirectory}\n" +
+                   "set configuration=${configuration}\n" +
+                   "set architecture=${architecture}\n" +
+                   "set runType=${runType}\n" +
+                   "set BenchviewCommitName=${BenchviewCommitName}\n" +
+                   "\"%WORKSPACE%\\build\\uploadperftobenchview.cmd\")
                 }
             }
 
