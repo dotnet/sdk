92b335b0bc38779d1987ce9cbe9f637424b9cbe4
jbeisner <jbeisner@microsoft.com>
2018-02-23 20:44:50 +0000

    Write-out the 'benchViewName' in the log.

--- build/uploadperftobenchview.cmd
+++ build/uploadperftobenchview.cmd
@@ -42,6 +42,7 @@ if "%GIT_BRANCH:~0,7%" == "origin/" (set GIT_BRANCH_WITHOUT_ORIGIN=%GIT_BRANCH:o
 set benchViewName=SDK perf %OS% %architecture% %configuration% %runType% %GIT_BRANCH_WITHOUT_ORIGIN%
 if /I "%runType%" == "private" (set benchViewName=%benchViewName% %BenchviewCommitName%)
 if /I "%runType%" == "rolling" (set benchViewName=%benchViewName% %GIT_COMMIT%)
+echo BenchViewName: "%benchViewName%"
 
 echo Creating: "%perfWorkingDirectory%\submission-metadata.json"
 py "%perfWorkingDirectory%\Microsoft.BenchView.JSONFormat\tools\submission-metadata.py" --name "%benchViewName%" --user-email "dotnet-bot@microsoft.com" -o "%perfWorkingDirectory%\submission-metadata.json"
