36205e5dfacf33d7b3da041631ce78fb0162f6c8
Nick Guerrera <nicholg@microsoft.com>
2018-03-04 17:39:28 -0800

    Speed up incremental package asset resolution
    Whenever project.assets.json changes (or more rarely, a setting influencing
    ResolvePackageAssets changes), write out a binary cache file for the next
    incremental build. The file has only the items that used by the build and
    they are written in a format that can be deserialized to MSBuild ITaskItem[]
    with minimal time and allocation.

--- src/Tasks/Microsoft.NET.Build.Tasks/ReportAssetsLogMessages.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ReportAssetsLogMessages.cs
@@ -9,9 +9,9 @@ using System.Linq;
 namespace Microsoft.NET.Build.Tasks
 {
     /// <summary>
-    /// Report Log Messages in the assets file to MSBuild and raise them as
-    /// DiagnosticMessage items that can be consumed downstream (e.g. by the
-    /// dependency node in the solution explorer)
+    /// Raise log messages in the assets file as DiagnosticMessage items
+    /// that can be consumed downstream (e.g. by the dependency node in
+    /// the solution explorer)
     /// </summary>
     public sealed class ReportAssetsLogMessages : TaskBase
     {
@@ -40,15 +40,14 @@ namespace Microsoft.NET.Build.Tasks
 
         public ReportAssetsLogMessages()
         {
-           _diagnostics = new DiagnosticsHelper(new MSBuildLog(Log));
+           _diagnostics = new DiagnosticsHelper();
         }
 
         #region Test Support
 
-        internal ReportAssetsLogMessages(LockFile lockFile, ILog logger)
+        internal ReportAssetsLogMessages(LockFile lockFile) : this()
         {
             _lockFile = lockFile;
-            _diagnostics = new DiagnosticsHelper(logger);
         }
 
         #endregion
