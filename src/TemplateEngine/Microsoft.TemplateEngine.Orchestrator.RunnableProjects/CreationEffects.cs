f1e56842cd59989aa275595354861cf8391b277b
Mike Lorbetske <mike.lorbetske@n3-p.com>
2018-07-29 19:16:23 -0700

    A little cleanup, respond to the review comment, call out parameters that trigger new behaviors, preserve existing behavior more directly
    Commit migrated from https://github.com/dotnet/templating/commit/62b6172c084d08786f4190f0a6e7f06bb1f51986

--- src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/CreationEffects.cs
+++ src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/CreationEffects.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.TemplateEngine.Abstractions;
-using Microsoft.TemplateEngine.Core;
 
 namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects
 {
