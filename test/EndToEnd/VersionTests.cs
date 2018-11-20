55afa33d8e90cedb7fcec41ad0173688739597d6
Daniel Plaisted <daplaist@microsoft.com>
2018-11-20 10:17:29 -0800

    Generate version file

---
+++ test/EndToEnd/VersionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using FluentAssertions;
+using Microsoft.DotNet.TestFramework;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Xunit;
+
+namespace EndToEnd.Tests
+{
+    public class VersionTests : TestBase
+    {
+        [Fact]
+        public void DotnetVersionReturnsCorrectVersion()
+        {
+            var result = new DotnetCommand()
+                .ExecuteWithCapturedOutput("--version");
+
+            result.Should().Pass();
+
+            var dotnetFolder = Path.GetDirectoryName(RepoDirectoriesProvider.DotnetUnderTest);
+
+            var sdkFolders = Directory.GetDirectories(Path.Combine(dotnetFolder, "sdk"));
+
+            sdkFolders.Length.Should().Be(1, "Only one SDK folder is expected in the layout");
+
+            var expectedSdkVersion = Path.GetFileName(sdkFolders.Single());
+
+            result.StdOut.Trim().Should().Be(expectedSdkVersion);
+        }
+    }
+}
