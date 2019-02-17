51e83c1da311feb786f8e3a8c9edb9510aea7d9b
Jonathon Marolf <jmarolf@microsoft.com>
2019-02-17 14:12:44 -0800

    updating license text

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,4 +1,5 @@
-using System;
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
 using Xunit;
 
 namespace tests
