249d14cdcb59290643eda1ffc0c2a873a68a429c
Peter Huene <pehuene@microsoft.com>
2019-03-27 13:58:57 -0700

    Fix apphost creation when using a publish profile.
    When publishing an ASP.NET project using a publish profile that sets
    `RuntimeIdentifier`, various apphost-related properties are calculated before
    the RID is set when the publish profile is imported.
    
    This typically results in the expected file names for the restored assets not
    having the correct prefixes and suffixes for the target RID.  For example, if a
    Windows RID is set in the publish profile, the expected apphost asset is named
    `apphost` instead of the correct `apphost.exe`. This causes the apphost
    customization to be skipped because the expected apphost asset wasn't found.
    
    The fix is to delay setting these properties until after the publish profile
    has been imported by the Web SDK.  This is accomplished by moving the
    properties into a target that is invoked based on every place in the SDK
    where the properties are used.
    
    Fixes #10647.

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAWebProject.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAWebProject.cs
@@ -77,7 +77,6 @@ namespace Microsoft.NET.Publish.Tests
                 .HaveStdOutContaining("Hello World!");
         }
 
-
         [Theory]
         [InlineData("Microsoft.AspNetCore.App")]
         [InlineData("Microsoft.AspNetCore.All")]
@@ -135,6 +134,89 @@ namespace Microsoft.NET.Publish.Tests
                 .Pass()
                 .And
                 .HaveStdOutContaining("Hello World!");
+        }
+
+        [Theory]
+        [InlineData(false, false)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(true, true)]
+        public void It_publishes_with_a_publish_profile(bool selfContained, bool useAppHost)
+        {
+            var tfm = "netcoreapp2.2";
+
+            var testProject = new TestProject()
+            {
+                Name = "WebWithPublishProfile",
+                TargetFrameworks = tfm,
+                IsSdkProject = true,
+                IsExe = true,
+            };
+
+            testProject.AdditionalProperties.Add("AspNetCoreHostingModel", "InProcess");
+            testProject.PackageReferences.Add(new TestPackageReference("Microsoft.AspNetCore.App"));
+            testProject.PackageReferences.Add(new TestPackageReference("Microsoft.AspNetCore.Razor.Design", version: "2.2.0", privateAssets: "all"));
+
+            var testProjectInstance = _testAssetsManager.CreateTestProject(testProject)
+                .WithProjectChanges(
+                    (filename, project) =>
+                    {
+                        project.Root.Attribute("Sdk").Value = "Microsoft.NET.Sdk.Web";
+                    });
+
+            var projectDirectory = Path.Combine(testProjectInstance.Path, testProject.Name);
+            var publishProfilesDirectory = Path.Combine(projectDirectory, "Properties", "PublishProfiles");
+            Directory.CreateDirectory(publishProfilesDirectory);
+
+            File.WriteAllText(Path.Combine(publishProfilesDirectory, "test.pubxml"), $@"
+<Project>
+  <PropertyGroup>
+    <RuntimeIdentifier>{EnvironmentInfo.GetCompatibleRid(tfm)}</RuntimeIdentifier>
+    <SelfContained>{selfContained}</SelfContained>
+    <UseAppHost>{selfContained || useAppHost}</UseAppHost>
+  </PropertyGroup>
+</Project>
+");
+
+            var command = new PublishCommand(Log, projectDirectory);
+            command
+                .Execute("/restore", "/p:PublishProfile=test")
+                .Should()
+                .Pass();
+
+            var output = command.GetOutputDirectory(tfm);
+
+            output.Should().HaveFiles(new[] {
+                $"{testProject.Name}.dll",
+                $"{testProject.Name}.pdb",
+                $"{testProject.Name}.deps.json",
+                $"{testProject.Name}.runtimeconfig.json",
+                "web.config",
+            });
+
+            if (selfContained)
+            {
+                output.Should().HaveFiles(new[] {
+                    $"{FileConstants.DynamicLibPrefix}hostfxr{FileConstants.DynamicLibSuffix}",
+                    $"{FileConstants.DynamicLibPrefix}hostpolicy{FileConstants.DynamicLibSuffix}",
+                });
+            }
+            else
+            {
+                output.Should().NotHaveFiles(new[] {
+                    $"{FileConstants.DynamicLibPrefix}hostfxr{FileConstants.DynamicLibSuffix}",
+                    $"{FileConstants.DynamicLibPrefix}hostpolicy{FileConstants.DynamicLibSuffix}",
+                });
+            }
+
+            if (selfContained || useAppHost)
+            {
+                output.Should().HaveFile($"{testProject.Name}{Constants.ExeSuffix}");
+            }
+            else
+            {
+                output.Should().NotHaveFile($"{testProject.Name}{Constants.ExeSuffix}");
+            }
         }
     }
 }
