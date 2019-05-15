b8a5c72550f7a41e73cbc4f299f4b426bba1551e
William Li <wul@microsoft.com>
2019-05-15 14:51:43 -0700

    revert and set hard coded value

--- build/RunPerfTestsOnHelix.fullframework.cmd
+++ build/RunPerfTestsOnHelix.fullframework.cmd
@@ -14,10 +14,21 @@ set GIT_COMMIT=%3
 set GIT_BRANCH=%4
 set runType=%5
 set TestFullMSBuild=true
+set DOTNET_SDK_TEST_MSBUILD_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+
+REM  Since dotnet.exe was locked; we exclude it from the helix-payload.
+REM    Run a restore to re-install the SDK.
+echo "Running a 'build.ps1 -restore'"
+powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%HELIX_CORRELATION_PAYLOAD%\eng\common\build.ps1""" -configuration %configuration% -restore"
+
+REM  Since the Microsoft.NET.PerformanceTests.runtimeconfig.dev.json has a hard-coded path to the NuGet root, we exclude all test harnesses from the helix-payload.
+REM    Build the PerformanceTests harness on the Helix machine.
+echo "Building:'Microsoft.NET.PerformanceTests.dll'"
+%HELIX_CORRELATION_PAYLOAD%\.dotnet\dotnet.exe msbuild %HELIX_CORRELATION_PAYLOAD%\src\Tests\Microsoft.NET.PerformanceTests\Microsoft.NET.PerformanceTests.csproj /t:build /p:configuration=%configuration% /p:NUGET_PACKAGES=%HELIX_CORRELATION_PAYLOAD%\.packages
 
 REM  Run the performance tests and collect performance data.
 echo "Running the performance tests and collecting data"
-powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%HELIX_CORRELATION_PAYLOAD%\eng\common\build.ps1""" -restore -build -configuration %configuration% -ci -performanceTest /p:PerfIterations=%PerfIterations% /p:NUGET_PACKAGES=%HELIX_CORRELATION_PAYLOAD%\.packages"
+powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%HELIX_CORRELATION_PAYLOAD%\eng\common\build.ps1""" -configuration %configuration% -ci -performanceTest /p:PerfIterations=%PerfIterations%"
 IF %ERRORLEVEL% GTR 0 exit %ERRORLEVEL%
 echo "Performance tests completed"
 
