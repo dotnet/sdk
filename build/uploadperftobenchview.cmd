3751f3ae7be00800cae29e637d71ee4e4f4d095f
William Li <wul@microsoft.com>
2019-05-21 23:09:22 -0700

    Test unify RunPerfTestsOnHelix.cmd

--- build/uploadperftobenchview.cmd
+++ build/uploadperftobenchview.cmd
@@ -49,7 +49,7 @@ if "%GIT_BRANCH:~0,7%" == "origin/" (set GIT_BRANCH_WITHOUT_ORIGIN=%GIT_BRANCH:o
 
 for /f %%x in ('powershell -NoProfile -NoLogo -Command "Get-Date -Date (Get-Date).ToUniversalTime() -UFormat '%%Y-%%m-%%dT%%H:%%M:%%SZ'"') do (set timeStamp=%%x)
 
-set benchViewName=SDK perf %OS% %architecture% %configuration%-%TestFullMSBuild% %runType% %GIT_BRANCH_WITHOUT_ORIGIN%
+set benchViewName=SDK perf %OS% %architecture% %configuration% TestFullMSBuild-%TestFullMSBuild% %runType% %GIT_BRANCH_WITHOUT_ORIGIN%
 if /I "%runType%" == "private" (set benchViewName=%benchViewName% %BenchviewCommitName%)
 if /I "%runType%" == "rolling" (set benchViewName=%benchViewName% %GIT_COMMIT%)
 echo BenchViewName: "%benchViewName%"
@@ -81,7 +81,6 @@ echo Creating: "%perfWorkingDirectory%\submission.json"
                     --config-name "%configuration%" ^
                     --config Configuration "%configuration%" ^
                     --config TestFullMSBuild "%TestFullMSBuild%" ^
-                    --config ExtraConfig "testconfig" ^
                     --config OS "%OS%" ^
                     --architecture "%architecture%" ^
                     --machinepool "perfsnake" ^
