8cdde662b23eae7f5634da0dc6e7096e888ccfd0
Sean Peters <sepeters@microsoft.com>
2019-06-14 17:49:05 -0700

    Refactored template search  for easier extension and testing
    Commit migrated from https://github.com/dotnet/templating/commit/19cf6a4855ce8ceedd4a6539985b59fdcb93e14a

--- src/TemplateEngine/Microsoft.TemplateSearch.Common/TemplateSearchCoordinator.cs
+++ src/TemplateEngine/Microsoft.TemplateSearch.Common/TemplateSearchCoordinator.cs
@@ -9,42 +9,39 @@ using Microsoft.TemplateEngine.Edge.Template;
 
 namespace Microsoft.TemplateSearch.Common
 {
-    public abstract class TemplateSearchCoordinator : ITemplateSearchCoordinator
+    public class TemplateSearchCoordinator
     {
-        public TemplateSearchCoordinator(IEngineEnvironmentSettings environmentSettings, string inputTemplateName, string defaultLanguage)
+        public TemplateSearchCoordinator(IEngineEnvironmentSettings environmentSettings, string inputTemplateName, string defaultLanguage, Func<IReadOnlyList<ITemplateNameSearchResult>, IReadOnlyList<ITemplateMatchInfo>> matchFilter)
         {
             _environmentSettings = environmentSettings;
             _inputTemplateName = inputTemplateName;
             _defaultLanguage = defaultLanguage;
+            _matchFilter = matchFilter;
             _isSearchPerformed = false;
         }
 
         protected readonly IEngineEnvironmentSettings _environmentSettings;
         protected readonly string _inputTemplateName;
         protected readonly string _defaultLanguage;
+        protected Func<IReadOnlyList<ITemplateNameSearchResult>, IReadOnlyList<ITemplateMatchInfo>> _matchFilter;
         private bool _isSearchPerformed;
         protected SearchResults _searchResults;
 
-        public async Task<bool> CoordinateSearchAsync()
+        public async Task<SearchResults> SearchAsync()
         {
-            await EnsureSearchResults();
+            await EnsureSearchResultsAsync();
 
-            return HandleSearchResults();
+            return _searchResults;
         }
 
-        // return true if there were any search results, false otherwise.
-        protected abstract bool HandleSearchResults();
-
-        protected abstract Func<IReadOnlyList<ITemplateNameSearchResult>, IReadOnlyList<ITemplateMatchInfo>> MatchFilter { get; }
-
-        protected async Task EnsureSearchResults()
+        protected async Task EnsureSearchResultsAsync()
         {
             if (_isSearchPerformed)
             {
                 return;
             }
 
-            TemplateSearcher searcher = new TemplateSearcher(_environmentSettings, _defaultLanguage, MatchFilter);
+            TemplateSearcher searcher = new TemplateSearcher(_environmentSettings, _defaultLanguage, _matchFilter);
             IReadOnlyList<IInstallUnitDescriptor> existingInstallDescriptors;
 
             if (_environmentSettings.SettingsLoader is SettingsLoader settingsLoader)
