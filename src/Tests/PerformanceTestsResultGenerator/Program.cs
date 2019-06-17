7e0418957b0a9fbcc6adfd39f3b3250fd145051c
William Li <wul@microsoft.com>
2019-06-17 16:42:22 -0700

    Init converter-project test

---
+++ src/Tests/PerformanceTestsResultGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PerformanceTestsResultGenerator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Option optionThatTakesFileInfo = new Option(
+                "--file-option",
+                "An option whose argument is parsed as a FileInfo",
+                new Argument<FileInfo>());
+
+            // Add them to the root command
+            var rootCommand = new RootCommand();
+            rootCommand.Description = "Performance tests result generator";
+            rootCommand.AddOption(optionThatTakesFileInfo);
+
+            rootCommand.Handler = CommandHandler.Create<int, bool, FileInfo>((intOption, boolOption, fileOption) =>
+            {
+                Console.WriteLine($"The value for --file-option is: {fileOption?.FullName ?? "null"}");
+            });
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
+        }
+    }
+}
