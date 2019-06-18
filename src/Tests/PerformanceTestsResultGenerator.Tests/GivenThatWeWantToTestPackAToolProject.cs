df15ec1fabb3ec47225e9510ed5d7dc104317c86
William Li <wul@microsoft.com>
2019-06-17 17:20:46 -0700

    Add tests template

---
+++ src/Tests/PerformanceTestsResultGenerator.Tests/GivenThatWeWantToTestPackAToolProject.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using FluentAssertions;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Commands;
+using NuGet.Packaging;
+using Xunit;
+using Xunit.Abstractions;
+using Microsoft.NET.TestFramework.Assertions;
+
+namespace Microsoft.NET.ToolPack.Tests
+{
+    public class GivenPerformanceTestsResultGenerator : SdkTest
+    {
+        public GivenPerformanceTestsResultGenerator(ITestOutputHelper log) : base(log)
+        {
+        }
+
+        [Fact]
+        public void It_can_convert_xmls()
+        {
+
+        }
+    }
+}
