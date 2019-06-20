fec9dc301125d23e1b96b784ad0eeb27eceb4c5f
William Li <wul@microsoft.com>
2019-06-19 21:17:13 -0700

    Can batch convert

--- src/Tests/PerformanceTestsResultGenerator.Tests/GivenPerformanceTestsResultGenerator.cs
+++ src/Tests/PerformanceTestsResultGenerator.Tests/GivenPerformanceTestsResultGenerator.cs
@@ -17,6 +17,8 @@ using Reporting;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using PerformanceTestsResultGenerator;
 
 namespace Microsoft.NET.ToolPack.Tests
 {
@@ -27,53 +29,41 @@ namespace Microsoft.NET.ToolPack.Tests
         }
 
         [Fact]
-        public void It_can_convert_xmls()
+        public void Given_stream_It_can_convert_xmls_to_TestObject()
         {
-            var settings = new JsonSerializerSettings();
-            var resolver = new DefaultContractResolver();
-            resolver.NamingStrategy = new CamelCaseNamingStrategy() { ProcessDictionaryKeys = false };
-            settings.ContractResolver = resolver;
-            JsonConvert.SerializeObject(GenerateTestsFromXml(), Formatting.Indented, settings).Should().Be("3");
-
-        }
-
-        public static List<Test> GenerateTestsFromXml()
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(ScenarioBenchmark));
-
             StreamReader reader = new StreamReader(Path.Combine("PerformanceResultSample", "20190618003138-WPF hello world.xml"));
-            var scenarioBenchmark = (ScenarioBenchmark)serializer.Deserialize(reader);
-            Console.WriteLine(scenarioBenchmark.Name);
-            var tests = new List<Test>();
-            foreach (ScenarioBenchmarkTest scenarioBenchmarkTest in scenarioBenchmark.Tests)
-            {
-                var test = new Test();
-                test.Categories.Add("DotnetCoreSdk");
-                test.Name = scenarioBenchmark.Name + "." + scenarioBenchmarkTest.Name;
-                test.Counters.Add(new Counter() {
-                    Name = scenarioBenchmarkTest.Performance.metrics.ExecutionTime.displayName,
-                    TopCounter = true,
-                    DefaultCounter = true,
-                    HigherIsBetter = false,
-                    MetricName = scenarioBenchmarkTest.Performance.metrics.ExecutionTime.unit,
-                    Results = scenarioBenchmarkTest
-                        .Performance
-                        .iterations
-                        .Select(i => decimal.ToDouble(i.ExecutionTime))
-                        .ToList()
-                });
-
-                tests.Add(test);
-            }
-
-            return tests;
+            var test = XunitPerformanceResultConverter.GenerateTestsFromXml(reader);
+            AssertTestsObjectEqualToExpectedJson(test, File.ReadAllText(Path.Combine("PerformanceResultSample", "expectTestResult.json")));
         }
 
+        [Fact]
+        public void Given_perfWorkingDirectory_it_can_combine_xml_results()
+        {
+            List<Test> tests = XunitPerformanceResultConverter.BatchGenerateTests(new DirectoryInfo("PerformanceResultSample"));
+            AssertTestsObjectEqualToExpectedJson(tests, File.ReadAllText(Path.Combine("PerformanceResultSample", "expectBatchTestResult.json")));
+        }
 
         [Fact]
         public void It_can_get_commit_timestamp()
         {
             Reporting.Reporter.GetCommitTimestamp("29bf0a82d9d9e20b6da067b3bf2cb05bf0504e47", TestContext.GetRepoRoot()).Should().Be(DateTime.Parse("2019-04-23T13:59:53+00:00"));
+        }
+
+        private static void AssertTestsObjectEqualToExpectedJson(List<Test> testObject, string expectedJson)
+        {
+
+            var settings = new JsonSerializerSettings();
+            var resolver = new DefaultContractResolver();
+            resolver.NamingStrategy = new CamelCaseNamingStrategy() { ProcessDictionaryKeys = false };
+            settings.ContractResolver = resolver;
+
+            string generatedJson = JsonConvert.SerializeObject(testObject, Formatting.Indented, settings);
+            var generatedJObject = JsonConvert.DeserializeObject<JArray>(generatedJson);
+            var expectedJObject = JsonConvert.DeserializeObject<JArray>(expectedJson);
+
+            JToken.DeepEquals(generatedJObject, expectedJObject).
+                Should()
+                .BeTrue();
         }
     }
 }
---
+++ src/Tests/PerformanceTestsResultGenerator/XunitPerformanceResultConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Reporting;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Linq;
+
+namespace PerformanceTestsResultGenerator
+{
+    public static class XunitPerformanceResultConverter
+    {
+        public static List<Test> GenerateTestsFromXml(StreamReader stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ScenarioBenchmark));
+
+            var scenarioBenchmark = (ScenarioBenchmark)serializer.Deserialize(stream);
+            Console.WriteLine(scenarioBenchmark.Name);
+            var tests = new List<Test>();
+            foreach (ScenarioBenchmarkTest scenarioBenchmarkTest in scenarioBenchmark.Tests)
+            {
+                var test = new Test();
+                test.Categories.Add("DotnetCoreSdk");
+                test.Name = scenarioBenchmark.Name + "." + scenarioBenchmarkTest.Name;
+                test.Counters.Add(new Counter()
+                {
+                    Name = scenarioBenchmarkTest.Performance.metrics.ExecutionTime.displayName,
+                    TopCounter = true,
+                    DefaultCounter = true,
+                    HigherIsBetter = false,
+                    MetricName = scenarioBenchmarkTest.Performance.metrics.ExecutionTime.unit,
+                    Results = scenarioBenchmarkTest
+                        .Performance
+                        .iterations
+                        .Select(i => decimal.ToDouble(i.ExecutionTime))
+                        .ToList()
+                });
+
+                tests.Add(test);
+            }
+
+            return tests;
+        }
+
+        public static List<Test> BatchGenerateTests(DirectoryInfo directory)
+        {
+            return directory.EnumerateFiles()
+                .Where(f => string.Equals(Path.GetExtension(f.FullName), ".xml", StringComparison.OrdinalIgnoreCase))
+                .SelectMany(xmlFile => GenerateTestsFromXml(new StreamReader(xmlFile.FullName))).ToList();
+        }
+    }
+}
