4d599b87fac73ebb14cd1360780b65020b50088a
William Li <wul@microsoft.com>
2019-06-20 20:04:56 -0700

    Finish upload command

--- src/Tests/PerformanceTestsResultGenerator/Program.cs
+++ src/Tests/PerformanceTestsResultGenerator/Program.cs
@@ -20,25 +20,38 @@ namespace PerformanceTestsResultGenerator
                 "repository root that contain .git directory",
                 new Argument<DirectoryInfo>());
 
+            Option sas = new Option(
+                "--sas",
+                "shared access signatures",
+                new Argument<string>());
+
             // Add them to the root command
             RootCommand rootCommand = new RootCommand();
             rootCommand.Description = "Performance tests result generator";
             rootCommand.AddOption(outputPath);
             rootCommand.AddOption(repositoryRoot);
+            rootCommand.AddOption(sas);
 
             var result = rootCommand.Parse(args);
             var outputPathValue = result.ValueForOption<FileInfo>("output");
 
             if (outputPathValue == null)
             {
-                throw new PerformanceTestsResultGeneratorException("--output option is required." + outputPathValue);
+                throw new PerformanceTestsResultGeneratorException("--output option is required.");
             }
 
             var repositoryRootValue = result.ValueForOption<DirectoryInfo>("repository-root");
 
             if (repositoryRootValue == null)
             {
-                throw new PerformanceTestsResultGeneratorException("--repository-root is required." + repositoryRootValue);
+                throw new PerformanceTestsResultGeneratorException("--repository-root is required.");
+            }
+
+            var sasValue = result.ValueForOption<string>("sas");
+
+            if (sasValue == null)
+            {
+                throw new PerformanceTestsResultGeneratorException("--sas is required.");
             }
 
             Reporter reporter = Reporter.CreateReporter(repositoryRootValue);
@@ -46,6 +59,7 @@ namespace PerformanceTestsResultGenerator
             Console.WriteLine("Generated json:" + Environment.NewLine + generatedJson);
             File.WriteAllText(outputPathValue.FullName, generatedJson);
 
+            Uploader.Upload(outputPathValue, sasValue);
             return 0;
         }
     }
---
+++ src/Tests/PerformanceTestsResultGenerator/Uploader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Azure.Storage.Blob;
+
+namespace PerformanceTestsResultGenerator
+{
+    public static class Uploader
+    {
+        // SAS https://docs.microsoft.com/en-us/azure/storage/common/storage-dotnet-shared-access-signature-part-1
+        public static void Upload(FileInfo generatedResult, string sas)
+        {
+            var newNameForUpload = CreateCopyWithNewName(generatedResult);
+            Console.WriteLine($"Rename the file to {newNameForUpload.FullName}");
+            string uploadUrl = string.Format("{0}{1}", "https://pvscmdupload.blob.core.windows.net/results", sas);
+
+            var cloudBlockBlob = new CloudBlockBlob(new Uri(uploadUrl));
+
+            Console.WriteLine($"Start to upload");
+            cloudBlockBlob.UploadFromFileAsync(newNameForUpload.FullName).GetAwaiter().GetResult();
+
+            Console.WriteLine($"Done uploading");
+        }
+        private static FileInfo CreateCopyWithNewName(FileInfo generatedResult)
+        {
+            var newName = string.Format("{0}-{1}", Path.Join(
+                                                    generatedResult.DirectoryName,
+                                                    Environment.GetEnvironmentVariable("HELIX_CORRELATION_ID")),
+                                                    generatedResult.Name);
+            return generatedResult.CopyTo(newName);
+        }
+    }
+}
