b71f63d63cb45c7a2509a7fefe78bb188cd05bee
William Li <wul@microsoft.com>
2019-08-16 13:24:34 -0700

    Block publishing in single C++ and building exe

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishACppCliProject.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishACppCliProject.cs
@@ -4,6 +4,7 @@
 using System.IO;
 using System.Linq;
 using FluentAssertions;
+using Microsoft.NET.Build.Tasks;
 using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
@@ -11,28 +12,22 @@ using Xunit.Abstractions;
 
 namespace Microsoft.NET.Build.Tests
 {
-    public class GivenThatWeWantToBuildACppCliProject : SdkTest
+    public class GivenThatWeWantToPublishACppCliProject : SdkTest
     {
-        public GivenThatWeWantToBuildACppCliProject(ITestOutputHelper log) : base(log)
+        public GivenThatWeWantToPublishACppCliProject(ITestOutputHelper log) : base(log)
         {
         }
 
         [FullMSBuildOnlyFact]
-        public void It_builds_and_runs()
+        public void When_referenced_by_csharp_project_it_publishes_and_runs()
         {
             var testAsset = _testAssetsManager
                 .CopyTestAsset("NetCoreCsharpAppReferenceCppCliLib")
                 .WithSource()
                 .Restore(Log, "NETCoreCppCliTest.sln");
 
-            // build projects separately with BuildProjectReferences=false to simulate VS build behavior
-            new BuildCommand(Log, Path.Combine(testAsset.TestRoot, "NETCoreCppCliTest"))
-                .Execute("-p:Platform=x64")
-                .Should()
-                .Pass();
-
-            new BuildCommand(Log, Path.Combine(testAsset.TestRoot, "CSConsoleApp"))
-                .Execute(new string[] { "-p:Platform=x64", "-p:BuildProjectReferences=false" })
+            new PublishCommand(Log, Path.Combine(testAsset.TestRoot, "CSConsoleApp"))
+                .Execute(new string[] { "-p:Platform=x64" })
                 .Should()
                 .Pass();
 
@@ -40,6 +35,7 @@ namespace Microsoft.NET.Build.Tests
                 new DirectoryInfo(Path.Combine(testAsset.TestRoot, "CSConsoleApp", "bin")).GetDirectories().Single().FullName,
                 "Debug",
                 "netcoreapp3.0",
+                "publish",
                 "CSConsoleApp.exe");
 
             var runCommand = new RunExeCommand(Log, exe);
@@ -49,6 +45,21 @@ namespace Microsoft.NET.Build.Tests
                 .Pass()
                 .And
                 .HaveStdOutContaining("Hello, World!");
+        }
+
+        [FullMSBuildOnlyFact]
+        public void When_not_referenced_by_csharp_project_it_fails_to_publish()
+        {
+            var testAsset = _testAssetsManager
+                .CopyTestAsset("NetCoreCsharpAppReferenceCppCliLib")
+                .WithSource()
+                .Restore(Log, "NETCoreCppCliTest.sln");
+
+            new PublishCommand(Log, Path.Combine(testAsset.TestRoot, "NETCoreCppCliTest"))
+                .Execute(new string[] { "-p:Platform=x64" })
+                .Should()
+                .Fail()
+                .And.HaveStdOutContaining(Strings.NoSupportCppPublishDotnetCore);
         }
     }
 }
