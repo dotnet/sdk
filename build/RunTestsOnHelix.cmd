3fb7d38e2d34aac39d9295f7371d509a9a4f6a4d
William Li <wul@microsoft.com>
2019-10-01 16:34:10 -0700

    64 bits powershell

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -1,4 +1,4 @@
-%WINDIR%\sysnative\cmd.exe /c %HELIX_CORRELATION_PAYLOAD%\t\eng\dogfood.cmd
+%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command %HELIX_CORRELATION_PAYLOAD%\t\eng\dogfood.ps1
 set MSBuildSDKsPath=%HELIX_CORRELATION_PAYLOAD%\s
 set DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR=%MSBuildSDKsPath%
 set MicrosoftNETBuildExtensionsTargets=%MSBuildSDKsPath%\Microsoft.NET.Build.Extensions\msbuildExtensions\Microsoft\Microsoft.NET.Build.Extensions\Microsoft.NET.Build.Extensions.targets
