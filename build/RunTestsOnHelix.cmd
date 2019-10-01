105360bb9e4df0a830b2aa186b8cc138e20dcd92
William Li <wul@microsoft.com>
2019-09-30 18:04:57 -0700

    fix dotnetpath

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -2,5 +2,5 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command %HELIX_CORRELATION_PAYLOA
 set MSBuildSDKsPath=%HELIX_CORRELATION_PAYLOAD%\s
 set DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR=%MSBuildSDKsPath%
 set MicrosoftNETBuildExtensionsTargets=%MSBuildSDKsPath%\Microsoft.NET.Build.Extensions\msbuildExtensions\Microsoft\Microsoft.NET.Build.Extensions\Microsoft.NET.Build.Extensions.targets
-set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\t
+set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\t\.dotnet
 set PATH=%DOTNET_ROOT%;%PATH%
