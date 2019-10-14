a59fd27f37ffe6fcc829886e32d54150294b327b
William Li <wul@microsoft.com>
2019-10-14 15:51:32 -0700

    Fix isposixshell

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -6,3 +6,4 @@ set DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR=%MSBuildSDKsPath%
 set MicrosoftNETBuildExtensionsTargets=%MSBuildSDKsPath%\Microsoft.NET.Build.Extensions\msbuildExtensions\Microsoft\Microsoft.NET.Build.Extensions\Microsoft.NET.Build.Extensions.targets
 set DOTNET_ROOT=%CD%\t\.dotnet
 set PATH=%DOTNET_ROOT%;%PATH%
+set TestFullMSBuild=%1
