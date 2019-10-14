83f3c4552323f45d838e9fd6423282f8cd7b816b
William Li <wul@microsoft.com>
2019-10-14 13:44:29 -0700

    Do not modify correlation payload

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -1,6 +1,8 @@
-%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command %HELIX_CORRELATION_PAYLOAD%\t\eng\dogfood.ps1
+REM cannot modify HELIX_CORRELATION_PAYLOAD so copy to workitem payload
+robocopy %HELIX_CORRELATION_PAYLOAD%\t t /E
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command t\eng\dogfood.ps1
 set MSBuildSDKsPath=%HELIX_CORRELATION_PAYLOAD%\s
 set DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR=%MSBuildSDKsPath%
 set MicrosoftNETBuildExtensionsTargets=%MSBuildSDKsPath%\Microsoft.NET.Build.Extensions\msbuildExtensions\Microsoft\Microsoft.NET.Build.Extensions\Microsoft.NET.Build.Extensions.targets
-set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\t\.dotnet
+set DOTNET_ROOT=t\.dotnet
 set PATH=%DOTNET_ROOT%;%PATH%
