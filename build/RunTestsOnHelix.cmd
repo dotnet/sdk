e44a3717b3dc7fca25c71da3a78f533f88f69cf0
William Li <wul@microsoft.com>
2019-10-31 15:55:36 -0700

    Add missing TestFullMSBuild: 'true'

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -3,5 +3,5 @@ set DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR=%MSBuildSDKsPath%
 set MicrosoftNETBuildExtensionsTargets=%MSBuildSDKsPath%\Microsoft.NET.Build.Extensions\msbuildExtensions\Microsoft\Microsoft.NET.Build.Extensions\Microsoft.NET.Build.Extensions.targets
 set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\d
 set PATH=%DOTNET_ROOT%;%PATH%
-set TestFullMSBuild=%1
+set TestFullMSBuild=true
 dir %HELIX_CORRELATION_PAYLOAD%\d
