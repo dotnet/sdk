22d6f04a548c5aaedae71100e0295d682948f2d5
William Li <wul@microsoft.com>
2019-11-01 13:06:15 -0700

    Upload arcade and get full-msbuild-path

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -5,4 +5,4 @@ set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\d
 set PATH=%DOTNET_ROOT%;%PATH%
 
 REM Use powershell to call partical Arcade logic to get full framework msbuild path and assign it
-FOR /F "tokens=*" %g IN ('PowerShell -Command "C:\work\sdk\eng\print-full-msbuild-path.ps1"') do (SET DOTNET_SDK_TEST_MSBUILD_PATH=%g)
+FOR /F "tokens=*" %g IN ('PowerShell -Command "%HELIX_CORRELATION_PAYLOAD%\t\eng\print-full-msbuild-path.ps1"') do (SET DOTNET_SDK_TEST_MSBUILD_PATH=%g)
