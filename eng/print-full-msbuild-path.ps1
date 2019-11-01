9be58c008528ddeff9ebeda4237a2914988b0d2a
William Li <wul@microsoft.com>
2019-11-01 13:24:48 -0700

    no check for full msbuild

--- eng/print-full-msbuild-path.ps1
+++ eng/print-full-msbuild-path.ps1
@@ -1,9 +1,7 @@
 . $PSScriptRoot\common\tools.ps1
-function InitializeCustomSDKToolset {
-  if ($env:TestFullMSBuild -eq "true") {
-     $env:DOTNET_SDK_TEST_MSBUILD_PATH = InitializeVisualStudioMSBuild -install:$false -vsRequirements:$GlobalJson.tools.'vs-opt'
-  }
+function GetFullMsbuildPath {
+    $env:DOTNET_SDK_TEST_MSBUILD_PATH = InitializeVisualStudioMSBuild -install:$false -vsRequirements:$GlobalJson.tools.'vs-opt'
 }
 
-InitializeCustomSDKToolset
+GetFullMsbuildPath
 $env:DOTNET_SDK_TEST_MSBUILD_PATH
