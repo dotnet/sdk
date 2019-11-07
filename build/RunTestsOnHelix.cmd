ac4c81ac8092ed9323948e9d94e0f4c260464421
William Li <wul@microsoft.com>
2019-11-06 21:09:43 -0800

    Add in condition

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -3,6 +3,9 @@ set DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR=%MSBuildSDKsPath%
 set MicrosoftNETBuildExtensionsTargets=%MSBuildSDKsPath%\Microsoft.NET.Build.Extensions\msbuildExtensions\Microsoft\Microsoft.NET.Build.Extensions\Microsoft.NET.Build.Extensions.targets
 set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\d
 set PATH=%DOTNET_ROOT%;%PATH%
+set TestFullMSBuild=%1
 
 REM Use powershell to call partical Arcade logic to get full framework msbuild path and assign it
-FOR /F "tokens=*" %%g IN ('PowerShell -ExecutionPolicy ByPass -File "%HELIX_CORRELATION_PAYLOAD%\t\eng\print-full-msbuild-path.ps1"') do (SET DOTNET_SDK_TEST_MSBUILD_PATH=%%g)
+if "%TestFullMSBuild%"=="true" (
+    FOR /F "tokens=*" %%g IN ('PowerShell -ExecutionPolicy ByPass -File "%HELIX_CORRELATION_PAYLOAD%\t\eng\print-full-msbuild-path.ps1"') do (SET DOTNET_SDK_TEST_MSBUILD_PATH=%%g)
+)
