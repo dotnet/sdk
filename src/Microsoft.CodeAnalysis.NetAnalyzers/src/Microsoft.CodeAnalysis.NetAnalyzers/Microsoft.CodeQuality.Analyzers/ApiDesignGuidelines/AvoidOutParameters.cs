f6f4f61702519460107bc9bf09c1e600e82706be
Amaury Levé <evangelink@gmail.com>
2020-01-08 16:35:46 +0100

    CA1021: Avoid out parameters

---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.CodeQuality.Analyzers/ApiDesignGuidelines/AvoidOutParameters.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class AvoidOutParameters : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA1021";
+
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.AvoidOutParametersTitle), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.AvoidOutParametersMessage), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.AvoidOutParametersDescription), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(RuleId,
+                                                                             s_localizableTitle,
+                                                                             s_localizableMessage,
+                                                                             DiagnosticCategory.Design,
+                                                                             DiagnosticHelpers.DefaultDiagnosticSeverity,
+                                                                             isEnabledByDefault: DiagnosticHelpers.EnabledByDefaultIfNotBuildingVSIX,
+                                                                             description: s_localizableDescription,
+                                                                             helpLinkUri: "https://docs.microsoft.com/en-us/visualstudio/code-quality/ca1021",
+                                                                             customTags: FxCopWellKnownDiagnosticTags.PortedFxCopRule);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext analysisContext)
+        {
+            analysisContext.EnableConcurrentExecution();
+            analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            analysisContext.RegisterCompilationStartAction(csac =>
+            {
+                var outAttributeType = csac.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemRuntimeInteropServicesOutAttribute);
+
+                csac.RegisterSymbolAction(sac =>
+                {
+                    var methodSymbol = (IMethodSymbol)sac.Symbol;
+
+                    var numberOfOutParams = methodSymbol.Parameters.Count(p => IsOutParameter(p, outAttributeType));
+
+                    if (numberOfOutParams >= 1 && !IsTryPatternMethod(methodSymbol, outAttributeType, numberOfOutParams))
+                    {
+                        sac.ReportDiagnostic(methodSymbol.CreateDiagnostic(Rule));
+                    }
+                }, SymbolKind.Method);
+            });
+        }
+
+        private static bool IsOutParameter(IParameterSymbol parameterSymbol, INamedTypeSymbol? outAttributeSymbol) =>
+            parameterSymbol.RefKind == RefKind.Out ||
+            // Handle VB.NET special case for out parameters
+            (parameterSymbol.RefKind == RefKind.Ref && parameterSymbol.HasAttribute(outAttributeSymbol));
+
+        private static bool IsTryPatternMethod(IMethodSymbol methodSymbol, INamedTypeSymbol? outAttributeSymbol, int numberOfOutParams) =>
+            methodSymbol.Name.StartsWith("Try", StringComparison.Ordinal) &&
+            methodSymbol.ReturnType.SpecialType == SpecialType.System_Boolean &&
+            methodSymbol.Parameters.Length >= 1 &&
+            IsOutParameter(methodSymbol.Parameters[methodSymbol.Parameters.Length - 1], outAttributeSymbol) &&
+            numberOfOutParams == 1;
+    }
+}
---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.CodeQuality.Analyzers/ApiDesignGuidelines/AvoidOutParametersTests.cs
@@ -0,0 +1,291 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.AvoidOutParameters,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using VerifyVB = Test.Utilities.VisualBasicCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.AvoidOutParameters,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines.UnitTests
+{
+    public class AvoidOutParametersTests
+    {
+        [Fact]
+        public async Task SimpleCases_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class C
+{
+    public void M1(out C c)
+    {
+        c = null;
+    }
+
+    public void M2(string s, out C c)
+    {
+        c = null;
+    }
+
+    public void M3(string s1, out C c, string s2)
+    {
+        c = null;
+    }
+
+    public void M4(out C c, out string s1)
+    {
+        c = null;
+        s1 = null;
+    }
+}",
+                GetCSharpExpectedResult(4, 17),
+                GetCSharpExpectedResult(9, 17),
+                GetCSharpExpectedResult(14, 17),
+                GetCSharpExpectedResult(19, 17));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System.Runtime.InteropServices
+
+Public Class C
+    Public Sub M1(<Out> ByRef c As C)
+        c = Nothing
+    End Sub
+
+    Public Sub M2(ByVal s As String, <Out> ByRef c As C)
+        c = Nothing
+    End Sub
+
+    Public Sub M3(ByVal s1 As String, <Out> ByRef c As C, ByVal s2 As String)
+        c = Nothing
+    End Sub
+
+    Public Sub M4(<Out> ByRef c As C, <Out> ByRef s1 As String)
+        c = Nothing
+        s1 = Nothing
+    End Sub
+End Class",
+                GetBasicExpectedResult(5, 16),
+                GetBasicExpectedResult(9, 16),
+                GetBasicExpectedResult(13, 16),
+                GetBasicExpectedResult(17, 16));
+        }
+
+        [Fact]
+        public async Task MultipleOut_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class C
+{
+    public void M1(out C c, out string s1)
+    {
+        c = null;
+        s1 = null;
+    }
+
+    public void M2(out C c, string s1, out string s2)
+    {
+        c = null;
+        s2 = null;
+    }
+}",
+                GetCSharpExpectedResult(4, 17),
+                GetCSharpExpectedResult(10, 17));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System.Runtime.InteropServices
+
+Public Class C
+    Public Sub M1(<Out> ByRef c As C, <Out> ByRef s1 As String)
+        c = Nothing
+        s1 = Nothing
+    End Sub
+
+    Public Sub M2(<Out> ByRef c As C, ByVal s1 As String, <Out> ByRef s2 As String)
+        c = Nothing
+        s2 = Nothing
+    End Sub
+End Class",
+                GetBasicExpectedResult(5, 16),
+                GetBasicExpectedResult(10, 16));
+        }
+
+        [Fact]
+        public async Task OutAndRef_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class C
+{
+    public void M1(out C c, ref string s1)
+    {
+        c = null;
+    }
+
+    public void M2(out C c, string s1, out string s2, ref string s3)
+    {
+        c = null;
+        s2 = null;
+    }
+}",
+                GetCSharpExpectedResult(4, 17),
+                GetCSharpExpectedResult(9, 17));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System.Runtime.InteropServices
+
+Public Class C
+    Public Sub M1(<Out> ByRef c As C, ByRef s1 As String)
+        c = Nothing
+    End Sub
+
+    Public Sub M2(<Out> ByRef c As C, ByVal s1 As String, <Out> ByRef s2 As String, ByRef s3 As String)
+        c = Nothing
+        s2 = Nothing
+    End Sub
+End Class",
+                GetBasicExpectedResult(5, 16),
+                GetBasicExpectedResult(9, 16));
+        }
+
+        [Fact]
+        public async Task Ref_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class C
+{
+    public void M(ref string s1)
+    {
+    }
+}");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Public Class C
+    Public Sub M(ByRef s1 As String)
+    End Sub
+End Class");
+        }
+
+        [Fact]
+        public async Task TryPattern_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System.Collections.Generic;
+
+public class C
+{
+    private Dictionary<string, C> dict = new Dictionary<string, C>();
+
+    public static bool TryParse(string s, out C result)
+    {
+        result = null;
+        return false;
+    }
+
+    private bool TryGetOrAdd(string key, C valueIfNotFound, out C result)
+    {
+        if (dict.ContainsKey(key))
+        {
+            result = dict[key];
+            return true;
+        }
+
+        dict[key] = valueIfNotFound;
+        result = valueIfNotFound;
+        return false;
+    }
+
+    public static bool Try(out C c)
+    {
+        c = null;
+        return true;
+    }
+}");
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System.Collections.Generic
+Imports System.Runtime.InteropServices
+
+Public Class C
+    Private dict = New Dictionary(Of String, C)
+
+    Public Shared Function TryParse(ByVal s As String, <Out> ByRef result As C) As Boolean
+        result = Nothing
+        Return False
+    End Function
+
+    Private Function TryGetOrAdd(ByVal key As String, ByVal valueIfNotFound As C, <Out> ByRef result As C) As Boolean
+        If dict.ContainsKey(key) Then
+            result = dict(key)
+            Return True
+        End If
+        dict(key) = valueIfNotFound
+        result = valueIfNotFound
+        Return False
+    End Function
+
+    Public Shared Function [Try](<Out> ByRef c As C) As Boolean
+        c = Nothing
+        Return True
+    End Function
+End Class");
+        }
+
+        [Fact]
+        public async Task InvalidTryPattern_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+public class C
+{
+    public void TryM1(out C c)
+    {
+        c = null;
+    }
+
+    public bool TryM2(out C c, string s)
+    {
+        c = null;
+        return false;
+    }
+
+    public static bool TRY_PARSE(string s, out C c)
+    {
+        c = null;
+        return true;
+    }
+}",
+                GetCSharpExpectedResult(4, 17),
+                GetCSharpExpectedResult(9, 17),
+                GetCSharpExpectedResult(15, 24));
+
+            await VerifyVB.VerifyAnalyzerAsync(@"
+Imports System.Runtime.InteropServices
+
+Public Class C
+    Public Sub TryM1(<Out> ByRef c As C)
+        c = Nothing
+    End Sub
+
+    Public Function TryM2(<Out> ByRef c As C, ByVal s As String) As Boolean
+        c = Nothing
+        Return False
+    End Function
+
+    Public Shared Function TRY_PARSE(ByVal s As String, <Out> ByRef c As C) As Boolean
+        c = Nothing
+        Return True
+    End Function
+End Class",
+                GetCSharpExpectedResult(5, 16),
+                GetCSharpExpectedResult(9, 21),
+                GetCSharpExpectedResult(14, 28));
+        }
+
+        private static DiagnosticResult GetCSharpExpectedResult(int line, int col) =>
+            VerifyCS.Diagnostic().WithLocation(line, col);
+
+        private static DiagnosticResult GetBasicExpectedResult(int line, int col) =>
+            VerifyVB.Diagnostic().WithLocation(line, col);
+    }
+}
