fc2a2362441bd96fbfdd62cd7e2c92eaa0003f14
jpshrader <46694288+jpshrader@users.noreply.github.com>
2020-02-11 17:41:13 -0500

    format-500: Add in stringComparison again, turns out it's needed

--- src/CodeFormatter.cs
+++ src/CodeFormatter.cs
@@ -358,7 +358,7 @@ namespace Microsoft.CodeAnalysis.Tools
                 // Ignore generated code files.
                 return true;
             }
-            else if (!filesToIgnore.IsEmpty && filesToIgnore.Any(f => document.FilePath.StartsWith(f)))
+            else if (!filesToIgnore.IsEmpty && filesToIgnore.Any(f => document.FilePath.StartsWith(f, StringComparison.OrdinalIgnoreCase)))
             {
                 // Ignore file in, or under a folder in the list to exclude
                 return true;
