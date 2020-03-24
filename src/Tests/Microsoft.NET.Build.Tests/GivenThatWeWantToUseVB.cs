98a57918ab35c3b491f45b212d98e72ebdf37b4a
Sarah Oslund <sfoslund@microsoft.com>
2020-03-24 11:16:02 -0700

    Removing testing work arounds for referencing targeting packs

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToUseVB.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToUseVB.cs
@@ -40,13 +40,6 @@ namespace Microsoft.NET.Build.Tests
         [InlineData("netcoreapp3.0", false)]
         public void It_builds_a_simple_vb_project(string targetFramework, bool isExe)
         {
-            if (targetFramework == "net45" && !TestProject.ReferenceAssembliesAreInstalled("v4.5"))
-            {
-                // skip net45 when we do not have .NET Framework 4.5 reference assemblies
-                // due to https://github.com/dotnet/core-sdk/issues/3228
-                return;
-            }
-
             var (expectedVBRuntime, expectedOutputFiles) = GetExpectedOutputs(targetFramework, isExe);
 
             var testProject = new TestProject
