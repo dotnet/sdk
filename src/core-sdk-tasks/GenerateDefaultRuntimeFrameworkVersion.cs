a1fa03d072abf066c6fdf8ec2f0a16a67f145818
Sarah Oslund <sfoslund@microsoft.com>
2020-03-26 10:01:21 -0700

    Adding automatic generation of DefaultRuntimeFrameworkVersions

---
+++ src/core-sdk-tasks/GenerateDefaultRuntimeFrameworkVersion.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Versioning;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class GenerateDefaultRuntimeFrameworkVersion : Task
+    {
+        [Required]
+        public string RuntimePackVersion { get; set; }
+
+        [Output]
+        public string DefaultRuntimeFrameworkVersion { get; set; }
+
+        public override bool Execute()
+        {
+            if (NuGetVersion.TryParse(RuntimePackVersion, out var version))
+            {
+                if (version.IsPrerelease && version.Patch == 0)
+                {
+                    DefaultRuntimeFrameworkVersion = RuntimePackVersion;
+                }
+                else
+                {
+                    DefaultRuntimeFrameworkVersion = new NuGetVersion(version.Major, version.Minor, 0).ToFullString();
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
