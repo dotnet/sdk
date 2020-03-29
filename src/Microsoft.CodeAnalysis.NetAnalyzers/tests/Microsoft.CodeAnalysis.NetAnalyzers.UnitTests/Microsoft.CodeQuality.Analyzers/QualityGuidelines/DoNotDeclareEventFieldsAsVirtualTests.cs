763c0081042fda82e0f93a8bdfe19081c882c138
Amaury Levé <evangelink@gmail.com>
2020-03-29 19:47:25 +0200

    CA1070: Do not declare event fields as virtual

---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.CodeQuality.Analyzers/QualityGuidelines/DoNotDeclareEventFieldsAsVirtualTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
+    Microsoft.CodeQuality.Analyzers.QualityGuidelines.DoNotDeclareEventFieldsAsVirtual,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace Microsoft.CodeQuality.Analyzers.UnitTests.QualityGuidelines
+{
+    public class DoNotDeclareEventFieldsAsVirtualTests
+    {
+        [Fact]
+        public async Task EventFieldVirtual_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+public class C
+{
+    public virtual event EventHandler ThresholdReached;
+}",
+                VerifyCS.Diagnostic().WithLocation(5, 39));
+        }
+
+        [Fact]
+        public async Task EventPropertyVirtual_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+public class C
+{
+    public event EventHandler ThresholdReached
+    {
+        add
+        {
+        }
+        remove
+        {
+        }
+    }
+}");
+        }
+
+        [Fact]
+        public async Task EventFieldVirtualAllAccessibilities_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(@"
+using System;
+public class C
+{
+    public virtual event EventHandler [|Event1|];
+    protected virtual event EventHandler [|Event2|];
+    internal virtual event EventHandler [|Event3|];
+    protected internal virtual event EventHandler [|Event4|];
+}");
+        }
+    }
+}
