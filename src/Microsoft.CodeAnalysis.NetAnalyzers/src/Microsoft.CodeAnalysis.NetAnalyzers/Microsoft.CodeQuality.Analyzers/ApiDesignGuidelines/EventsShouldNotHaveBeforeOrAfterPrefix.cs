35b39f014a9884de316ed60f5329fc7165b7c164
Amaury Levé <evangelink@gmail.com>
2020-04-15 14:59:40 +0200

    Add StartsWith check upfront

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.CodeQuality.Analyzers/ApiDesignGuidelines/EventsShouldNotHaveBeforeOrAfterPrefix.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.CodeQuality.Analyzers/ApiDesignGuidelines/EventsShouldNotHaveBeforeOrAfterPrefix.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
 using Analyzer.Utilities;
 using Analyzer.Utilities.Extensions;
@@ -12,6 +13,9 @@ namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
     public sealed class EventsShouldNotHaveBeforeOrAfterPrefix : DiagnosticAnalyzer
     {
         internal const string RuleId = "CA1713";
+
+        private const string AfterKeyword = "After";
+        private const string BeforeKeyword = "Before";
 
         private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.EventsShouldNotHaveBeforeOrAfterPrefixTitle), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
         private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftCodeQualityAnalyzersResources.EventsShouldNotHaveBeforeOrAfterPrefixMessage), MicrosoftCodeQualityAnalyzersResources.ResourceManager, typeof(MicrosoftCodeQualityAnalyzersResources));
@@ -29,7 +33,7 @@ namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
-        private static readonly ImmutableHashSet<string> s_invalidPrefixes = ImmutableHashSet.Create("After", "Before");
+        private static readonly ImmutableHashSet<string> s_invalidPrefixes = ImmutableHashSet.Create(AfterKeyword, BeforeKeyword);
 
         public override void Initialize(AnalysisContext analysisContext)
         {
@@ -38,7 +42,15 @@ namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
 
             analysisContext.RegisterSymbolAction(context =>
             {
-                var wordParse = new WordParser(context.Symbol.Name, WordParserOptions.SplitCompoundWords);
+                var eventName = context.Symbol.Name;
+
+                if (!eventName.StartsWith(BeforeKeyword, StringComparison.Ordinal) &&
+                    !eventName.StartsWith(AfterKeyword, StringComparison.Ordinal))
+                {
+                    return;
+                }
+
+                var wordParse = new WordParser(eventName, WordParserOptions.SplitCompoundWords);
 
                 if (wordParse.NextWord() is string firstWord &&
                     s_invalidPrefixes.Contains(firstWord) &&
