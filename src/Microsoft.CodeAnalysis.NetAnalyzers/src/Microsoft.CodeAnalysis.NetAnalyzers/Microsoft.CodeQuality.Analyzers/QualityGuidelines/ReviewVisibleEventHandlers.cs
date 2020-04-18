7de78277093e1c4aba12fadefc5f5b799510424c
Amaury Levé <evangelink@gmail.com>
2020-04-18 23:16:09 +0200

    Fix call after API change

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.CodeQuality.Analyzers/QualityGuidelines/ReviewVisibleEventHandlers.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.CodeQuality.Analyzers/QualityGuidelines/ReviewVisibleEventHandlers.cs
@@ -57,7 +57,7 @@ namespace Microsoft.CodeQuality.Analyzers.QualityGuidelines
                     }
 
                     // FxCop compat: only analyze externally visible symbols by default.
-                    if (!method.MatchesConfiguredVisibility(context.Options, Rule, context.CancellationToken))
+                    if (!method.MatchesConfiguredVisibility(context.Options, Rule, context.Compilation, context.CancellationToken))
                     {
                         return;
                     }
