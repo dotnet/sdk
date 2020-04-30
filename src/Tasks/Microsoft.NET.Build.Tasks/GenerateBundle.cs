6c4007ab2245fa25622de8ad6d40bfb58224fc97
Swaroop Sridhar <swaroop.sridhar@microsoft.com>
2020-04-30 00:27:25 -0700

    [release/5.0.1xx-preview4] Single-File: Close AppHost stream when reusing extraction
    Customer Scenario
    Self-contained Apps published as a single-file fail at run-time.
    
    Problem
    
    Single-file bundles are now processed in the framework rather than the apphost (https://github.com/dotnet/runtime/pull/34274).
    This means that hostpolicy and hostfxr DLLs are excluded from being bundled themselves.
    In the case of self-contained single-file apps, these files need to be separate files until static-apphost is available.
    This needs to be ensured by the SDK; otherwise app execution will fail.
    
    Solution
    
    This change fixes the problem by adapting the SDK to:
    * Pass the correct target-OS settings (otherwise cross-targetting builds will not work correctly)
    * Copy files excluded by the Bundler to the publish directory.
    
    The stage-0 netcoreapp is updated to preview4, because preview2 apphost is not compatible with preview4 bundler.
    
    Risk
    Low

--- src/Tasks/Microsoft.NET.Build.Tasks/GenerateBundle.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/GenerateBundle.cs
@@ -5,6 +5,8 @@ using Microsoft.Build.Framework;
 using Microsoft.NET.HostModel.Bundle;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.NET.Build.Tasks
 {
@@ -19,12 +21,23 @@ namespace Microsoft.NET.Build.Tasks
         [Required]
         public string OutputDir { get; set; }
         [Required]
+        public string TargetFrameworkVersion { get; set; }
+        [Required]
+        public string RuntimeIdentifier { get; set; }
+
+        [Required]
         public bool ShowDiagnosticOutput { get; set; }
+
+        [Output]
+        public ITaskItem[] ExcludedFiles { get; set; }
 
         protected override void ExecuteCore()
         {
+            OSPlatform targetOS = RuntimeIdentifier.StartsWith("win") ? OSPlatform.Windows :
+                                  RuntimeIdentifier.StartsWith("osx") ? OSPlatform.OSX : OSPlatform.Linux;
+
             BundleOptions options = BundleOptions.BundleAllContent | (IncludeSymbols ? BundleOptions.BundleSymbolFiles : BundleOptions.None);
-            var bundler = new Bundler(AppHostName, OutputDir, options, diagnosticOutput: ShowDiagnosticOutput);
+            var bundler = new Bundler(AppHostName, OutputDir, options, targetOS, new Version(TargetFrameworkVersion), ShowDiagnosticOutput);
             var fileSpec = new List<FileSpec>(FilesToBundle.Length);
 
             foreach (var item in FilesToBundle)
@@ -34,6 +47,12 @@ namespace Microsoft.NET.Build.Tasks
             }
 
             bundler.GenerateBundle(fileSpec);
+			
+			// Certain files are excluded from the bundle, based on BundleOptions.
+			// For example, native files and contents files are excluded by default.
+			// Return the set of excluded files in ExcludedFiles, so that they can be placed in the publish directory.
+			
+            ExcludedFiles = FilesToBundle.Zip(fileSpec, (item, spec) => (spec.Excluded) ? item : null).Where(x => x != null).ToArray();
         }
     }
 }
