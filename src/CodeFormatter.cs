529c2197510a4192777939df8cd803f9397c791b
Wojciech Cibor <wojciech.cibor@gmail.com>
2020-05-20 20:08:12 +0200

    Add using directive formatting

--- src/CodeFormatter.cs
+++ src/CodeFormatter.cs
@@ -29,6 +29,7 @@ namespace Microsoft.CodeAnalysis.Tools
             new FinalNewlineFormatter(),
             new EndOfLineFormatter(),
             new CharsetFormatter(),
+            new ImportsFormatter(),
         }.ToImmutableArray();
 
         public static async Task<WorkspaceFormatResult> FormatWorkspaceAsync(
---
+++ src/Formatters/ImportsFormatter.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.CodingConventions;
+
+namespace Microsoft.CodeAnalysis.Tools.Formatters
+{
+    /// <summary>
+    /// ImportsFormatter that uses the <see cref="Formatter"/> to format document import directives.
+    /// </summary>
+    internal sealed class ImportsFormatter : DocumentFormatter
+    {
+        // TODO: Use warning from Resources.
+        protected override string FormatWarningDescription => "Fix imports.";
+
+        protected override async Task<SourceText> FormatFileAsync(
+            Document document,
+            SourceText sourceText,
+            OptionSet options,
+            ICodingConventionsSnapshot codingConventions,
+            FormatOptions formatOptions,
+            ILogger logger,
+            CancellationToken cancellationToken)
+        {
+            var formattedDocument = await Formatter.OrganizeImportsAsync(document, cancellationToken).ConfigureAwait(false);
+            return await formattedDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
+
