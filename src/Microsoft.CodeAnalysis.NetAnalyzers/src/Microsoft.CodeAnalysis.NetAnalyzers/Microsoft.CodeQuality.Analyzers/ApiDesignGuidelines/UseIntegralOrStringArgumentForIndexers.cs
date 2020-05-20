418658366596d82c4b25dc93b7a0fd32e88cb51a
Amaury Levé <evangelink@gmail.com>
2020-05-20 09:56:06 +0200

    Refactor implementation

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.CodeQuality.Analyzers/ApiDesignGuidelines/UseIntegralOrStringArgumentForIndexers.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.CodeQuality.Analyzers/ApiDesignGuidelines/UseIntegralOrStringArgumentForIndexers.cs
@@ -31,16 +31,17 @@ namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
                                                                              isPortedFxCopRule: true,
                                                                              isDataflowRule: false);
 
-        private static readonly SpecialType[] s_allowedSpecialTypes = new[] {
-                        SpecialType.System_String,
-                        SpecialType.System_Int16,
-                        SpecialType.System_Int32,
-                        SpecialType.System_Int64,
-                        SpecialType.System_Object,
-                        SpecialType.System_UInt16,
-                        SpecialType.System_UInt32,
-                        SpecialType.System_UInt64
-                        };
+        private static readonly ImmutableHashSet<SpecialType> s_allowedSpecialTypes =
+            ImmutableHashSet.Create(
+                SpecialType.System_String,
+                SpecialType.System_Int16,
+                SpecialType.System_Int32,
+                SpecialType.System_Int64,
+                SpecialType.System_Object,
+                SpecialType.System_UInt16,
+                SpecialType.System_UInt32,
+                SpecialType.System_UInt64
+            );
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
@@ -70,30 +71,36 @@ namespace Microsoft.CodeQuality.Analyzers.ApiDesignGuidelines
         private void AnalyzeSymbol(SymbolAnalysisContext context, ImmutableHashSet<INamedTypeSymbol> allowedTypes)
         {
             var symbol = (IPropertySymbol)context.Symbol;
-            if (symbol.IsIndexer &&
-                !symbol.IsOverride &&
-                symbol.MatchesConfiguredVisibility(context.Options, Rule, context.Compilation, context.CancellationToken))
+            if (!symbol.IsIndexer || symbol.IsOverride)
             {
-                if (symbol.GetParameters().Length == 1)
-                {
-                    ITypeSymbol paramType = symbol.GetParameters()[0].Type;
+                return;
+            }
 
-                    if (paramType.TypeKind == TypeKind.TypeParameter)
-                    {
-                        return;
-                    }
+            if (symbol.Parameters.Length != 1)
+            {
+                return;
+            }
 
-                    if (paramType.TypeKind == TypeKind.Enum)
-                    {
-                        paramType = ((INamedTypeSymbol)paramType).EnumUnderlyingType;
-                    }
+            ITypeSymbol paramType = symbol.Parameters[0].Type;
 
-                    if (!s_allowedSpecialTypes.Contains(paramType.SpecialType) &&
-                        !allowedTypes.Contains(paramType))
-                    {
-                        context.ReportDiagnostic(symbol.CreateDiagnostic(Rule));
-                    }
-                }
+            if (paramType.TypeKind == TypeKind.TypeParameter)
+            {
+                return;
+            }
+
+            if (paramType.TypeKind == TypeKind.Enum)
+            {
+                paramType = ((INamedTypeSymbol)paramType).EnumUnderlyingType;
+            }
+
+            if (s_allowedSpecialTypes.Contains(paramType.SpecialType) || allowedTypes.Contains(paramType))
+            {
+                return;
+            }
+
+            if (symbol.MatchesConfiguredVisibility(context.Options, Rule, context.Compilation, context.CancellationToken))
+            {
+                context.ReportDiagnostic(symbol.CreateDiagnostic(Rule));
             }
         }
     }
