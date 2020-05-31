33b340d71d673b0c324cb2b3dfef18522c4b1d76
Joey Robichaud <jorobich@microsoft.com>
2020-05-31 16:37:56 -0700

    Add issue link to ImportsFormatter workaround

--- src/Formatters/ImportsFormatter.cs
+++ src/Formatters/ImportsFormatter.cs
@@ -37,6 +37,7 @@ namespace Microsoft.CodeAnalysis.Tools.Formatters
                 }
 
                 // Because the Formatter does not abide the `end_of_line` option we have to fix up the ends of the organized lines.
+                // See https://github.com/dotnet/roslyn/issues/44136
                 var organizedSourceText = await organizedDocument.GetTextAsync(cancellationToken);
                 return await _endOfLineFormatter.FormatFileAsync(organizedDocument, organizedSourceText, optionSet, analyzerConfigOptions, formatOptions, logger, cancellationToken);
             }
