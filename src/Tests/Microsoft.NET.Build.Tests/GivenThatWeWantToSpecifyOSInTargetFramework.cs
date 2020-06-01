50f8c83ff2c421811eb62ef60e0db3ff6c562969
Sarah Oslund <sfoslund@microsoft.com>
2020-06-01 15:13:28 -0700

    Add SupportedTargetPlatforms props and error on unsupported TPI

---
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToSpecifyOSInTargetFramework.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using FluentAssertions;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Commands;
+using Xunit;
+using Xunit.Abstractions;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.ProjectConstruction;
+
+namespace Microsoft.NET.Build.Tests
+{
+    public class GivenThatWeWantToSpecifyOSInTargetFramework : SdkTest
+    {
+        public GivenThatWeWantToSpecifyOSInTargetFramework(ITestOutputHelper log) : base(log)
+        {}
+
+        [Theory]
+        [InlineData("windows")]
+        [InlineData("macos")]
+        [InlineData("ios")]
+        [InlineData("android")]
+        public void It_passes_on_supported_os(string targetPlatform)
+        {
+            TestProject testProject = new TestProject()
+            {
+                Name = "SupportedOS",
+                IsSdkProject = true,
+                TargetFrameworks = "net5.0"
+            };
+            testProject.AdditionalProperties["TargetPlatformIdentifier"] = targetPlatform;
+
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var build = new BuildCommand(Log, Path.Combine(testAsset.Path, testProject.Name));
+            build.Execute() 
+                .Should()
+                .Pass();
+        }
+
+        [Fact]
+        public void It_fails_on_unsupported_os()
+        {
+            TestProject testProject = new TestProject()
+            {
+                Name = "UnsupportedOS",
+                IsSdkProject = true,
+                TargetFrameworks = "net5.0"
+            };
+            testProject.AdditionalProperties["TargetPlatformIdentifier"] = "customos";
+
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var build = new BuildCommand(Log, Path.Combine(testAsset.Path, testProject.Name));
+            build.Execute()
+                .Should()
+                .Fail()
+                .And
+                .HaveStdOutContaining("NETSDK1132");
+        }
+    }
+}
