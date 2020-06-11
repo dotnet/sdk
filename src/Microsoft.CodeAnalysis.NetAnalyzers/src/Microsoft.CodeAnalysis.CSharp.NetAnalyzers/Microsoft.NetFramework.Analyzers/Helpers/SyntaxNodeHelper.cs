c6e1ae86971ac473343395e843a764223a19bba7
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-06-11 20:17:14 +0200

    Update SyntaxNodeHelper.cs

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.CSharp.NetAnalyzers/Microsoft.NetFramework.Analyzers/Helpers/SyntaxNodeHelper.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.CSharp.NetAnalyzers/Microsoft.NetFramework.Analyzers/Helpers/SyntaxNodeHelper.cs
@@ -17,7 +17,7 @@ namespace Microsoft.NetFramework.CSharp.Analyzers.Helpers
         private CSharpSyntaxNodeHelper()
         { }
 
-        public override ITypeSymbol? GetClassDeclarationTypeSymbol(SyntaxNode? node, SemanticModel semanticModel) // TODO: Should I add optional CancellationToken?
+        public override ITypeSymbol? GetClassDeclarationTypeSymbol(SyntaxNode? node, SemanticModel semanticModel)
         {
             if (node == null)
             {
