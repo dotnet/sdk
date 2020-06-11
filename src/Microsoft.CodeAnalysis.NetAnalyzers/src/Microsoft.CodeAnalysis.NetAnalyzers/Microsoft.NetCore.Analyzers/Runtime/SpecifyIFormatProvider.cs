d3a3112824bf89dae26675728350e44eb5a01e3c
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-06-11 20:23:19 +0200

    Update src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/SpecifyIFormatProvider.cs

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/SpecifyIFormatProvider.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/SpecifyIFormatProvider.cs
@@ -206,7 +206,7 @@ namespace Microsoft.NetCore.Analyzers.Runtime
                         {
                             var semanticModel = argument.SemanticModel;
 
-                            var symbol = semanticModel.GetSymbolInfo(argument.Value.Syntax, oaContext.CancellationToken).Symbol; // Does it matter here to use csaContext.CancellationToken or oaContext.CancellationToken?
+                            var symbol = semanticModel.GetSymbolInfo(argument.Value.Syntax, oaContext.CancellationToken).Symbol;
 
                             if (symbol != null &&
                                 (symbol.Equals(currentUICultureProperty) ||
