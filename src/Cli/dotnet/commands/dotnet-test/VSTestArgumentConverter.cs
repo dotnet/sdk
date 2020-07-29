3572a114569aa4b5b766057a5148b3b919228426
nohwnd <me@jakubjares.com>
2020-07-29 15:01:26 +0200

    Add --blame options to dotnet vstest and dotnet test + dll

--- src/Cli/dotnet/commands/dotnet-test/VSTestArgumentConverter.cs
+++ src/Cli/dotnet/commands/dotnet-test/VSTestArgumentConverter.cs
@@ -50,6 +50,15 @@ namespace Microsoft.DotNet.Cli
             "--interactive"
         };
 
+        private readonly string _blame = "--blame";
+        private readonly string _blameCrash = "--blame-crash";
+        private readonly string _blameCrashDumpType = "--blame-crash-dump-type";
+        private readonly string _blameCrashCollectAlways = "--blame-crash-collect-always";
+        private readonly string _blameHang = "--blame-hang";
+        private readonly string _blameHangDumpType = "--blame-hang-dump-type";
+        private readonly string _blameHangTimeout = "--blame-hang-timeout";
+
+
         /// <summary>
         /// Converts the given arguments to vstest parsable arguments
         /// </summary>
@@ -62,6 +71,17 @@ namespace Microsoft.DotNet.Cli
             ignoredArgs = new List<string>();
 
             string activeArgument = null;
+
+            bool blame = false;
+
+            bool collectCrashDump = false;
+            string collectCrashDumpType = null;
+            bool collectCrashDumpAlways = false;
+
+            bool collectHangDump = false;
+            string collectHangDumpType = null;
+            string collectHangDumpTimeout = null;
+
             foreach (var arg in args)
             {
                 if (arg == "--")
@@ -101,6 +121,53 @@ namespace Microsoft.DotNet.Cli
                             continue;
                         }
 
+                        if (Eq(argValues[0], _blame))
+                        {
+                            blame = true;
+                        }
+
+                        // Any blame-crash param implies that we collect crash dump
+                        if (Eq(argValues[0], _blameCrash))
+                        {
+                            blame = true;
+                            collectCrashDump = true;
+                        }
+
+                        if (Eq(argValues[0], _blameCrashCollectAlways))
+                        {
+                            blame = true;
+                            collectCrashDump = true;
+                            collectCrashDumpAlways = true;
+                        }
+
+                        if (Eq(argValues[0], _blameCrashDumpType))
+                        {
+                            blame = true;
+                            collectCrashDump = true;
+                            collectCrashDumpType = argValues[1];
+                        }
+
+                        // Any blame-hang param implies that we collect hang dump
+                        if (Eq(argValues[0], _blameHang))
+                        {
+                            blame = true;
+                            collectHangDump = true;
+                        }
+
+                        if (Eq(argValues[0], _blameHangDumpType))
+                        {
+                            blame = true;
+                            collectHangDump = true;
+                            collectHangDumpType = argValues[1];
+                        }
+
+                        if (Eq(argValues[0], _blameHangTimeout))
+                        {
+                            blame = true;
+                            collectHangDump = true;
+                            collectHangDumpTimeout = argValues[1];
+                        }
+
                         // Check if the argument is shortname
                         if (ArgumentMapping.TryGetValue(argValues[0].ToLower(), out var longName))
                         {
@@ -111,10 +178,33 @@ namespace Microsoft.DotNet.Cli
                     }
                     else
                     {
-                        activeArgument = arg.ToLower();
-                        if (ArgumentMapping.TryGetValue(activeArgument, out var value))
+                        if (Eq(arg, _blame))
                         {
-                            activeArgument = value;
+                            blame = true;
+                        }
+                        else if (Eq(arg, _blameCrash))
+                        {
+                            blame = true;
+                            collectCrashDump = true;
+                        }
+                        else if (Eq(arg, _blameCrashCollectAlways))
+                        {
+                            blame = true;
+                            collectCrashDump = true;
+                            collectCrashDumpAlways = true;
+                        }
+                        else if (Eq(arg, _blameHang))
+                        {
+                            blame = true;
+                            collectHangDump = true;
+                        }
+                        else
+                        {
+                            activeArgument = arg.ToLower();
+                            if (ArgumentMapping.TryGetValue(activeArgument, out var value))
+                            {
+                                activeArgument = value;
+                            }
                         }
                     }
                 }
@@ -129,6 +219,44 @@ namespace Microsoft.DotNet.Cli
                         ignoredArgs.Add(activeArgument);
                         ignoredArgs.Add(arg);
                     }
+                    else if (Eq(activeArgument, _blame))
+                    {
+                        blame = true;
+                    }
+                    else if (Eq(activeArgument, _blameCrash))
+                    {
+                        blame = true;
+                        collectCrashDump = true;
+                    }
+                    else if (Eq(activeArgument, _blameCrashCollectAlways))
+                    {
+                        blame = true;
+                        collectCrashDump = true;
+                        collectCrashDumpAlways = true;
+                    }
+                    else if (Eq(activeArgument, _blameCrashDumpType))
+                    {
+                        blame = true;
+                        collectCrashDump = true;
+                        collectCrashDumpType = arg;
+                    }
+                    else if (Eq(activeArgument, _blameHang))
+                    {
+                        blame = true;
+                        collectHangDump = true;
+                    }
+                    else if (Eq(activeArgument, _blameHangDumpType))
+                    {
+                        blame = true;
+                        collectHangDump = true;
+                        collectHangDumpType = arg;
+                    }
+                    else if (Eq(activeArgument, _blameHangTimeout))
+                    {
+                        blame = true;
+                        collectHangDump = true;
+                        collectHangDumpTimeout = arg;
+                    }
                     else
                     {
                         newArgList.Add(string.Concat(activeArgument, ":", arg));
@@ -138,7 +266,30 @@ namespace Microsoft.DotNet.Cli
                 }
                 else
                 {
-                    newArgList.Add(arg);
+                    if (Eq(arg, _blame))
+                    {
+                        blame = true;
+                    }
+                    else if (Eq(arg, _blameCrash))
+                    {
+                        blame = true;
+                        collectCrashDump = true;
+                    }
+                    else if (Eq(arg, _blameCrashCollectAlways))
+                    {
+                        blame = true;
+                        collectCrashDump = true;
+                        collectCrashDumpAlways = true;
+                    }
+                    else if (Eq(arg, _blameHang))
+                    {
+                        blame = true;
+                        collectHangDump = true;
+                    }
+                    else
+                    {
+                        newArgList.Add(arg);
+                    }
                 }
             }
 
@@ -151,6 +302,49 @@ namespace Microsoft.DotNet.Cli
                 else
                 {
                     newArgList.Add(activeArgument);
+                }
+            }
+
+            if (blame)
+            {
+                string crashDumpArgs = null;
+                string hangDumpArgs = null;
+
+                if (collectCrashDump)
+                {
+                    crashDumpArgs = "CollectDump";
+                    if (collectCrashDumpAlways)
+                    {
+                        crashDumpArgs += ";CollectAlways=true";
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(collectCrashDumpType))
+                    {
+                        crashDumpArgs += $";DumpType={collectCrashDumpType}";
+                    }
+                }
+
+                if (collectHangDump)
+                {
+                    hangDumpArgs = "CollectHangDump";
+                    if (!string.IsNullOrWhiteSpace(collectHangDumpType))
+                    {
+                        hangDumpArgs += $";DumpType={collectHangDumpType}";
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(collectHangDumpTimeout))
+                    {
+                        hangDumpArgs += $";TestTimeout={collectHangDumpTimeout}";
+                    }
+                }
+
+                if (collectCrashDump || collectHangDump)
+                {
+                    newArgList.Add($@"--blame:""{string.Join(";", crashDumpArgs, hangDumpArgs).Trim(';')}""");
+                }
+                else
+                {
+                    newArgList.Add("--blame");
                 }
             }
 
@@ -170,6 +364,11 @@ namespace Microsoft.DotNet.Cli
                 return;
             }
             newArgList.Add(verbosityString + verbosity);
+        }
+
+        private bool Eq(string left, string right)
+        {
+            return string.Equals(left, right, System.StringComparison.OrdinalIgnoreCase);
         }
     }
 }
