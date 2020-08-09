fe92bccff6c78ee4709c02effcee59714a4ef933
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-08-09 19:26:11 +0200

    Fix summary

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.CSharp.NetAnalyzers/Microsoft.NetCore.Analyzers/Performance/CSharpDoNotUseCountWhenAnyCanBeUsed.Fixer.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.CSharp.NetAnalyzers/Microsoft.NetCore.Analyzers/Performance/CSharpDoNotUseCountWhenAnyCanBeUsed.Fixer.cs
@@ -18,7 +18,7 @@ namespace Microsoft.NetCore.CSharp.Analyzers.Performance
     public sealed class CSharpDoNotUseCountWhenAnyCanBeUsedFixer : DoNotUseCountWhenAnyCanBeUsedFixer
     {
         /// <summary>
-        /// Tries the get a fixer the specified <paramref name="node" />.
+        /// Tries to get a fixer for the specified <paramref name="node" />.
         /// </summary>
         /// <param name="node">The node to get a fixer for.</param>
         /// <param name="operation">The operation to get the fixer from.</param>
