9b9a90177ab4db398e9058625ab4bebdda054da1
Joey Robichaud <jorobich@microsoft.com>
2020-08-15 18:44:29 -0700

    Fix formatting

--- src/Utilities/SourceFileMatcher.cs
+++ src/Utilities/SourceFileMatcher.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.Extensions.FileSystemGlobbing;
 
 namespace Microsoft.CodeAnalysis.Tools.Utilities
--- tests/CodeFormatterTests.cs
+++ tests/CodeFormatterTests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
