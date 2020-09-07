79578372780782dd5f7bf8aba8216d9fffe74284
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2020-09-07 17:35:55 +0200

    Added <summary> to LaunchSettingsModel

--- src/Cli/dotnet/LaunchSettings/ProjectLaunchSettingsModel.cs
+++ src/Cli/dotnet/LaunchSettings/ProjectLaunchSettingsModel.cs
@@ -8,16 +8,31 @@ namespace Microsoft.DotNet.LaunchSettings
 {
     public class ProjectLaunchSettingsModel
     {
+        /// <summary>
+        /// The arguments to pass to the target being run.
+        /// </summary>
         public string CommandLineArgs { get; set; }
 
+        /// <summary>
+        /// Whether or not to launched a browser.
+        /// </summary>
         public bool LaunchBrowser { get; set; }
 
+        /// <summary>
+        /// The relative URL to launch in the browser.
+        /// </summary>
         public string LaunchUrl { get; set; }
 
+        /// <summary>
+        /// A semi-colon delimited list of URL(s) to configure for the web server.
+        /// </summary>
         public string ApplicationUrl { get; set; }
 
         public string DotNetRunMessages { get; set; }
 
+        /// <summary>
+        /// Environment variables to set.
+        /// </summary>
         public Dictionary<string, string> EnvironmentVariables { get; } = new Dictionary<string, string>(StringComparer.Ordinal);
     }
 }
