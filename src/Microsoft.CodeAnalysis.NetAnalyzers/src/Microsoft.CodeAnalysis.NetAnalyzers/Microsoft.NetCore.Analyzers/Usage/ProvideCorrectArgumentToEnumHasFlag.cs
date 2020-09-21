5ebd0121813cd3e0af548f131a725e0b87508b6d
Amaury Levé <evangelink@gmail.com>
2020-09-21 23:00:18 +0200

    Fix broken test

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Usage/ProvideCorrectArgumentToEnumHasFlag.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Usage/ProvideCorrectArgumentToEnumHasFlag.cs
@@ -59,8 +59,9 @@ namespace Microsoft.NetCore.Analyzers.Usage
                         invocation.Instance == null ||
                         invocation.TargetMethod.Name != "HasFlag" ||
                         invocation.Arguments[0].Value is not IConversionOperation conversion ||
+                        invocation.Instance.Type == null ||
                         invocation.Instance.Type.TypeKind == TypeKind.TypeParameter ||
-                        conversion.Operand.Type.TypeKind == TypeKind.TypeParameter)
+                        conversion.Operand.Type?.TypeKind == TypeKind.TypeParameter)
                     {
                         return;
                     }
