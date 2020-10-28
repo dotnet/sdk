bae479dd4ae860363265f710cbfa95a1f06106e7
Vlada Shubina <vshubina@microsoft.com>
2020-10-28 14:53:50 +0200

    - implemented multiple shrinkable columns for HelpFormatter (dotnet/templating#2606)
    * implemented multiple shrinkable columns for HelpFormatter
    * implemented minimum width per column for shrinkable columns
    * added tests for HelpFormatter
    
    Commit migrated from https://github.com/dotnet/templating/commit/cc09b061dcee8d191b78f06d757048bd2e0a0dfd

--- test/TemplateEngine/Microsoft.TemplateEngine.Mocks/MockEngineEnvironmentSettings.cs
+++ test/TemplateEngine/Microsoft.TemplateEngine.Mocks/MockEngineEnvironmentSettings.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,10 +9,32 @@ namespace Microsoft.TemplateEngine.Mocks
     {
         public ISettingsLoader SettingsLoader { get { throw new NotImplementedException(); } }
 
-        public ITemplateEngineHost Host { get { throw new NotImplementedException(); } }
+        public ITemplateEngineHost Host { get; set; }
 
-        public IEnvironment Environment { get { throw new NotImplementedException(); } }
+        public IEnvironment Environment { get; set; }
 
         public IPathInfo Paths { get { throw new NotImplementedException(); } }
+    }
+
+    public class MockEnvironment : IEnvironment
+    {
+        public string NewLine { get; set; } = Environment.NewLine;
+
+        public int ConsoleBufferWidth { get; set; } = 160;
+
+        public string ExpandEnvironmentVariables(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEnvironmentVariable(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyDictionary<string, string> GetEnvironmentVariables()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
