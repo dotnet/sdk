daae6a62013a57fa2f5480714ca1790fef06aaad
William Li <wul@microsoft.com>
2020-11-04 21:41:09 -0800

    Pass down MicrosoftNETBuildExtensionsTargets from env

--- src/Tests/Microsoft.NET.TestFramework/ToolsetInfo.cs
+++ src/Tests/Microsoft.NET.TestFramework/ToolsetInfo.cs
@@ -266,8 +266,18 @@ namespace Microsoft.NET.TestFramework
             {
                 ret.FullFrameworkMSBuildPath = ResolveCommand("MSBuild");
             }
+<<<<<<< HEAD
        
             if (repoRoot != null && ret.ShouldUseFullFrameworkMSBuild)
+=======
+
+            var microsoftNETBuildExtensionsTargetsFromEnvironment = Environment.GetEnvironmentVariable("MicrosoftNETBuildExtensionsTargets");
+            if (!string.IsNullOrWhiteSpace(microsoftNETBuildExtensionsTargetsFromEnvironment))
+            {
+                ret.MicrosoftNETBuildExtensionsPathOverride = Path.GetDirectoryName(microsoftNETBuildExtensionsTargetsFromEnvironment);
+            }
+            else if (repoRoot != null && ret.ShouldUseFullFrameworkMSBuild)
+>>>>>>> 275f58fde... Pass down MicrosoftNETBuildExtensionsTargets from env
             {
                 //  Find path to Microsoft.NET.Build.Extensions for full framework
                 string sdksPath = Path.Combine(repoArtifactsDir, "bin", configuration, "Sdks");
