eec37b9c26113401133e90e3237ca75c7def3eca
William Li <wul@microsoft.com>
2020-11-06 14:39:22 -0800

    No longer need DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -1,5 +1,3 @@
-set TestSubjectMSBuildSDKsPath=%HELIX_CORRELATION_PAYLOAD%\s
-set DOTNET_MSBUILD_SDK_RESOLVER_SDKS_DIR=%TestSubjectMSBuildSDKsPath%
 set MicrosoftNETBuildExtensionsTargets=%HELIX_CORRELATION_PAYLOAD%\ex\msbuildExtensions\Microsoft\Microsoft.NET.Build.Extensions\Microsoft.NET.Build.Extensions.targets
 set DOTNET_ROOT=%HELIX_CORRELATION_PAYLOAD%\d
 set PATH=%DOTNET_ROOT%;%PATH%
