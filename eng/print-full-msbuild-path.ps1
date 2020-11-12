e4ec19b81590c393f4b94befd65c74b17fe17074
William Li <wul@microsoft.com>
2020-11-12 09:43:38 -0800

    install true

--- eng/print-full-msbuild-path.ps1
+++ eng/print-full-msbuild-path.ps1
@@ -1,6 +1,6 @@
 . $PSScriptRoot\common\tools.ps1
 function GetFullMsbuildPath {
-    $env:DOTNET_SDK_TEST_MSBUILD_PATH = InitializeVisualStudioMSBuild -install:$false -vsRequirements:$GlobalJson.tools.'vs-opt'
+    $env:DOTNET_SDK_TEST_MSBUILD_PATH = InitializeVisualStudioMSBuild -install:$true -vsRequirements:$GlobalJson.tools.'vs-opt'
 }
 
 GetFullMsbuildPath
