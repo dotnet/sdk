bf656708fb0d95f3177fbdbf629bb065eac75c72
Amaury Levé <amaury.leve@gmail.com>
2020-12-01 12:18:19 +0100

    Add tests for implicit record

--- src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.CodeQuality.Analyzers/Maintainability/ReviewUnusedParametersTests.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.CodeQuality.Analyzers/Maintainability/ReviewUnusedParametersTests.cs
@@ -1197,6 +1197,17 @@ public class Class1
             }.RunAsync();
         }
 
+        [Fact, WorkItem(4462, "https://github.com/dotnet/roslyn-analyzers/issues/4462")]
+        public async Task CA1801_CSharp_ImplicitRecord()
+        {
+            await new VerifyCS.Test
+            {
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                TestCode = @"public record Person(string Name, int Age = 0);",
+            }.RunAsync();
+        }
+
         #endregion
 
         #region Unit tests for analyzer diagnostic(s)
@@ -1369,6 +1380,24 @@ public class C
         }
     }
 }");
+        }
+
+        [Fact, WorkItem(4462, "https://github.com/dotnet/roslyn-analyzers/issues/4462")]
+        public async Task CA1801_CSharp_Record()
+        {
+            await new VerifyCS.Test
+            {
+                LanguageVersion = CodeAnalysis.CSharp.LanguageVersion.CSharp9,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
+                TestCode = @"
+public record OtherPerson
+{
+    public string Name { get; init; }
+
+    public OtherPerson(string name, int [|age|] = 0)
+        => Name = name;
+}",
+            }.RunAsync();
         }
 
         #endregion
