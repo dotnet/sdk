0764c3c758f7db68d132d4eb589630d888c01c46
William Li <wul@microsoft.com>
2021-01-21 16:14:24 -0800

    Update SDKCustomCreateXUnitWorkItemsWithTestExclusion.cs

--- src/Tests/HelixTasks/SDKCustomCreateXUnitWorkItemsWithTestExclusion.cs
+++ src/Tests/HelixTasks/SDKCustomCreateXUnitWorkItemsWithTestExclusion.cs
@@ -134,7 +134,7 @@ namespace Microsoft.DotNet.SdkCustomHelix.Sdk
 
             string msbuildAdditionalSdkResolverFolder = IsPosixShell ? "" : "-msbuildAdditionalSdkResolverFolder %HELIX_CORRELATION_PAYLOAD%\\r";
 
-            var scheduler = new AssemblyScheduler(methodLimit: 20);
+            var scheduler = new AssemblyScheduler(methodLimit: 10);
             var assemblyPartitionInfos = scheduler.Schedule(targetPath);
 
             var partitionedWorkItem = new List<ITaskItem>();
