63721d4be35887eb30349f94391a7bde9c734528
Safia Abdalla <safia@microsoft.com>
2021-02-01 11:38:07 -0800

    Address feedback from peer reivew

--- src/Tests/Microsoft.NET.TestFramework/AspNetSdkTest.cs
+++ src/Tests/Microsoft.NET.TestFramework/AspNetSdkTest.cs
@@ -16,32 +16,33 @@ namespace Microsoft.NET.TestFramework
 {
     public abstract class AspNetSdkTest : SdkTest
     {
-        private readonly IEnumerable<System.Reflection.AssemblyMetadataAttribute> TestAssemblyMetadata;
+        private readonly IEnumerable<System.Reflection.AssemblyMetadataAttribute> _testAssemblyMetadata;
+        private List<string> _nuGetFeedsRetained = new List<string>(){ "dotnet-public", "dotnet6", "dotnet6-transport"};
+        
         public readonly string DefaultTfm;
 
-        private List<string> NuGetFeedsRetained = new List<string>(){"dotnet-public", "dotnet6", "dotnet6-transport"};
-
-        protected AspNetSdkTest(ITestOutputHelper log) : base(log) {
+        protected AspNetSdkTest(ITestOutputHelper log) : base(log)
+        {
             var assembly = Assembly.GetCallingAssembly();
-            TestAssemblyMetadata = assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
-            DefaultTfm = TestAssemblyMetadata.SingleOrDefault(a => a.Key == "AspNetTestTfm").Value;
+            _testAssemblyMetadata = assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
+            DefaultTfm = _testAssemblyMetadata.SingleOrDefault(a => a.Key == "AspNetTestTfm").Value;
         }
 
         private void CopyAndModifyNugetConfig(string projectDirectory)
         {
             // Copy the config at the root of the repo to `projectDirectory`
-            var repoRoot = TestAssemblyMetadata.SingleOrDefault(a => a.Key == "ArtifactsTmpDir").Value;
-            var configAtRoot = Path.Combine(repoRoot, "nuget.config");
-            File.Copy(configAtRoot, Path.Combine(projectDirectory, "nuget.config"));
+            var repoRoot = _testAssemblyMetadata.SingleOrDefault(a => a.Key == "ArtifactsTmpDir").Value;
+            var configAtRoot = Path.Combine(repoRoot, "NuGet.config");
+            File.Copy(configAtRoot, Path.Combine(projectDirectory, "NuGet.config"));
 
-            // Remove sources not in `NugetFeedsRetained`
-            var nugetConfig = Settings.LoadSpecificSettings(projectDirectory, "nuget.config");
+            // Remove sources not in `_nuGetFeedsRetained`
+            var nugetConfig = Settings.LoadSpecificSettings(projectDirectory, "NuGet.config");
             var packageSourcesSection =  nugetConfig.GetSection(ConfigurationConstants.PackageSources);
             var sources = packageSourcesSection?.Items.OfType<SourceItem>();
 
             foreach (var source in sources)
             {
-                if (!NuGetFeedsRetained.Contains(source.Key))
+                if (!_nuGetFeedsRetained.Contains(source.Key))
                 {
                     nugetConfig.Remove(ConfigurationConstants.PackageSources, source);
                 }
