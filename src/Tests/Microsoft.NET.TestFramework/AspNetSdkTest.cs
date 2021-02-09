ab959cacfbae3d1ab84859a724fd58bc3deae6cd
Safia Abdalla <safia@microsoft.com>
2021-02-09 14:21:10 -0800

    Clean up project files

--- src/Tests/Microsoft.NET.TestFramework/AspNetSdkTest.cs
+++ src/Tests/Microsoft.NET.TestFramework/AspNetSdkTest.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -10,16 +9,19 @@ using System.Runtime.CompilerServices;
 using System.Xml.Linq;
 using Microsoft.NET.TestFramework;
 using Xunit.Abstractions;
-using NuGet.Configuration;
 
 namespace Microsoft.NET.TestFramework
 {
     public abstract class AspNetSdkTest : SdkTest
-    {   
-        private static readonly IEnumerable<System.Reflection.AssemblyMetadataAttribute> TestAssemblyMetadata = Assembly.GetCallingAssembly().GetCustomAttributes<AssemblyMetadataAttribute>();	        public readonly string DefaultTfm  = "net6.0";
-        public readonly string DefaultTfm = TestAssemblyMetadata.SingleOrDefault(a => a.Key == "AspNetTestTfm").Value;
+    {
+        public readonly string DefaultTfm;
 
-        protected AspNetSdkTest(ITestOutputHelper log) : base(log) { }
+        protected AspNetSdkTest(ITestOutputHelper log) : base(log)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+            var testAssemblyMetadata = assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
+            DefaultTfm = testAssemblyMetadata.SingleOrDefault(a => a.Key == "AspNetTestTfm").Value;
+        }
 
         public TestAsset CreateAspNetSdkTestAsset(
             string testAsset,
