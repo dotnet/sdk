fc075cf5f30eabad43f61aafbfb504c17c0f5ac2
Youssef Victor <youssefvictor00@gmail.com>
2021-02-09 17:00:14 +0200

    Inherit from AbstractGlobalizationDiagnosticAnalyzer

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/SpecifyCultureInfo.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/SpecifyCultureInfo.cs
@@ -7,6 +7,7 @@ using Analyzer.Utilities;
 using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.NetAnalyzers;
 using Microsoft.CodeAnalysis.Operations;
 
 namespace Microsoft.NetCore.Analyzers.Runtime
@@ -15,7 +16,7 @@ namespace Microsoft.NetCore.Analyzers.Runtime
     /// CA1304: Specify CultureInfo
     /// </summary>
     [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
-    public sealed class SpecifyCultureInfoAnalyzer : DiagnosticAnalyzer
+    public sealed class SpecifyCultureInfoAnalyzer : AbstractGlobalizationDiagnosticAnalyzer
     {
         internal const string RuleId = "CA1304";
 
@@ -34,56 +35,50 @@ namespace Microsoft.NetCore.Analyzers.Runtime
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
-        public override void Initialize(AnalysisContext context)
+        protected override void InitializeWorker(CompilationStartAnalysisContext compilationContext)
         {
-            context.EnableConcurrentExecution();
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            var obsoleteAttributeType = compilationContext.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemObsoleteAttribute);
 
-            context.RegisterCompilationStartAction(csaContext =>
+            var cultureInfoType = compilationContext.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemGlobalizationCultureInfo);
+            if (cultureInfoType != null)
             {
-                var obsoleteAttributeType = csaContext.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemObsoleteAttribute);
-
-                var cultureInfoType = csaContext.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemGlobalizationCultureInfo);
-                if (cultureInfoType != null)
+                compilationContext.RegisterOperationAction(oaContext =>
                 {
-                    csaContext.RegisterOperationAction(oaContext =>
+                    var invocationExpression = (IInvocationOperation)oaContext.Operation;
+                    var targetMethod = invocationExpression.TargetMethod;
+                    if (targetMethod.ContainingType == null || targetMethod.ContainingType.IsErrorType() || targetMethod.IsGenericMethod)
                     {
-                        var invocationExpression = (IInvocationOperation)oaContext.Operation;
-                        var targetMethod = invocationExpression.TargetMethod;
-                        if (targetMethod.ContainingType == null || targetMethod.ContainingType.IsErrorType() || targetMethod.IsGenericMethod)
-                        {
-                            return;
-                        }
+                        return;
+                    }
 
-                        if (oaContext.Options.IsConfiguredToSkipAnalysis(Rule, targetMethod, oaContext.ContainingSymbol, oaContext.Compilation, oaContext.CancellationToken))
-                        {
-                            return;
-                        }
+                    if (oaContext.Options.IsConfiguredToSkipAnalysis(Rule, targetMethod, oaContext.ContainingSymbol, oaContext.Compilation, oaContext.CancellationToken))
+                    {
+                        return;
+                    }
 
-                        IEnumerable<IMethodSymbol> methodsWithSameNameAsTargetMethod = targetMethod.ContainingType.GetMembers(targetMethod.Name).OfType<IMethodSymbol>().WhereMethodDoesNotContainAttribute(obsoleteAttributeType).ToList();
-                        if (methodsWithSameNameAsTargetMethod.HasFewerThan(2))
-                        {
-                            return;
-                        }
+                    IEnumerable<IMethodSymbol> methodsWithSameNameAsTargetMethod = targetMethod.ContainingType.GetMembers(targetMethod.Name).OfType<IMethodSymbol>().WhereMethodDoesNotContainAttribute(obsoleteAttributeType).ToList();
+                    if (methodsWithSameNameAsTargetMethod.HasFewerThan(2))
+                    {
+                        return;
+                    }
 
-                        var correctOverloads = methodsWithSameNameAsTargetMethod.GetMethodOverloadsWithDesiredParameterAtLeadingOrTrailing(targetMethod, cultureInfoType).ToList();
+                    var correctOverloads = methodsWithSameNameAsTargetMethod.GetMethodOverloadsWithDesiredParameterAtLeadingOrTrailing(targetMethod, cultureInfoType).ToList();
 
-                        // If there are two matching overloads, one with CultureInfo as the first parameter and one with CultureInfo as the last parameter,
-                        // report the diagnostic on the overload with CultureInfo as the last parameter, to match the behavior of FxCop.
-                        var correctOverload = correctOverloads.FirstOrDefault(overload => overload.Parameters.Last().Type.Equals(cultureInfoType)) ?? correctOverloads.FirstOrDefault();
+                    // If there are two matching overloads, one with CultureInfo as the first parameter and one with CultureInfo as the last parameter,
+                    // report the diagnostic on the overload with CultureInfo as the last parameter, to match the behavior of FxCop.
+                    var correctOverload = correctOverloads.FirstOrDefault(overload => overload.Parameters.Last().Type.Equals(cultureInfoType)) ?? correctOverloads.FirstOrDefault();
 
-                        if (correctOverload != null)
-                        {
-                            oaContext.ReportDiagnostic(
-                                invocationExpression.Syntax.CreateDiagnostic(
-                                    Rule,
-                                    targetMethod.ToDisplayString(SymbolDisplayFormats.ShortSymbolDisplayFormat),
-                                    oaContext.ContainingSymbol.ToDisplayString(SymbolDisplayFormats.ShortSymbolDisplayFormat),
-                                    correctOverload.ToDisplayString(SymbolDisplayFormats.ShortSymbolDisplayFormat)));
-                        }
-                    }, OperationKind.Invocation);
-                }
-            });
+                    if (correctOverload != null)
+                    {
+                        oaContext.ReportDiagnostic(
+                            invocationExpression.Syntax.CreateDiagnostic(
+                                Rule,
+                                targetMethod.ToDisplayString(SymbolDisplayFormats.ShortSymbolDisplayFormat),
+                                oaContext.ContainingSymbol.ToDisplayString(SymbolDisplayFormats.ShortSymbolDisplayFormat),
+                                correctOverload.ToDisplayString(SymbolDisplayFormats.ShortSymbolDisplayFormat)));
+                    }
+                }, OperationKind.Invocation);
+            }
         }
     }
 }
