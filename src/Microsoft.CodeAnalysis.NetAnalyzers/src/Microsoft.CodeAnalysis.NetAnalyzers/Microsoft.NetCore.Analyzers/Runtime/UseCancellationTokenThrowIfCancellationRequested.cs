c59c49a6dc372a4ac6075c7e7b2f568a152d8905
NewellClark <newellofthefuture@gmail.com>
2021-02-16 14:59:13 -0500

    Add affirmative tests

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/UseCancellationTokenThrowIfCancellationRequested.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/UseCancellationTokenThrowIfCancellationRequested.cs
@@ -3,8 +3,12 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Resx = Microsoft.NetCore.Analyzers.MicrosoftNetCoreAnalyzersResources;
@@ -34,6 +38,60 @@ namespace Microsoft.NetCore.Analyzers.Runtime
 
         public override void Initialize(AnalysisContext context)
         {
+        }
+
+        //  Use readonly struct to avoid allocations.
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+        internal readonly struct RequiredSymbols
+#pragma warning restore CA1815 // Override equals and operator equals on value types
+        {
+            public static bool TryGetSymbols(Compilation compilation, out RequiredSymbols symbols)
+            {
+                symbols = default;
+                INamedTypeSymbol boolType = compilation.GetSpecialType(SpecialType.System_Boolean);
+                if (boolType is null)
+                    return false;
+
+                if (!compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemThreadingCancellationToken, out INamedTypeSymbol? cancellationTokenType))
+                    return false;
+                IMethodSymbol? throwIfCancellationRequestedMethod = cancellationTokenType.GetMembers(nameof(CancellationToken.ThrowIfCancellationRequested))
+                    .OfType<IMethodSymbol>()
+                    .GetFirstOrDefaultMemberWithParameterInfos();
+                IPropertySymbol? isCancellationRequestedProperty = cancellationTokenType.GetMembers(nameof(CancellationToken.IsCancellationRequested))
+                    .OfType<IPropertySymbol>()
+                    .FirstOrDefault();
+                if (throwIfCancellationRequestedMethod is null || isCancellationRequestedProperty is null)
+                    return false;
+
+                if (!compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemOperationCanceledException, out INamedTypeSymbol? operationCanceledExceptionType))
+                    return false;
+                IMethodSymbol? operationCanceledExceptionDefaultCtor = operationCanceledExceptionType.InstanceConstructors
+                    .GetFirstOrDefaultMemberWithParameterInfos();
+                IMethodSymbol? operationCanceledExceptionTokenCtor = operationCanceledExceptionType.InstanceConstructors
+                    .GetFirstOrDefaultMemberWithParameterInfos(ParameterInfo.GetParameterInfo(cancellationTokenType));
+                if (operationCanceledExceptionDefaultCtor is null || operationCanceledExceptionTokenCtor is null)
+                    return false;
+
+                symbols = new RequiredSymbols
+                {
+                    BoolType = boolType,
+                    CancellationTokenType = cancellationTokenType,
+                    OperationCanceledExceptionType = operationCanceledExceptionType,
+                    ThrowIfCancellationRequestedMethod = throwIfCancellationRequestedMethod,
+                    IsCancellationRequestedProperty = isCancellationRequestedProperty,
+                    OperationCanceledExceptionDefaultCtor = operationCanceledExceptionDefaultCtor,
+                    OperationCanceledExceptionTokenCtor = operationCanceledExceptionTokenCtor
+                };
+                return true;
+            }
+
+            public INamedTypeSymbol BoolType { get; init; }
+            public INamedTypeSymbol CancellationTokenType { get; init; }
+            public INamedTypeSymbol OperationCanceledExceptionType { get; init; }
+            public IMethodSymbol ThrowIfCancellationRequestedMethod { get; init; }
+            public IPropertySymbol IsCancellationRequestedProperty { get; init; }
+            public IMethodSymbol OperationCanceledExceptionDefaultCtor { get; init; }
+            public IMethodSymbol OperationCanceledExceptionTokenCtor { get; init; }
         }
 
         private static LocalizableString CreateResource(string resourceName)
--- src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.NetCore.Analyzers/Runtime/UseCancellationTokenThrowIfCancellationRequestedTests.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.NetCore.Analyzers/Runtime/UseCancellationTokenThrowIfCancellationRequestedTests.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
 using Xunit;
 
 using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
@@ -14,5 +18,176 @@ namespace Microsoft.NetCore.Analyzers.Runtime.UnitTests
 {
     public class UseCancellationTokenThrowIfCancellationRequestedTests
     {
+        public static IEnumerable<object[]> Data_OperationCanceledExceptionCtors
+        {
+            get
+            {
+                yield return new[] { "OperationCanceledException()" };
+                yield return new[] { "OperationCanceledException(token)" };
+            }
+        }
+
+        #region Reports Diagnostics
+        [Theory]
+        [MemberData(nameof(Data_OperationCanceledExceptionCtors))]
+        public Task SimpleAffirmativeCheck_ReportedAndFixed_CS(string operationCanceledExceptionCtor)
+        {
+            string testStatements = Markup($@"
+if (token.IsCancellationRequested)
+    throw new {operationCanceledExceptionCtor};", 0);
+            string fixedStatements = @"
+token.ThrowIfCancellationRequested();";
+
+            var test = new VerifyCS.Test
+            {
+                TestCode = CS.CreateBlock(testStatements),
+                FixedCode = CS.CreateBlock(fixedStatements),
+                ExpectedDiagnostics = { CS.DiagnosticAt(0) },
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50
+            };
+            return test.RunAsync();
+        }
+
+        [Theory]
+        [MemberData(nameof(Data_OperationCanceledExceptionCtors))]
+        public Task SimpleAffirmativeCheck_ReportedAndFixed_VB(string operationCanceledExceptionCtor)
+        {
+            string testStatements = Markup($@"
+If token.IsCancellationRequested Then
+    Throw New {operationCanceledExceptionCtor}
+End If", 0);
+            string fixedStatements = @"
+token.ThrowIfCancellationRequested()";
+
+            var test = new VerifyVB.Test
+            {
+                TestCode = VB.CreateBlock(testStatements),
+                FixedCode = VB.CreateBlock(fixedStatements),
+                ExpectedDiagnostics = { VB.DiagnosticAt(0) },
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50
+            };
+            return test.RunAsync();
+        }
+
+        [Theory]
+        [MemberData(nameof(Data_OperationCanceledExceptionCtors))]
+        public Task NegatedCheckWithElse_ReportedAndFixed_CS(string operationCanceledExceptionCtor)
+        {
+            const string members = @"
+private CancellationToken token;
+private void DoSomething() { }";
+            string testStatements = Markup($@"
+if (!token.IsCancellationRequested)
+    DoSomething();
+else
+    throw new {operationCanceledExceptionCtor};", 0);
+            string fixedStatements = @"
+token.ThrowIfCancellationRequested();
+DoSomething();";
+
+            var test = new VerifyCS.Test
+            {
+                TestCode = CS.CreateBlock(testStatements, members),
+                FixedCode = CS.CreateBlock(fixedStatements, members),
+                ExpectedDiagnostics = { CS.DiagnosticAt(0) },
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50
+            };
+            return test.RunAsync();
+        }
+
+        [Theory]
+        [MemberData(nameof(Data_OperationCanceledExceptionCtors))]
+        public Task NegatedCheckWithElse_ReportedAndFixed_VB(string operationCanceledExceptionCtor)
+        {
+            const string members = @"
+Private token As CancellationToken
+Private Sub DoSomething()
+End Sub";
+            string testStatements = Markup($@"
+If Not token.IsCancellationRequested Then
+    DoSomething()
+Else
+    Throw New {operationCanceledExceptionCtor}
+End If", 0);
+            string fixedStatements = @"
+token.ThrowIfCancellationRequested()
+DoSomething()";
+
+            var test = new VerifyVB.Test
+            {
+                TestCode = VB.CreateBlock(testStatements, members),
+                FixedCode = VB.CreateBlock(fixedStatements, members),
+                ExpectedDiagnostics = { VB.DiagnosticAt(0) },
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50
+            };
+            return test.RunAsync();
+        }
+        #endregion
+
+        #region Helpers
+        private static class CS
+        {
+            public const string Usings = @"
+using System;
+using System.Threading;";
+
+            public static string CreateBlock(string statements, string members)
+            {
+                return Usings + @"
+public partial class Body
+{
+" + IndentLines(members, "    ") + @"
+    public void Run()
+    {
+" + IndentLines(statements, "        ") + @"
+    }
+}";
+            }
+
+            /// <summary>
+            /// Creates a test class with a single private CancellationToken member called 'token'.
+            /// </summary>
+            public static string CreateBlock(string statements) => CreateBlock(statements, @"private CancellationToken token;");
+
+            public static DiagnosticResult DiagnosticAt(int markupKey) => VerifyCS.Diagnostic(Rule).WithLocation(markupKey);
+        }
+
+        private static class VB
+        {
+            public const string Usings = @"
+Imports System
+Imports System.Threading";
+
+            public static string CreateBlock(string statements, string members)
+            {
+                return Usings + @"
+Partial Public Class Body
+
+" + IndentLines(members, "    ") + @"
+    Public Sub Run()
+
+" + IndentLines(statements, "        ") + @"
+    End Sub
+End Class";
+            }
+
+            public static string CreateBlock(string statements) => CreateBlock(statements, @"Private token As CancellationToken");
+
+            public static DiagnosticResult DiagnosticAt(int markupKey) => VerifyVB.Diagnostic(Rule).WithLocation(markupKey);
+        }
+
+        private static string IndentLines(string lines, string indent)
+        {
+            return indent + lines.TrimStart().Replace(Environment.NewLine, indent + Environment.NewLine, StringComparison.Ordinal);
+        }
+
+        private static string Markup(string text, int markupKey, bool removeLeadingWhitespace = true)
+        {
+            text = removeLeadingWhitespace ? text.TrimStart() : text;
+            return $"{{|#{markupKey}:{text}|}}";
+        }
+
+        private static DiagnosticDescriptor Rule => UseCancellationTokenThrowIfCancellationRequested.Rule;
+        #endregion
     }
 }
