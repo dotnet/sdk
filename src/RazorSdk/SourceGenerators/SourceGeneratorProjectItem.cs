062348b611b1df8d37bf160c9239e770ea2f6f2b
Safia Abdalla <safia@microsoft.com>
2021-03-20 21:46:16 -0700

    Cache SourceText computed from AdditionalFile

--- src/RazorSdk/SourceGenerators/SourceGeneratorProjectItem.cs
+++ src/RazorSdk/SourceGenerators/SourceGeneratorProjectItem.cs
@@ -2,15 +2,19 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Text;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Text;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.NET.Sdk.Razor.SourceGenerators
 {
     internal class SourceGeneratorProjectItem : RazorProjectItem
     {
+        private static readonly ConcurrentDictionary<string, SourceText> _sourceTextCache = new();
+
         private readonly string _fileKind;
 
         private readonly GeneratorExecutionContext _context;
@@ -24,15 +28,12 @@ namespace Microsoft.NET.Sdk.Razor.SourceGenerators
             AdditionalText = additionalText;
             CssScope = cssScope;
             _context = context;
-            var text = AdditionalText.GetText();
-            if (text is null)
-            {
-                _context.ReportDiagnostic(Diagnostic.Create(RazorDiagnostics.SourceTextNotFoundDescriptor, Location.None, filePath));
-            }
-            else 
+            var text = GetSourceTextFromAdditionalFile(additionalText, context, relativePhysicalPath, filePath);
+            if (text is not null)
             {
                 RazorSourceDocument = new SourceTextRazorSourceDocument(AdditionalText.Path, relativePhysicalPath, text);
             }
+            
         }
 
         public AdditionalText AdditionalText { get; }
@@ -54,5 +55,26 @@ namespace Microsoft.NET.Sdk.Razor.SourceGenerators
         public override Stream Read() 
             => throw new NotSupportedException("This API should not be invoked. We should instead be relying on " +
                 "the RazorSourceDocument associated with this item instead.");
+
+        private static SourceText? GetSourceTextFromAdditionalFile(AdditionalText additionalText, GeneratorExecutionContext context, string relativePhysicalPath, string filePath)
+        {
+            SourceText? text = null;
+            if (!_sourceTextCache.TryGetValue(additionalText.Path, out text))
+            {
+                text = additionalText.GetText();
+                if (text is not null)
+                {
+                    _sourceTextCache[additionalText.Path] = text;
+                }
+                
+            }
+
+            if (text is null)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(RazorDiagnostics.SourceTextNotFoundDescriptor, Location.None, filePath));
+            }
+            
+            return text;
+        }
     }
 }
