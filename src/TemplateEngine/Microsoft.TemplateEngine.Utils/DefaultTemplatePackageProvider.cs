2c639dd0144f74c825c0f5274bf66f95b82ac998
David Karlaš <david.karlas@microsoft.com>
2021-03-24 14:14:39 +0100

    Added DefaultTemplatePackageProvider
    Commit migrated from https://github.com/dotnet/templating/commit/544c290d587226b658f6f4e31285b3e874a74690

---
+++ src/TemplateEngine/Microsoft.TemplateEngine.Utils/DefaultTemplatePackageProvider.cs
@@ -0,0 +1,70 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Abstractions.PhysicalFileSystem;
+using Microsoft.TemplateEngine.Abstractions.TemplatePackages;
+
+#nullable enable
+
+namespace Microsoft.TemplateEngine.Utils
+{
+    /// <summary>
+    /// This is generic provider that can be used by different factories that have
+    /// fixed list of ".nupkgs" or "folders". And don't want to re-implement this interface.
+    /// </summary>
+    public class DefaultTemplatePackageProvider : ITemplatePackagesProvider
+    {
+        private readonly IEngineEnvironmentSettings _environmentSettings;
+        private readonly IEnumerable<string> _nupkgs;
+        private readonly IEnumerable<string> _folders;
+
+        public ITemplatePackagesProviderFactory Factory { get; }
+
+        public DefaultTemplatePackageProvider(ITemplatePackagesProviderFactory factory, IEngineEnvironmentSettings environmentSettings, IEnumerable<string>? nupkgs = null, IEnumerable<string>? folders = null)
+        {
+            Factory = factory;
+            _environmentSettings = environmentSettings;
+            _nupkgs = nupkgs ?? Array.Empty<string>();
+            _folders = folders ?? Array.Empty<string>();
+        }
+
+        public event Action? SourcesChanged;
+
+        public void TriggerSourcesChangedEvent()
+        {
+            SourcesChanged?.Invoke();
+        }
+
+        public Task<IReadOnlyList<ITemplatePackage>> GetAllSourcesAsync(CancellationToken cancellationToken)
+        {
+            var expandedNupkgs = _nupkgs.SelectMany(p => InstallRequestPathResolution.Expand(p, _environmentSettings));
+            var expandedFolders = _folders.SelectMany(p => InstallRequestPathResolution.Expand(p, _environmentSettings));
+
+            var list = new List<ITemplatePackage>();
+            foreach (var nupkg in expandedNupkgs)
+            {
+                list.Add(new TemplatePackage(this, nupkg, GetLastWriteTimeUtc(nupkg)));
+            }
+            foreach (var folder in expandedFolders)
+            {
+                list.Add(new TemplatePackage(this, folder, GetLastWriteTimeUtc(folder)));
+            }
+            return Task.FromResult<IReadOnlyList<ITemplatePackage>>(list);
+        }
+
+        private DateTime GetLastWriteTimeUtc(string path)
+        {
+            if (_environmentSettings.Host.FileSystem is IFileLastWriteTimeSource fileSystem)
+                return fileSystem.GetLastWriteTimeUtc(path);
+            return File.GetLastWriteTimeUtc(path);
+        }
+    }
+}
