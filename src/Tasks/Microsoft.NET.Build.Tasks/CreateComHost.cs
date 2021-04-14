f9995e27020aed28912c7c941bb457a4a1bb5413
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-04-14 15:19:04 -0700

    Implement sdk support for embedding tlbs into the comhost.

--- src/Tasks/Microsoft.NET.Build.Tasks/CreateComHost.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/CreateComHost.cs
@@ -20,19 +20,85 @@ namespace Microsoft.NET.Build.Tasks
         [Required]
         public string ClsidMapPath { get; set; }
 
+        public ITaskItem[] TypeLibraries { get; set; }
+
         protected override void ExecuteCore()
         {
             try
             {
+                if (!TryCreateTypeLibraryIdDictionary(TypeLibraries, out var typeLibIdMap))
+                {
+                    return;
+                }
                 ComHost.Create(
                     ComHostSourcePath,
                     ComHostDestinationPath,
-                    ClsidMapPath);
+                    ClsidMapPath,
+                    typeLibIdMap);
             }
             catch (ComHostCustomizationUnsupportedOSException)
             {
                 Log.LogError(Strings.CannotEmbedClsidMapIntoComhost);
             }
+            catch (TypeLibraryDoesNotExistException ex)
+            {
+                Log.LogError(Strings.TypeLibraryDoesNotExist, ex.Path);
+            }
+            catch (InvalidTypeLibraryIdException ex)
+            {
+                Log.LogError(Strings.InvalidTypeLibraryId, ex.Id.ToString(), ex.Path);
+            }
+        }
+
+        private bool TryCreateTypeLibraryIdDictionary(ITaskItem[] typeLibraries, out Dictionary<int, string> typeLibraryIdMap)
+        {
+            typeLibraryIdMap = null;
+            if (typeLibraries.Length == 0)
+            {
+                return true;
+            }
+            else if (typeLibraries.Length == 1)
+            {
+                int id = 1;
+                string idMetadata = typeLibraries[0].GetMetadata("Id");
+                if (!string.IsNullOrEmpty(idMetadata))
+                {
+                    if (!int.TryParse(idMetadata, out id) || id == 0)
+                    {
+                        Log.LogError(Strings.InvalidTypeLibraryId, idMetadata, typeLibraries[0].ItemSpec);
+                        return false;
+                    }
+                }
+                typeLibraryIdMap = new Dictionary<int, string> { { id, typeLibraries[0].ItemSpec } };
+                return true;
+            }
+            typeLibraryIdMap = new Dictionary<int, string>();
+            foreach (ITaskItem typeLibrary in typeLibraries)
+            {
+                string idMetadata = typeLibrary.GetMetadata("Id");
+                if (string.IsNullOrEmpty(idMetadata))
+                {
+                    Log.LogError(Strings.MissingTypeLibraryId, typeLibrary.ItemSpec);
+                    return false;
+                }
+
+                if (!int.TryParse(idMetadata, out int id) || id == 0)
+                {
+                    Log.LogError(Strings.InvalidTypeLibraryId, idMetadata, typeLibrary.ItemSpec);
+                    return false;
+                }
+
+                if (typeLibraryIdMap.ContainsKey(id))
+                {
+                    Log.LogError(Strings.DuplicateTypeLibraryIds, idMetadata, typeLibraryIdMap[id], typeLibrary.ItemSpec);
+                    return false;
+                }
+                else
+                {
+                    typeLibraryIdMap[id] = typeLibrary.ItemSpec;
+                }
+            }
+            return true;
         }
     }
 }
