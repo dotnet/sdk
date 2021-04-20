8cdea620381002e039175a66fe30a6c7a89ef24e
Bekir Ozturk <bozturk@microsoft.com>
2021-04-20 23:38:02 +0200

    Fix SA1001 - Commas must be spaced correctly
    Commit migrated from https://github.com/dotnet/templating/commit/537ca2258f05612fb8322ef7eaf97728a0518ea9

--- test/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/MacroTests/JoinMacroTest.cs
+++ test/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/MacroTests/JoinMacroTest.cs
@@ -30,8 +30,8 @@ namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests.Macro
 
             List<KeyValuePair<string, string>> definitions = new List<KeyValuePair<string, string>>
             {
-                new KeyValuePair<string,string>("const",constantValue),
-                new KeyValuePair<string,string>("ref",referenceSymbolName)
+                new KeyValuePair<string, string>("const", constantValue),
+                new KeyValuePair<string, string>("ref", referenceSymbolName)
             };
 
             JoinMacroConfig macroConfig = new JoinMacroConfig(variableName, null, definitions, separator);
