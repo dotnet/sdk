0f8c55f9ef3c71e26e34782051fd549060e71aba
Jacques Eloff <joeloff@users.noreply.github.com>
2021-05-03 11:59:38 -0700

    PR feedback

--- src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/GivenAResolveRuntimePackAssetsTask.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/GivenAResolveRuntimePackAssetsTask.cs
@@ -6,28 +6,34 @@ using System.IO;
 using System.Linq;
 using FluentAssertions;
 using Microsoft.Build.Utilities;
+using Microsoft.NET.TestFramework;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Microsoft.NET.Build.Tasks.UnitTests
 {
-    public class GivenAResolveRuntimePackAssetsTask
+    public class GivenAResolveRuntimePackAssetsTask : SdkTest
     {
+        public GivenAResolveRuntimePackAssetsTask(ITestOutputHelper log) : base(log)
+        {
+        }
+
         [Fact]
         public void ItFiltersSatelliteResources()
         {
-            string projectDirectory = Path.Combine(Path.GetTempPath(), "runtimePackSatelliteAssets");
+            var testDirectory = _testAssetsManager.CreateTestDirectory().Path;
 
             var task = new ResolveRuntimePackAssets()
             {
-                BuildEngine = new MockBuildEngine(), 
+                BuildEngine = new MockBuildEngine(),
                 FrameworkReferences = new TaskItem[] { new TaskItem("TestFramework") },
                 ResolvedRuntimePacks = new TaskItem[]
                 {
-                    new TaskItem("TestRuntimePack", 
+                    new TaskItem("TestRuntimePack",
                     new Dictionary<string, string> {
                         { "FrameworkName", "TestFramework" },
                         { "RuntimeIdentifier", "test-rid" },
-                        { "PackageDirectory", projectDirectory },
+                        { "PackageDirectory", testDirectory },
                         { "PackageVersion", "0.1.0" },
                         { "IsTrimmable", "false" }
                     })
@@ -35,10 +41,10 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
                 SatelliteResourceLanguages = new TaskItem[] { new TaskItem("de") }
             };
 
-            Directory.CreateDirectory(Path.Combine(projectDirectory, "data"));
+            Directory.CreateDirectory(Path.Combine(testDirectory, "data"));
 
             File.WriteAllText(
-                Path.Combine(projectDirectory, "data", "RuntimeList.xml"),
+                Path.Combine(testDirectory, "data", "RuntimeList.xml"),
 @"<FileList Name="".NET Core 3.1"" TargetFrameworkIdentifier="".NETCoreApp"" TargetFrameworkVersion=""3.1"" FrameworkName=""Microsoft.NETCore.App"">
   <File Type=""Resources"" Path=""runtimes/de/a.resources.dll"" Culture=""de"" FileVersion=""0.0.0.0"" />
   <File Type=""Resources"" Path=""runtimes/cs/a.resources.dll"" Culture=""cs"" FileVersion=""0.0.0.0"" />
@@ -47,11 +53,6 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
             task.Execute();
             task.RuntimePackAssets.Should().HaveCount(1);
             task.RuntimePackAssets.FirstOrDefault().ItemSpec.Should().Contain(@"runtimes\de\a.resources.dll");
-
-            if (Directory.Exists(projectDirectory))
-            {
-                Directory.Delete(projectDirectory, recursive: true);
-            }
         }
     }
 }
