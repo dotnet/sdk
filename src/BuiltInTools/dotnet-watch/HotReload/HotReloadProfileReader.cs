d2d9fd17529f14a1ce5b1f985841a04191181a44
Pranav K <prkrishn@hotmail.com>
2021-05-03 11:44:43 -0700

    Use ProjectCapability to determine hot reloadibility
    dotnet-watch currently relies on the presence of a value in the launchSettings.json to
    determine if a project supports hot reload. This is problematic for workloads other than
    ASP.NET Core that do not contain a launchSettings.
    
    This change introduces a ProjectCapability in the SDK that is configured for projects targeting 6.0 or newer
    and updates dotnet-watch to start using this capability. In addition, for the "esoteric" cases of BlazorWasm
    and BlazorWasm-hosted, we'll rely on the presence of additional capabilities rather than the profile.

---
+++ src/BuiltInTools/dotnet-watch/HotReload/HotReloadProfileReader.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using Microsoft.Build.Graph;
+using Microsoft.Extensions.Tools.Internal;
+
+namespace Microsoft.DotNet.Watcher.Tools
+{
+    public static class HotReloadProfileReader
+    {
+        public static HotReloadProfile InferHotReloadProfile(ProjectGraph projectGraph, IReporter reporter)
+        {
+            var queue = new Queue<ProjectGraphNode>(projectGraph.EntryPointNodes);
+
+            var seenAspNetCoreProject = false;
+
+            while (queue.Count > 0)
+            {
+                var next = queue.Dequeue();
+                var projectCapability = next.ProjectInstance.GetItems("ProjectCapability");
+
+                foreach (var item in projectCapability)
+                {
+                    if (item.EvaluatedInclude == "AspNetCore")
+                    {
+                        seenAspNetCoreProject = true;
+                    }
+                    else if (item.EvaluatedInclude == "WebAssembly")
+                    {
+                        if (seenAspNetCoreProject)
+                        {
+                            reporter.Verbose("HotReloadProfile: BlazorHosted.");
+                            return HotReloadProfile.BlazorHosted;
+                        }
+
+                        reporter.Verbose("HotReloadProfile: BlazorWebAssembly.");
+                        return HotReloadProfile.BlazorWebAssembly;
+                    }
+                }
+
+                foreach (var project in next.ProjectReferences)
+                {
+                    queue.Enqueue(project);
+                }
+            }
+
+            reporter.Verbose("HotReloadProfile: Default.");
+            return HotReloadProfile.Default;
+        }
+    }
+}
