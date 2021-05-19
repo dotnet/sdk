7e986529be50b2717f0e08f6d72275df68b76512
Vlada Shubina <vshubina@microsoft.com>
2021-05-19 20:00:34 +0300

    nullable enable in public APIs of Orchestrator.RunnableProjects
    Commit migrated from https://github.com/dotnet/templating/commit/41d0f421a36aa9f18369593ac383cd072d6c77ae

--- src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/Abstractions/IOperationConfig.cs
+++ src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/Abstractions/IOperationConfig.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System.Collections.Generic;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Abstractions.Mount;
