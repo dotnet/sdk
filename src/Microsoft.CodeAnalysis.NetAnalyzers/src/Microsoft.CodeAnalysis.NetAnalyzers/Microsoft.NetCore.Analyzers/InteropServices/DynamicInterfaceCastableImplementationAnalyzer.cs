6463815536f63974a0880d9ad6b67e1b13bd0ec0
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-05-21 09:27:04 -0700

    Sketch out the IDIC analyzer and "implement interfaces" code fixer.

---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/InteropServices/DynamicInterfaceCastableImplementationAnalyzer.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.NetCore.Analyzers.InteropServices
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    internal class DynamicInterfaceCastableImplementationAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string InterfaceMethodsMissingImplementationRuleId = "CA2250";
+
+        private static readonly DiagnosticDescriptor InterfaceMethodsMissingImplementation =
+            DiagnosticDescriptorHelper.Create(
+                InterfaceMethodsMissingImplementationRuleId,
+                "",
+                "",
+                DiagnosticCategory.Usage,
+                RuleLevel.BuildWarning,
+                "",
+                isPortedFxCopRule: false,
+                isDataflowRule: false);
+
+        internal const string MethodsDeclaredOnImplementationTypeMustBeVirtualRuleId = "CA2251";
+
+        private static readonly DiagnosticDescriptor MethodsDeclaredOnImplementationTypeMustBeVirtual =
+            DiagnosticDescriptorHelper.Create(
+                MethodsDeclaredOnImplementationTypeMustBeVirtualRuleId,
+                "",
+                "",
+                DiagnosticCategory.Usage,
+                RuleLevel.BuildWarning,
+                "",
+                isPortedFxCopRule: false,
+                isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.RegisterSymbolAction(context => AnalyzeType(context), SymbolKind.NamedType);
+        }
+
+        private const string DynamicCastableImplementationAttributeTypeName = "System.Runtime.InteropServices.DynamicInterfaceCastableImplementationAttribute";
+
+        private static void AnalyzeType(SymbolAnalysisContext context)
+        {
+            INamedTypeSymbol targetType = (INamedTypeSymbol)context.Symbol;
+
+            if (targetType.TypeKind != TypeKind.Interface)
+            {
+                return;
+            }
+
+            bool isDynamicInterfaceImplementation = false;
+            foreach (var attribute in targetType.GetAttributes())
+            {
+                if (attribute.AttributeClass.ToDisplayString(SymbolDisplayFormats.QualifiedTypeAndNamespaceSymbolDisplayFormat) == DynamicCastableImplementationAttributeTypeName)
+                {
+                    isDynamicInterfaceImplementation = true;
+                    break;
+                }
+            }
+
+            if (!isDynamicInterfaceImplementation)
+            {
+                return;
+            }
+
+            bool missingMethodImplementations = false;
+            foreach (var iface in targetType.AllInterfaces)
+            {
+                foreach (var member in iface.GetMembers())
+                {
+                    if (!member.IsStatic && targetType.FindImplementationForInterfaceMember(member) is null)
+                    {
+                        missingMethodImplementations = true;
+                        break;
+                    }
+                }
+            }
+
+            if (missingMethodImplementations)
+            {
+                context.ReportDiagnostic(targetType.CreateDiagnostic(InterfaceMethodsMissingImplementation));
+            }
+
+            foreach (var member in targetType.GetMembers())
+            {
+                if (member.IsVirtual)
+                {
+                    // Emit diagnostic for non-concrete method on implementation interface
+                    context.ReportDiagnostic(member.CreateDiagnostic(MethodsDeclaredOnImplementationTypeMustBeVirtual, member.ToDisplayString()));
+                }
+            }
+        }
+    }
+}
---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/InteropServices/ImplementInterfacesOnDynamicCastableImplementationFixer.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Analyzer.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace Microsoft.NetCore.Analyzers.InteropServices
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp), Shared]
+    public class ImplementInterfacesOnDynamicCastableImplementationFixer : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(DynamicInterfaceCastableImplementationAnalyzer.InterfaceMethodsMissingImplementationRuleId);
+
+        public override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            SyntaxGenerator generator = SyntaxGenerator.GetGenerator(context.Document);
+            SyntaxNode root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            SyntaxNode enclosingNode = root.FindNode(context.Span);
+            SyntaxNode declaration = generator.GetDeclaration(enclosingNode);
+            if (declaration == null)
+            {
+                return;
+            }
+            Diagnostic diagnostic = context.Diagnostics.First();
+            context.RegisterCodeFix(
+                new MyCodeAction(
+                    MicrosoftNetCoreAnalyzersResources.ImplementInterfacesOnDynamicCastableImplementation,
+                    async ct => await ImplementInterfacesOnDynamicCastableImplementation(declaration, context.Document, context.CancellationToken).ConfigureAwait(false),
+                    equivalenceKey: nameof(MicrosoftNetCoreAnalyzersResources.ImplementInterfacesOnDynamicCastableImplementation)),
+                diagnostic);
+        }
+
+        private static async Task<Document> ImplementInterfacesOnDynamicCastableImplementation(
+            SyntaxNode declaration,
+            Document document,
+            CancellationToken ct)
+        {
+            var editor = await DocumentEditor.CreateAsync(document, ct).ConfigureAwait(false);
+
+            INamedTypeSymbol type = (INamedTypeSymbol)editor.SemanticModel.GetDeclaredSymbol(declaration, ct);
+            var generator = editor.Generator;
+
+            var defaultMethodBodyStatements = generator.DefaultMethodBody(editor.SemanticModel.Compilation);
+            List<SyntaxNode> generatedMembers = new List<SyntaxNode>();
+            foreach (var iface in type.AllInterfaces)
+            {
+                foreach (var member in iface.GetMembers())
+                {
+                    if (!member.IsStatic && type.FindImplementationForInterfaceMember(member) is null)
+                    {
+                        generatedMembers.Add(generator.AsPrivateInterfaceImplementation(
+                            member.Kind switch
+                            {
+                                SymbolKind.Method => GenerateMethodImplementation((IMethodSymbol)member),
+                                SymbolKind.Property => GeneratePropertyImplementation((IPropertySymbol)member),
+                                SymbolKind.Event => GenerateEventImplementation((IEventSymbol)member),
+                                _ => throw new InvalidOperationException()
+                            },
+                            generator.NameExpression(member.ContainingType)));
+                    }
+                }
+            }
+
+            editor.ReplaceNode(declaration, generator.AddMembers(declaration, generatedMembers));
+
+            return editor.GetChangedDocument();
+
+            SyntaxNode GenerateMethodImplementation(IMethodSymbol method)
+            {
+                return generator.MethodDeclaration(method, defaultMethodBodyStatements);
+            }
+
+            SyntaxNode GeneratePropertyImplementation(IPropertySymbol property)
+            {
+                return generator.PropertyDeclaration(
+                    property,
+                    getAccessorStatements: defaultMethodBodyStatements,
+                    setAccessorStatements: defaultMethodBodyStatements);
+            }
+
+            SyntaxNode GenerateEventImplementation(IEventSymbol evt)
+            {
+                return generator.CustomEventDeclaration(
+                    evt,
+                    addAccessorStatements: defaultMethodBodyStatements,
+                    removeAccessorStatements: defaultMethodBodyStatements);
+            }
+        }
+
+        private class MyCodeAction : DocumentChangeAction
+        {
+            public MyCodeAction(string title, Func<CancellationToken, Task<Document>> createChangedDocument, string equivalenceKey)
+                : base(title, createChangedDocument, equivalenceKey)
+            {
+            }
+        }
+    }
+}
