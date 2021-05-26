32c0b4cc2c340d49d298023cf12356a3e2240716
Bekir Ozturk <bozturk@microsoft.com>
2021-05-26 19:24:08 +0200

    Integration tests for template localizer export errors
    Commit migrated from https://github.com/dotnet/templating/commit/49ce0716eb2b38c001d404c21483dd0ea8ae6b7a

---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Cli.TestHelper/BasicCommand.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.NET.TestFramework.Commands;
+using Xunit.Abstractions;
+
+namespace Microsoft.TemplateEngine.TestHelper
+{
+    public class BasicCommand : TestCommand
+    {
+        private readonly string _processName;
+
+        public BasicCommand(ITestOutputHelper log, string processName, params string[] args) : base(log)
+        {
+            _processName = processName;
+            Arguments.AddRange(args);
+        }
+
+        protected override SdkCommandSpec CreateCommand(IEnumerable<string> args)
+        {
+            var sdkCommandSpec = new SdkCommandSpec()
+            {
+                FileName = _processName,
+                Arguments = args.ToList(),
+                WorkingDirectory = WorkingDirectory
+            };
+            return sdkCommandSpec;
+        }
+    }
+}
---
+++ test/TemplateEngine/Microsoft.TemplateEngine.TemplateLocalizer.IntegrationTests/ExportCommandFailureTests.cs
@@ -0,0 +1,157 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.TemplateEngine.TestHelper;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.TemplateEngine.TemplateLocalizer.IntegrationTests
+{
+    public class ExportCommandFailureTests : IDisposable
+    {
+        private string _workingDirectory;
+
+        private readonly ITestOutputHelper _log;
+
+        public ExportCommandFailureTests(ITestOutputHelper log)
+        {
+            _log = log;
+
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            _workingDirectory = Path.Combine(Path.GetTempPath(), "Microsoft.TemplateEngine.TemplateLocalizer.IntegrationTests", Path.GetRandomFileName());
+            Directory.CreateDirectory(_workingDirectory);
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(_workingDirectory, true);
+        }
+
+        [Fact]
+        public async Task PostActionsShouldHaveIds()
+        {
+            string json = @"{
+    ""postActions"": [
+        {
+        },
+    ]
+}";
+            (await CreateTemplateAndExport(json))
+                .Execute()
+                .Should()
+                .ExitWith(1)
+                .And.HaveStdOutContaining("Generating localization files for a template.json has failed.")
+                .And.HaveStdOutContaining("Json element \"postActions/0\" must have a member \"id\".");
+        }
+
+        [Fact]
+        public async Task PostActionIdsAreUnique()
+        {
+            string json = @"{
+    ""postActions"": [
+        {
+            ""id"": ""postAction1""
+        },
+        {
+            ""id"": ""postAction1""
+        }
+    ]
+}";
+            (await CreateTemplateAndExport(json))
+                .Execute()
+                .Should()
+                .ExitWith(1)
+                .And.HaveStdOutContaining("Generating localization files for a template.json has failed.")
+                .And.HaveStdOutContaining(@"Each child of ""//postActions"" should have a unique id. Currently, the id ""postActions/postAction1"" is shared by multiple children.");
+        }
+
+        [Fact]
+        public async Task SingleManualInstructionDoesntNeedId()
+        {
+            string json = @"{
+    ""postActions"": [
+        {
+            ""id"": ""postActionId"",
+            ""manualInstructions"": [
+                {
+                    ""text"": ""some text""
+                }
+            ]
+        }
+    ]
+}";
+            (await CreateTemplateAndExport(json))
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And.HaveStdOutContaining("Localization files were successfully generated");
+        }
+
+        [Fact]
+        public async Task MultipleManualInstructionShouldHaveIds()
+        {
+            string json = @"{
+    ""postActions"": [
+        {
+            ""id"": ""postActionId"",
+            ""manualInstructions"": [
+                {
+                    ""text"": ""some text""
+                },
+                {
+                    ""text"": ""some other text""
+                }
+            ]
+        }
+    ]
+}";
+            (await CreateTemplateAndExport(json))
+                .Execute()
+                .Should()
+                .ExitWith(1)
+                .And.HaveStdOutContaining("Generating localization files for a template.json has failed.")
+                .And.HaveStdOutContaining(@"Json element ""manualInstructions/0"" must have a member ""id"".");
+        }
+
+        [Fact]
+        public async Task ManualInstructionIdsAreUnique()
+        {
+            string json = @"{
+    ""postActions"": [
+        {
+            ""id"": ""postActionId"",
+            ""manualInstructions"": [
+                {
+                    ""id"": ""mi""
+                },
+                {
+                    ""id"": ""mi""
+                },
+            ]
+        }
+    ]
+}";
+            (await CreateTemplateAndExport(json))
+                .Execute()
+                .Should()
+                .ExitWith(1)
+                .And.HaveStdOutContaining("Generating localization files for a template.json has failed.")
+                .And.HaveStdOutContaining("Each child of \"//postActions/0/manualInstructions\" should have a unique id. Currently, the id \"manualInstructions/mi\" is shared by multiple children.");
+        }
+
+        private async Task<BasicCommand> CreateTemplateAndExport(string templateJsonContent)
+        {
+            string filePath = Path.Combine(_workingDirectory, Path.GetRandomFileName() + ".json");
+            await File.WriteAllTextAsync(filePath, templateJsonContent);
+            return new BasicCommand(_log, "Microsoft.TemplateEngine.TemplateLocalizer.exe", "export", filePath);
+        }
+    }
+}
