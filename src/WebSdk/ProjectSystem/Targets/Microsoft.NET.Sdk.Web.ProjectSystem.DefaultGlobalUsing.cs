61a436d0aa4bd8a57e7ce0b3111f1959fe95ee44
John Luo <johluo@microsoft.com>
2021-06-23 15:01:37 -0700

    Update

--- src/WebSdk/ProjectSystem/Targets/Microsoft.NET.Sdk.Web.ProjectSystem.DefaultGlobalUsing.cs
+++ src/WebSdk/ProjectSystem/Targets/Microsoft.NET.Sdk.Web.ProjectSystem.DefaultGlobalUsing.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 global using global::System;
 global using global::System.Collections.Generic;
 global using global::System.Linq;
