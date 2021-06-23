647a8fb8fcdc3c45d79fddbb7d396f3d62bcecc6
John Luo <johluo@microsoft.com>
2021-06-22 20:01:04 -0700

    Add global usings for WebSdk projects

---
+++ src/WebSdk/ProjectSystem/content/Microsoft.NET.Sdk.Web.ProjectSystem.DefaultGlobalUsing.cs
@@ -0,0 +1,15 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Net.Http.Json;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
