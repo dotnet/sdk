493f3aa07e46fbcb3db5009e18bda4fd179bb105
Youssef1313 <youssefvictor00@gmail.com>
2021-06-28 14:19:39 +0200

    Fix issue resulted from fixall

--- src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.NetCore.Analyzers/Publish/AvoidAssemblyLocationInSingleFileTests.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.NetCore.Analyzers/Publish/AvoidAssemblyLocationInSingleFileTests.cs
@@ -8,6 +8,7 @@ using VerifyCS = Test.Utilities.CSharpCodeFixVerifier<
     Microsoft.NetCore.Analyzers.Publish.AvoidAssemblyLocationInSingleFile,
     Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
 using static Analyzer.Utilities.MSBuildPropertyOptionNames;
+using static Microsoft.NetCore.Analyzers.Publish.AvoidAssemblyLocationInSingleFile;
 
 namespace Microsoft.NetCore.Analyzers.Publish.UnitTests
 {
