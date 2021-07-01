1d9e0f20a73f58d6f7b4e84a53f18ae3eca8422e
Vlada Shubina <vshubina@microsoft.com>
2021-07-01 15:22:04 +0300

    added test for reading host data file
    Commit migrated from https://github.com/dotnet/templating/commit/da5429ec220e66f7a738b540b602d4e3c3b2b92b

--- test/TemplateEngine/Microsoft.TemplateSearch.TemplateDiscovery.IntegrationTests/TemplateDiscoveryTests.cs
+++ test/TemplateEngine/Microsoft.TemplateSearch.TemplateDiscovery.IntegrationTests/TemplateDiscoveryTests.cs
@@ -62,5 +62,82 @@ namespace Microsoft.TemplateSearch.TemplateDiscovery.IntegrationTests
                 .And.NotHaveStdOutContaining("Exception")
                 .And.HaveStdOutContaining("Microsoft.Azure.WebJobs.ProjectTemplates");
         }
+
+        [Fact]
+        public void CanReadCliData()
+        {
+            string testDir = TestUtils.CreateTemporaryFolder();
+            using var packageManager = new PackageManager();
+            string packageLocation = packageManager.PackTestTemplatesNuGetPackage();
+
+            new DotnetCommand(
+                _log,
+                "Microsoft.TemplateSearch.TemplateDiscovery.dll",
+                "--basePath",
+                testDir,
+                "--packagesPath",
+                Path.GetDirectoryName(packageLocation),
+                "-v")
+                .Execute()
+                .Should()
+                .ExitWith(0);
+
+            string cacheFilePath = Path.Combine(testDir, "SearchCache", "NuGetTemplateSearchInfo.json");
+            Assert.True(File.Exists(cacheFilePath));
+
+            var settingsPath = TestUtils.CreateTemporaryFolder();
+
+            new DotnetNew3Command(_log)
+                  .WithCustomHive(settingsPath)
+                  .WithEnvironmentVariable("DOTNET_NEW_SEARCH_FILE_OVERRIDE", cacheFilePath)
+                  .WithEnvironmentVariable("DOTNET_CLI_CONTEXT_VERBOSE", "true")
+                  .Execute()
+                  .Should()
+                  .ExitWith(0)
+                  .And.NotHaveStdErr();
+
+            new DotnetNew3Command(_log, "CliHostFile", "--search")
+                .WithCustomHive(settingsPath)
+                .WithEnvironmentVariable("DOTNET_NEW_SEARCH_FILE_OVERRIDE", cacheFilePath)
+                .WithEnvironmentVariable("DOTNET_CLI_CONTEXT_VERBOSE", "true")
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And.NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Exception")
+                .And.HaveStdOutContaining("TestAssets.TemplateWithCliHostFile")
+                .And.HaveStdOutContaining("Microsoft.TemplateEngine.TestTemplates");
+
+            new DotnetNew3Command(_log, "--search", "--param")
+                 .WithCustomHive(settingsPath)
+                 .WithEnvironmentVariable("DOTNET_NEW_SEARCH_FILE_OVERRIDE", cacheFilePath)
+                 .WithEnvironmentVariable("DOTNET_CLI_CONTEXT_VERBOSE", "true")
+                 .Execute()
+                 .Should()
+                 .ExitWith(0)
+                 .And.NotHaveStdErr()
+                 .And.NotHaveStdOutContaining("Exception")
+                 .And.HaveStdOutContaining("TestAssets.TemplateWithCliHostFile")
+                 .And.HaveStdOutContaining("Microsoft.TemplateEngine.TestTemplates");
+
+            new DotnetNew3Command(_log, "--search", "-p")
+                .WithCustomHive(settingsPath)
+                .WithEnvironmentVariable("DOTNET_NEW_SEARCH_FILE_OVERRIDE", cacheFilePath)
+                .WithEnvironmentVariable("DOTNET_CLI_CONTEXT_VERBOSE", "true")
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And.NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Exception")
+                .And.HaveStdOutContaining("TestAssets.TemplateWithCliHostFile")
+                .And.HaveStdOutContaining("Microsoft.TemplateEngine.TestTemplates");
+
+            new DotnetNew3Command(_log, "--search", "--test-param")
+                .WithCustomHive(settingsPath)
+                .WithEnvironmentVariable("DOTNET_NEW_SEARCH_FILE_OVERRIDE", cacheFilePath)
+                .WithEnvironmentVariable("DOTNET_CLI_CONTEXT_VERBOSE", "true")
+                .Execute()
+                .Should().Fail();
+        }
     }
 }
