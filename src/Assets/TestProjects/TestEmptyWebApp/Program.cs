0785147c12cd40e0224253fd98423e2faa9c1f7b
John Luo <johluo@microsoft.com>
2021-07-01 23:03:03 -0700

    Add unit tests

---
+++ src/Assets/TestProjects/TestEmptyWebApp/Program.cs
@@ -0,0 +1,12 @@
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", (Func<string>)(() => "Hello World!"));
+
+app.Run();
---
+++ src/Assets/TestProjects/TestWorkerApp/Program.cs
@@ -0,0 +1,18 @@
+
+namespace WorkerApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
