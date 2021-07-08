ae450c4fc3ec9dc47ea0ad51b98845e829fd9f0c
Pranav K <prkrishn@hotmail.com>
2021-07-08 16:36:34 -0700

    PR feedback

---
+++ src/Tests/Microsoft.NET.Build.Tests/MetadataUpdaterSupportTest.cs
@@ -0,0 +1,68 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using FluentAssertions;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.NET.Build.Tests
+{
+    public class MetadataUpdaterSupportTest : SdkTest
+    {
+        public MetadataUpdaterSupportTest(ITestOutputHelper log) : base(log)
+        {
+        }
+
+        [Fact]
+        public void It_Configures_MetadataUpdaterSupport_InReleaseBuilds()
+        {
+            var targetFramework = "net6.0";
+            var testAsset = _testAssetsManager
+                .CopyTestAsset("HelloWorld")
+                .WithSource()
+                .WithTargetFramework("net6.0");
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand
+                .Execute("/p:Configuration=Release")
+                .Should()
+                .Pass();
+
+            var outputDirectory = buildCommand.GetOutputDirectory(targetFramework, configuration: "Release");
+            var runtimeConfigPath = Path.Combine(outputDirectory.FullName, "HelloWorld.runtimeconfig.json");
+
+            File.Exists(runtimeConfigPath).Should().BeTrue();
+
+            var fileContents = File.ReadAllText(runtimeConfigPath);
+            fileContents.Should().Contain("\"System.Reflection.Metadata.MetadataUpdater.IsSupported\": false");
+        }
+
+        [Fact]
+        public void It_Configures_MetadataUpdaterSupport_InDebugBuilds()
+        {
+            var targetFramework = "net6.0";
+            var testAsset = _testAssetsManager
+                .CopyTestAsset("HelloWorld")
+                .WithSource()
+                .WithTargetFramework("net6.0");
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand
+                .Execute()
+                .Should()
+                .Pass();
+
+            var outputDirectory = buildCommand.GetOutputDirectory(targetFramework);
+            var runtimeConfigPath = Path.Combine(outputDirectory.FullName, "HelloWorld.runtimeconfig.json");
+
+            File.Exists(runtimeConfigPath).Should().BeTrue();
+
+            var fileContents = File.ReadAllText(runtimeConfigPath);
+            fileContents.Should().NotContain("\"System.Reflection.Metadata.MetadataUpdater.IsSupported\": false");
+        }
+    }
+}
