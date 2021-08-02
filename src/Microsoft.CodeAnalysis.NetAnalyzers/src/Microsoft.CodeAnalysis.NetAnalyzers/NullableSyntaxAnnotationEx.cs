19e051786eda390f59aee00ec327ba486b4534ee
Jonathon Marolf <jmarolf@microsoft.com>
2021-08-02 10:35:03 -0700

    prefer `is not null` to `is object`

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/NullableSyntaxAnnotationEx.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/NullableSyntaxAnnotationEx.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
 using System.Reflection;
 using Microsoft.CodeAnalysis;
@@ -13,7 +13,7 @@ namespace Analyzer.Utilities.Lightup
         static NullableSyntaxAnnotationEx()
         {
             var nullableSyntaxAnnotation = typeof(Workspace).Assembly.GetType("Microsoft.CodeAnalysis.CodeGeneration.NullableSyntaxAnnotation", throwOnError: false);
-            if (nullableSyntaxAnnotation is object)
+            if (nullableSyntaxAnnotation is not null)
             {
                 Oblivious = (SyntaxAnnotation?)nullableSyntaxAnnotation.GetField(nameof(Oblivious), BindingFlags.Static | BindingFlags.Public)?.GetValue(null);
                 AnnotatedOrNotAnnotated = (SyntaxAnnotation?)nullableSyntaxAnnotation.GetField(nameof(AnnotatedOrNotAnnotated), BindingFlags.Static | BindingFlags.Public)?.GetValue(null);
