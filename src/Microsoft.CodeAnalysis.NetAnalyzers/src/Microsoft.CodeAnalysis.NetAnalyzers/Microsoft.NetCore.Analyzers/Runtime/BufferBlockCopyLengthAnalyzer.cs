ff1b392bd91929dc4ea900459c7d44d312d9f120
Mahdi Varposhti <35560951+mahdiva@users.noreply.github.com>
2021-08-13 09:25:53 -0600

    Buffer.BlockCopy Analyzer (#5242)
    * Add Buffer.BlockCopy Length Analyzer
    
    * Fix alphabetical order
    
    * Address PR comments
    
    * Change category to Reliability
    
    * Refactor
    
    * Address PR comments
    
    * New comments
    
    * New unit tests
    
    * NIT
    
    * Fix build errors
    
    * Skip Array.CreateInstance unit test

---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/BufferBlockCopyLengthAnalyzer.cs
@@ -0,0 +1,161 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Runtime
+{
+    /// <summary>
+    /// Check for the intended use of .Length on arrays passed into Buffer.BlockCopy
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class BufferBlockCopyLengthAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA2018";
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.BufferBlockCopyLengthTitle), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.BufferBlockCopyLengthMessage), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.BufferBlockCopyDescription), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+
+        internal static DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(RuleId,
+                                                                                      s_localizableTitle,
+                                                                                      s_localizableMessage,
+                                                                                      DiagnosticCategory.Reliability,
+                                                                                      RuleLevel.BuildWarning,
+                                                                                      s_localizableDescription,
+                                                                                      isPortedFxCopRule: false,
+                                                                                      isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterCompilationStartAction(context =>
+            {
+                if (!context.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemBuffer, out INamedTypeSymbol? bufferType))
+                {
+                    return;
+                }
+
+                INamedTypeSymbol arrayType = context.Compilation.GetSpecialType(SpecialType.System_Array);
+                INamedTypeSymbol int32Type = context.Compilation.GetSpecialType(SpecialType.System_Int32);
+
+                // Buffer.BlockCopy(Array src, int srcOffset, Array dst, int dstOffset, int count)
+                ParameterInfo[] blockCopyParameterInfo = {
+                    ParameterInfo.GetParameterInfo(arrayType, false, 1, false),
+                    ParameterInfo.GetParameterInfo(int32Type, false, 1, false),
+                    ParameterInfo.GetParameterInfo(arrayType, false, 1, false),
+                    ParameterInfo.GetParameterInfo(int32Type, false, 1, false),
+                    ParameterInfo.GetParameterInfo(int32Type, false, 1, false)};
+
+                IMethodSymbol? blockCopyMethod = bufferType
+                   .GetMembers("BlockCopy")
+                   .OfType<IMethodSymbol>()
+                   .GetFirstOrDefaultMemberWithParameterInfos(blockCopyParameterInfo);
+
+                if (blockCopyMethod is null)
+                {
+                    return;
+                }
+
+                ISymbol arrayLengthProperty = arrayType
+                    .GetMembers("Length")
+                    .FirstOrDefault(p => p is IPropertySymbol);
+
+                context.RegisterOperationAction(context =>
+                {
+                    var invocationOperation = (IInvocationOperation)context.Operation;
+
+                    if (!invocationOperation.TargetMethod.Equals(blockCopyMethod, SymbolEqualityComparer.Default))
+                    {
+                        return;
+                    }
+
+                    ImmutableArray<IArgumentOperation> arguments = IOperationExtensions.GetArgumentsInParameterOrder(invocationOperation.Arguments);
+
+                    IArgumentOperation sourceArgument = arguments[0];
+                    IArgumentOperation destinationArgument = arguments[2];
+                    IArgumentOperation countArgument = arguments[4];
+
+                    bool CheckArrayLengthLocalReference(IArgumentOperation targetArgument, IPropertyReferenceOperation lengthPropertyArgument)
+                    {
+                        if (targetArgument.Value is IConversionOperation targetArgumentValue)
+                        {
+                            if (lengthPropertyArgument.Instance.GetReferencedMemberOrLocalOrParameter() == targetArgumentValue.Operand.GetReferencedMemberOrLocalOrParameter())
+                            {
+                                IArrayTypeSymbol countArgumentArrayTypeSymbol = (IArrayTypeSymbol)lengthPropertyArgument.Instance.Type;
+                                if (countArgumentArrayTypeSymbol.ElementType.SpecialType != SpecialType.System_Byte &&
+                                countArgumentArrayTypeSymbol.ElementType.SpecialType != SpecialType.System_SByte)
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+                        return false;
+                    }
+
+                    bool CheckLengthPropertyOnByteOrSByteArrays(IPropertyReferenceOperation countArgument)
+                    {
+                        if (countArgument.Property.Equals(arrayLengthProperty))
+                        {
+                            return CheckArrayLengthLocalReference(sourceArgument, countArgument) || CheckArrayLengthLocalReference(destinationArgument, countArgument);
+                        }
+
+                        return false;
+                    }
+
+                    if (countArgument.Value is IPropertyReferenceOperation countArgumentValue && CheckLengthPropertyOnByteOrSByteArrays(countArgumentValue))
+                    {
+                        context.ReportDiagnostic(countArgument.Value.CreateDiagnostic(Rule));
+                    }
+                    else
+                    {
+                        if (countArgument.Value is not ILocalReferenceOperation localReferenceOperation)
+                        {
+                            return;
+                        }
+
+                        SemanticModel semanticModel = countArgument.SemanticModel;
+                        CancellationToken cancellationToken = context.CancellationToken;
+
+                        ILocalSymbol localArgumentDeclaration = localReferenceOperation.Local;
+
+                        SyntaxReference declaringSyntaxReference = localArgumentDeclaration.DeclaringSyntaxReferences.FirstOrDefault();
+                        if (declaringSyntaxReference is null)
+                        {
+                            return;
+                        }
+
+                        if (semanticModel.GetOperationWalkingUpParentChain(declaringSyntaxReference.GetSyntax(cancellationToken), cancellationToken) is not IVariableDeclaratorOperation variableDeclaratorOperation)
+                        {
+                            return;
+                        }
+
+                        IVariableInitializerOperation variableInitializer = variableDeclaratorOperation.Initializer;
+
+                        if (variableInitializer is not null && variableInitializer.Value is IPropertyReferenceOperation variableInitializerPropertyReference &&
+                        CheckLengthPropertyOnByteOrSByteArrays(variableInitializerPropertyReference))
+                        {
+                            context.ReportDiagnostic(countArgument.Value.CreateDiagnostic(Rule));
+                        }
+                        else if (variableDeclaratorOperation.Parent is IVariableDeclarationOperation variableDeclarationOperation &&
+                        variableDeclarationOperation.Initializer is not null &&
+                        variableDeclarationOperation.Initializer.Value is IPropertyReferenceOperation variableInitializerPropertyReferenceVB &&
+                        CheckLengthPropertyOnByteOrSByteArrays(variableInitializerPropertyReferenceVB))
+                        {
+                            context.ReportDiagnostic(countArgument.Value.CreateDiagnostic(Rule));
+                        }
+                    }
+                },
+                OperationKind.Invocation);
+            });
+        }
+    }
+}
--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/RulesMissingDocumentation.md
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/RulesMissingDocumentation.md
@@ -11,6 +11,7 @@ CA1842 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-r
 CA1843 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/ca1843> | Do not use 'WaitAll' with a single task |
 CA1848 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/ca1848> | Use the LoggerMessage delegates |
 CA2017 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/ca2017> | Parameter count mismatch |
+CA2018 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/ca2018> | 'Buffer.BlockCopy' expects the number of bytes to be copied for the 'count' argument |
 CA2252 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/ca2252> | This API requires opting into preview features |
 CA2253 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/ca2253> | Named placeholders should not be numeric values |
 CA2254 | <https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/ca2254> | Template should be a static expression |
