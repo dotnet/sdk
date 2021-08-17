cb650d9fc0f7a0408e1e4904f6ba9697dfba531c
Maryam Ariyan <maryam.ariyan@microsoft.com>
2021-08-17 11:01:30 -0700

    Fixes exception on LoggerMessageAnalyzer for missing named placeholder

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/LoggerMessageDefineAnalyzer.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/LoggerMessageDefineAnalyzer.cs
@@ -223,7 +223,7 @@ namespace Microsoft.NetCore.Analyzers.Runtime
                 {
                     context.ReportDiagnostic(formatExpression.CreateDiagnostic(CA2253Rule));
                 }
-                else if (char.IsLower(valueName[0]))
+                else if (valueName.Length > 0 && char.IsLower(valueName[0]))
                 {
                     context.ReportDiagnostic(formatExpression.CreateDiagnostic(CA1727Rule));
                 }
