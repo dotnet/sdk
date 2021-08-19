8511121e5fd50289142202e1994d1c567a78d717
Vlada Shubina <vshubina@microsoft.com>
2021-08-19 11:23:59 +0300

    moved to without-name-matching approach
    Commit migrated from https://github.com/dotnet/templating/commit/2750e65592418a7c0e5887330eafb4165e0f34d7

--- src/TemplateEngine/Microsoft.TemplateSearch.TemplateDiscovery/Program.cs
+++ src/TemplateEngine/Microsoft.TemplateSearch.TemplateDiscovery/Program.cs
@@ -17,13 +17,24 @@ namespace Microsoft.TemplateSearch.TemplateDiscovery
         private static async Task Main(string[] args)
         {
             RootCommand rootCommand = CreateCommand();
-            rootCommand.Handler = CommandHandler.Create(ExecuteAsync);
             await rootCommand.InvokeAsync(args).ConfigureAwait(false);
         }
 
-        private static async Task<int> ExecuteAsync(CommandArgs args)
+        private static async Task<int> ExecuteAsync(
+            DirectoryInfo basePath,
+            bool allowPreviewPacks,
+            int pageSize,
+            bool onePage,
+            bool savePacks,
+            bool noTemplateJsonFilter,
+            bool verbose,
+            IEnumerable<SupportedQueries>? queries,
+            DirectoryInfo? packagesPath)
         {
-            PackSourceChecker packSourceChecker = NuGetPackSourceCheckerFactory.CreatePackSourceChecker(args);
+            Verbose.IsEnabled = verbose;
+            CommandArgs config = new CommandArgs(basePath, allowPreviewPacks, pageSize, onePage, savePacks, noTemplateJsonFilter, queries, packagesPath);
+
+            PackSourceChecker packSourceChecker = NuGetPackSourceCheckerFactory.CreatePackSourceChecker(config);
 
             var cts = new CancellationTokenSource();
             Console.CancelKeyPress += (s, e) =>
@@ -36,7 +47,7 @@ namespace Microsoft.TemplateSearch.TemplateDiscovery
             try
             {
                 PackSourceCheckResult checkResults = await packSourceChecker.CheckPackagesAsync(cts.Token).ConfigureAwait(false);
-                PackCheckResultReportWriter.WriteResults(args.OutputPath, checkResults);
+                PackCheckResultReportWriter.WriteResults(config.OutputPath, checkResults);
                 return 0;
             }
             catch (TaskCanceledException)
@@ -49,38 +60,42 @@ namespace Microsoft.TemplateSearch.TemplateDiscovery
 
         private static RootCommand CreateCommand()
         {
-            RootCommand rootCommand = new RootCommand("Generates the template package search cache file based on the packages available on NuGet.org.");
-            rootCommand.AddOption(new Option<DirectoryInfo>("--basePath")
+            Option<DirectoryInfo> basePathOption = new Option<DirectoryInfo>("--basePath")
             {
                 Arity = ArgumentArity.ExactlyOne,
                 Description = "The root dir for output for this run.",
                 IsRequired = true
-            }.LegalFilePathsOnly());
+            }.LegalFilePathsOnly();
 
-            rootCommand.AddOption(new Option("--allowPreviewPacks")
+            Option<bool> allowPreviewPacksOption = new Option<bool>("--allowPreviewPacks")
             {
                 Description = "Include preview packs in the results (by default, preview packs are ignored and the latest stable pack is used.",
-            });
-            rootCommand.AddOption(new Option<int>("--pageSize", getDefaultValue: () => _defaultPageSize)
+            };
+
+            Option<int> pageSizeOption = new Option<int>("--pageSize", getDefaultValue: () => _defaultPageSize)
             {
                 Description = "(debugging) The chunk size for interactions with the source.",
-            });
-            rootCommand.AddOption(new Option("--onePage")
+            };
+
+            Option<bool> onePageOption = new Option<bool>("--onePage")
             {
                 Description = "(debugging) Only process one page of template packs.",
-            });
-            rootCommand.AddOption(new Option("--savePacks")
+            };
+
+            Option<bool> savePacksOption = new Option<bool>("--savePacks")
             {
                 Description = "Don't delete downloaded candidate packs (by default, they're deleted at the end of a run).",
-            });
-            rootCommand.AddOption(new Option("--noTemplateJsonFilter")
+            };
+
+            Option<bool> noTemplateJsonFilterOption = new Option<bool>("--noTemplateJsonFilter")
             {
                 Description = "Don't prefilter packs that don't contain any template.json files (this filter is applied by default).",
-            });
-            rootCommand.AddOption(new Option(new[] { "-v", "--verbose" })
+            };
+
+            Option<bool> verboseOption = new Option<bool>(new[] { "-v", "--verbose" })
             {
                 Description = "Verbose output for template processing.",
-            });
+            };
 
             Option<SupportedQueries[]> queriesOption = new Option<SupportedQueries[]>("--queries")
             {
@@ -89,14 +104,38 @@ namespace Microsoft.TemplateSearch.TemplateDiscovery
                 AllowMultipleArgumentsPerToken = true,
             };
             queriesOption.FromAmong(Enum.GetValues<SupportedQueries>().Select(e => e.ToString()).ToArray());
-            rootCommand.AddOption(queriesOption);
 
-            rootCommand.AddOption(new Option<DirectoryInfo>("--packagesPath")
+            Option<DirectoryInfo> packagesPathOption = (new Option<DirectoryInfo>("--packagesPath")
             {
                 Description = $"Path to pre-downloaded packages. If specified, the packages won't be downloaded from NuGet.org.",
             }.ExistingOnly());
 
+            RootCommand rootCommand = new RootCommand("Generates the template package search cache file based on the packages available on NuGet.org.")
+            {
+                basePathOption,
+                allowPreviewPacksOption,
+                pageSizeOption,
+                onePageOption,
+                savePacksOption,
+                noTemplateJsonFilterOption,
+                verboseOption,
+                queriesOption,
+                packagesPathOption
+            };
+
             rootCommand.TreatUnmatchedTokensAsErrors = true;
+            rootCommand.Handler = CommandHandler.Create(
+                basePathOption,
+                allowPreviewPacksOption,
+                pageSizeOption,
+                onePageOption,
+                savePacksOption,
+                noTemplateJsonFilterOption,
+                verboseOption,
+                queriesOption,
+                packagesPathOption,
+                ExecuteAsync);
+
             return rootCommand;
         }
     }
