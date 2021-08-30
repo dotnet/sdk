e1b906568c2a35462ee31a6be95968612a050fe6
Tim Hannifin <timhann@microsoft.com>
2021-08-30 14:10:07 -0700

    Add analyzers for Microsoft.IdentityModel.Tokens (Wilson) Security Issues
    Ensure key validation isn't explicitly disabled by setting the relevant setter on the validation object to false:
    * RequireExpirationTime
    * ValidateAudience
    * ValidateIssuer
    * ValidateLifetime
    
    Ensure key validation isn't implicitly disabled by having the relevant delegate always return true:
    * AudienceValidator
    * LifetimeValidator
    
    NOTE: There's another key implicit case for IssuerValidator, however the logic there is more complex and would require Data Flow Analysis (DFA). Since DFA has a different performance profile anyways, shipping these analyzers first which can be accomplished via static analysis. We also have CodeQL work in the pipe separately from this to address more complicated DFA flows.

---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Security/DoNotDisableTokenValidationChecks.cs
@@ -0,0 +1,91 @@
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Security
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotDisableTokenValidationChecks : DiagnosticAnalyzer
+    {
+        // Set of properties on Microsoft.IdentityModel.Tokens.TokenValidationParameters which shouldn't be set to false.
+        private readonly string[] PropertiesWhichShouldNotBeFalse = new string[]
+        {
+            "RequireExpirationTime",
+            "ValidateAudience",
+            "ValidateIssuer",
+            "ValidateLifetime",
+        };
+
+        internal const string DiagnosticId = "CA5404";  // TODO: Somewhere in documentation I saw I need to create a CA5404.md file, don't recall where.
+        private static readonly LocalizableString s_Title = new LocalizableResourceString(
+            nameof(MicrosoftNetCoreAnalyzersResources.DoNotDisableTokenValidationChecks),
+            MicrosoftNetCoreAnalyzersResources.ResourceManager,
+            typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_Message = new LocalizableResourceString(
+            nameof(MicrosoftNetCoreAnalyzersResources.DoNotDisableTokenValidationChecksMessage),
+            MicrosoftNetCoreAnalyzersResources.ResourceManager,
+            typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_Description = new LocalizableResourceString(
+            nameof(MicrosoftNetCoreAnalyzersResources.DoNotDisableTokenValidationChecksDescription),
+            MicrosoftNetCoreAnalyzersResources.ResourceManager,
+            typeof(MicrosoftNetCoreAnalyzersResources));
+
+        internal static DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(
+                DiagnosticId,
+                s_Title,
+                s_Message,
+                DiagnosticCategory.Security,
+                RuleLevel.BuildWarning,
+                description: s_Description,
+                isPortedFxCopRule: false,
+                isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+            {
+                var compilation = compilationStartAnalysisContext.Compilation;
+                var tokenValidationParamsTypeSymbol = compilation.GetOrCreateTypeByMetadataName(
+                    WellKnownTypeNames.MicrosoftIdentityModelTokensTokenValidationParameters);
+
+                if (tokenValidationParamsTypeSymbol == null)
+                {
+                    return;
+                }
+
+                compilationStartAnalysisContext.RegisterOperationAction(operationAnalysisContext =>
+                {
+                    var simpleAssignmentOperation = (ISimpleAssignmentOperation)operationAnalysisContext.Operation;
+
+                    if (simpleAssignmentOperation.Target is IPropertyReferenceOperation propertyReferenceOperation)
+                    {
+                        var property = propertyReferenceOperation.Property;
+
+                        if (property.ContainingType.Equals(tokenValidationParamsTypeSymbol) &&
+                            simpleAssignmentOperation.Value.ConstantValue.HasValue &&
+                            simpleAssignmentOperation.Value.ConstantValue.Value.Equals(false) &&
+                            PropertiesWhichShouldNotBeFalse.Contains(property.Name))
+                        {
+                            operationAnalysisContext.ReportDiagnostic(
+                                    simpleAssignmentOperation.CreateDiagnostic(
+                                        Rule, property.Name));
+                        }
+                    }
+                }, OperationKind.SimpleAssignment);
+            });
+        }
+    }
+}
