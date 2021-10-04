b1760caf1348d99747bc43f3ec403083d5986bdd
Vlada Shubina <vshubina@microsoft.com>
2021-10-04 17:59:06 +0200

    tests draft
    Commit migrated from https://github.com/dotnet/templating/commit/d768ce87ffb9394561a258fb93f29c4ef2ff004c

---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Tasks.IntegrationTests/LocalizeTemplateTests.cs
@@ -0,0 +1,106 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
+using Microsoft.TemplateEngine.TestHelper;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.TemplateEngine.Tasks.IntegrationTests
+{
+    public class LocalizeTemplateTests
+    {
+        private readonly ITestOutputHelper _log;
+
+        public LocalizeTemplateTests(ITestOutputHelper log)
+        {
+            _log = log;
+        }
+
+        [Fact]
+        public void CanRunTask()
+        {
+            string tmpDir = TestUtils.CreateTemporaryFolder();
+            TestUtils.DirectoryCopy("Resources\\BasicTemplatePackage", tmpDir, true);
+
+            new DotnetCommand(_log, "add", "TemplatePackage.csproj", "package", "Microsoft.TemplateEngine.Tasks", "-s", Path.GetFullPath("Packages"), "--prerelease")
+              .WithWorkingDirectory(tmpDir)
+              .Execute()
+              .Should()
+              .Pass();
+
+            new DotnetCommand(_log, "build")
+                .WithWorkingDirectory(tmpDir)
+                .Execute()
+                .Should()
+                .Pass();
+
+            string locFolder = Path.Combine(tmpDir, "content\\TemplateWithSourceName\\.template.config\\localize");
+
+            Assert.True(Directory.Exists(locFolder));
+            Assert.Equal(14, Directory.GetFiles(locFolder).Length);
+            Assert.True(File.Exists(Path.Combine(locFolder, "templatestrings.de.json")));
+
+            Directory.Delete(tmpDir, true);
+        }
+
+        [Fact]
+        public void CanRunTaskSelectedLangs()
+        {
+            string tmpDir = TestUtils.CreateTemporaryFolder();
+            TestUtils.DirectoryCopy("Resources\\TemplatePackageEnDe", tmpDir, true);
+
+            new DotnetCommand(_log, "add", "TemplatePackage.csproj", "package", "Microsoft.TemplateEngine.Tasks", "-s", Path.GetFullPath("Packages"), "--prerelease")
+              .WithWorkingDirectory(tmpDir)
+              .Execute()
+              .Should()
+              .Pass();
+
+            new DotnetCommand(_log, "build")
+                .WithWorkingDirectory(tmpDir)
+                .Execute()
+                .Should()
+                .Pass();
+
+            string locFolder = Path.Combine(tmpDir, "content\\TemplateWithSourceName\\.template.config\\localize");
+
+            Assert.True(Directory.Exists(locFolder));
+            Assert.Equal(2, Directory.GetFiles(locFolder).Length);
+            Assert.True(File.Exists(Path.Combine(locFolder, "templatestrings.de.json")));
+            Assert.False(File.Exists(Path.Combine(locFolder, "templatestrings.fr.json")));
+
+            Directory.Delete(tmpDir, true);
+        }
+
+        [Fact]
+        public void CanRunTaskSelectedTemplates()
+        {
+            string tmpDir = TestUtils.CreateTemporaryFolder();
+            TestUtils.DirectoryCopy("Resources\\TemplatePackagePartiallyLocalized", tmpDir, true);
+
+            new DotnetCommand(_log, "add", "TemplatePackage.csproj", "package", "Microsoft.TemplateEngine.Tasks", "-s", Path.GetFullPath("Packages"), "--prerelease")
+              .WithWorkingDirectory(tmpDir)
+              .Execute()
+              .Should()
+              .Pass();
+
+            new DotnetCommand(_log, "build")
+                .WithWorkingDirectory(tmpDir)
+                .Execute()
+                .Should()
+                .Pass();
+
+            string locFolder = Path.Combine(tmpDir, "content\\localized\\.template.config\\localize");
+            string noLocFolder = Path.Combine(tmpDir, "content\\non-localized\\.template.config\\localize");
+
+            Assert.True(Directory.Exists(locFolder));
+            Assert.Equal(14, Directory.GetFiles(locFolder).Length);
+            Assert.True(File.Exists(Path.Combine(locFolder, "templatestrings.de.json")));
+            Assert.False(Directory.Exists(noLocFolder));
+
+            Directory.Delete(tmpDir, true);
+        }
+
+    }
+}
