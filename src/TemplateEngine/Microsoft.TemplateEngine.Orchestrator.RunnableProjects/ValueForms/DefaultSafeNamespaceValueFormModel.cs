78e43326ce5e10119cb201b2c3b92189d64fae84
Bekir Ozturk <bozturk@microsoft.com>
2021-10-13 08:58:44 +0200

    Safe namespace for surrogate pairs
    Commit migrated from https://github.com/dotnet/templating/commit/1b88509007eb9bfb0b44ee11dd4c30f1849ad892

--- src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/ValueForms/DefaultSafeNamespaceValueFormModel.cs
+++ src/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/ValueForms/DefaultSafeNamespaceValueFormModel.cs
@@ -1,8 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Utilities;
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.ValueForms
@@ -10,14 +16,14 @@ namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.ValueForms
     internal class DefaultSafeNamespaceValueFormModel : IValueForm
     {
         internal const string FormName = "safe_namespace";
-        private readonly string _name;
+        private readonly string? _name;
 
         internal DefaultSafeNamespaceValueFormModel()
             : this(null)
         {
         }
 
-        internal DefaultSafeNamespaceValueFormModel(string name)
+        internal DefaultSafeNamespaceValueFormModel(string? name)
         {
             _name = name;
         }
@@ -33,10 +39,37 @@ namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.ValueForms
 
         public virtual string Process(IReadOnlyDictionary<string, IValueForm> forms, string value)
         {
-            string workingValue = Regex.Replace(value, @"(^\s+|\s+$)", "");
-            workingValue = Regex.Replace(workingValue, @"(((?<=\.)|^)((?=\d)|\.)|[^\w\.])|(\.$)", "_");
+            const char invalidCharacterReplacement = '_';
 
-            return workingValue;
+            value = value ?? throw new ArgumentNullException(nameof(value));
+            value = value.Trim();
+
+            StringBuilder safeValueStr = new StringBuilder(value.Length);
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (i < value.Length - 1 && char.IsSurrogatePair(value[i], value[i + 1]))
+                {
+                    safeValueStr.Append(invalidCharacterReplacement);
+                    // Skip both chars that make up this symbol.
+                    i++;
+                    continue;
+                }
+
+                bool isValidFirstCharacter = safeValueStr.Length == 0 && UnicodeCharacterUtilities.IsIdentifierStartCharacter(value[i]);
+                bool isValidPartCharacter = safeValueStr.Length > 0 && UnicodeCharacterUtilities.IsIdentifierPartCharacter(value[i]);
+
+                if (isValidFirstCharacter || isValidPartCharacter)
+                {
+                    safeValueStr.Append(value[i]);
+                }
+                else
+                {
+                    safeValueStr.Append(invalidCharacterReplacement);
+                }
+            }
+
+            return safeValueStr.ToString();
         }
     }
 }
