46b41c3e07c1cdd45b4816553a989c8edab6b55c
Bekir Ozturk <bozturk@microsoft.com>
2021-10-13 12:11:29 +0200

    Tests for DefaultSafeNamespaceValueFormModel
    Commit migrated from https://github.com/dotnet/templating/commit/c1d10c776888eee7587202455c9632782fb4e37c

---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/ValueFormTests/DefaultLowerSafeNamespaceValueFormModelTests.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.ValueForms;
+using Xunit;
+
+namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests.ValueFormTests
+{
+    public class DefaultLowerSafeNamespaceValueFormModelTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("Ⅻ〇˙–⿻𠀀𠀁𪛕𪛖", "ⅻ〇_______")]
+        [InlineData("𒁊𒁫¶ĚΘঊਇ", "___ěθঊਇ")]
+        [InlineData("9heLLo", "_hello")]
+        [InlineData("broken-clock32", "broken_clock32")]
+        [InlineData(";MyWord;", "_myword_")]
+        [InlineData("&&*", "___")]
+        [InlineData("ÇağrışımÖrüntüsü", "çağrışımörüntüsü")]
+        [InlineData("number of sockets", "number_of_sockets")]
+        [InlineData("НоваяПеременная", "новаяпеременная")]
+        public void LowerSafeNamespaceWorksAsExpected(string input, string expected)
+        {
+            var model = new DefaultLowerSafeNamespaceValueFormModel();
+            string actual = model.Process(null, input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/ValueFormTests/DefaultSafeNamespaceValueFormModelTests.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.ValueForms;
+using Xunit;
+
+namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests.ValueFormTests
+{
+    public class DefaultSafeNamespaceValueFormModelTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("Ⅻ〇˙–⿻𠀀𠀁𪛕𪛖", "Ⅻ〇_______")]
+        [InlineData("𒁊𒁫¶ĚΘঊਇ", "___ĚΘঊਇ")]
+        [InlineData("9heLLo", "_heLLo")]
+        [InlineData("broken-clock32", "broken_clock32")]
+        [InlineData(";MyWord;", "_MyWord_")]
+        [InlineData("&&*", "___")]
+        [InlineData("ÇağrışımÖrüntüsü", "ÇağrışımÖrüntüsü")]
+        [InlineData("number of sockets", "number_of_sockets")]
+        [InlineData("НоваяПеременная", "НоваяПеременная")]
+        public void SafeNamespaceWorksAsExpected(string input, string expected)
+        {
+            var model = new DefaultSafeNamespaceValueFormModel();
+            string actual = model.Process(null, input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
