5d83c64885193f33b7bde0a989680bc31bfe8096
Pranav K <prkrishn@hotmail.com>
2021-11-04 11:47:55 -0700

    Ensure references added using "Reference" are correctly copied in BlazorWASM projects
    As part of .NET6, we rely on OriginalItemSpec to copy items around. Unfortunately there is an issue with RAR where references
    added using `Reference` have an incorrect ItemSpec (intentionally so). This change worksaround this case by special casing references
    from projects where the OriginalItemSpec differs from it's full path.
    
    Fixes https://github.com/dotnet/aspnetcore/issues/37574

---
+++ src/Tests/Microsoft.NET.Sdk.BlazorWebAssembly.Tests/ComputeBlazorBuildAssetsTest.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Moq;
+using Xunit;
+
+namespace Microsoft.NET.Sdk.BlazorWebAssembly.Tests;
+
+public class ComputeBlazorBuildAssetsTest
+{
+    [Fact]
+    public void Execute_FixesReferencesTo()
+    {
+        // Arrange
+        var taskInstance = new ComputeBlazorBuildAssets
+        {
+            Candidates = new[] 
+            {
+                new TaskItem(@"x:\refassembly\file.dll", new Dictionary<string, string>()
+                {
+                    ["OriginalItemSpec"] = @"x:\MyRefProject\bin\Debug\net6.0\MyRefProject.dll",
+                    ["ReferenceSourceTarget"] = "ProjectReference",
+                }),
+                new TaskItem(@"x:\MyRefProject\bin\Debug\net6.0\MyRefProject.dll", new Dictionary<string, string>()
+                {
+                    ["OriginalItemSpec"] = @"x:\MyRefProject\bin\Debug\net6.0\MyRefProject.dll",
+                    ["ReferenceSourceTarget"] = "ProjectReference",
+                }),
+            },
+            ProjectAssembly = new[]
+            {
+                new TaskItem(@"x:\MyProject\bin\Debug\MyProject.dll", new Dictionary<string, object>
+                {
+                    ["OriginalItemSpec"] = @"x:\MyProject\obj\Debug\MyProject.dll",
+                })
+            },
+            ProjectDebugSymbols = new[]
+            {
+                new TaskItem(@"x:\MyProject\bin\Debug\MyProject.pdb", new Dictionary<string, object>
+                {
+                    ["OriginalItemSpec"] = @"x:\MyProject\obj\Debug\MyProject.pdb",
+                })
+            },
+            SatelliteAssemblies = Array.Empty<ITaskItem>(),
+            ProjectSatelliteAssemblies = Array.Empty<ITaskItem>(),
+            BuildEngine = Mock.Of<IBuildEngine>(),
+        };
+
+        // Act
+        taskInstance.Execute();
+
+        // Assert
+        Assert.Collection(
+            taskInstance.AssetCandidates,
+            item =>
+            {
+                Assert.Equal(@"x:\refassembly\file.dll", item.ItemSpec);
+                Assert.Equal(@"x:\refassembly\file.dll", item.GetMetadata("OriginalItemSpec"));
+            },
+            item =>
+            {
+                Assert.Equal(@"x:\MyRefProject\bin\Debug\net6.0\MyRefProject.dll", item.ItemSpec);
+                Assert.Equal(@"x:\MyRefProject\bin\Debug\net6.0\MyRefProject.dll", item.GetMetadata("OriginalItemSpec"));
+            },
+            item =>
+            {
+                Assert.Equal(@"x:\MyProject\bin\Debug\MyProject.dll", item.ItemSpec);
+                Assert.Equal(@"x:\MyProject\bin\Debug\MyProject.dll", item.GetMetadata("OriginalItemSpec"));
+            },
+            item =>
+            {
+                Assert.Equal(@"x:\MyProject\bin\Debug\MyProject.pdb", item.ItemSpec);
+                Assert.Empty(item.GetMetadata("OriginalItemSpec"));
+            });
+    }
+}
+
