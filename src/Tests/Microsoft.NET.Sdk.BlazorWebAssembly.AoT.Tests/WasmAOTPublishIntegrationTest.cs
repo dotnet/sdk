5c309e4ae6c3eda457d88ca973a153ce3dfea86b
Tanay Parikh <taparik@microsoft.com>
2021-11-18 13:10:54 -0800

    Use UnitTests.proj for helix runs

--- src/Tests/Microsoft.NET.Sdk.BlazorWebAssembly.AoT.Tests/WasmAOTPublishIntegrationTest.cs
+++ src/Tests/Microsoft.NET.Sdk.BlazorWebAssembly.AoT.Tests/WasmAOTPublishIntegrationTest.cs
@@ -1,8 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#if RUN_AOT_TESTS
-
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -189,4 +187,3 @@ namespace Microsoft.NET.Sdk.BlazorWebAssembly.Tests
             });
     }
 }
-#endif
