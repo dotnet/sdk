87e5bcdba8597a4d3dccc6ffaf98c3f23c35b722
Tanay Parikh <TanayParikh@users.noreply.github.com>
2022-01-14 01:31:02 +0000

    String message

--- src/Tests/dotnet-watch.Tests/Utilities/WatchableApp.cs
+++ src/Tests/dotnet-watch.Tests/Utilities/WatchableApp.cs
@@ -125,7 +125,7 @@ namespace Microsoft.DotNet.Watcher.Tools
 
             // Make this timeout long because it depends much on the MSBuild compilation speed.
             // Slow machines may take a bit to compile and boot test apps
-            await Process.GetOutputLineAsync(Encoding.UTF8.GetBytes(WatchStartedMessage), TimeSpan.FromMinutes(2));
+            await Process.GetOutputLineAsync(WatchStartedMessage, TimeSpan.FromMinutes(2));
         }
 
         public void Dispose()
