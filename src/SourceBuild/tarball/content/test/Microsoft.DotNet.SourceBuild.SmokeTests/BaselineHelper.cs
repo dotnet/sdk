7ea24b31e94ceda37e12f1b21d46ff8af0cdcd7e
Michael Simons <msimons@microsoft.com>
2022-02-10 15:58:01 -0600

    Add test to compare msft and sb sdk contents (#13153)

--- src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/BaselineHelper.cs
+++ src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/BaselineHelper.cs
@@ -4,9 +4,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Microsoft.DotNet.SourceBuild.SmokeTests
 {
@@ -30,6 +32,35 @@ namespace Microsoft.DotNet.SourceBuild.SmokeTests
             }
 
             Assert.Null(message);
+        }
+
+        public static void Compare(string baselineFileName, string actual, ITestOutputHelper outputHelper)
+        {
+            string baselineFilePath = GetBaselineFilePath(baselineFileName);
+            string baseline = File.ReadAllText(baselineFilePath);
+
+            string? message = null;
+            if (baseline != actual)
+            {
+                string actualBaselineFilePath = Path.Combine(Environment.CurrentDirectory, $"{baselineFileName}");
+                File.WriteAllText(actualBaselineFilePath, actual);
+
+                // Retrieve a diff in order to provide a UX which calls out the diffs.
+                string diff = DiffFiles(baselineFilePath, actualBaselineFilePath, outputHelper);
+                message = $"{Environment.NewLine}Baseline '{baselineFilePath}' does not match actual '{actualBaselineFilePath}`.  {Environment.NewLine}"
+                    + $"{diff}{Environment.NewLine}";
+            }
+
+            Assert.Null(message);
+        }
+
+        public static string DiffFiles(string file1Path, string file2Path, ITestOutputHelper outputHelper)
+        {
+            (Process Process, string StdOut, string StdErr) diffResult =
+                ExecuteHelper.ExecuteProcess("git", $"diff --no-index {file1Path} {file2Path}", outputHelper);
+            Assert.Equal(1, diffResult.Process.ExitCode);
+
+            return diffResult.StdOut;
         }
 
         private static string GetBaselineFilePath(string baselineFileName) => Path.Combine(Directory.GetCurrentDirectory(), "baselines", baselineFileName);
