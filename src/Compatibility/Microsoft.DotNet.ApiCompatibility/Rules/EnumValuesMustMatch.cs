2d1aa80a835f9b5c9ab505f4c92909e83c25f390
smasher164 <akhilindurti@microsoft.com>
2022-05-20 15:22:36 -0700

    review feedback

--- src/Compatibility/Microsoft.DotNet.ApiCompatibility/Rules/EnumValuesMustMatch.cs
+++ src/Compatibility/Microsoft.DotNet.ApiCompatibility/Rules/EnumValuesMustMatch.cs
@@ -11,31 +11,38 @@ using Microsoft.DotNet.ApiCompatibility.Abstractions;
 
 namespace Microsoft.DotNet.ApiCompatibility.Rules
 {
+    /// <summary>
+    /// This class implements a rule to check that the constant values for an enum's fields don't change.
+    /// </summary>
     public class EnumValuesMustMatch : Rule
     {
-        public override void Initialize(RuleRunnerContext context)
-        {
-            context.RegisterOnTypeSymbolAction(RunOnTypeSymbol);
-        }
+        public override void Initialize(RuleRunnerContext context) => context.RegisterOnTypeSymbolAction(RunOnTypeSymbol);
 
-        private static bool isEnum(ITypeSymbol type)
-        {
-            return type != null && type.IsType && type.TypeKind == TypeKind.Enum;
-        }
         private void RunOnTypeSymbol(ITypeSymbol left, ITypeSymbol right, string leftName, string rightName, IList<CompatDifference> differences)
         {
-            if (!isEnum(left) || !isEnum(right))
+            // Ensure that this rule only runs on enums.
+            if (left is null || left.TypeKind != TypeKind.Enum)
             {
                 return;
             }
-            var leftMembers = left.GetMembers().Where(a => a.Kind == SymbolKind.Field).Select(a => ((IFieldSymbol)a)).ToDictionary(a => a.Name);
-            var rightMembers = right.GetMembers().Where(a => a.Kind == SymbolKind.Field).Select(a => ((IFieldSymbol)a)).ToDictionary(a => a.Name);
-            foreach (var entry in leftMembers)
+
+            // Build a map of the enum's fields, keyed by the field names.
+            Dictionary<string, IFieldSymbol> leftMembers = left.GetMembers()
+                .Where(a => a.Kind == SymbolKind.Field)
+                .Select(a => (IFieldSymbol)a)
+                .ToDictionary(a => a.Name);
+            Dictionary<string, IFieldSymbol> rightMembers = right.GetMembers()
+                .Where(a => a.Kind == SymbolKind.Field)
+                .Select(a => (IFieldSymbol)a)
+                .ToDictionary(a => a.Name);
+
+            // For each field that is present in the left and right, check that their constant values match.
+            // Otherwise, emit a diagnostic.
+            foreach (KeyValuePair<string, IFieldSymbol> lEntry in leftMembers)
             {
-                IFieldSymbol rField;
-                if (rightMembers.TryGetValue(entry.Key, out rField) && !entry.Value.ConstantValue.Equals(rField.ConstantValue))
+                if (rightMembers.TryGetValue(lEntry.Key, out IFieldSymbol rField) && !lEntry.Value.ConstantValue.Equals(rField.ConstantValue))
                 {
-                    var msg = string.Format(Resources.EnumValuesMustMatch, left.Name, entry.Key, entry.Value.ConstantValue, rField.ConstantValue);
+                    string msg = string.Format(Resources.EnumValuesMustMatch, left.Name, lEntry.Key, lEntry.Value.ConstantValue, rField.ConstantValue);
                     differences.Add(new CompatDifference(DiagnosticIds.EnumValuesMustMatch, msg, DifferenceType.Changed, rField));
                 }
             }
