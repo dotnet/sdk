d6f5fbcdf1686d404e068216d86971bc9875b9c2
smasher164 <akhilindurti@microsoft.com>
2022-05-23 11:57:17 -0700

    fix NRE in tests and address review feedback

--- src/Tests/Microsoft.DotNet.ApiCompatibility.Tests/Rules/EnumValuesMustMatchTests.cs
+++ src/Tests/Microsoft.DotNet.ApiCompatibility.Tests/Rules/EnumValuesMustMatchTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
 
 using System;
 using System.Collections.Generic;
@@ -16,7 +15,7 @@ namespace Microsoft.DotNet.ApiCompatibility.Tests
     public class EnumValuesMustMatchTests
     {
         [Fact]
-        public static void EnumValuesNotMatchingReported()
+        public static void DifferencesReported()
         {
             string leftSyntax = @"
 namespace CompatTests
@@ -46,8 +45,116 @@ namespace CompatTests
             ApiComparer differ = new();
             IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
             CompatDifference[] expected = new[]
-{
+            {
                 new CompatDifference(DiagnosticIds.EnumValuesMustMatch, string.Empty, DifferenceType.Changed, "F:CompatTests.First.A"),
+            };
+            Assert.Equal(expected, differences);
+        }
+
+        [Fact]
+        public static void RemovedEnum()
+        {
+            string leftSyntax = @"
+namespace CompatTests
+{
+  public enum First {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+  }
+  public enum Second {}
+}
+";
+
+            string rightSyntax = @"
+namespace CompatTests
+{
+  public enum First {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+  }
+}
+";
+            IAssemblySymbol left = SymbolFactory.GetAssemblyFromSyntax(leftSyntax);
+            IAssemblySymbol right = SymbolFactory.GetAssemblyFromSyntax(rightSyntax);
+            ApiComparer differ = new();
+            IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
+            Assert.NotEmpty(differences);
+        }
+
+        [Fact]
+        public static void AddedEnum()
+        {
+            string leftSyntax = @"
+namespace CompatTests
+{
+  public enum First {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+  }
+}
+";
+
+            string rightSyntax = @"
+namespace CompatTests
+{
+  public enum First {
+    A = 1,
+    B = 1,
+    C = 2,
+    D = 3,
+  }
+  public enum Second {}
+}
+";
+            IAssemblySymbol left = SymbolFactory.GetAssemblyFromSyntax(leftSyntax);
+            IAssemblySymbol right = SymbolFactory.GetAssemblyFromSyntax(rightSyntax);
+            ApiComparer differ = new();
+            IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
+            Assert.NotEmpty(differences);
+        }
+
+        [Fact]
+        public static void BackingStoreChanged()
+        {
+            string leftSyntax = @"
+namespace CompatTests
+{
+  public enum First: short {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+  }
+}
+";
+
+            string rightSyntax = @"
+namespace CompatTests
+{
+  public enum First: int {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+  }
+}
+";
+            IAssemblySymbol left = SymbolFactory.GetAssemblyFromSyntax(leftSyntax);
+            IAssemblySymbol right = SymbolFactory.GetAssemblyFromSyntax(rightSyntax);
+            ApiComparer differ = new();
+            IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
+            CompatDifference[] expected = new[]
+            {
+                new CompatDifference(DiagnosticIds.EnumValuesMustMatch, string.Empty, DifferenceType.Changed, "F:CompatTests.First.A"),
+                new CompatDifference(DiagnosticIds.EnumValuesMustMatch, string.Empty, DifferenceType.Changed, "F:CompatTests.First.B"),
+                new CompatDifference(DiagnosticIds.EnumValuesMustMatch, string.Empty, DifferenceType.Changed, "F:CompatTests.First.C"),
+                new CompatDifference(DiagnosticIds.EnumValuesMustMatch, string.Empty, DifferenceType.Changed, "F:CompatTests.First.D"),
             };
             Assert.Equal(expected, differences);
         }
