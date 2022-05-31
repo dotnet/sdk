17588c2bcf93750ac856007a3f4c0be8e220b7ee
smasher164 <akhilindurti@microsoft.com>
2022-05-31 15:04:44 -0700

    add rules to catch addition or removal of 'virtual' modifier
    Fixes #18682
    Fixes #18683

---
+++ src/Compatibility/Microsoft.DotNet.ApiCompatibility/Rules/CannotAddOrRemoveVirtualModifier.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.DotNet.ApiCompatibility.Abstractions;
+
+#nullable enable
+
+namespace Microsoft.DotNet.ApiCompatibility.Rules
+{
+    public class CannotAddOrRemoveVirtualModifier : Rule
+    {
+        public override void Initialize(RuleRunnerContext context) => context.RegisterOnMemberSymbolAction(RunOnMemberSymbol);
+
+        private void RunOnMemberSymbol(ISymbol left, ISymbol right, ITypeSymbol leftContainingType, ITypeSymbol rightContainingType, string leftName, string rightName, IList<CompatDifference> differences)
+        {
+            // Members must exist
+            if (left is null || right is null)
+            {
+                return;
+            }
+            // If the left member is virtual, it must not be sealed or inside a sealed type.
+            if (left.IsVirtual)
+            {
+                // If the right member is not virtual, emit a diagnostic
+                // that the virtual modifier cannot be removed.
+                if (!right.IsVirtual)
+                {
+                    differences.Add(new CompatDifference(
+                    DiagnosticIds.CannotRemoveVirtualModifier, string.Format(
+                        Resources.CannotRemoveVirtualModifier, left), DifferenceType.Removed, right));
+                }
+            }
+            // Otherwise if the left member is not virtual, ensure that
+            // we're either in strict mode, or the left member is not
+            // abstract (since it is legal to change abstract to virtual).
+            else if (Settings.StrictMode || !left.IsAbstract)
+            {
+                // If the right member is virtual, emit a diagnostic
+                // that the virtual modifier cannot be added.
+                if (right.IsVirtual)
+                {
+                    differences.Add(new CompatDifference(
+                    DiagnosticIds.CannotAddVirtualModifier, string.Format(
+                        Resources.CannotAddVirtualModifier, right), DifferenceType.Added, right));
+                }
+            }
+        }
+    }
+}
---
+++ src/Tests/Microsoft.DotNet.ApiCompatibility.Tests/Rules/CannotAddOrRemoveVirtualModifierTests.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.DotNet.ApiCompatibility.Abstractions;
+using Xunit;
+
+namespace Microsoft.DotNet.ApiCompatibility.Tests
+{
+    public class CannotAddOrRemoveVirtualModifierTests
+    {
+        [Fact]
+        public static void DifferencesReported()
+        {
+            string leftSyntax = @"
+namespace CompatTests
+{
+  public abstract class First {
+    public virtual void F() {}
+    public void G() {}
+    public abstract void H();
+  }
+}
+";
+            string rightSyntax = @"
+namespace CompatTests
+{
+  public abstract class First {
+    public void F() {}
+    public virtual void G() {}
+    public virtual void H() {}
+  }
+}
+";
+            IAssemblySymbol left = SymbolFactory.GetAssemblyFromSyntax(leftSyntax);
+            IAssemblySymbol right = SymbolFactory.GetAssemblyFromSyntax(rightSyntax);
+            ApiComparer differ = new();
+            IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
+            CompatDifference[] expected = new[]
+            {
+                new CompatDifference(DiagnosticIds.CannotRemoveVirtualModifier, string.Empty, DifferenceType.Removed, "M:CompatTests.First.F"),
+                new CompatDifference(DiagnosticIds.CannotAddVirtualModifier, string.Empty, DifferenceType.Added, "M:CompatTests.First.G"),
+            };
+            Assert.Equal(expected, differences);
+        }
+
+        [Fact]
+        public static void Null()
+        {
+            string leftSyntax = @"
+namespace CompatTests
+{
+  public class First {
+    public virtual void F() {}
+  }
+}
+";
+            string rightSyntax = @"
+namespace CompatTests
+{
+  public class First {
+    public virtual void G() {}
+  }
+}
+";
+            IAssemblySymbol left = SymbolFactory.GetAssemblyFromSyntax(leftSyntax);
+            IAssemblySymbol right = SymbolFactory.GetAssemblyFromSyntax(rightSyntax);
+            ApiComparer differ = new();
+            IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
+            Assert.NotEmpty(differences);
+        }
+
+        [Fact]
+        public static void StrictMode()
+        {
+            string leftSyntax = @"
+namespace CompatTests
+{
+  public abstract class First {
+    public virtual void F() {}
+    public void G() {}
+    public abstract void H();
+  }
+}
+";
+            string rightSyntax = @"
+namespace CompatTests
+{
+  public abstract class First {
+    public void F() {}
+    public virtual void G() {}
+    public virtual void H() {}
+  }
+}
+";
+            IAssemblySymbol left = SymbolFactory.GetAssemblyFromSyntax(leftSyntax);
+            IAssemblySymbol right = SymbolFactory.GetAssemblyFromSyntax(rightSyntax);
+            ApiComparer differ = new();
+            differ.StrictMode = true;
+            IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
+            CompatDifference[] expected = new[]
+            {
+                new CompatDifference(DiagnosticIds.CannotRemoveVirtualModifier, string.Empty, DifferenceType.Removed, "M:CompatTests.First.F"),
+                new CompatDifference(DiagnosticIds.CannotAddVirtualModifier, string.Empty, DifferenceType.Added, "M:CompatTests.First.G"),
+                new CompatDifference(DiagnosticIds.CannotAddVirtualModifier, string.Empty, DifferenceType.Added, "M:CompatTests.First.H"),
+            };
+            Assert.Equal(expected, differences);
+        }
+    }
+}
