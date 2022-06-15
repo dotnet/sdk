c9775c9feba82c96d0e1a5e80f51a97cfec23d4a
Chris Sienkiewicz <chsienki@microsoft.com>
2022-06-15 16:40:28 -0700

    Remove razor sdk files

--- src/RazorSdk/Tool/ExitCodes.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.NET.Sdk.Razor.Tool {
-    public enum ExitCodes {
-        ServerMutexFailure = -1,
-        Success = 0,
-        Failure = 1,
-        FailureRazorError = 2
-    }
-}
-
