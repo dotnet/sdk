63f471e09679d985d29e8c5b6ac7531e81ae8270
Chet Husk <chusk3@gmail.com>
2022-06-21 16:09:30 -0500

    try this way too

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToRunILLink.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToRunILLink.cs
@@ -1185,7 +1185,7 @@ namespace Microsoft.NET.Publish.Tests
 
             var publishCommand = new PublishCommand(testAsset);
             publishCommand.Execute($"/p:RuntimeIdentifier={rid}", "/p:PublishTrimmed=true", "/p:SuppressTrimAnalysisWarnings=false",
-                                    "/p:TreatWarningsAsErrors=true", "/p:ILLinkTreatWarningsAsErrors=false", "/p:NoWarn=IL2026%3BIL2046")
+                                    "/p:TreatWarningsAsErrors=true", "/p:ILLinkTreatWarningsAsErrors=false", "/p:NoWarn=IL2026%3BIL2046%3BNU1505")
                 .Should().Pass()
                 // This warning is produced by both the analyzer and the linker. Ignore it for this test.
                 .And.NotHaveStdOutContaining("warning IL2026")
