75b0f71340b1bc63af228822883ffc355170b8d8
Noah Gilson <noahgilson@microsoft.com>
2022-06-30 12:42:57 -0700

    Use taskkill to kill any hanging processes left by debuggers

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 @echo off
+taskkill /IM "dotnet.exe" /F /T > nul 2> nul
 powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0eng\common\build.ps1""" -build -restore %*"
 exit /b %ErrorLevel%
