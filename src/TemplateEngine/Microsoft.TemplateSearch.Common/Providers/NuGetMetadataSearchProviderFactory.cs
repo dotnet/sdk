19333cd1dfd833a8894687cefb7c8fa1d74c0ec5
Vlada Shubina <vshubina@microsoft.com>
2022-07-21 18:25:28 +0200

    exposed NuGetMetadataSearchProviderFactory component
    Commit migrated from https://github.com/dotnet/templating/commit/e5390c1eb26670f8177d3d7d6ed8820987c2082f

--- src/TemplateEngine/Microsoft.TemplateSearch.Common/Providers/NuGetMetadataSearchProviderFactory.cs
+++ src/TemplateEngine/Microsoft.TemplateSearch.Common/Providers/NuGetMetadataSearchProviderFactory.cs
@@ -8,13 +8,13 @@ using Microsoft.TemplateSearch.Common.Abstractions;
 
 namespace Microsoft.TemplateSearch.Common.Providers
 {
-    internal class NuGetMetadataSearchProviderFactory : ITemplateSearchProviderFactory
+    public sealed class NuGetMetadataSearchProviderFactory : ITemplateSearchProviderFactory
     {
-        public string DisplayName => "NuGet.org";
+        string ITemplateSearchProviderFactory.DisplayName => "NuGet.org";
 
-        public Guid Id => new Guid("6EA368C4-8A56-444C-91D1-55150B296BF2");
+        Guid IIdentifiedComponent.Id => new Guid("6EA368C4-8A56-444C-91D1-55150B296BF2");
 
-        public ITemplateSearchProvider CreateProvider(
+        ITemplateSearchProvider ITemplateSearchProviderFactory.CreateProvider(
             IEngineEnvironmentSettings environmentSettings,
             IReadOnlyDictionary<string, Func<object, object>> additionalDataReaders)
         {
