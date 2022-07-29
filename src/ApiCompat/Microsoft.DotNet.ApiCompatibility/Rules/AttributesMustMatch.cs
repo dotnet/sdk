eb3e0b7e45ce8231bf5ae683b37747f754b226b8
smasher164 <akhilindurti@microsoft.com>
2022-07-29 14:18:26 -0700

    add initial version of attribute change rule

---
+++ src/ApiCompat/Microsoft.DotNet.ApiCompatibility/Rules/AttributesMustMatch.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.DotNet.ApiCompatibility.Abstractions;
+
+namespace Microsoft.DotNet.ApiCompatibility.Rules
+{
+    public class AttributesMustMatch : Rule
+    {
+        public override void Initialize(RuleRunnerContext context)
+        {
+            // TODO: Right now, only RunOnTypeSymbol is executed because RuleRunner does not allow a single rule to map over types and members.
+            // Either change RuleRunner, or add another Rule for the Attributes changing on types.
+            // context.RegisterOnMemberSymbolAction(RunOnMemberSymbol);
+            context.RegisterOnTypeSymbolAction(RunOnTypeSymbol);
+        }
+
+        private void RunOnTypeSymbol(ITypeSymbol left, ITypeSymbol right, string leftName, string rightName, IList<CompatDifference> differences)
+        {
+            reportAttributeDifferences(left, left.GetAttributes(), right.GetAttributes(), differences);
+        }
+
+        private CompatDifference removedDifference(ISymbol containing, AttributeData attr)
+        {
+            string msg = string.Format(Resources.CannotRemoveAttribute, attr, containing);
+            return new CompatDifference(DiagnosticIds.CannotRemoveAttribute, msg, DifferenceType.Removed, attr.AttributeClass);
+        }
+
+        private CompatDifference addedDifference(ISymbol containing, AttributeData attr)
+        {
+            string msg = string.Format(Resources.CannotAddAttribute, attr, containing);
+            return new CompatDifference(DiagnosticIds.CannotAddAttribute, msg, DifferenceType.Added, attr.AttributeClass);
+        }
+
+        private CompatDifference changedDifference(string arg, AttributeData attr)
+        {
+            string msg = string.Format(Resources.CannotChangeAttribute, arg, attr);
+            return new CompatDifference(DiagnosticIds.CannotChangeAttribute, msg, DifferenceType.Changed, attr.AttributeClass);
+        }
+
+        private void reportAttributeDifferences(ISymbol containing, ImmutableArray<AttributeData> leftAttr, ImmutableArray<AttributeData> rightAttr, IList<CompatDifference> differences)
+        {
+            // TODO: Right now, we take the naive approach of asserting that attributes are defined in order.
+            // Is attribute behavior even order-sensitive?
+            // However, this may lead to confusing diagnostics, where an attribute exists but is in a different
+            // position in the list. Maybe change this behavior to treat attributes as sets?
+            // However, even then, we have to take into account differences in arguments passed.
+            var len = Math.Min(leftAttr.Length, rightAttr.Length);
+            for (int i = 0; i < len; i++)
+            {
+                if (!Settings.SymbolComparer.Equals(leftAttr[i].AttributeClass, rightAttr[i].AttributeClass))
+                {
+                    differences.Add(removedDifference(containing, leftAttr[i]));
+                    differences.Add(addedDifference(containing, rightAttr[i]));
+                    continue;
+                }
+                var leftArgs = leftAttr[i].ConstructorArguments;
+                var rightArgs = rightAttr[i].ConstructorArguments;
+                var argLen = Math.Min(leftArgs.Length, rightArgs.Length);
+                for (int j = 0; j < argLen; j++)
+                {
+                    if (!leftArgs[j].Equals(rightArgs[j]))
+                    {
+                        differences.Add(changedDifference(j.ToString(), leftAttr[i]));
+                    }
+                }
+                if (leftArgs.Length > rightArgs.Length)
+                {
+                    for (int j = argLen; j < leftArgs.Length; j++)
+                    {
+                        differences.Add(changedDifference(j.ToString(), leftAttr[i]));
+                    }
+                }
+                else if (leftArgs.Length < rightArgs.Length)
+                {
+                    for (int j = argLen; j < rightArgs.Length; j++)
+                    {
+                        differences.Add(changedDifference(j.ToString(), rightAttr[i]));
+                    }
+                }
+                var leftNamed = leftAttr[i].NamedArguments;
+                var rightNamed = rightAttr[i].NamedArguments;
+                var namedLen = Math.Min(leftNamed.Length, rightNamed.Length);
+                for (int j = 0; j < namedLen; j++)
+                {
+                    if (!leftNamed[j].Equals(rightNamed[j]))
+                    {
+                        differences.Add(changedDifference(leftNamed[j].Key, leftAttr[i]));
+                    }
+                }
+                if (leftNamed.Length > rightNamed.Length)
+                {
+                    for (int j = namedLen; j < leftNamed.Length; j++)
+                    {
+                        // Named argument removed
+                        differences.Add(changedDifference(leftNamed[j].Key, leftAttr[i]));
+                    }
+                }
+                else if (leftNamed.Length < rightNamed.Length)
+                {
+                    for (int j = namedLen; j < rightNamed.Length; j++)
+                    {
+                        // Named argument added
+                        differences.Add(changedDifference(rightNamed[j].Key, rightAttr[i]));
+                    }
+                }
+            }
+            if (leftAttr.Length > rightAttr.Length)
+            {
+                for (int j = len; j < leftAttr.Length; j++)
+                {
+                    differences.Add(removedDifference(containing, leftAttr[j]));
+                }
+            }
+            else if (leftAttr.Length < rightAttr.Length)
+            {
+                for (int j = len; j < rightAttr.Length; j++)
+                {
+                    differences.Add(addedDifference(containing, rightAttr[j]));
+                }
+            }
+        }
+        private void RunOnMemberSymbol(ISymbol left, ISymbol right, ITypeSymbol leftContainingType, ITypeSymbol rightContainingType, string leftName, string rightName, IList<CompatDifference> differences)
+        {
+            reportAttributeDifferences(left, left.GetAttributes(), right.GetAttributes(), differences);
+        }
+    }
+}
---
+++ src/Tests/Microsoft.DotNet.ApiCompatibility.Tests/Rules/AttributesMustMatchTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.DotNet.ApiCompatibility.Abstractions;
+using Microsoft.DotNet.ApiCompatibility.Tests;
+using Xunit;
+
+namespace Microsoft.DotNet.ApiCompatibility.Rules.Tests
+{
+    public class AttributesMustMatchTests
+    {
+        /*
+         * Tests for:
+         * - Types
+         * - Fields
+         * - Properties
+         * - Methods
+         * - Events
+         * - ReturnValues
+         * - Constructors
+         * - Generic Parameters
+         * 
+         * Grouped into:
+         * - Type
+         * - Member
+         */
+        [Fact]
+        public static void EnsureDiagnosticIsReported()
+        {
+            // TODO: Interesting behavior from Roslyn. It attaches an attribute
+            // to a class even if it is used inside it. This is why we have a newline
+            // after the class declaration.
+            string leftSyntax = @"
+namespace CompatTests
+{
+  using System;
+  using System.Runtime.InteropServices;
+  internal class FooAttribute : Attribute { }
+  internal class BarAttribute : Attribute { }
+  [Foo]
+  [Bar]
+  public class First {
+
+    [DllImport(""user32.dll"", SetLastError=true, ExactSpelling=false)]
+    public static extern void G();
+    [Foo]
+    [Bar]
+    public void F() {}
+  }
+}
+";
+            string rightSyntax = @"
+namespace CompatTests
+{
+  using System;
+  using System.Runtime.InteropServices;
+  internal class FooAttribute : Attribute { }
+  internal class BarAttribute : Attribute { }
+  [Bar]
+  public class First {
+
+    [DllImport(""user32.dll"", SetLastError=true, ExactSpelling=true)]
+    public static extern void G();
+    [Bar]
+    public void F() {}
+  }
+}
+";
+            IAssemblySymbol left = SymbolFactory.GetAssemblyFromSyntax(leftSyntax);
+            IAssemblySymbol right = SymbolFactory.GetAssemblyFromSyntax(rightSyntax);
+            ApiComparer differ = new();
+            IEnumerable<CompatDifference> differences = differ.GetDifferences(new[] { left }, new[] { right });
+            CompatDifference[] expected = new CompatDifference[] { };
+            Assert.Equal(expected, differences);
+        }
+    }
+}
