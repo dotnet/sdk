410073980528d715eadccff5d28171c99af9514b
Přemek Vysoký <premek.vysoky@microsoft.com>
2022-08-23 21:06:59 +0200

    Add a VMR initialization (bootstrap) process (#14343)
    Adds the possibility to initialize a fresh VMR off of the current `dotnet/installer` commit.
    Similarly to the tarball generation, the process can be triggered by running:
    ```
    ./build.sh /p:InitializeVMR=true /p:VmrDir=[Path were VMR will be created]
    ```

---
+++ src/VirtualMonoRepo/Tasks/RemoteFactory.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.DotNet.DarcLib;
+using Microsoft.DotNet.DarcLib.Helpers;
+
+namespace Microsoft.DotNet.VirtualMonoRepo.Tasks;
+
+internal class RemoteFactory : IRemoteFactory
+{
+    private readonly IProcessManager _processManager;
+    private readonly string _tmpPath;
+
+    public RemoteFactory(IProcessManager processManager, string tmpPath)
+    {
+        _processManager = processManager;
+        _tmpPath = tmpPath;
+    }
+
+    public Task<IRemote> GetBarOnlyRemoteAsync(Extensions.Logging.ILogger logger)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IRemote> GetRemoteAsync(string repoUrl, Extensions.Logging.ILogger logger)
+    {
+        var githubClient = new DarcLib.GitHubClient(
+            _processManager.GitExecutable,
+            accessToken: null,
+            logger,
+            _tmpPath,
+            cache: null);
+
+        return System.Threading.Tasks.Task.FromResult<IRemote>(new Remote(githubClient, barClient: null, logger));
+    }
+}
---
+++ src/VirtualMonoRepo/Tasks/VirtualMonoRepo_Initialize.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+using Microsoft.DotNet.DarcLib;
+using Microsoft.DotNet.DarcLib.Helpers;
+using Microsoft.DotNet.DarcLib.VirtualMonoRepo;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.DotNet.VirtualMonoRepo.Tasks;
+
+public class VirtualMonoRepo_Initialize : Build.Utilities.Task, ICancelableTask
+{
+    private readonly Lazy<IServiceProvider> _serviceProvider;
+    private readonly CancellationTokenSource _cancellationToken = new();
+
+    [Required]
+    public string Repository { get; set; }
+
+    [Required]
+    public string VmrPath { get; set; }
+
+    [Required]
+    public string TmpPath { get; set; }
+
+    public string Revision { get; set; }
+
+    public VirtualMonoRepo_Initialize()
+    {
+        _serviceProvider = new(CreateServiceProvider);
+    }
+
+    public override bool Execute() => ExecuteAsync().GetAwaiter().GetResult();
+
+    private async Task<bool> ExecuteAsync()
+    {
+        var factory = _serviceProvider.Value.GetRequiredService<IVmrManagerFactory>();
+        var vmrManager = await factory.CreateVmrManager(_serviceProvider.Value, VmrPath, TmpPath);
+        await vmrManager.InitializeVmr(Repository, Revision, false, _cancellationToken.Token);
+        return true;
+    }
+
+    public void Cancel() => _cancellationToken.Cancel();
+
+    private IServiceProvider CreateServiceProvider() => new ServiceCollection()
+        .AddLogging(b => b.AddConsole().AddFilter(l => l >= LogLevel.Information))
+        .AddTransient<IProcessManager>(sp => ActivatorUtilities.CreateInstance<ProcessManager>(sp, "git"))
+        .AddSingleton<ISourceMappingParser, SourceMappingParser>()
+        .AddSingleton<IVmrManagerFactory, VmrManagerFactory>()
+        .AddSingleton<IRemoteFactory>(sp => ActivatorUtilities.CreateInstance<RemoteFactory>(sp, TmpPath))
+        .BuildServiceProvider();
+}
