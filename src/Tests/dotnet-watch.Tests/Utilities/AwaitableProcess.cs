80bed6f5aae6d1243cdaff9d8f69d5416eea46ff
tmat <tomas.matousek@microsoft.com>
2022-11-09 20:16:11 -0800

    Simplify output testing

--- src/Tests/dotnet-watch.Tests/Utilities/AwaitableProcess.cs
+++ src/Tests/dotnet-watch.Tests/Utilities/AwaitableProcess.cs
@@ -9,6 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Microsoft.NET.TestFramework.Commands;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.DotNet.Watcher.Tools
@@ -75,43 +76,17 @@ namespace Microsoft.DotNet.Watcher.Tools
             WriteTestOutput($"{DateTime.Now}: process started: '{_process.StartInfo.FileName} {_process.StartInfo.Arguments}'");
         }
 
-        public Task<string> GetOutputLineAsyncWithConsoleHistoryAsync(string message)
-        {
-            if (_lines.Contains(message))
-            {
-                WriteTestOutput($"Found [msg == '{message}'] in console history.");
-                return Task.FromResult(message);
-            }
-            
-            WriteTestOutput($"Did not find [msg == '{message}'] in console history.");
-            return GetOutputLineAsync(message);
-        }
-
-        public async Task<string> GetOutputLineAsync(string message)
-        {
-            WriteTestOutput($"Waiting for output line [msg == '{message}']");
-            return await GetOutputLineAsync($"[msg == '{message}']", m => string.Equals(m, message, StringComparison.Ordinal), CancellationToken.None);
-        }
-
-        public async Task<string> GetOutputLineStartsWithAsync(string message)
-        {
-            WriteTestOutput($"Waiting for output line [msg.StartsWith('{message}')].");
-            return await GetOutputLineAsync($"[msg.StartsWith('{message}')]", m => m != null && m.StartsWith(message, StringComparison.Ordinal), CancellationToken.None);
-        }
-
-        private async Task<string> GetOutputLineAsync(string predicateName, Predicate<string> predicate, CancellationToken cancellationToken)
+        public async Task<string> GetOutputLineAsync(Predicate<string> predicate)
         {
             while (!_source.Completion.IsCompleted)
             {
-                while (await _source.OutputAvailableAsync(cancellationToken))
+                while (await _source.OutputAvailableAsync(CancellationToken.None))
                 {
-                    var next = await _source.ReceiveAsync(cancellationToken);
-                    _lines.Add(next);
-                    var match = predicate(next);
-                    WriteTestOutput($"{DateTime.Now}: recv: '{next}'. {(match ? "Matches" : "Does not match")} condition '{predicateName}'.");
-                    if (match)
+                    var line = await _source.ReceiveAsync(CancellationToken.None);
+                    _lines.Add(line);
+                    if (predicate(line))
                     {
-                        return next;
+                        return line;
                     }
                 }
             }
@@ -126,9 +101,7 @@ namespace Microsoft.DotNet.Watcher.Tools
             {
                 while (await _source.OutputAvailableAsync(cancellationToken))
                 {
-                    var next = await _source.ReceiveAsync(cancellationToken);
-                    WriteTestOutput($"{DateTime.Now}: recv: '{next}'");
-                    lines.Add(next);
+                    lines.Add(await _source.ReceiveAsync(cancellationToken));
                 }
             }
             return lines;
