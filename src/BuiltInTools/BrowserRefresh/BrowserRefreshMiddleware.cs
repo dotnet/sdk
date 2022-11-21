5367bc02df97b88e063510e5dd333a57ae184329
Mackinnon Buck <mackinnon.buck@gmail.com>
2022-11-21 10:40:37 -0800

    Revert "Hot reload script injection improvements (#27537)"
    This reverts commit 7d4ae9a8a2f401681e3c169b1e1fcc854a545d59.

--- src/BuiltInTools/BrowserRefresh/BrowserRefreshMiddleware.cs
+++ src/BuiltInTools/BrowserRefresh/BrowserRefreshMiddleware.cs
@@ -3,8 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
@@ -28,10 +26,10 @@ namespace Microsoft.AspNetCore.Watch.BrowserRefresh
             // We only need to support this for requests that could be initiated by a browser.
             if (IsBrowserDocumentRequest(context))
             {
-                // Use a custom stream to buffer the response body for rewriting.
-                using var memoryStream = new MemoryStream();
+                // Use a custom StreamWrapper to rewrite output on Write/WriteAsync
+                using var responseStreamWrapper = new ResponseStreamWrapper(context, _logger);
                 var originalBodyFeature = context.Features.Get<IHttpResponseBodyFeature>();
-                context.Features.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(memoryStream));
+                context.Features.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(responseStreamWrapper));
 
                 try
                 {
@@ -42,35 +40,15 @@ namespace Microsoft.AspNetCore.Watch.BrowserRefresh
                     context.Features.Set(originalBodyFeature);
                 }
 
-                if (memoryStream.TryGetBuffer(out var buffer) && buffer.Count > 0)
+                if (responseStreamWrapper.IsHtmlResponse && _logger.IsEnabled(LogLevel.Debug))
                 {
-                    var response = context.Response;
-                    var baseStream = response.Body;
-
-                    if (IsHtmlResponse(response))
+                    if (responseStreamWrapper.ScriptInjectionPerformed)
                     {
-                        Log.SetupResponseForBrowserRefresh(_logger);
-
-                        // Since we're changing the markup content, reset the content-length
-                        response.Headers.ContentLength = null;
-
-                        var scriptInjectionPerformed = await WebSocketScriptInjection.TryInjectLiveReloadScriptAsync(baseStream, buffer);
-                        if (scriptInjectionPerformed)
-                        {
-                            Log.BrowserConfiguredForRefreshes(_logger);
-                        }
-                        else if (response.Headers.TryGetValue(HeaderNames.ContentEncoding, out var contentEncodings))
-                        {
-                            Log.ResponseCompressionDetected(_logger, contentEncodings);
-                        }
-                        else
-                        {
-                            Log.FailedToConfiguredForRefreshes(_logger);
-                        }
+                        Log.BrowserConfiguredForRefreshes(_logger);
                     }
                     else
                     {
-                        await baseStream.WriteAsync(buffer);
+                        Log.FailedToConfiguredForRefreshes(_logger);
                     }
                 }
             }
@@ -114,41 +92,26 @@ namespace Microsoft.AspNetCore.Watch.BrowserRefresh
             return false;
         }
 
-        private bool IsHtmlResponse(HttpResponse response)
-            => (response.StatusCode == StatusCodes.Status200OK || response.StatusCode == StatusCodes.Status500InternalServerError) &&
-                MediaTypeHeaderValue.TryParse(response.ContentType, out var mediaType) &&
-                mediaType.IsSubsetOf(_textHtmlMediaType) &&
-                (!mediaType.Charset.HasValue || mediaType.Charset.Equals("utf-8", StringComparison.OrdinalIgnoreCase));
-
         internal static class Log
         {
             private static readonly Action<ILogger, Exception?> _setupResponseForBrowserRefresh = LoggerMessage.Define(
-                LogLevel.Debug,
+               LogLevel.Debug,
                 new EventId(1, "SetUpResponseForBrowserRefresh"),
-                "Response markup is scheduled to include browser refresh script injection.");
+               "Response markup is scheduled to include browser refresh script injection.");
 
             private static readonly Action<ILogger, Exception?> _browserConfiguredForRefreshes = LoggerMessage.Define(
-                LogLevel.Debug,
+               LogLevel.Debug,
                 new EventId(2, "BrowserConfiguredForRefreshes"),
-                "Response markup was updated to include browser refresh script injection.");
+               "Response markup was updated to include browser refresh script injection.");
 
             private static readonly Action<ILogger, Exception?> _failedToConfigureForRefreshes = LoggerMessage.Define(
-                LogLevel.Warning,
+               LogLevel.Debug,
                 new EventId(3, "FailedToConfiguredForRefreshes"),
-                "Unable to configure browser refresh script injection on the response. " +
-                $"Consider manually adding '{WebSocketScriptInjection.InjectedScript}' to the body of the page.");
-
-            private static readonly Action<ILogger, StringValues, Exception?> _responseCompressionDetected = LoggerMessage.Define<StringValues>(
-                LogLevel.Warning,
-                new EventId(4, "ResponseCompressionDetected"),
-                "Unable to configure browser refresh script injection on the response. " +
-                $"This may have been caused by the response's {HeaderNames.ContentEncoding}: '{{encoding}}'. " +
-                "Consider disabling response compression.");
+               "Unable to configure browser refresh script injection on the response.");
 
             public static void SetupResponseForBrowserRefresh(ILogger logger) => _setupResponseForBrowserRefresh(logger, null);
             public static void BrowserConfiguredForRefreshes(ILogger logger) => _browserConfiguredForRefreshes(logger, null);
             public static void FailedToConfiguredForRefreshes(ILogger logger) => _failedToConfigureForRefreshes(logger, null);
-            public static void ResponseCompressionDetected(ILogger logger, StringValues encoding) => _responseCompressionDetected(logger, encoding, null);
         }
     }
 }
