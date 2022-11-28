3a022ba5f95433f0a0ee6043465b85d168736a99
tmat <tomas.matousek@microsoft.com>
2022-11-28 15:40:56 -0800

    Add base class for all test classes using WatchableApp.

---
+++ src/Tests/dotnet-watch.Tests/Watch/Utilities/DotNetWatchTestBase.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using Microsoft.NET.TestFramework;
+using Xunit.Abstractions;
+
+namespace Microsoft.DotNet.Watcher.Tests;
+
+public abstract class DotNetWatchTestBase : IDisposable
+{
+    internal TestAssetsManager TestAssets { get; }
+    internal WatchableApp App { get; }
+
+    public DotNetWatchTestBase(ITestOutputHelper logger)
+    {
+        App = new WatchableApp(logger);
+        TestAssets = new TestAssetsManager(logger);
+    }
+
+    public ITestOutputHelper Logger => App.Logger;
+
+    public void Dispose()
+    {
+        App.Dispose();
+    }
+}
