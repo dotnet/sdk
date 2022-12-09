490bc874c31227053f75f2a29a0cbc8c8762c4ce
Youssef Victor <youssefvictor00@gmail.com>
2022-12-09 17:02:49 +0200

    Update DisableRuntimeMarshallingTests.cs

--- src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.NetCore.Analyzers/InteropServices/DisableRuntimeMarshallingTests.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/tests/Microsoft.CodeAnalysis.NetAnalyzers.UnitTests/Microsoft.NetCore.Analyzers/InteropServices/DisableRuntimeMarshallingTests.cs
@@ -1682,8 +1682,7 @@ End Structure
         {
             var test = new VerifyVB.Test
             {
-                ReferenceAssemblies = new ReferenceAssemblies("net7.0", new PackageIdentity("Microsoft.NETCore.App.Ref", "7.0.0-preview.1.22075.6"), Path.Combine("ref", "net7.0"))
-                    .WithNuGetConfigFilePath(Path.Combine(Path.GetDirectoryName(typeof(DisableRuntimeMarshallingTests).Assembly.Location), "NuGet.config")),
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net70,
                 TestCode = source,
                 FixedCode = source
             };
