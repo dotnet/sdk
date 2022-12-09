81f2467ff35ab52c3fb8bda6204fd919238877c0
smasher164 <akhilindurti@microsoft.com>
2022-12-08 18:48:04 -0800

    revert all changes to src/Tasks

--- src/Tasks/Common/Logger.cs
+++ src/Tasks/Common/Logger.cs
@@ -107,7 +107,7 @@ namespace Microsoft.NET.Build.Tasks
             DebugThrowMissingOrIncorrectCode(code, format, level);
 
             return new Message(
-                level,
+                level, 
                 text: string.Format(format, args),
                 code: code);
         }
