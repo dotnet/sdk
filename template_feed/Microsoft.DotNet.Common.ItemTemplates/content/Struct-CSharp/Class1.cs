0f823a1708ebbab15127f289e9b6c4e3bfa827b5
YuliiaKovalova <ykovalova@microsoft.com>
2022-12-13 14:19:43 +0100

    add struct template + localization

---
+++ template_feed/Microsoft.DotNet.Common.ItemTemplates/content/Struct-CSharp/Class1.cs
@@ -0,0 +1,19 @@
+﻿#if (!csharpFeature_ImplicitUsings)
+using System;
+
+#endif
+#if (csharpFeature_FileScopedNamespaces)
+namespace Company.ClassLibrary1;
+public struct Class1
+{
+
+}
+#else
+namespace Company.ClassLibrary1
+{
+    public struct Class1
+    {
+
+    }
+}
+#endif
