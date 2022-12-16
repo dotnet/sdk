68d9f7405ded3e6006f1db59ea7e6a6fc789c6aa
Michael Simons <msimons@microsoft.com>
2022-12-16 17:33:58 -0600

    re-enable dotnet-watch tests (#15148)

--- src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/DotNetWatchTests.cs
+++ src/SourceBuild/tarball/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/DotNetWatchTests.cs
@@ -13,7 +13,7 @@ public class DotNetWatchTests : SmokeTests
 {
     public DotNetWatchTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
 
-    //[Fact] - Renable with https://github.com/dotnet/source-build/issues/3123
+    [Fact]
     public void WatchTests()
     {
         string projectDirectory = DotNetHelper.ExecuteNew(DotNetTemplate.Console.GetName(), nameof(DotNetWatchTests));
