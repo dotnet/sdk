70659385155a789dfc6d70e3f4fad27d4211fb60
YuliiaKovalova <ykovalova@microsoft.com>
2022-12-21 12:30:28 +0100

    add class templates

---
+++ template_feed/Microsoft.DotNet.Common.ItemTemplates/content/Record-CSharp/Class1.cs
@@ -0,0 +1,25 @@
+﻿#if (csharpFeature_RecordClass)
+namespace Company.ClassLibrary1;
+public record class Class1
+{
+
+}
+#elseif (csharpFeature_Record)
+namespace Company.ClassLibrary1
+{
+    public record Class1
+    {
+
+    }
+}
+#else
+namespace Company.ClassLibrary1
+{
+    //Record was added in C# 9 and later, so Class was used instead. 
+    //See more info: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/record
+    public class Class1
+    {
+
+    }
+}
+#endif
