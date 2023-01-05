f1a11017250e831331300b3d65312e44507d5d1a
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2023-01-05 11:10:02 +0100

    fix the floating issue with tests run in VerifySettingFixture..ctor
    Commit migrated from https://github.com/dotnet/templating/commit/5c4ba2495410e07d9f36518ca43eb44b80192704

--- test/TemplateEngine/Microsoft.TemplateEngine.IDE.IntegrationTests/VerifySettingsFixture.cs
+++ test/TemplateEngine/Microsoft.TemplateEngine.IDE.IntegrationTests/VerifySettingsFixture.cs
@@ -7,24 +7,21 @@ namespace Microsoft.TemplateEngine.IDE.IntegrationTests
 {
     public class VerifySettingsFixture : IDisposable
     {
-        private static bool s_called;
-
-        public VerifySettingsFixture()
+        private static readonly Lazy<bool> Called = new Lazy<bool>(() =>
         {
-            if (s_called)
-            {
-                return;
-            }
-            s_called = true;
             DerivePathInfo(
-                (_, _, type, method) => new(
-                    directory: "Approvals",
-                    typeName: type.Name,
-                    methodName: method.Name));
+               (_, _, type, method) => new(
+                   directory: "Approvals",
+                   typeName: type.Name,
+                   methodName: method.Name));
 
             // Customize diff output of verifier
             VerifyDiffPlex.Initialize(OutputType.Compact);
-        }
+
+            return true;
+        });
+
+        public VerifySettingsFixture() => _ = Called.Value;
 
         public void Dispose() { }
     }
