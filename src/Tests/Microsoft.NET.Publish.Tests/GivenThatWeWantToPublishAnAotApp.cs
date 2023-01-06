0d8bfa67357d8e10e8ea567532b45e09c59273b8
Eric Erhardt <eric.erhardt@microsoft.com>
2023-01-06 12:31:12 -0600

    Respond to PR feedback.
    Fix test to build correctly.

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAnAotApp.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAnAotApp.cs
@@ -17,6 +17,7 @@ using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
 using Microsoft.NET.TestFramework.ProjectConstruction;
+using Newtonsoft.Json.Linq;
 using Xunit;
 using Xunit.Abstractions;
 using static Microsoft.NET.Publish.Tests.PublishTestUtils;
@@ -707,30 +708,27 @@ namespace Microsoft.NET.Publish.Tests
 
         [RequiresMSBuildVersionTheory("17.0.0.32901")]
         [InlineData(ToolsetInfo.CurrentTargetFramework)]
-        public void It_builds_with_dynamiccodesupport_false(string targetFramework)
+        public void It_builds_with_dynamiccodesupport_false_when_publishaot_true(string targetFramework)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                var projectName = "DynamicCodeSupportFalseApp";
-                var testProject = CreateHelloWorldTestProject(targetFramework, projectName, true);
-                testProject.AdditionalProperties["PublishAot"] = "true";
-                var testAsset = _testAssetsManager.CreateTestProject(testProject);
+            var projectName = "DynamicCodeSupportFalseApp";
+            var testProject = CreateHelloWorldTestProject(targetFramework, projectName, true);
+            testProject.AdditionalProperties["PublishAot"] = "true";
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
 
-                var buildCommand = new DotnetBuildCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name));
-                buildCommand
-                    .Execute()
-                    .Should()
-                    .Pass();
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand
+                .Execute()
+                .Should()
+                .Pass();
 
-                string outputDirectory = buildCommand.GetOutputDirectory(targetFramework: targetFramework).FullName;
-                string runtimeConfigFile = Path.Combine(outputDirectory, $"{projectName}.runtimeconfig.json");
-                string runtimeConfigContents = File.ReadAllText(runtimeConfigFile);
+            string outputDirectory = buildCommand.GetOutputDirectory(targetFramework: targetFramework).FullName;
+            string runtimeConfigFile = Path.Combine(outputDirectory, $"{projectName}.runtimeconfig.json");
+            string runtimeConfigContents = File.ReadAllText(runtimeConfigFile);
 
-                JObject runtimeConfig = JObject.Parse(runtimeConfigContents);
-                JToken configProperties = runtimeConfig["runtimeOptions"]["configProperties"];
-                configProperties["System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported"].Value<bool>()
-                    .Should().BeFalse();
-            }
+            JObject runtimeConfig = JObject.Parse(runtimeConfigContents);
+            JToken configProperties = runtimeConfig["runtimeOptions"]["configProperties"];
+            configProperties["System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported"].Value<bool>()
+                .Should().BeFalse();
         }
 
         private void CheckIlcVersions(string projectPath, string targetFramework, string rid, string expectedVersion)
