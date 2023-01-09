99855b05a9de0ce1ac31a53b81e4e5707551686a
Frederik Gheysels <frederik.gheysels@codit.eu>
2023-01-09 16:52:56 +0100

    Initialize ModifyJsonFile post action

--- src/Cli/dotnet/commands/dotnet-new/NewCommandParser.cs
+++ src/Cli/dotnet/commands/dotnet-new/NewCommandParser.cs
@@ -133,6 +133,7 @@ namespace Microsoft.DotNet.Cli
                 (typeof(IPostActionProcessor), new DotnetAddPostActionProcessor()),
                 (typeof(IPostActionProcessor), new DotnetSlnPostActionProcessor()),
                 (typeof(IPostActionProcessor), new DotnetRestorePostActionProcessor()),
+                (typeof(IPostActionProcessor), new DotnetModifyJsonPostActionProcessor())
             });
             if (!disableSdkTemplates)
             {
