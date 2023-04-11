122814603fb1bec4e7b0b111960d5bb71488f48d
Levi Broderick <levib@microsoft.com>
2023-04-11 12:22:17 -0700

    Restore whitespace, remove unneeded usings

--- src/Cli/Microsoft.DotNet.Cli.Utils/FilePermissionSettingException.cs
+++ src/Cli/Microsoft.DotNet.Cli.Utils/FilePermissionSettingException.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Runtime.Serialization;
 
 namespace Microsoft.DotNet.Cli.Utils
 {
@@ -19,5 +18,5 @@ namespace Microsoft.DotNet.Cli.Utils
         public FilePermissionSettingException(string message, Exception innerException) : base(message, innerException)
         {
         }
-   }
+    }
 }
--- src/Microsoft.Win32.Msi/WindowsInstallerException.cs
+++ src/Microsoft.Win32.Msi/WindowsInstallerException.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.ComponentModel;
-using System.Runtime.Serialization;
 
 namespace Microsoft.Win32.Msi
 {
