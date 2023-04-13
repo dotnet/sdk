0279e052d4284168a63200b7b713ca416bf25c90
Gang Wang <v-gaw@microsoft.com>
2023-04-13 18:25:47 +0800

    Add the test that verifies generated join symbol with Ref to derived symbol can be evaluated correctly (dotnet/templating#6378)
    Commit migrated from https://github.com/dotnet/templating/commit/9ec5bcabd3e95afc47d3ca6662f2a60d1aeec3b3

---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/SnapshotTests.cs
@@ -0,0 +1,77 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#if NET
+
+using Microsoft.Extensions.Logging;
+using Microsoft.TemplateEngine.Authoring.TemplateApiVerifier;
+using Microsoft.TemplateEngine.Authoring.TemplateVerifier;
+using Microsoft.TemplateEngine.TestHelper;
+using Microsoft.TemplateEngine.Tests;
+using Xunit.Abstractions;
+
+namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests
+{
+    [Collection("Verify Tests")]
+    public class SnapshotTests : TestBase
+    {
+        private readonly ILogger _log;
+
+        public SnapshotTests(ITestOutputHelper log)
+        {
+            _log = new XunitLoggerProvider(log).CreateLogger("TestRun");
+        }
+
+        [Fact]
+        public Task TestGeneratedSymbolWithRefToDerivedSymbol()
+        {
+            string templateLocation = GetTestTemplateLocation("TemplateWithGeneratedSymbolWithRefToDerivedSymbol");
+            var templateParams = new Dictionary<string, string?>()
+            {
+                { "NugetToolName", "nuget" }
+            };
+            string workingDir = TestUtils.CreateTemporaryFolder();
+
+            TemplateVerifierOptions options =
+                new TemplateVerifierOptions(templateName: "TestAssets.TemplateGenSymWithRefToDerivedSym")
+                {
+                    TemplatePath = templateLocation,
+                    OutputDirectory = workingDir,
+                    DoNotAppendTemplateArgsToScenarioName = true,
+                    DoNotPrependTemplateNameToScenarioName = true,
+                    SnapshotsDirectory = "Approvals"
+                }
+                .WithInstantiationThroughTemplateCreatorApi(templateParams);
+
+            VerificationEngine engine = new VerificationEngine(_log);
+            return engine.Execute(options);
+        }
+
+        [Fact]
+        public Task TestGeneratedSymbolWithRefToDerivedSymbol_DifferentOrder()
+        {
+            string templateLocation = GetTestTemplateLocation("TemplateWithGenSymbolWithRefToDerivedSymbol_DifferentOrder");
+            var templateParams = new Dictionary<string, string?>()
+            {
+                { "NugetToolName", "nuget" }
+            };
+            string workingDir = TestUtils.CreateTemporaryFolder();
+
+            TemplateVerifierOptions options =
+                new TemplateVerifierOptions(templateName: "TestAssets.TemplateGenSymWithRefToDerivedSym_DiffOrder")
+                {
+                    TemplatePath = templateLocation,
+                    OutputDirectory = workingDir,
+                    DoNotAppendTemplateArgsToScenarioName = true,
+                    DoNotPrependTemplateNameToScenarioName = true,
+                    SnapshotsDirectory = "Approvals"
+                }
+                .WithInstantiationThroughTemplateCreatorApi(templateParams);
+
+            VerificationEngine engine = new VerificationEngine(_log);
+            return engine.Execute(options);
+        }
+    }
+}
+
+#endif
---
+++ test/TemplateEngine/Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests/VerifySettingsFixture.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using VerifyTests.DiffPlex;
+
+namespace Microsoft.TemplateEngine.Orchestrator.RunnableProjects.UnitTests
+{
+    public class VerifySettingsFixture : IDisposable
+    {
+        private static bool s_called;
+
+        public VerifySettingsFixture()
+        {
+            if (s_called)
+            {
+                return;
+            }
+            s_called = true;
+            DerivePathInfo(
+                (_, _, type, method) => new(
+                    directory: "Approvals",
+                    typeName: type.Name,
+                    methodName: method.Name));
+
+            // Customize diff output of verifier
+            VerifyDiffPlex.Initialize(OutputType.Compact);
+        }
+
+        public void Dispose() { }
+    }
+}
