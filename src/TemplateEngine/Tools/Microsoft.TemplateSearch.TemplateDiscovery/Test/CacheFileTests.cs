110e202cf97d1f7e17ec73914ff5199f8d56228b
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2023-05-29 17:57:17 +0200

    enable 7.0.300 for cacheUpdate test (dotnet/templating#6637)
    Commit migrated from https://github.com/dotnet/templating/commit/20b4a5bc3108526102fb0ae9f58429131acc9906

--- src/TemplateEngine/Tools/Microsoft.TemplateSearch.TemplateDiscovery/Test/CacheFileTests.cs
+++ src/TemplateEngine/Tools/Microsoft.TemplateSearch.TemplateDiscovery/Test/CacheFileTests.cs
@@ -83,15 +83,14 @@ namespace Microsoft.TemplateSearch.TemplateDiscovery.Test
             Console.WriteLine($"Running tests on .NET {sdkVersion} for: {metadataPath}.");
             CanSearch(workingDirectory, metadataPath);
 
-            // TODO enable then stable version is available
             //7.0.300
-            //sdkVersion = "7.0.300";
-            //workingDirectory = TestUtils.CreateTemporaryFolder(sdkVersion);
-            //UseSdkVersion(workingDirectory, requestedSdkVersion: sdkVersion, resolvedVersionPattern: "7.0.3", rollForward: "latestPatch");
-            //Console.WriteLine($"Running tests on .NET {sdkVersion} for: {legacyMetadataPath}.");
-            //CanSearch(workingDirectory, legacyMetadataPath);
-            //Console.WriteLine($"Running tests on .NET {sdkVersion} for: {metadataPath}.");
-            //CanSearch(workingDirectory, metadataPath);
+            sdkVersion = "7.0.300";
+            workingDirectory = TestUtils.CreateTemporaryFolder(sdkVersion);
+            UseSdkVersion(workingDirectory, requestedSdkVersion: sdkVersion, resolvedVersionPattern: "7.0.3", rollForward: "latestPatch");
+            Console.WriteLine($"Running tests on .NET {sdkVersion} for: {legacyMetadataPath}.");
+            CanSearch(workingDirectory, legacyMetadataPath);
+            Console.WriteLine($"Running tests on .NET {sdkVersion} for: {metadataPath}.");
+            CanSearch(workingDirectory, metadataPath);
 
             //latest
             workingDirectory = TestUtils.CreateTemporaryFolder("latest");
