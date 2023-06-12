19bddb850b0a9227eb9fda3596edc8a5131f5a72
Adam Sitnik <adam.sitnik@gmail.com>
2023-06-12 18:29:50 +0200

    Simplify benchmarks

--- perf/FormattedFiles.cs
+++ perf/FormattedFiles.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Logging;
 
 namespace Microsoft.CodeAnalysis.Tools.Perf.Micro
 {
-    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
     public class FormattedFiles
     {
         private const string UnformattedProjectPath = "tests/projects/for_code_formatter/unformatted_project/";
