1e62b073fc3fd09bf96019b1fc3256b14fb2b78f
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2023-06-19 21:26:54 -0700

    Analyzer and fixer that recommend case insensitive string comparison (#6662)
    * Resources and documentation
    
    * Analyzer
    
    * Fixer
    
    * Base test class
    
    * C# tests
    
    * VisualBasic tests
    
    * Address suggestion: Inline localizable strings that are used only once.
    
    * Address suggestion: Prefer GetSpecialType for string.
    
    * Use explicit.
    
    * Do not pass argument to diagnostic that does not need it.
    
    * Address suggestion: Use Length instead of Count()
    
    * Address suggestion: Improve name of caseChangingMethodName
    
    * Address suggestion: Remove token cancellation handling code
    
    * Address suggestion: Remove unused cancellation token argument.
    
    * Address suggestion: Add missing fixer attribute, seal class
    
    * Analyzer: Remove unused using.
    
    * Address suggestion: Use better test pattern. Simplify tests.
    
    * More tests
    
    * Address suggestion: Await tests.
    
    * Fix spacing warning
    
    * Handle all 3 IndexOf overloads that can be converted to the StringComparison overload.
    
    * Address suggestion: Walk up parenthesized operations
    
    * Add "No diagnostic" tests
    
    * Do not fix CompareTo, only suggest.
    
    * Remove unused CompareTo fixer code and fix some warnings.
    
    * Add missing entry for second variant of diagnostic rule message in "RulesMissingDocumentation.md".
    
    * Revert md and sarif changes to fix CI
    
    * Address suggestion: Use WalkUpParentheses
    
    * Add short string for the codefix CodeAction title
    
    * Consume codefix CodeAction short string in title
    
    * Fix CI
    
    * Remove some unnecessary fixer code
    
    * Use better equivalenceKey
    
    * Diagnose inverted pattern
    
    * Tests for named arguments
    
    * Change resources to be able to address named parameters
    
    * src: Address named parameters
    
    * tests: Adjust tests to verify named parameters
    
    * Move ExportCodeFixProvider and Shared attributes to derived types.
    
    * public sealed fixers
    
    * Improved resource description mentioning allocation. Use single quotes, minor spacing.
    
    * Remove commented code
    
    * Update xlfs
    
    * Remove commented code
    
    * Rename to GetNewArguments
    
    * Fix build

---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Performance/RecommendCaseInsensitiveStringComparison.Analyzer.cs
@@ -0,0 +1,238 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Performance
+{
+    using static MicrosoftNetCoreAnalyzersResources;
+
+    /// <summary>
+    /// CA1862: Prefer the StringComparison method overloads to perform case-insensitive string comparisons.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class RecommendCaseInsensitiveStringComparisonAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string RuleId = "CA1862";
+
+        internal const string StringComparisonInvariantCultureIgnoreCaseName = "InvariantCultureIgnoreCase";
+        internal const string StringComparisonCurrentCultureIgnoreCaseName = "CurrentCultureIgnoreCase";
+        internal const string StringToLowerMethodName = "ToLower";
+        internal const string StringToUpperMethodName = "ToUpper";
+        internal const string StringToLowerInvariantMethodName = "ToLowerInvariant";
+        internal const string StringToUpperInvariantMethodName = "ToUpperInvariant";
+        internal const string StringContainsMethodName = "Contains";
+        internal const string StringIndexOfMethodName = "IndexOf";
+        internal const string StringStartsWithMethodName = "StartsWith";
+        internal const string StringCompareToMethodName = "CompareTo";
+        internal const string StringComparerCompareMethodName = "Compare";
+        internal const string StringParameterName = "value";
+        internal const string StringComparisonParameterName = "comparisonType";
+
+        internal static readonly DiagnosticDescriptor RecommendCaseInsensitiveStringComparisonRule = DiagnosticDescriptorHelper.Create(
+            RuleId,
+            CreateLocalizableResourceString(nameof(RecommendCaseInsensitiveStringComparisonTitle)),
+            CreateLocalizableResourceString(nameof(RecommendCaseInsensitiveStringComparisonMessage)),
+            DiagnosticCategory.Performance,
+            RuleLevel.IdeSuggestion,
+            CreateLocalizableResourceString(nameof(RecommendCaseInsensitiveStringComparisonDescription)),
+            isPortedFxCopRule: false,
+            isDataflowRule: false);
+
+        internal static readonly DiagnosticDescriptor RecommendCaseInsensitiveStringComparerRule = DiagnosticDescriptorHelper.Create(
+            RuleId,
+            CreateLocalizableResourceString(nameof(RecommendCaseInsensitiveStringComparerTitle)),
+            CreateLocalizableResourceString(nameof(RecommendCaseInsensitiveStringComparerMessage)),
+            DiagnosticCategory.Performance,
+            RuleLevel.IdeSuggestion,
+            CreateLocalizableResourceString(nameof(RecommendCaseInsensitiveStringComparerDescription)),
+            isPortedFxCopRule: false,
+            isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
+            RecommendCaseInsensitiveStringComparisonRule, RecommendCaseInsensitiveStringComparerRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterCompilationStartAction(AnalyzeCompilationStart);
+        }
+
+        private void AnalyzeCompilationStart(CompilationStartAnalysisContext context)
+        {
+            // Retrieve the essential types: string, StringComparison, StringComparer
+
+            INamedTypeSymbol stringType = context.Compilation.GetSpecialType(SpecialType.System_String);
+            INamedTypeSymbol int32Type = context.Compilation.GetSpecialType(SpecialType.System_Int32);
+
+            if (!context.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemStringComparison, out INamedTypeSymbol? stringComparisonType))
+            {
+                return;
+            }
+
+            if (!context.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemStringComparer, out INamedTypeSymbol? stringComparerType))
+            {
+                return;
+            }
+
+            // Retrieve the offending parameterless methods: ToLower, ToLowerInvariant, ToUpper, ToUpperInvariant
+
+            IMethodSymbol? toLowerParameterlessMethod = stringType.GetMembers(StringToLowerMethodName).OfType<IMethodSymbol>().GetFirstOrDefaultMemberWithParameterInfos();
+            if (toLowerParameterlessMethod == null)
+            {
+                return;
+            }
+
+            IMethodSymbol? toLowerInvariantParameterlessMethod = stringType.GetMembers(StringToLowerInvariantMethodName).OfType<IMethodSymbol>().GetFirstOrDefaultMemberWithParameterInfos();
+            if (toLowerInvariantParameterlessMethod == null)
+            {
+                return;
+            }
+
+            IMethodSymbol? toUpperParameterlessMethod = stringType.GetMembers(StringToUpperMethodName).OfType<IMethodSymbol>().GetFirstOrDefaultMemberWithParameterInfos();
+            if (toUpperParameterlessMethod == null)
+            {
+                return;
+            }
+
+            IMethodSymbol? toUpperInvariantParameterlessMethod = stringType.GetMembers(StringToUpperInvariantMethodName).OfType<IMethodSymbol>().GetFirstOrDefaultMemberWithParameterInfos();
+            if (toUpperInvariantParameterlessMethod == null)
+            {
+                return;
+            }
+
+            // Create the different expected parameter combinations
+
+            ParameterInfo[] stringParameter = new[]
+            {
+                ParameterInfo.GetParameterInfo(stringType)
+            };
+
+            ParameterInfo[] stringInt32Parameters = new[]
+            {
+                ParameterInfo.GetParameterInfo(stringType),
+                ParameterInfo.GetParameterInfo(int32Type)
+            };
+
+            ParameterInfo[] stringInt32Int32Parameters = new[]
+            {
+                ParameterInfo.GetParameterInfo(stringType),
+                ParameterInfo.GetParameterInfo(int32Type),
+                ParameterInfo.GetParameterInfo(int32Type)
+            };
+
+            // Retrieve the diagnosable string overload methods: Contains, IndexOf (3 overloads), StartsWith, CompareTo
+
+            // Contains(string)
+            IMethodSymbol? containsStringMethod = stringType.GetMembers(StringContainsMethodName).OfType<IMethodSymbol>().GetFirstOrDefaultMemberWithParameterInfos(stringParameter);
+            if (containsStringMethod == null)
+            {
+                return;
+            }
+
+            // StartsWith(string)
+            IMethodSymbol? startsWithStringMethod = stringType.GetMembers(StringStartsWithMethodName).OfType<IMethodSymbol>().GetFirstOrDefaultMemberWithParameterInfos(stringParameter);
+            if (startsWithStringMethod == null)
+            {
+                return;
+            }
+
+            IEnumerable<IMethodSymbol> indexOfMethods = stringType.GetMembers(StringIndexOfMethodName).OfType<IMethodSymbol>();
+
+            // IndexOf(string)
+            IMethodSymbol? indexOfStringMethod = indexOfMethods.GetFirstOrDefaultMemberWithParameterInfos(stringParameter);
+            if (indexOfStringMethod == null)
+            {
+                return;
+            }
+
+            // IndexOf(string, int startIndex)
+            IMethodSymbol? indexOfStringInt32Method = indexOfMethods.GetFirstOrDefaultMemberWithParameterInfos(stringInt32Parameters);
+            if (indexOfStringInt32Method == null)
+            {
+                return;
+            }
+
+            // IndexOf(string, int startIndex, int count)
+            IMethodSymbol? indexOfStringInt32Int32Method = indexOfMethods.GetFirstOrDefaultMemberWithParameterInfos(stringInt32Int32Parameters);
+            if (indexOfStringInt32Int32Method == null)
+            {
+                return;
+            }
+
+            // CompareTo(string)
+            IMethodSymbol? compareToStringMethod = stringType.GetMembers(StringCompareToMethodName).OfType<IMethodSymbol>().GetFirstOrDefaultMemberWithParameterInfos(stringParameter);
+            if (compareToStringMethod == null)
+            {
+                return;
+            }
+
+            // Retrieve the StringComparer properties that need to be flagged: CurrentCultureIgnoreCase, InvariantCultureIgnoreCase
+
+            IEnumerable<IPropertySymbol> ccicPropertyGroup = stringComparerType.GetMembers(StringComparisonCurrentCultureIgnoreCaseName).OfType<IPropertySymbol>();
+            if (!ccicPropertyGroup.Any())
+            {
+                return;
+            }
+
+            IEnumerable<IPropertySymbol> icicPropertyGroup = stringComparerType.GetMembers(StringComparisonInvariantCultureIgnoreCaseName).OfType<IPropertySymbol>();
+            if (!icicPropertyGroup.Any())
+            {
+                return;
+            }
+
+            context.RegisterOperationAction(context =>
+            {
+                IInvocationOperation caseChangingInvocation = (IInvocationOperation)context.Operation;
+                IMethodSymbol caseChangingMethod = caseChangingInvocation.TargetMethod;
+
+                if (!caseChangingMethod.Equals(toLowerParameterlessMethod) &&
+                    !caseChangingMethod.Equals(toLowerInvariantParameterlessMethod) &&
+                    !caseChangingMethod.Equals(toUpperParameterlessMethod) &&
+                    !caseChangingMethod.Equals(toUpperInvariantParameterlessMethod))
+                {
+                    return;
+                }
+
+                // Ignore parenthesized operations
+                IOperation? ancestor = caseChangingInvocation.WalkUpParentheses().WalkUpConversion().Parent;
+
+                IInvocationOperation diagnosableInvocation;
+                if (ancestor is IInvocationOperation invocationAncestor)
+                {
+                    diagnosableInvocation = invocationAncestor;
+                }
+                else if (ancestor is IArgumentOperation argumentAncestor && argumentAncestor.Parent is IInvocationOperation argumentInvocationAncestor)
+                {
+                    diagnosableInvocation = argumentInvocationAncestor;
+                }
+                else
+                {
+                    return;
+                }
+
+                IMethodSymbol diagnosableMethod = diagnosableInvocation.TargetMethod;
+
+                if (diagnosableMethod.Equals(containsStringMethod) ||
+                    diagnosableMethod.Equals(startsWithStringMethod) ||
+                    diagnosableMethod.Equals(indexOfStringMethod) ||
+                    diagnosableMethod.Equals(indexOfStringInt32Method) ||
+                    diagnosableMethod.Equals(indexOfStringInt32Int32Method))
+                {
+                    context.ReportDiagnostic(diagnosableInvocation.CreateDiagnostic(RecommendCaseInsensitiveStringComparisonRule, diagnosableMethod.Name));
+                }
+                else if (diagnosableMethod.Equals(compareToStringMethod))
+                {
+                    context.ReportDiagnostic(diagnosableInvocation.CreateDiagnostic(RecommendCaseInsensitiveStringComparerRule));
+                }
+            }, OperationKind.Invocation);
+        }
+    }
+}
---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Performance/RecommendCaseInsensitiveStringComparison.Fixer.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Microsoft.NetCore.Analyzers.Performance
+{
+    using RCISCAnalyzer = RecommendCaseInsensitiveStringComparisonAnalyzer;
+
+    /// <summary>
+    /// CA1862: Prefer the StringComparison method overloads to perform case-insensitive string comparisons.
+    /// </summary>
+    public abstract class RecommendCaseInsensitiveStringComparisonFixer : CodeFixProvider
+    {
+        protected abstract List<SyntaxNode> GetNewArguments(SyntaxGenerator generator, IInvocationOperation mainInvocationOperation,
+            INamedTypeSymbol stringComparisonType, out SyntaxNode? mainInvocationInstance);
+
+        public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(RCISCAnalyzer.RuleId);
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            CancellationToken ct = context.CancellationToken;
+
+            Document doc = context.Document;
+
+            SyntaxNode root = await doc.GetSyntaxRootAsync(ct).ConfigureAwait(false);
+
+            if (root.FindNode(context.Span, getInnermostNodeForTie: true) is not SyntaxNode node)
+            {
+                return;
+            }
+
+            SemanticModel model = await doc.GetSemanticModelAsync(ct).ConfigureAwait(false);
+
+            if (model.GetOperation(node, ct) is not IInvocationOperation invocation)
+            {
+                return;
+            }
+
+            if (model.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemStringComparison)
+                is not INamedTypeSymbol stringComparisonType)
+            {
+                return;
+            }
+
+            Task<Document> createChangedDocument(CancellationToken _) => FixInvocationAsync(doc, root,
+                invocation, stringComparisonType, invocation.TargetMethod.Name);
+
+            string title = string.Format(
+                MicrosoftNetCoreAnalyzersResources.RecommendCaseInsensitiveStringComparerStringComparisonCodeFixTitle, invocation.TargetMethod.Name);
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title,
+                    createChangedDocument,
+                    equivalenceKey: nameof(MicrosoftNetCoreAnalyzersResources.RecommendCaseInsensitiveStringComparerStringComparisonCodeFixTitle)),
+                context.Diagnostics);
+        }
+
+        private Task<Document> FixInvocationAsync(Document doc, SyntaxNode root, IInvocationOperation mainInvocation,
+            INamedTypeSymbol stringComparisonType, string diagnosableMethodName)
+        {
+            // Defensive check: The max number of arguments is held by IndexOf
+            Debug.Assert(mainInvocation.Arguments.Length <= 3);
+
+            // For the Diagnosable methods Contains(string) and StartsWith(string)
+            // If we have this code ('a' and 'b' are string instances):
+            //     A) a.CaseChanging().Diagnosable(b);
+            //     B) a.Diagnosable(b.CaseChanging());
+            // We want to convert any of them to:
+            //     a.Diagnosable(b, StringComparison.DesiredCultureDesiredCase);
+
+            // For IndexOf we have 3 options:
+            //    A.1) a.CaseChanging().IndexOf(b)
+            //    A.2) a.IndexOf(b.CaseChanging())
+            //    B.1) a.CaseChanging().IndexOf(b, startIndex: n)
+            //    B.2) a.IndexOf(b.CaseChanging(), startIndex: n)
+            //    C.1) a.CaseChanging().IndexOf(b, startIndex: n, count: m)
+            //    C.2) a.IndexOf(b.CaseChanging(), startIndex: n, count: m)
+            // We want to convert them to:
+            //    A) a.IndexOf(b, StringComparison.Desired)
+            //    B) a.IndexOf(b, startIndex: n, StringComparison.Desired)
+            //    C) a.IndexOf(b, startIndex: n, count: m, StringComparison.Desired)
+
+            SyntaxGenerator generator = SyntaxGenerator.GetGenerator(doc);
+
+            // Defensive check: Should not fix string.CompareTo
+            Debug.Assert(diagnosableMethodName is
+                RCISCAnalyzer.StringContainsMethodName or
+                RCISCAnalyzer.StringIndexOfMethodName or
+                RCISCAnalyzer.StringStartsWithMethodName);
+
+            List<SyntaxNode> newArguments = GetNewArguments(generator, mainInvocation, stringComparisonType, out SyntaxNode? mainInvocationInstance);
+
+            SyntaxNode stringMemberAccessExpression = generator.MemberAccessExpression(mainInvocationInstance, mainInvocation.TargetMethod.Name);
+
+            SyntaxNode newInvocation = generator.InvocationExpression(stringMemberAccessExpression, newArguments).WithTriviaFrom(mainInvocation.Syntax);
+
+            SyntaxNode newRoot = generator.ReplaceNode(root, mainInvocation.Syntax, newInvocation);
+            return Task.FromResult(doc.WithSyntaxRoot(newRoot));
+        }
+
+        protected static SyntaxNode GetNewStringComparisonArgument(SyntaxGenerator generator,
+            INamedTypeSymbol stringComparisonType, string caseChangingApproachName, bool isAnyArgumentNamed)
+        {
+            // Generate the enum access expression for "StringComparison.DesiredCultureDesiredCase"
+            SyntaxNode stringComparisonEnumValueAccess = generator.MemberAccessExpression(
+                generator.TypeExpressionForStaticMemberAccess(stringComparisonType),
+                generator.IdentifierName(caseChangingApproachName));
+
+            // Convert the above into an argument node, then append it to the argument list: "b, StringComparison.DesiredCultureDesiredCase"
+            // If at least one of the pre-existing arguments is named, then the StringComparison enum value needs to be named too
+            SyntaxNode stringComparisonArgument = isAnyArgumentNamed ?
+                generator.Argument(name: RCISCAnalyzer.StringComparisonParameterName, RefKind.None, stringComparisonEnumValueAccess) :
+                generator.Argument(stringComparisonEnumValueAccess);
+
+            return stringComparisonArgument;
+        }
+
+        protected static string GetCaseChangingApproach(string methodName)
+        {
+            if (methodName is RCISCAnalyzer.StringToLowerMethodName or RCISCAnalyzer.StringToUpperMethodName)
+            {
+                return RCISCAnalyzer.StringComparisonCurrentCultureIgnoreCaseName;
+            }
+
+            Debug.Assert(methodName is RCISCAnalyzer.StringToLowerInvariantMethodName or RCISCAnalyzer.StringToUpperInvariantMethodName);
+            return RCISCAnalyzer.StringComparisonInvariantCultureIgnoreCaseName;
+        }
+    }
+}
