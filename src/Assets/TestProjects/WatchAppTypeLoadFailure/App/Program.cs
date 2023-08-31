7ec6e05222861b60fbd974df7fa0aedbe5b5a336
tmat <tomas.matousek@microsoft.com>
2023-08-31 12:58:45 -0700

    Avoid loading all types when invoking Hot Reload handlers, handle type load failures

---
+++ src/Assets/TestProjects/WatchAppTypeLoadFailure/App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+
+[assembly: MetadataUpdateHandler(typeof(UpdateHandler))]
+
+// delete the dependency dll to cause load failure of DepSubType
+var depPath = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location!)!, "Dep.dll");
+File.Delete(depPath);
+Console.WriteLine($"File deleted: {depPath}");
+
+while (true)
+{
+    Printer.Print();
+    Thread.Sleep(100);
+}
+
+static class UpdateHandler
+{
+    public static void UpdateApplication(Type[] types)
+    {
+        Console.WriteLine($"Updated types: {(types == null ? "<null>" : types.Length == 0 ? "<empty>" : string.Join(",", types.Select(t => t.Name)))}");
+    }
+}
---
+++ src/Tests/dotnet-watch.Tests/HotReload/ApplyDeltaTests.cs
@@ -0,0 +1,72 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.Watcher.Tests
+{
+    public class ApplyDeltaTests : DotNetWatchTestBase
+    {
+        public ApplyDeltaTests(ITestOutputHelper logger)
+            : base(logger)
+        {
+        }
+
+        [Fact]
+        public async Task ChangeFileInDependency()
+        {
+            var testAsset = TestAssets.CopyTestAsset("WatchAppWithProjectDeps")
+                .WithSource()
+                .Path;
+
+            var projectDir = Path.Combine(testAsset, "AppWithDeps");
+            var dependencyDir = Path.Combine(testAsset, "Dependency");
+
+            await App.StartWatcherAsync(projectDir);
+            await App.AssertOutputLineStartsWith("Hello!");
+
+            var newSrc = """
+                public class Lib
+                {
+                    public static void Print()
+                        => System.Console.WriteLine("Changed!");
+                }
+                """;
+
+            File.WriteAllText(Path.Combine(dependencyDir, "Foo.cs"), newSrc);
+            await App.AssertOutputLineStartsWith("Changed!");
+        }
+
+        [Fact]
+        public async Task HandleTypeLoadFailure()
+        {
+            var testAsset = TestAssets.CopyTestAsset("WatchAppTypeLoadFailure")
+                .WithSource()
+                .Path;
+
+            var projectDir = Path.Combine(testAsset, "App");
+
+            App.DotnetWatchArgs.Add("--verbose");
+            await App.StartWatcherAsync(projectDir);
+            await App.AssertOutputLineStartsWith("Hello!");
+
+            var newSrc = """
+                class DepSubType : Dep
+                {
+                    int F() => 2;
+                }
+
+                class Printer
+                {
+                    public static void Print()
+                    {
+                        Console.WriteLine("Changed!");
+                    }
+                }
+                """;
+
+            File.WriteAllText(Path.Combine(projectDir, "Update.cs"), newSrc);
+
+            // Handler is called before the app is resumed: 
+            await App.AssertOutputLineStartsWith("Updated types: Printer", failure: line => line.Contains("Changed!"));
+        }
+    }
+}
