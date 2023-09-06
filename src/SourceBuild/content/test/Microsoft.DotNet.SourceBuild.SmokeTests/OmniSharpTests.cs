bc48581b07edc2119f9b63f6dbbf1e59903299d0
Nikola Milosavljevic <nikolam@microsoft.com>
2023-09-06 14:51:12 -0700

    Add sourcelink tests (#17309)
    Co-authored-by: Matt Thalman <mthalman@microsoft.com>

--- src/SourceBuild/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/OmniSharpTests.cs
+++ src/SourceBuild/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/OmniSharpTests.cs
@@ -21,7 +21,7 @@ public class OmniSharpTests : SmokeTests
     // Update version as new releases become available: https://github.com/OmniSharp/omnisharp-roslyn/releases
     private const string OmniSharpReleaseVersion = "1.39.8";
 
-    private string OmniSharpDirectory { get; } = Path.Combine(Directory.GetCurrentDirectory(), "omnisharp");
+    private string OmniSharpDirectory { get; } = Path.Combine(Directory.GetCurrentDirectory(), nameof(OmniSharpTests));
 
     public OmniSharpTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
 
--- src/SourceBuild/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/Utilities.cs
+++ src/SourceBuild/content/test/Microsoft.DotNet.SourceBuild.SmokeTests/Utilities.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.DotNet.SourceBuild.SmokeTests;
@@ -60,6 +61,19 @@ public static class Utilities
         }
     }
 
+    public static void ExtractNupkg(string package, string outputDir)
+    {
+        Directory.CreateDirectory(outputDir);
+
+        using ZipArchive zip = ZipFile.OpenRead(package);
+        foreach (ZipArchiveEntry entry in zip.Entries)
+        {
+            string outputPath = Path.Combine(outputDir, entry.FullName);
+            Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+            entry.ExtractToFile(outputPath);
+        }
+    }
+
     public static async Task RetryAsync(Func<Task> executor, ITestOutputHelper outputHelper)
     {
         await Utilities.RetryAsync(
@@ -103,5 +117,13 @@ public static class Utilities
             Thread.Sleep(TimeSpan.FromSeconds(waitTime));
             exception = await executor();
         }
+    }
+
+    public static string GetFile(string path, string pattern)
+    {
+        string[] files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
+        Assert.False(files.Length > 1, $"Found multiple files matching the pattern {pattern}: {Environment.NewLine}{string.Join(Environment.NewLine, files)}");
+        Assert.False(files.Length == 0, $"Did not find any files matching the pattern {pattern}");
+        return files[0];
     }
 }
