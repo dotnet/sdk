e109734dc9379780960459c987ef1a98df43a0ab
Nathan Mytelka <Forgind@users.noreply.github.com>
2023-09-18 13:09:26 -0700

    Actually retain the changes

--- src/Cli/dotnet/ParseResultExtensions.cs
+++ src/Cli/dotnet/ParseResultExtensions.cs
@@ -64,7 +64,7 @@ namespace Microsoft.DotNet.Cli
             var runArgs = subargs.Contains("--") ? subargs.GetRange(subargs.IndexOf("--"), subargs.Count() - subargs.IndexOf("--")) : new List<string>();
             subargs = subargs.Contains("--") ? subargs.GetRange(0, subargs.IndexOf("--")) : subargs;
 
-            subargs.SkipWhile(arg => DiagOption.Aliases.Contains(arg));
+            subargs = subargs.SkipWhile(arg => DiagOption.Aliases.Contains(arg)).ToList();
             if (subargs[0].Equals("dotnet"))
             {
                 subargs.RemoveAt(0);
