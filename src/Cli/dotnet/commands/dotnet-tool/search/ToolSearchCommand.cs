53d7ad626aba67765522e572c27e75c5ca95b4c2
Nigusu Yenework <nyenework@microsoft.com>
2023-10-04 17:05:31 -0700

    rebase

--- src/Cli/dotnet/commands/dotnet-tool/search/ToolSearchCommand.cs
+++ src/Cli/dotnet/commands/dotnet-tool/search/ToolSearchCommand.cs
@@ -26,7 +26,7 @@ namespace Microsoft.DotNet.Tools.Tool.Search
         public override int Execute()
         {
             var isDetailed = _parseResult.GetValue(ToolSearchCommandParser.DetailOption);
-            NugetToolSearchApiParameter nugetSearchApiParameter = new NugetToolSearchApiParameter(_parseResult, "dotnettool");
+            NugetSearchApiParameter nugetSearchApiParameter = new NugetSearchApiParameter(_parseResult);
             IReadOnlyCollection<SearchResultPackage> searchResultPackages =
                 NugetSearchApiResultDeserializer.Deserialize(
                     _nugetToolSearchApiRequest.GetResult(nugetSearchApiParameter).GetAwaiter().GetResult());
