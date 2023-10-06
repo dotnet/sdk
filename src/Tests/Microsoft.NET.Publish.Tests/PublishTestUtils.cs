94b53140555cea6b6267383ac3215203204c7b8d
Sven Boemer <sbomer@gmail.com>
2023-10-06 17:06:39 +0000

    Fix illink expected warnings for net9.0

--- src/Tests/Microsoft.NET.Publish.Tests/PublishTestUtils.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/PublishTestUtils.cs
@@ -43,6 +43,13 @@ namespace Microsoft.NET.Publish.Tests
             new object[] { ToolsetInfo.CurrentTargetFramework },
             new object[] { ToolsetInfo.NextTargetFramework },
         };
+
+        // This list should contain all supported TFMs after net8.0
+        public static IEnumerable<object[]> Net8Plus { get; } = new List<object[]>
+        {
+            new object[] { ToolsetInfo.CurrentTargetFramework },
+            new object[] { ToolsetInfo.NextTargetFramework },
+        };
 #else
 #error If building for a newer TFM, please update the values above to include both the old and new TFMs.
 #endif
