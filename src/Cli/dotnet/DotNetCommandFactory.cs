ef551fa4ca06d14a21cb278dd98a5b2fa8af78c2
Michael Yanni <miyanni@microsoft.com>
2023-10-17 14:25:38 -0700

    Fixed build error by adding required namespace for AsynchronousCliAction.

--- src/Cli/dotnet/DotNetCommandFactory.cs
+++ src/Cli/dotnet/DotNetCommandFactory.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.CommandLine.Invocation;
 using System.Diagnostics;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.CommandFactory;
