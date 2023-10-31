052feaca384f2bdcf999d0725e31ff0ea7ac77e6
Nigusu Yenework <nyenework@microsoft.com>
2023-10-30 22:25:03 -0700

    update xplat command to package search

---
+++ src/Cli/dotnet/commands/dotnet-package/PackageCommandParser.cs
@@ -0,0 +1,21 @@
+﻿
+using System.CommandLine;
+using Microsoft.DotNet.Tools.NuGet;
+using LocalizableStrings = Microsoft.DotNet.Tools.Package.Search.LocalizableStrings;
+
+namespace Microsoft.DotNet.Cli
+{
+    internal class PackageCommandParser
+    {
+        public static readonly string DocsLink = "";
+
+        public static CliCommand GetCommand()
+        {
+            CliCommand command = new DocumentedCommand("package", DocsLink);
+            command.SetAction((parseResult) => parseResult.HandleMissingCommand());
+            command.Subcommands.Add(PackageSearchCommandParser.GetCommand());
+
+            return command;
+        }
+    } 
+}
