fb48ee51edb53bfc277ed34ff616c0b4285a5c56
Nigusu Yenework <nyenework@microsoft.com>
2023-11-02 11:36:10 -0700

    update help output

--- src/Cli/dotnet/commands/dotnet-package/search/PackageSearchCommand.cs
+++ src/Cli/dotnet/commands/dotnet-package/search/PackageSearchCommand.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.DotNet.Tools.NuGet;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.DotNet.Tools.NuGet;
 using System.CommandLine;
 
 namespace Microsoft.DotNet.Cli
