4b1b10299f1f02dbbbbb53a5db0385d03638a44a
Nigusu Yenework <nyenework@microsoft.com>
2023-11-03 12:35:29 -0700

    Remove unused using

--- src/Cli/dotnet/commands/dotnet-package/PackageCommandParser.cs
+++ src/Cli/dotnet/commands/dotnet-package/PackageCommandParser.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
-using LocalizableStrings = Microsoft.DotNet.Tools.Package.Search.LocalizableStrings;
 
 namespace Microsoft.DotNet.Cli
 {
