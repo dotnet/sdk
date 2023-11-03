4fc540fbc47172a0f64464e1200c21a8a088d5bf
Nigusu Yenework <nyenework@microsoft.com>
2023-11-03 12:32:44 -0700

    Update help output

--- src/Cli/dotnet/commands/dotnet-package/PackageCommandParser.cs
+++ src/Cli/dotnet/commands/dotnet-package/PackageCommandParser.cs
@@ -1,6 +1,7 @@
-﻿
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System.CommandLine;
-using Microsoft.DotNet.Tools.NuGet;
 using LocalizableStrings = Microsoft.DotNet.Tools.Package.Search.LocalizableStrings;
 
 namespace Microsoft.DotNet.Cli
