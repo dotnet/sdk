3c592a9091ff8bf4f1c9e029bf53456a90fb59f7
annie <59816815+JL03-Yue@users.noreply.github.com>
2023-11-06 13:47:10 -0800

    accept bare version as NuGet exact version for ,NET tools and add a test

--- src/Cli/dotnet/commands/dotnet-tool/install/ParseResultExtension.cs
+++ src/Cli/dotnet/commands/dotnet-tool/install/ParseResultExtension.cs
@@ -1,16 +1,20 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Parsing;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.NugetSearch;
 using Microsoft.DotNet.ToolPackage;
 using Microsoft.DotNet.Tools.Tool.Search;
+using Newtonsoft.Json.Linq;
 using NuGet.Versioning;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Microsoft.DotNet.Tools.Tool.Install
 {
@@ -36,6 +40,14 @@ namespace Microsoft.DotNet.Tools.Tool.Install
             }
 
             VersionRange versionRange = null;
+
+            // accept 'bare' versions and interpret 'bare' versions as NuGet exact versions
+            string text = packageVersion?.Trim();
+            if (!string.IsNullOrEmpty(packageVersion) && IsBarePackageVersion(text))
+            {
+                packageVersion = "[" + packageVersion + "]";
+            }
+
             if (!string.IsNullOrEmpty(packageVersion) && !VersionRange.TryParse(packageVersion, out versionRange))
             {
                 throw new GracefulException(
@@ -44,6 +56,15 @@ namespace Microsoft.DotNet.Tools.Tool.Install
                         packageVersion));
             }
             return versionRange;
+        }
+
+        private static bool IsBarePackageVersion (string packageVersion)
+        {
+            // Define a regular expression pattern to match the "a.b.c" format
+            string pattern = @"^\d+\.\d+\.\d+$";
+
+            // Use Regex.IsMatch to check if the input matches the pattern
+            return Regex.IsMatch(packageVersion, pattern);
         }
     }
 }
