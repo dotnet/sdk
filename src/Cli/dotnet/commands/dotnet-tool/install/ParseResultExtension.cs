bea26d5ba167cd24588006daededc07cbdca1652
annie <59816815+JL03-Yue@users.noreply.github.com>
2023-11-08 14:22:22 -0800

    remove excessive usings

--- src/Cli/dotnet/commands/dotnet-tool/install/ParseResultExtension.cs
+++ src/Cli/dotnet/commands/dotnet-tool/install/ParseResultExtension.cs
@@ -1,20 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
 using System.CommandLine;
-using System.CommandLine.Parsing;
-using System.Linq;
-using System.Text.RegularExpressions;
 using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Cli.Utils;
-using Microsoft.DotNet.NugetSearch;
-using Microsoft.DotNet.ToolPackage;
-using Microsoft.DotNet.Tools.Tool.Search;
-using Newtonsoft.Json.Linq;
 using NuGet.Versioning;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Microsoft.DotNet.Tools.Tool.Install
 {
