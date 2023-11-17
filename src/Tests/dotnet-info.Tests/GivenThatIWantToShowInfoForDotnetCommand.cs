c4820be2a6e5e23f918e9dbd2ef495b30d825a1e
Mark Li <limarksmail@gmail.com>
2023-11-16 18:32:08 -0800

    Json testing

--- src/Tests/dotnet-info.Tests/GivenThatIWantToShowInfoForDotnetCommand.cs
+++ src/Tests/dotnet-info.Tests/GivenThatIWantToShowInfoForDotnetCommand.cs
@@ -1,9 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Xunit;
-using Xunit.Abstractions;
 using Microsoft.DotNet.Tools.Info;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 
 namespace Microsoft.DotNet.Info.Tests
 {
@@ -11,13 +12,16 @@ namespace Microsoft.DotNet.Info.Tests
     {
         private const string InfoTextRegex =
 @"\.NET SDK:\s*(?:.*\n?)+?Runtime Environment:\s*(?:.*\n?)+?\.NET workloads installed:\s*(?:.*\n?)+?";
+        private const string RuntimeEnv = "RuntimeEnv";
+        private const string Sdk = "Sdk";
+        private const string Workloads = "Workloads";
 
         public GivenThatIWantToShowInfoForDotnetCommand(ITestOutputHelper log) : base(log)
         {
         }
 
         [Fact]
-        public void WhenInfoCommandIsPassedToDotnetItPrintsUsage()
+        public void WhenInfoCommandIsPassedToDotnetItPrintsInfo()
         {
             var cmd = new DotnetCommand(Log, "info")
                 .Execute();
@@ -26,13 +30,34 @@ namespace Microsoft.DotNet.Info.Tests
         }
 
         [Fact]
-        public void WhenInfoCommandWithTextOptionIsPassedToDotnetItPrintsUsage()
+        public void WhenInfoCommandWithTextOptionIsPassedToDotnetItPrintsInfo()
         {
             var cmd = new DotnetCommand(Log, "info")
                   .Execute("--format", "text");
 
             cmd.Should().Pass();
             cmd.StdOut.Should().MatchRegex(InfoTextRegex);
+        }
+
+        [Fact]
+        public void WhenInfoCommandWithJsonOptionIsPassedToDotnetItPrintsJsonInfo()
+        {
+            var cmd = new DotnetCommand(Log, "info")
+                  .Execute("--format", "json");
+
+            cmd.Should().Pass();
+
+            JToken parsedJson = JToken.Parse(cmd.StdOut);
+
+            var expectedKeys = new List<string> { Sdk, RuntimeEnv, Workloads };
+            foreach (var key in expectedKeys)
+            {
+                Assert.True(parsedJson[key] != null, $"Expected key '{key}' not found in JSON output.");
+            }
+
+            Assert.True(parsedJson[Sdk].Type == JTokenType.Object, "SDK should be an object.");
+            Assert.True(parsedJson[RuntimeEnv].Type == JTokenType.Object, "RuntimeEnvironment should be an object.");
+            Assert.True(parsedJson[Workloads].Type == JTokenType.Array, "Workloads should be an array.");
         }
 
         [Fact]
