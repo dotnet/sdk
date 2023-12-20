d69568fe8f504f8b25370bdb28f525195cbe5a15
Forgind <12969783+Forgind@users.noreply.github.com>
2023-12-19 17:14:48 -0800

    Use options universally

--- src/Cli/dotnet/commands/dotnet-workload/InstallStateContents.cs
+++ src/Cli/dotnet/commands/dotnet-workload/InstallStateContents.cs
@@ -11,18 +11,20 @@ namespace Microsoft.DotNet.Workloads.Workload
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Dictionary<string, string> Manifests { get; set; }
 
+        private static readonly JsonSerializerOptions s_options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+        };
+
         public static InstallStateContents FromString(string contents)
         {
-            return JsonSerializer.Deserialize<InstallStateContents>(contents);
+            return JsonSerializer.Deserialize<InstallStateContents>(contents, s_options);
         }
 
         public override string ToString()
         {
-            return JsonSerializer.Serialize<InstallStateContents>(this, new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                WriteIndented = true,
-            });
+            return JsonSerializer.Serialize<InstallStateContents>(this, s_options);
         }
     }
 }
