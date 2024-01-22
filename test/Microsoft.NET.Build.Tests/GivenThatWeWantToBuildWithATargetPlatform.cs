ffd47f8bf09f92f14097a8e2089539d3f72a852f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-01-22 19:28:13 +0100

    Update tests to not hardcode translated string.

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildWithATargetPlatform.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildWithATargetPlatform.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.NET.Build.Tasks;
+
 namespace Microsoft.NET.Build.Tests
 {
     public class GivenThatWeWantToBuildWithATargetPlatform : SdkTest
@@ -125,7 +127,7 @@ namespace Microsoft.NET.Build.Tests
                 .And
                 .HaveStdOutContaining("NETSDK1140")
                 .And
-                .HaveStdOutContaining("111.0 is not a valid TargetPlatformVersion")
+                .HaveStdOutContaining(string.Format(Strings.InvalidTargetPlatformVersion, "111.0", "ios", "222.0").Split ('\n', '\r') [0])
                 .And
                 .HaveStdOutContaining("222.0");
         }
