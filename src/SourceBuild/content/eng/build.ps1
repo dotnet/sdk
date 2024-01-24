cc9bb977eee969157c427124e075f13803543998
Viktor Hofer <viktor.hofer@microsoft.com>
2024-01-24 21:01:24 +0100

    Enable build.sh for non source build scenarios (#18358)

--- src/SourceBuild/content/eng/build.ps1
+++ src/SourceBuild/content/eng/build.ps1
@@ -1,28 +1,37 @@
 [CmdletBinding(PositionalBinding=$false)]
 Param(
-  [string][Alias('c')]$configuration = "Release",
-  [switch] $clean,
+  # Common settings
   [switch][Alias('bl')]$binaryLog,
+  [string][Alias('c')]$configuration = "Release",
+  [string][Alias('v')]$verbosity = "minimal",
+
+  # Actions
+  [switch]$clean,
+  [switch][Alias('h')]$help,
+
+  # Advanced settings
+  [switch]$ci,
+  [switch][Alias('cwb')]$cleanWhileBuilding,
   [switch][Alias('nobl')]$excludeCIBinarylog,
-  [switch] $ci,
   [switch] $prepareMachine,
-  [switch] $help,
   [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
 )
 
 function Get-Usage() {
   Write-Host "Common settings:"
-  Write-Host "  -configuration <value>  Build configuration: 'Debug' or 'Release' (short: -c). [Default: Release]"
   Write-Host "  -binaryLog              Output binary log (short: -bl)"
-  Write-Host "  -help                   Print help and exit"
+  Write-Host "  -configuration <value>  Build configuration: 'Debug' or 'Release' (short: -c). [Default: Release]"
+  Write-Host "  -verbosity <value>      Msbuild verbosity: q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic] (short: -v)"
   Write-Host ""
 
   Write-Host "Actions:"
   Write-Host "  -clean                  Clean the solution"
+  Write-Host "  -help                   Print help and exit (short: -h)"
   Write-Host ""
 
   Write-Host "Advanced settings:"
   Write-Host "  -ci                     Set when running on CI server"
+  Write-Host "  -cleanWhileBuilding     Cleans each repo after building (reduces disk space usage, short: -cwb)"
   Write-Host "  -excludeCIBinarylog     Don't output binary log (short: -nobl)"
   Write-Host "  -prepareMachine         Prepare machine for CI run, clean up processes after build"
   Write-Host ""
@@ -34,6 +43,16 @@ function Get-Usage() {
 # They should be pulled from the local feeds.
 $env:NUGET_PACKAGES="$RepoRoot\prereqs\packages\restored\"
 
+if ($help) {
+  Get-Usage
+  exit 0
+}
+
+$arguments=""
+if ($cleanWhileBuilding) {
+  $arguments += " /p:CleanWhileBuilding=true"
+}
+
 function Build {
   InitializeToolset
 
@@ -42,9 +61,9 @@ function Build {
 
   MSBuild $buildProj `
     $bl `
-    --tl:off `
     /p:Configuration=$configuration `
-    @properties
+    $arguments
+    @properties `
 }
 
 try {
@@ -56,17 +75,6 @@ try {
     exit 0
   }
 
-  if ($help -or (($null -ne $properties) -and ($properties.Contains('/help') -or $properties.Contains('/?')))) {
-    Print-Usage
-    exit 0
-  }
-
-  if ($ci) {
-    if (-not $excludeCIBinarylog) {
-      $binaryLog = $true
-    }
-    $nodeReuse = $false
-  }
 
   Build
 }
