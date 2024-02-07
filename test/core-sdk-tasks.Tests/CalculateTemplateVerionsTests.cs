b2ad2d8ac9662aa83393295d5d0fd1e4a6540b1c
Michael Yanni <miyanni@microsoft.com>
2024-02-07 11:27:29 -0800

    Fix CalculateTemplateVerionsTests to access the properties of the return type for comparison since a ValueTuple is no longer used.

--- test/core-sdk-tasks.Tests/CalculateTemplateVerionsTests.cs
+++ test/core-sdk-tasks.Tests/CalculateTemplateVerionsTests.cs
@@ -11,10 +11,10 @@ namespace EndToEnd
         {
             var result = CalculateTemplateVersions.Calculate("3.1.0");
 
-            result.Should()
-                .Be(("3.1.1", "3.1", "3.1.1"),
-                    "the patch is 1 higher than aspnetTemplateVersion " +
-                    "due to https://github.com/dotnet/core-sdk/issues/6243");
+            // The patch is 1 higher than aspnetTemplateVersion due to https://github.com/dotnet/core-sdk/issues/6243
+            result.InstallPath.Should().Be("3.1.1");
+            result.MajorMinorVersion.Should().Be("3.1");
+            result.MajorMinorPatchVersion.Should().Be("3.1.1");
         }
 
         [Fact]
@@ -22,8 +22,9 @@ namespace EndToEnd
         {
             var result = CalculateTemplateVersions.Calculate("3.0.0-alpha.1.20071.6");
 
-            result.Should()
-                .Be(("3.0.1-alpha.1.20071.6", "3.0", "3.0.1"));
+            result.InstallPath.Should().Be("3.0.1-alpha.1.20071.6");
+            result.MajorMinorVersion.Should().Be("3.0");
+            result.MajorMinorPatchVersion.Should().Be("3.0.1");
         }
 
         [Fact]
@@ -31,10 +32,10 @@ namespace EndToEnd
         {
             var result = CalculateTemplateVersions.Calculate("5.1.0");
 
-            result.Should()
-                .Be(("5.1.0", "5.1", "5.1.0"),
-                     "the patch align with AspNetCoreTemplateMajorVersion again, " +
-                     "since there is no non-deterministic existing ComponentId under Major version 5.");
+            // The patch align with AspNetCoreTemplateMajorVersion again, since there is no non-deterministic existing ComponentId under Major version 5.
+            result.InstallPath.Should().Be("5.1.0");
+            result.MajorMinorVersion.Should().Be("5.1");
+            result.MajorMinorPatchVersion.Should().Be("5.1.0");
         }
 
         [Fact]
@@ -42,8 +43,9 @@ namespace EndToEnd
         {
             var result = CalculateTemplateVersions.Calculate("5.0.0-alpha.1.20071.6");
 
-            result.Should()
-                .Be(("5.0.0-alpha.1.20071.6", "5.0", "5.0.0"));
+            result.InstallPath.Should().Be("5.0.0-alpha.1.20071.6");
+            result.MajorMinorVersion.Should().Be("5.0");
+            result.MajorMinorPatchVersion.Should().Be("5.0.0");
         }
     }
 }
