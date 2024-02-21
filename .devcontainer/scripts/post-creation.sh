0f027a8495a3197e0d16eba03db7eec9419c7bfd
Weihan Li <weihanli@outlook.com>
2024-02-21 11:24:06 +0800

    Update post-creation.sh
    add run eng/sdk-build-env.sh

--- .devcontainer/scripts/post-creation.sh
+++ .devcontainer/scripts/post-creation.sh
@@ -2,3 +2,5 @@
 # Also run the full restore on the repo so that go-to definition
 # and other language features will be available in C# files
 ./restore.sh
+# Run eng/sdk-build-env.sh to ensure everything is set up correctly for VSCode
+./eng/sdk-build-env.sh
