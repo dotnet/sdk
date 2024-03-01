8f7fd09ec305b36be9f56c0aab311760ef9328b3
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2024-03-01 10:42:14 -0800

    PR Feedback:
    - Extract common archive diffing into a base class
    - Move Diff utils to a separate class
    - Return all valid archives and assert only one valid SDK archive
    - Use Arcade VersionIdentifier util class

--- src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.SdkArchiveDiff/FindArchiveDiffs.cs
+++ src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.SdkArchiveDiff/FindArchiveDiffs.cs
@@ -1,23 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using Task = System.Threading.Tasks.Task;
 
 public class FindArchiveDiffs : Microsoft.Build.Utilities.Task, ICancelableTask
 {
-    public class ArchiveItem
-    {
-        public required string Path { get; init; }
-    }
-
     [Required]
     public required ITaskItem BaselineArchive { get; init; }
 
@@ -29,10 +20,6 @@ public class FindArchiveDiffs : Microsoft.Build.Utilities.Task, ICancelableTask
 
     private CancellationTokenSource _cancellationTokenSource = new();
     private CancellationToken cancellationToken => _cancellationTokenSource.Token;
-    public void Cancel()
-    {
-        _cancellationTokenSource.Cancel();
-    }
 
     public override bool Execute()
     {
@@ -49,98 +36,14 @@ public class FindArchiveDiffs : Microsoft.Build.Utilities.Task, ICancelableTask
         var baselineFiles = baseline.GetFileNames();
         var testFiles = test.GetFileNames();
         ContentDifferences =
-            GetDiffs(baselineFiles, testFiles, PathWithVersions.Equal, PathWithVersions.GetVersionlessPath, cancellationToken)
-            .Select(FromDiff)
+            Diff.GetDiffs(baselineFiles, testFiles, VersionIdentifier.AreVersionlessEqual, static p => VersionIdentifier.RemoveVersions(p, "{VERSION}"), cancellationToken)
+            .Select(Diff.TaskItemFromDiff)
             .ToArray();
         return true;
     }
 
-    static ITaskItem FromDiff((string, DifferenceKind) diff)
+    public void Cancel()
     {
-        var item = new TaskItem(diff.Item1);
-        item.SetMetadata("Kind", Enum.GetName(diff.Item2));
-        return item;
-    }
-
-    public enum DifferenceKind
-    {
-        Added,
-        Removed,
-        Unchanged
-    }
-
-    public static List<(string, DifferenceKind DifferenceKind)> GetDiffs(
-        string[] originalPathsWithVersions,
-        string[] modifiedPathsWithVersions,
-        Func<string, string, bool> equalityComparer,
-        Func<string, string>? formatter = null,
-        CancellationToken cancellationToken = default)
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        formatter ??= static s => s;
-        // Edit distance algorithm: https://en.wikipedia.org/wiki/Longest_common_subsequence
-
-        int[,] dp = new int[originalPathsWithVersions.Length + 1, modifiedPathsWithVersions.Length + 1];
-
-        // Initialize first row and column
-        for (int i = 0; i <= originalPathsWithVersions.Length; i++)
-        {
-            dp[i, 0] = i;
-        }
-        for (int j = 0; j <= modifiedPathsWithVersions.Length; j++)
-        {
-            dp[0, j] = j;
-        }
-
-        // Compute edit distance
-        for (int i = 1; i <= originalPathsWithVersions.Length; i++)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            for (int j = 1; j <= modifiedPathsWithVersions.Length; j++)
-            {
-                if (equalityComparer(originalPathsWithVersions[i - 1], modifiedPathsWithVersions[j - 1]))
-                {
-                    dp[i, j] = dp[i - 1, j - 1];
-                }
-                else
-                {
-                    dp[i, j] = 1 + Math.Min(dp[i - 1, j], dp[i, j - 1]);
-                }
-            }
-        }
-
-        // Trace back the edits
-        int row = originalPathsWithVersions.Length;
-        int col = modifiedPathsWithVersions.Length;
-
-        List<(string, DifferenceKind)> formattedDiff = [];
-        while (row > 0 || col > 0)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            var baselineItem = originalPathsWithVersions[row - 1];
-            var testItem = modifiedPathsWithVersions[col - 1];
-            if (row > 0 && col > 0 && PathWithVersions.Equal(baselineItem, testItem))
-            {
-                formattedDiff.Add((formatter(originalPathsWithVersions[row - 1]), DifferenceKind.Unchanged));
-                row--;
-                col--;
-            }
-            else if (col > 0 && (row == 0 || dp[row, col - 1] <= dp[row - 1, col]))
-            {
-                formattedDiff.Add((formatter(modifiedPathsWithVersions[col - 1]), DifferenceKind.Added));
-                col--;
-            }
-            else if (row > 0 && (col == 0 || dp[row, col - 1] > dp[row - 1, col]))
-            {
-                formattedDiff.Add((formatter(originalPathsWithVersions[row - 1]), DifferenceKind.Removed));
-                row--;
-            }
-            else
-            {
-                throw new UnreachableException();
-            }
-        }
-        formattedDiff.Reverse();
-        return formattedDiff;
+        _cancellationTokenSource.Cancel();
     }
 }
--- src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.SdkArchiveDiff/GetClosestOfficialSdk.cs
+++ src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.SourceBuild.Tasks.SdkArchiveDiff/GetClosestOfficialSdk.cs
@@ -1,73 +1,44 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 
-public class GetClosestOfficialSdk : Microsoft.Build.Utilities.Task, ICancelableTask
+public class GetClosestOfficialSdk : GetClosestArchive
 {
-    [Required]
-    public required string BuiltSdkPath { get; init; }
+    protected override string ArchiveName => "dotnet-sdk";
 
-    [Output]
-    public string ClosestOfficialSdkPath { get; set; } = "";
+    HttpClient client = new HttpClient(new HttpClientHandler() { AllowAutoRedirect = false });
 
-    public override bool Execute()
+    private string? closestVersion;
+    private string? closestUrl;
+
+    public override async Task<string?> GetLatestOfficialArchiveUrl()
     {
-        return Task.Run(ExecuteAsync).Result;
-    }
-
-    private CancellationTokenSource _cancellationTokenSource = new();
-    private CancellationToken cancellationToken => _cancellationTokenSource.Token;
-    public void Cancel()
-    {
-        _cancellationTokenSource.Cancel();
-    }
-
-    public async Task<bool> ExecuteAsync()
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        var (versionString, rid, extension) = Archive.GetInfoFromArchivePath(BuiltSdkPath);
-
-        string downloadUrl = GetLatestOfficialSdkUrl(versionString, rid, extension);
-
-        Log.LogMessage(MessageImportance.High, $"Downloading {downloadUrl}");
-        var handler = new HttpClientHandler()
-        {
-            AllowAutoRedirect = false
-        };
-        var client = new HttpClient(handler);
-        var redirectResponse = await client.GetAsync(downloadUrl, cancellationToken);
+        // Channel in the form of 9.0.1xx
+        var channel = BuiltVersion[..5] + "xx";
+        var akaMsUrl = $"https://aka.ms/dotnet/{channel}/daily/{ArchiveName}-{BuiltRid}{ArchiveExtension}";
+        var redirectResponse = await client.GetAsync(akaMsUrl, CancellationToken);
         // aka.ms returns a 301 for valid redirects and a 302 to Bing for invalid URLs
         if (redirectResponse.StatusCode != HttpStatusCode.Moved)
         {
-            Log.LogMessage(MessageImportance.High, $"Failed to download '{downloadUrl}': invalid aka.ms URL");
-            return true;
+            Log.LogMessage(MessageImportance.High, $"Failed to find package at '{akaMsUrl}': invalid aka.ms URL");
+            return null;
         }
-        var packageResponse = await client.GetAsync(redirectResponse.Headers.Location!, cancellationToken);
-
-        var packageUriPath = packageResponse.RequestMessage!.RequestUri!.LocalPath;
-        string downloadedVersion = PathWithVersions.GetVersionInPath(packageUriPath).ToString();
-
-        ClosestOfficialSdkPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + $".dotnet-sdk-{downloadedVersion}-{rid}{extension}");
-        Log.LogMessage($"Copying {packageUriPath} to {ClosestOfficialSdkPath}");
-        using (var file = File.Create(ClosestOfficialSdkPath))
-        {
-            await packageResponse.Content.CopyToAsync(file, cancellationToken);
-        }
-
-        return true;
+        closestUrl = redirectResponse.Headers.Location!.ToString();
+        closestVersion = VersionIdentifier.GetVersion(closestUrl);
+        return closestUrl;
     }
 
-    string GetLatestOfficialSdkUrl(string versionString, string rid, string extension)
+    public override async Task<string?> GetClosestOfficialArchiveVersion()
     {
-        // Channel in the form of 9.0.1xx
-        var channel = versionString[..5] + "xx";
-        return $"https://aka.ms/dotnet/{channel}/daily/dotnet-sdk-{rid}{extension}";
+        if (closestUrl is not null)
+        {
+            return closestVersion;
+        }
+        _ = await GetLatestOfficialArchiveUrl();
+        return closestVersion;
     }
-
 }
