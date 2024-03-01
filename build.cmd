2c517adc27e0f17a7b68ca159be1edc044988eb7
Shen Chen <shech@microsoft.com>
2024-02-29 16:05:23 -0800

    Add NoLogo back in build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -msbuildEngine dotnet %*"
+powershell -NoLogo -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -msbuildEngine dotnet %*"
 exit /b %ErrorLevel%
