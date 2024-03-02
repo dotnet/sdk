dda6b8948a128efb1a5452a6b947ec38e275d75e
Chet Husk <chusk3@gmail.com>
2024-03-02 14:09:43 -0600

    Use long-but-reasonable max timeout instead of infinite

--- src/Containers/Microsoft.NET.Build.Containers/Registry/DefaultRegistryAPI.cs
+++ src/Containers/Microsoft.NET.Build.Containers/Registry/DefaultRegistryAPI.cs
@@ -14,6 +14,14 @@ internal class DefaultRegistryAPI : IRegistryAPI
     private readonly HttpClient _client;
     private readonly ILogger _logger;
 
+    // Empirical value - Unoptimized .NET application layers can be ~200MB
+    // * .NET Runtime (~80MB)
+    // * ASP.NET Runtime (~25MB)
+    // * application and dependencies - variable, but _probably_ not more than the BCL?
+    // Given a 200MB target and a 1Mb/s upload speed, we'd expect an upload speed of 27m:57s.
+    // Making this a round 30 for convenience.
+    private static TimeSpan LongRequestTimeout = TimeSpan.FromMinutes(30);
+
     internal DefaultRegistryAPI(string registryName, Uri baseUri, ILogger logger)
     {
         bool isAmazonECRRegistry = baseUri.IsAmazonECRRegistry();
@@ -33,7 +41,7 @@ internal class DefaultRegistryAPI : IRegistryAPI
         var innerHandler = new SocketsHttpHandler()
         {
             UseCookies = false,
-            // the rest of the HTTP stack has an infinite timeout (see below) but we should still have a reasonable timeout for the initial connection
+            // the rest of the HTTP stack has an very long timeout (see below) but we should still have a reasonable timeout for the initial connection
             ConnectTimeout = TimeSpan.FromSeconds(30)
         };
 
@@ -56,8 +64,7 @@ internal class DefaultRegistryAPI : IRegistryAPI
 
         HttpClient client = new(clientHandler)
         {
-            // blob upload operations can take quite a while, we should allow them to take as long as they need
-            Timeout = Timeout.InfiniteTimeSpan
+            Timeout = LongRequestTimeout
         };
 
         client.DefaultRequestHeaders.Add("User-Agent", $".NET Container Library v{Constants.Version}");
