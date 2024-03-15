77714bb2a0dad5fdcf6892a931a9b6436820e2b9
Michael Simons <msimons@microsoft.com>
2024-03-15 09:45:04 -0500

    Move the source-build script back to the root of the repo (#19059)

--- src/SourceBuild/content/.devcontainer/prebuilt-sdk/init.sh
+++ src/SourceBuild/content/.devcontainer/prebuilt-sdk/init.sh
@@ -3,7 +3,7 @@
 source="${BASH_SOURCE[0]}"
 script_root="$( cd -P "$( dirname "$source" )" && pwd )"
 
-"$script_root"/../../eng/prep-source-build.sh
+"$script_root"/../../prep-source-build.sh
 
 cp "$script_root/../synchronize-vmr.sh" "/workspaces/"
 "$script_root"/../../build.sh --online --clean-while-building || exit 0
