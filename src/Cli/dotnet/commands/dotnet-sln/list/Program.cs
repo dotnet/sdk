ea551d45e9224ee43daa8043af8778cc04ed7529
Johannes Cornelis Draaijer <jdraaijer@microsoft.com>
2024-03-27 13:00:45 +0100

    Add --format to dotnet sln list

--- src/Cli/dotnet/commands/dotnet-sln/list/Program.cs
+++ src/Cli/dotnet/commands/dotnet-sln/list/Program.cs
@@ -2,23 +2,29 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
+using System.Text.Json;
 using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Cli.Sln.Internal;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.Tools.Common;
+using Microsoft.DotNet.Tools.Tool.List;
 
 namespace Microsoft.DotNet.Tools.Sln.List
 {
     internal class ListProjectsInSolutionCommand : CommandBase
     {
+        private readonly static JsonSerializerOptions s_noEscapeJsonSerializerOptions = new() { Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
+
         private readonly string _fileOrDirectory;
         private readonly bool _displaySolutionFolders;
+        private readonly SlnListReportOutputFormat _outputFormat;
 
         public ListProjectsInSolutionCommand(
             ParseResult parseResult) : base(parseResult)
         {
             _fileOrDirectory = parseResult.GetValue(SlnCommandParser.SlnArgument);
             _displaySolutionFolders = parseResult.GetValue(SlnListParser.SolutionFolderOption);
+            _outputFormat = parseResult.GetValue(SlnListParser.OutputFormatOption);
         }
 
         public override int Execute()
@@ -50,12 +56,26 @@ namespace Microsoft.DotNet.Tools.Sln.List
             {
                 Array.Sort(paths);
 
-                string header = _displaySolutionFolders ? LocalizableStrings.SolutionFolderHeader : LocalizableStrings.ProjectsHeader;
-                Reporter.Output.WriteLine($"{header}");
-                Reporter.Output.WriteLine(new string('-', header.Length));
-                foreach (string slnProject in paths)
+                if (_outputFormat == SlnListReportOutputFormat.console_with_header)
                 {
-                    Reporter.Output.WriteLine(slnProject);
+                    string header = _displaySolutionFolders ? LocalizableStrings.SolutionFolderHeader : LocalizableStrings.ProjectsHeader;
+                    Reporter.Output.WriteLine($"{header}");
+                    Reporter.Output.WriteLine(new string('-', header.Length));
+                }
+
+                switch (_outputFormat)
+                {
+                    case SlnListReportOutputFormat.console:
+                    case SlnListReportOutputFormat.console_with_header:
+                        foreach (string slnProject in paths)
+                        {
+                            Reporter.Output.WriteLine(slnProject);
+                        }
+                        break;
+                    case SlnListReportOutputFormat.json:
+                        var jsonArray = JsonSerializer.Serialize(paths, s_noEscapeJsonSerializerOptions);
+                        Reporter.Output.WriteLine(jsonArray);
+                        break;
                 }
             }
             return 0;
---
+++ src/Cli/dotnet/commands/dotnet-sln/list/SlnListReportOutputFormat.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.Tools.Sln.List
+{
+    internal enum SlnListReportOutputFormat
+    {
+        console_with_header = 0,
+        console = 1,
+        json = 2,
+    }
+}
