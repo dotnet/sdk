eff17db0d42f95fe0debadd3c51415118a41a2a1
Jason Zhai <v-wuzhai@microsoft.com>
2024-04-18 02:03:37 -0700

    Use the RuntimeFrameworkVersion for EndToEnd_NoAPI_ProjectType test

--- test/Microsoft.NET.Build.Containers.IntegrationTests/EndToEndTests.cs
+++ test/Microsoft.NET.Build.Containers.IntegrationTests/EndToEndTests.cs
@@ -365,7 +365,8 @@ public class EndToEndTests : IDisposable
             $"/p:ContainerRegistry={DockerRegistryManager.LocalRegistry}",
             $"/p:ContainerRepository={imageName}",
             $"/p:ContainerImageTag={imageTag}",
-            "/p:UseRazorSourceGenerator=false")
+            "/p:UseRazorSourceGenerator=false",
+            $"/p:RuntimeFrameworkVersion=9.0.0-preview.3.24172.9")
             .WithEnvironmentVariable("NUGET_PACKAGES", privateNuGetAssets.FullName)
             .WithWorkingDirectory(newProjectDir.FullName)
             .Execute();
