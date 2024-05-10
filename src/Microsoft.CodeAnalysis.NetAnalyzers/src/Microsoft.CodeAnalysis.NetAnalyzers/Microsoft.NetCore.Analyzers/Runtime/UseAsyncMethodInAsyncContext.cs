a7ab488ddd81d3af2fd80800f362faa95e24e1bd
Collin Alpert <collinalpert@gmail.com>
2024-05-10 19:53:18 +0200

    Prevent SemaphoreSlim.Wait(0) from triggering CA1849

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/UseAsyncMethodInAsyncContext.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/UseAsyncMethodInAsyncContext.cs
@@ -5,6 +5,8 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Analyzer.Utilities;
 using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
@@ -83,6 +85,20 @@ namespace Microsoft.NetCore.Analyzers.Runtime
                     return;
                 }
 
+                INamedTypeSymbol? semaphoreSlimType = wellKnownTypeProvider.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemThreadingSemaphoreSlim);
+                INamedTypeSymbol? timeSpanType = wellKnownTypeProvider.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemTimeSpan);
+                INamedTypeSymbol intType = context.Compilation.GetSpecialType(SpecialType.System_Int32);
+                IFieldSymbol? timeSpanZero = timeSpanType?.GetMembers(nameof(TimeSpan.Zero))
+                    .OfType<IFieldSymbol>()
+                    .FirstOrDefault();
+                ISet<IMethodSymbol> semaphoreSlimWaitMethods = semaphoreSlimType
+                    ?.GetMembers(nameof(SemaphoreSlim.Wait))
+                    .OfType<IMethodSymbol>()
+                    .Where(m => m.Parameters.Length > 0
+                                && (SymbolEqualityComparer.Default.Equals(m.Parameters[0].Type, intType)
+                                    || SymbolEqualityComparer.Default.Equals(m.Parameters[0].Type, timeSpanType)))
+                    .ToSet() ?? ImmutableHashSet<IMethodSymbol>.Empty;
+
                 ImmutableArray<IMethodSymbol> excludedMethods = GetExcludedMethods(wellKnownTypeProvider);
                 context.RegisterOperationAction(context =>
                 {
@@ -91,7 +107,9 @@ namespace Microsoft.NetCore.Analyzers.Runtime
                         if (context.Operation is IInvocationOperation invocationOperation)
                         {
                             var methodSymbol = invocationOperation.TargetMethod;
-                            if (excludedMethods.Contains(methodSymbol.OriginalDefinition, SymbolEqualityComparer.Default) || InspectAndReportBlockingMemberAccess(context, methodSymbol, syncBlockingSymbols, SymbolKind.Method))
+                            if (excludedMethods.Contains(methodSymbol.OriginalDefinition, SymbolEqualityComparer.Default)
+                                || InspectAndReportBlockingMemberAccess(context, methodSymbol, syncBlockingSymbols, SymbolKind.Method)
+                                || IsSemaphoreSlimWaitWithZeroArgumentInvocation(invocationOperation, timeSpanZero, semaphoreSlimWaitMethods))
                             {
                                 // Don't return double-diagnostics.
                                 return;
@@ -146,6 +164,21 @@ namespace Microsoft.NetCore.Analyzers.Runtime
                     }
                 }, OperationKind.Invocation, OperationKind.PropertyReference);
             });
+        }
+
+        private static bool IsSemaphoreSlimWaitWithZeroArgumentInvocation(IInvocationOperation invocation, IFieldSymbol? timeSpanZero, ISet<IMethodSymbol> semaphoreSlimWaitMethods)
+        {
+            if(!semaphoreSlimWaitMethods.Contains(invocation.TargetMethod, SymbolEqualityComparer.Default) || invocation.Arguments.IsEmpty)
+            {
+                return false;
+            }
+
+            IOperation argumentValue = invocation.Arguments[0].Value;
+
+            return argumentValue.HasConstantValue(0)
+                   || timeSpanZero is not null
+                   && argumentValue is IFieldReferenceOperation fieldReference
+                   && SymbolEqualityComparer.Default.Equals(fieldReference.Field, timeSpanZero);
         }
 
         private static SymbolDisplayFormat GetLanguageSpecificFormat(IOperation operation) =>
