6a55db0220c6ba6d4a75009da247d78e15362e4a
Viktor Hofer <viktor.hofer@microsoft.com>
2024-05-21 16:34:53 +0000

    Improve repo artifacts logging
    1. Include the version when logging new packages
    2. Read the repo asset manifest files only once
    3. Make the log target respect incremental builds

--- src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.UnifiedBuild.Tasks/GetKnownPackagesFromAssetManifests.cs
+++ src/SourceBuild/content/eng/tools/tasks/Microsoft.DotNet.UnifiedBuild.Tasks/GetKnownPackagesFromAssetManifests.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-using System.Linq;
 using System.Xml.Linq;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -13,7 +11,7 @@ namespace Microsoft.DotNet.UnifiedBuild.Tasks
     /// <summary>
     /// Get a list of MSBuild Items that represent the packages described in the asset manifests.
     /// </summary>
-    public sealed class GetKnownPackagesFromAssetManifests : Task
+    public sealed class GetKnownArtifactsFromAssetManifests : Build.Utilities.Task
     {
         [Required]
         public ITaskItem[] AssetManifests { get; set; }
@@ -21,13 +19,25 @@ namespace Microsoft.DotNet.UnifiedBuild.Tasks
         [Output]
         public ITaskItem[] KnownPackages { get; set; }
 
+        [Output]
+        public ITaskItem[] KnownBlobs { get; set; }
+
         public override bool Execute()
         {
-            var knownPackages = from assetManifest in AssetManifests
-                                let doc = XDocument.Load(assetManifest.ItemSpec)
-                                from package in doc.Root.Descendants("Package")
-                                select new TaskItem(package.Attribute("Id").Value, new Dictionary<string, string>{ { "Version", package.Attribute("Version").Value } });
-            KnownPackages = knownPackages.ToArray();
+            XDocument[] xDocuments = AssetManifests
+                .Select(manifest => XDocument.Load(manifest.ItemSpec))
+                .ToArray();
+
+            KnownPackages = xDocuments
+                .SelectMany(doc => doc.Root!.Descendants("Package"))
+                .Select(package => new TaskItem(package.Attribute("Id")!.Value, new Dictionary<string, string> { { "Version", package.Attribute("Version")!.Value } }))
+                .ToArray();
+
+            KnownBlobs = xDocuments
+                .SelectMany(doc => doc.Root!.Descendants("Blob"))
+                .Select(asset => new TaskItem(asset.Attribute("Path")!.Value))
+                .ToArray();
+
             return true;
         }
     }
