fe160a70b4f724f894f7a2314c50273e9f68f5ba
Javier Calvarro Nelson <jacalvar@microsoft.com>
2024-05-25 15:31:36 +0200

    Isolate pack projects

---
+++ test/Microsoft.NET.Sdk.Razor.Tests/IsolatedNuGetPackageFolderAspNetSdkBaselineTest.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.NET.Sdk.Razor.Tests
+{
+    public class IsolatedNuGetPackageFolderAspNetSdkBaselineTest : AspNetSdkBaselineTest
+    {
+        public IsolatedNuGetPackageFolderAspNetSdkBaselineTest(ITestOutputHelper log, string restoreNugetPackagePath) : base(log)
+        {
+            TestContext.Current.NuGetCachePath = Path.GetFullPath(Path.Combine(_testAssetsManager.TestAssetsRoot, restoreNugetPackagePath));
+        }
+    }
+}
+
