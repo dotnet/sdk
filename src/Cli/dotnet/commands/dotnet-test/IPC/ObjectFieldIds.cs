7c04a05155ce28c7c2fdcf0cee154223c84ac521
mariam-abdulla <mabdullah@microsoft.com>
2024-05-29 14:03:43 +0200

    Revert "Add comment to ObjectFieldIds"
    This reverts commit 1c38cc119e99293260048730f5fc9767417c6cbf.

--- src/Cli/dotnet/commands/dotnet-test/IPC/ObjectFieldIds.cs
+++ src/Cli/dotnet/commands/dotnet-test/IPC/ObjectFieldIds.cs
@@ -1,10 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// WARNING: Please note this file needs to be kept aligned with the one in the dotnet sdk.
-// The protocol follows the concept of optional properties.
-// The id is used to identify the property in the stream and it will be skipped if it's not recognized.
-// We can add new properties with new ids, but we CANNOT change the existing ids (to support backwards compatibility).
 namespace Microsoft.DotNet.Tools.Test
 {
     internal class CommandLineOptionMessagesFieldsId
