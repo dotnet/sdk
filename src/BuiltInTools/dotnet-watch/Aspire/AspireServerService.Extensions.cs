746012f6da19eb816af00ac18af8ea58d95abe66
tmat <tomas.matousek@microsoft.com>
2024-05-31 15:19:05 -0700

    Aspire

---
+++ src/BuiltInTools/dotnet-watch/Aspire/AspireServerService.Extensions.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.DotNet.Watcher;
+
+namespace Microsoft.WebTools.AspireServer;
+
+internal partial class AspireServerService : IRuntimeProcessLauncher
+{
+    public async ValueTask<IEnumerable<(string name, string value)>> GetEnvironmentVariablesAsync(CancellationToken cancelationToken)
+    {
+        var environment = await GetServerConnectionEnvironmentAsync(cancelationToken).ConfigureAwait(false);
+        return environment.Select(kvp => (kvp.Key, kvp.Value));
+    }
+}
