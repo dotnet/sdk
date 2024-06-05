dfc7cb7e006ded070dbbbc9e6df6e51f3b7f7d3b
mariam-abdulla <mabdullah@microsoft.com>
2024-06-05 14:50:20 +0200

    delete PathDotnetTest.ps1

--- src/Cli/dotnet/PathDotnetTest.ps1
+++
@@ -1,3 +0,0 @@
-& dotnet build
-Copy-Item C:\code\sdk\artifacts\bin\Microsoft.NET.Sdk.Testing.Tasks\Debug\net9.0\Microsoft.NET.Sdk.Testing.Tasks.dll -Destination C:\code\sdk\artifacts\bin\redist\Debug\dotnet\sdk\9.0.100-dev
-Copy-Item C:\code\sdk\artifacts\bin\dotnet\Debug\net9.0\dotnet.dll -Destination  C:\code\sdk\artifacts\bin\redist\Debug\dotnet\sdk\9.0.100-dev
