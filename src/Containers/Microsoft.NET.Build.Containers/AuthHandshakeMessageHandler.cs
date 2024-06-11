de2497352cd3c71f9720ce0bc3b996d198f5aff8
MichalPavlik <michalpavlik@outlook.com>
2024-06-11 17:21:57 +0200

    Added missing using

--- src/Containers/Microsoft.NET.Build.Containers/AuthHandshakeMessageHandler.cs
+++ src/Containers/Microsoft.NET.Build.Containers/AuthHandshakeMessageHandler.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Concurrent;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
