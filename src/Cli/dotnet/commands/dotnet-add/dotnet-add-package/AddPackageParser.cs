1ac1e561672cd202bf98218eef498ff6282a1d7e
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-06-12 15:28:24 +0200

    Fix typo

--- src/Cli/dotnet/commands/dotnet-add/dotnet-add-package/AddPackageParser.cs
+++ src/Cli/dotnet/commands/dotnet-add/dotnet-add-package/AddPackageParser.cs
@@ -105,7 +105,7 @@ namespace Microsoft.DotNet.Cli
                 yield return packageId;
             }
 
-            stream.Dispose();
+            result.Dispose();
         }
 
         internal static IEnumerable<string> EnumerablePackageIdFromQueryResponse(Stream result)
