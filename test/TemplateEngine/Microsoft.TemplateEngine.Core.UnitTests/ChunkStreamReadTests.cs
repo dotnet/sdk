5ce0340ad0d0c6f99f509cf9806f705ed31f32e0
Simon Cropp <simon.cropp@gmail.com>
2024-06-13 21:29:43 +1000

    re use ChunkMemoryStream  helper (dotnet/templating#8110)
    Commit migrated from https://github.com/dotnet/templating/commit/68bb8e44b9caf1c1d2cf7cf27acdf174cefae517

--- test/TemplateEngine/Microsoft.TemplateEngine.Core.UnitTests/ChunkStreamReadTests.cs
+++ test/TemplateEngine/Microsoft.TemplateEngine.Core.UnitTests/ChunkStreamReadTests.cs
@@ -141,29 +141,5 @@ namespace Microsoft.TemplateEngine.Core.UnitTests
 
             Verify(Encoding.UTF8, output, changed, "Hello    \r\n    There    \r\n    You", "Hello    \r\n    You");
         }
-
-        private class ChunkMemoryStream : MemoryStream
-        {
-            private readonly int _chunkSize;
-
-            internal ChunkMemoryStream(int chunkSize) : base()
-            {
-                _chunkSize = chunkSize;
-            }
-
-            internal ChunkMemoryStream(byte[] buffer, int chunkSize) : base(buffer)
-            {
-                _chunkSize = chunkSize;
-            }
-
-            public override int Read(byte[] buffer, int offset, int count)
-            {
-                if (count > _chunkSize)
-                {
-                    count = _chunkSize;
-                }
-                return base.Read(buffer, offset, count);
-            }
-        }
     }
 }
