b97b9a6d593de2cd370018571154ec4707569361
Michael Yanni <miyanni@microsoft.com>
2024-06-19 10:17:21 -0700

    Fixed namespaces for existing EndToEnd tests. Removed TelemetryOptOutDefaultTests.cs as this conceptually doesn't make sense when Installer is in the SDK repo, as this test was against the official build of the SDK. Now, we're not running tests against the official build due to duration and agent resources.

--- test/EndToEnd.Tests/GivenNetFrameworkSupportsNetStandard2.cs
+++ test/EndToEnd.Tests/GivenNetFrameworkSupportsNetStandard2.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace EndToEnd
+namespace EndToEnd.Tests
 {
     public class GivenNetFrameworkSupportsNetStandard2(ITestOutputHelper log) : SdkTest(log)
     {
