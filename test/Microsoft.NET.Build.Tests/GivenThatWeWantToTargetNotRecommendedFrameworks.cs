4a9f77049701f077c8a38b8195361d0b88d66eaf
Marc Paine <marcpop@microsoft.com>
2024-06-20 14:34:29 -0700

    Add test for net standard 2 and 2.1 Change the logic to use the LessThan intrinsic

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToTargetNotRecommendedFrameworks.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToTargetNotRecommendedFrameworks.cs
@@ -40,6 +40,32 @@ namespace Microsoft.NET.Build.Tests
                 .HaveStdOutContaining("NETSDK1215");
         }
 
+        [Theory]
+        [InlineData("NetStandard2.0")]
+        [InlineData("NetStandard2.1")]
+        public void It_should_not_warn_when_framework_not_recommended(string targetFrameworks)
+        {
+            var testProject = new TestProject()
+            {
+                Name = $"NotRecommended{targetFrameworks}",
+                TargetFrameworks = targetFrameworks,
+                IsExe = false
+            };
+
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var buildCommand = new BuildCommand(testAsset);
+
+            var result = buildCommand
+                .Execute();
+
+            result
+                .Should()
+                .Pass()
+                .And
+                .NotHaveStdOutContaining("NETSDK1215");
+        }
+
         [Fact]
         public void It_only_checks_for_netcoreapp_not_recommended_frameworks()
         {
