e9ea3c119c27fc7a03136388495fe56abf5c740f
Michael Yanni <miyanni@microsoft.com>
2024-07-15 11:55:02 -0700

    Got GivenWindowsApp test working. Moved more tests over prior to editing them.

--- test/EndToEnd.Tests/GivenWindowsApp.cs
+++ test/EndToEnd.Tests/GivenWindowsApp.cs
@@ -1,13 +1,6 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-//using Microsoft.DotNet.TestFramework;
-//using WindowsOnlyTheoryAttribute = Microsoft.DotNet.Tools.Test.Utilities.WindowsOnlyTheoryAttribute;
-//using BuildCommand = Microsoft.DotNet.Tools.Test.Utilities.BuildCommand;
-//using RunCommand = Microsoft.DotNet.Tools.Test.Utilities.RunCommand;
-//using TestBase = Microsoft.DotNet.Tools.Test.Utilities.TestBase;
-//using static Microsoft.DotNet.Tools.Test.Utilities.TestCommandExtensions;
-
 namespace EndToEnd.Tests
 {
     public class GivenWindowsApp(ITestOutputHelper log) : SdkTest(log)
@@ -21,34 +14,23 @@ namespace EndToEnd.Tests
         [InlineData("10.0.22621.0")]
         public void ItCanBuildAndRun(string targetPlatformVersion)
         {
-            //var testInstance = TestAssets.Get(TestAssetKinds.TestProjects, "UseCswinrt")
-            //var testInstance = _testAssetsManager.
-            //    .CreateInstance("UseCswinrt" + targetPlatformVersion)
-            //    .WithSourceFiles();
-
-            //var testInstance = _testAssetsManager
-            //    .CopyTestAsset($"UseCswinrt{targetPlatformVersion}", testAssetSubdirectory: TestAssetSubdirectories.TestProjects)
-            //    .WithSource();
-
             var testInstance = _testAssetsManager
-                .CreateTestProject(new TestProject($"UseCswinrt{targetPlatformVersion}"))
+                .CopyTestAsset("UseCswinrt", identifier: targetPlatformVersion)
                 .WithSource();
 
-            //var projectPath = Path.Combine(testInstance.Path, "consolecswinrt.csproj");
-            var projectPath = Path.Combine(testInstance.Path, "UseCswinrt.csproj");
+            var projectPath = Path.Combine(testInstance.Path, "consolecswinrt.csproj");
             var project = XDocument.Load(projectPath);
             var ns = project.Root.Name.Namespace;
 
             //  Update TargetFramework to the right version of .NET Core
             project.Root.Element(ns + "PropertyGroup")
                 .Add(new XElement(ns + "TargetPlatformVersion", targetPlatformVersion));
-            //project.Root.Element(ns + "PropertyGroup")
-            //    .Element(ns + "TargetFramework").Value = TestAssetInfo.currentTfm;
+            project.Root.Element(ns + "PropertyGroup")
+                .Element(ns + "TargetFramework").Value = "net9.0";
             project.Save(projectPath);
 
-            new BuildCommand(Log, projectPath)
-                    //.WithProjectFile(new FileInfo(testInstance.Root.FullName))
-                    .Execute().Should().Pass();
+            new BuildCommand(Log, testInstance.TestRoot)
+                .Execute().Should().Pass();
 
             new DotnetCommand(Log, "run")
                 .WithWorkingDirectory(testInstance.Path)
