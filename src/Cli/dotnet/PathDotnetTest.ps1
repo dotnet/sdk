a2df997f0eb060766047a63f4da6da05e3a35ef8
mariam-abdulla <mabdullah@microsoft.com>
2024-07-18 10:37:33 +0200

    remove unused files

--- src/Cli/dotnet/PathDotnetTest.ps1
+++
@@ -1,3 +0,0 @@
-﻿& dotnet build
-Copy-Item C:\Code\sdk\artifacts\bin\redist\Debug\dotnet\sdk\9.0.100-dev\Sdks\Microsoft.NET.Sdk\tools\net9.0\Microsoft.NET.Build.Tasks.dll -Destination C:\code\sdk\artifacts\bin\redist\Debug\dotnet\sdk\9.0.100-dev
-Copy-Item C:\code\sdk\artifacts\bin\dotnet\Debug\net9.0\dotnet.dll -Destination  C:\code\sdk\artifacts\bin\redist\Debug\dotnet\sdk\9.0.100-dev
