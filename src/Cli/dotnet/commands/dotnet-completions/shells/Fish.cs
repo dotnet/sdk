2d288774933440f3bb9009cc3b5c01522997c0b1
Chet Husk <chusk3@gmail.com>
2024-07-27 17:37:04 -0500

    Spike out initial completion script emit commands

---
+++ src/Cli/dotnet/commands/dotnet-completions/shells/Fish.cs
@@ -0,0 +1,33 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.Cli.Completions.Shells;
+
+public class FishShellProvider : IShellProvider
+{
+    public string ArgumentName => "fish";
+
+    private static readonly string _dynamicCompletionScript =
+        """
+        # zsh parameter completion for the dotnet CLI
+
+        _dotnet_zsh_complete()
+        {
+        local completions=("$(dotnet complete "$words")")
+
+        # If the completion list is empty, just continue with filename selection
+        if [ -z "$completions" ]
+        then
+            _arguments '*::arguments: _normal'
+            return
+        fi
+
+        # This is not a variable assignment, don't remove spaces!
+        _values = "${(ps:\n:)completions}"
+        }
+
+        compdef _dotnet_zsh_complete dotnet
+        """;
+
+    public string GenerateCompletions(System.CommandLine.CliCommand command) => _dynamicCompletionScript;
+}
