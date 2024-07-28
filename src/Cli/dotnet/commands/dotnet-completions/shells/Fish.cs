bbc0196d619bdf9fe8f01460c4e2957f92420dad
Chet Husk <chusk3@gmail.com>
2024-07-27 20:25:05 -0500

    Update contents to include locations and commented instructions where possible

--- src/Cli/dotnet/commands/dotnet-completions/shells/Fish.cs
+++ src/Cli/dotnet/commands/dotnet-completions/shells/Fish.cs
@@ -9,24 +9,10 @@ public class FishShellProvider : IShellProvider
 
     private static readonly string _dynamicCompletionScript =
         """
-        # zsh parameter completion for the dotnet CLI
+        # fish parameter completion for the dotnet CLI
+        # add the following to your config.fish to enable completions
 
-        _dotnet_zsh_complete()
-        {
-        local completions=("$(dotnet complete "$words")")
-
-        # If the completion list is empty, just continue with filename selection
-        if [ -z "$completions" ]
-        then
-            _arguments '*::arguments: _normal'
-            return
-        fi
-
-        # This is not a variable assignment, don't remove spaces!
-        _values = "${(ps:\n:)completions}"
-        }
-
-        compdef _dotnet_zsh_complete dotnet
+        complete -f -c dotnet -a "(dotnet complete (commandline -cp))"
         """;
 
     public string GenerateCompletions(System.CommandLine.CliCommand command) => _dynamicCompletionScript;
