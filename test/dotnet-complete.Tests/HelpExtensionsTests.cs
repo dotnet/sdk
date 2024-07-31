9ceeaa9cb605b3e7ecbc8f714126b6da432741a0
Chet Husk <chusk3@gmail.com>
2024-07-31 16:34:26 -0500

    Add tests for shell completion helpers

---
+++ test/dotnet-complete.Tests/HelpExtensionsTests.cs
@@ -0,0 +1,46 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.Cli.Complete.Tests;
+
+using Microsoft.DotNet.Cli.Completions.Shells;
+using System.CommandLine;
+using System.CommandLine.Help;
+
+public class HelpExtensionsTests
+{
+    [Fact]
+    public void HelpOptionOnlyShowsUsefulNames()
+    {
+        new HelpOption().Names().Should().BeEquivalentTo(["--help", "-h"]);
+    }
+
+    [Fact]
+    public void OptionNamesListNameThenAliases()
+    {
+        new CliOption<string>("--name", "-n", "--nombre").Names().Should().Equal(["--name", "-n", "--nombre"]);
+    }
+
+    [Fact]
+    public void OptionsWithNoAliasesHaveOnlyOneName()
+    {
+        new CliOption<string>("--name").Names().Should().Equal(["--name"]);
+    }
+
+    [Fact]
+    public void HeirarchicalOptionsAreFlattened()
+    {
+        var parentCommand = new CliCommand("parent");
+        var childCommand = new CliCommand("child");
+        parentCommand.Subcommands.Add(childCommand);
+        parentCommand.Options.Add(new CliOption<string>("--parent-global") { Recursive = true });
+        parentCommand.Options.Add(new CliOption<string>("--parent-local") { Recursive = false });
+        parentCommand.Options.Add(new CliOption<string>("--parent-global-but-hidden") { Recursive = true, Hidden = true });
+
+        childCommand.Options.Add(new CliOption<string>("--child-local"));
+        childCommand.Options.Add(new CliOption<string>("--child-hidden") { Hidden = true });
+
+        // note: no parent-local or parent-global-but-hidden options, and no locally hidden options
+        childCommand.HeirarchicalOptions().Select(c => c.Name).Should().Equal(["--parent-global", "--child-local"]);
+    }
+}
