1bcdc05906286789993c2682b504aaf342ccbea2
Jerome Laban <jerome@platform.uno>
2024-08-02 10:57:50 -0400

    test: Add handler validation

--- test/TestAssets/TestProjects/WatchAppMissingAssemblyFailure/Dep/Dep.cs
+++ test/TestAssets/TestProjects/WatchAppMissingAssemblyFailure/Dep/Dep.cs
@@ -1,7 +1,23 @@
-﻿public class Dep
+﻿namespace Dep;
+
+public class DepType
 {
     void F()
     {
         Console.WriteLine(1);
+    }
+}
+
+static class UpdateHandler
+{
+    // Lock to avoid the updated Print method executing concurrently with the update handler.
+    public static object Guard = new object();
+
+    public static void UpdateApplication(Type[] types)
+    {
+        lock (Guard)
+        {
+            Console.WriteLine($"Dep Updated types: {(types == null ? "<null>" : types.Length == 0 ? "<empty>" : string.Join(",", types.Select(t => t.Name)))}");
+        }
     }
 }
