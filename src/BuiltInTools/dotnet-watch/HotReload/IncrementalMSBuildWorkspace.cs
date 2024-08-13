213c3fae3d6fa836ec1507f5913859b8069faac1
Tomas Matousek <tomat@microsoft.com>
2024-08-13 17:41:32 +0200

    Hot Reload: Handle file deletions correctly

--- src/BuiltInTools/dotnet-watch/HotReload/IncrementalMSBuildWorkspace.cs
+++ src/BuiltInTools/dotnet-watch/HotReload/IncrementalMSBuildWorkspace.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Tools.Internal;
 using Microsoft.CodeAnalysis.ExternalAccess.Watch.Api;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.DotNet.Watcher.Internal;
 
 namespace Microsoft.DotNet.Watcher.Tools;
 
@@ -106,13 +107,24 @@ internal class IncrementalMSBuildWorkspace : Workspace
             }).ToImmutableArray();
     }
 
-    public async ValueTask UpdateFileContentAsync(IEnumerable<FileItem> changedFiles, CancellationToken cancellationToken)
+    public async ValueTask UpdateFileContentAsync(IEnumerable<ChangedFile> changedFiles, CancellationToken cancellationToken)
     {
         var updatedSolution = CurrentSolution;
 
-        foreach (var changedFile in changedFiles)
+        var documentsToRemove = new List<DocumentId>();
+
+        foreach (var (changedFile, change) in changedFiles)
         {
+            // when a file is added we reevaluate the project:
+            Debug.Assert(change != ChangeKind.Add);
+
             var documentIds = updatedSolution.GetDocumentIdsWithFilePath(changedFile.FilePath);
+            if (change == ChangeKind.Delete)
+            {
+                documentsToRemove.AddRange(documentIds);
+                continue;
+            }
+
             foreach (var documentId in documentIds)
             {
                 var textDocument = updatedSolution.GetDocument(documentId)
@@ -140,8 +152,16 @@ internal class IncrementalMSBuildWorkspace : Workspace
             }
         }
 
+        updatedSolution = RemoveDocuments(updatedSolution, documentsToRemove);
+
         await ReportSolutionFilesAsync(SetCurrentSolution(updatedSolution), cancellationToken);
     }
+
+    private static Solution RemoveDocuments(Solution solution, IEnumerable<DocumentId> ids)
+        => solution
+        .RemoveDocuments(ids.Where(id => solution.GetDocument(id) != null).ToImmutableArray())
+        .RemoveAdditionalDocuments(ids.Where(id => solution.GetAdditionalDocument(id) != null).ToImmutableArray())
+        .RemoveAnalyzerConfigDocuments(ids.Where(id => solution.GetAnalyzerConfigDocument(id) != null).ToImmutableArray());
 
     private static async ValueTask<SourceText> GetSourceTextAsync(string filePath, CancellationToken cancellationToken)
     {
