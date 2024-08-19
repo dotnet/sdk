ffbf797561f6f7dee43b8c45baef674d9ab5fc85
Phil Henning <phenning@microsoft.com>
2024-08-19 10:00:25 -0400

    Add RemoveJsonPropertyPostAction for symmetry with AddJsonPropertyPostAction

---
+++ src/Cli/Microsoft.TemplateEngine.Cli/PostActionProcessors/RemoveJsonPropertyPostActionProcessor.cs
@@ -0,0 +1,202 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Abstractions.PhysicalFileSystem;
+
+namespace Microsoft.TemplateEngine.Cli.PostActionProcessors
+{
+    internal class RemoveJsonPropertyPostActionProcessor : PostActionProcessorBase
+    {
+        private const string JsonFileNameArgument = "jsonFileName";
+        private const string ParentPropertyPathArgument = "parentPropertyPath";
+        private const string JsonPropertyNameArgument = "jsonPropertyName";
+
+        private static readonly JsonSerializerOptions SerializerOptions = new()
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true
+        };
+
+        private static readonly JsonDocumentOptions DeserializerOptions = new()
+        {
+            AllowTrailingCommas = true,
+            CommentHandling = JsonCommentHandling.Skip
+        };
+
+        public override Guid Id => ActionProcessorId;
+
+        internal static Guid ActionProcessorId { get; } = new Guid("73406DFC-1255-4798-86E8-DF66AB9F7A18");
+
+        protected override bool ProcessInternal(
+            IEngineEnvironmentSettings environment,
+            IPostAction action,
+            ICreationEffects creationEffects,
+            ICreationResult templateCreationResult,
+            string outputBasePath)
+        {
+            if (!action.Args.TryGetValue(JsonFileNameArgument, out string? jsonFileName))
+            {
+                Reporter.Error.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Error_ArgumentNotConfigured, JsonFileNameArgument));
+                return false;
+            }
+
+            IReadOnlyList<string> jsonFiles = FindFilesInCurrentFolderOrParentFolder(environment.Host.FileSystem, outputBasePath, jsonFileName);
+
+            if (jsonFiles.Count == 0)
+            {
+                return true;
+            }
+
+            if (jsonFiles.Count > 1)
+            {
+                Reporter.Error.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Error_MultipleJsonFiles, jsonFileName));
+                return false;
+            }
+
+            var jsonElementProperties = JsonContentParameters.CreateFromPostAction(action);
+
+            if (jsonElementProperties is null)
+            {
+                return false;
+            }
+
+            JsonNode? newJsonContent = RemoveElementFromJson(
+                environment.Host.FileSystem,
+                jsonFiles[0],
+                jsonElementProperties!.ParentProperty,
+                ":",
+                jsonElementProperties.JsonPropertyName,
+                action);
+
+            if (newJsonContent is null)
+            {
+                return false;
+            }
+
+            environment.Host.FileSystem.WriteAllText(jsonFiles[0], newJsonContent.ToJsonString(SerializerOptions));
+
+            Reporter.Output.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Succeeded, jsonFileName));
+
+            return true;
+        }
+
+        private static JsonNode? RemoveElementFromJson(IPhysicalFileSystem fileSystem, string targetJsonFile, string? propertyPath, string propertyPathSeparator, string propertyName, IPostAction action)
+        {
+            JsonNode? jsonContent = JsonNode.Parse(fileSystem.ReadAllText(targetJsonFile), nodeOptions: null, documentOptions: DeserializerOptions);
+
+            if (jsonContent is null)
+            {
+                return null;
+            }
+
+            JsonNode? parentProperty = FindJsonNode(jsonContent, propertyPath, propertyPathSeparator, createPath);
+
+            if (parentProperty is not null && parentProperty[propertyName] is not null)
+            {
+                parentProperty.AsObject().Remove(propertyName);
+            }
+
+            return jsonContent;
+        }
+
+        private static JsonNode? FindJsonNode(JsonNode content, string? nodePath, string pathSeparator, bool createPath)
+        {
+            if (nodePath is null)
+            {
+                return content;
+            }
+
+            string[] properties = nodePath.Split(pathSeparator);
+
+            JsonNode? node = content;
+
+            foreach (string property in properties)
+            {
+                if (node is null)
+                {
+                    return null;
+                }
+
+                JsonNode? childNode = node[property];
+                if (childNode is null && createPath)
+                {
+                    node[property] = childNode = new JsonObject();
+                }
+
+                node = childNode;
+            }
+
+            return node;
+        }
+
+        private static string[] FindFilesInCurrentFolderOrParentFolder(
+            IPhysicalFileSystem fileSystem,
+            string startPath,
+            string matchPattern)
+        {
+            string? directory = fileSystem.DirectoryExists(startPath) ? startPath : Path.GetDirectoryName(startPath);
+
+            if (directory is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int numberOfUpLevels = 0;
+
+            do
+            {
+                Reporter.Verbose.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Verbose_AttemptingToFindJsonFile, matchPattern, directory));
+                string[] filesInDir = fileSystem.EnumerateFileSystemEntries(directory, matchPattern, SearchOption.AllDirectories).ToArray();
+
+                if (filesInDir.Length > 0)
+                {
+                    return filesInDir;
+                }
+
+                directory = Path.GetPathRoot(directory) != directory ? Directory.GetParent(directory)?.FullName : null;
+                numberOfUpLevels++;
+            }
+            while (directory is not null && numberOfUpLevels <= 1);
+
+            return Array.Empty<string>();
+        }
+
+        private class JsonContentParameters
+        {
+            private JsonContentParameters(string? parentProperty, string jsonPropertyName)
+            {
+                ParentProperty = parentProperty;
+                JsonPropertyName = jsonPropertyName;
+            }
+
+            public string? ParentProperty { get; }
+
+            public string JsonPropertyName { get; }
+
+            /// <summary>
+            /// Creates an instance of <see cref="JsonContentParameters"/> based on the configured arguments in the Post Action.
+            /// </summary>
+            /// <param name="action"></param>
+            /// <returns>A <see cref="JsonContentParameters"/> instance, or null if no instance could be created.</returns>
+            public static JsonContentParameters? CreateFromPostAction(IPostAction action)
+            {
+                action.Args.TryGetValue(ParentPropertyPathArgument, out string? parentProperty);
+                action.Args.TryGetValue(JsonPropertyNameArgument, out string? jsonPropertyName);
+
+                if (jsonPropertyName is null)
+                {
+                    Reporter.Error.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Error_ArgumentNotConfigured, JsonPropertyNameArgument));
+                    return null;
+                }
+
+                return new JsonContentParameters(parentProperty, jsonPropertyName);
+            }
+        }
+    }
+}
---
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/PostActionTests/RemoveJsonPropertyPostActionTests.cs
@@ -0,0 +1,277 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json.Nodes;
+
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Cli.PostActionProcessors;
+using Microsoft.TemplateEngine.Mocks;
+using Microsoft.TemplateEngine.TestHelper;
+
+using Moq;
+
+namespace Microsoft.TemplateEngine.Cli.UnitTests.PostActionTests
+{
+    public class RemoveJsonPropertyPostActionTests : IClassFixture<EnvironmentSettingsHelper>
+    {
+        private readonly IEngineEnvironmentSettings _engineEnvironmentSettings;
+
+        public RemoveJsonPropertyPostActionTests(EnvironmentSettingsHelper environmentSettingsHelper)
+        {
+            _engineEnvironmentSettings = environmentSettingsHelper.CreateEnvironment(hostIdentifier: GetType().Name, virtualize: true);
+        }
+
+        [Fact]
+        public void FailsWhenParentPropertyPathIsInvalid()
+        {
+            string targetBasePath = _engineEnvironmentSettings.GetTempVirtualizedPath();
+
+            CreateJsonFile(targetBasePath, "json.json", @"{""property1"":{""property2"":{""property3"":""foo""}}}");
+
+            string parentPropertyPath = "property1:propertyX:property2";
+
+            IPostAction postAction = new MockPostAction(default, default, default, default, default!)
+            {
+                ActionId = RemoveJsonPropertyPostActionProcessor.ActionProcessorId,
+                Args = new Dictionary<string, string>
+                {
+                    ["jsonFileName"] = "json.json",
+                    ["parentPropertyPath"] = parentPropertyPath,
+                    ["jsonPropertyName"] = "property3",
+                }
+            };
+
+            Mock<IReporter> mockReporter = new();
+
+            mockReporter.Setup(r => r.WriteLine(It.IsAny<string>()))
+                .Verifiable();
+
+            Reporter.SetError(mockReporter.Object);
+
+            RemoveJsonPropertyPostActionProcessor processor = new();
+
+            bool result = processor.Process(
+                _engineEnvironmentSettings,
+                postAction,
+                new MockCreationEffects(),
+                new MockCreationResult(),
+                targetBasePath);
+
+            Assert.False(result);
+            mockReporter.Verify(r => r.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Error_ParentPropertyPathInvalid, parentPropertyPath)), Times.Once);
+        }
+
+        [Fact]
+        public void FailsWhenPropertyPathCasingIsNotCorrect()
+        {
+            string targetBasePath = _engineEnvironmentSettings.GetTempVirtualizedPath();
+
+            string originalJsonContent = @"{""property1"":{""property2"":{""property3"":""foo""}}}";
+
+            string jsonFilePath = CreateJsonFile(targetBasePath, "json.json", originalJsonContent);
+
+            IPostAction postAction = new MockPostAction(default, default, default, default, default!)
+            {
+                ActionId = RemoveJsonPropertyPostActionProcessor.ActionProcessorId,
+                Args = new Dictionary<string, string>
+                {
+                    ["jsonFileName"] = "json.json",
+                    ["parentPropertyPath"] = "property1:Property2",
+                    ["jsonPropertyName"] = "property3",
+                }
+            };
+
+            RemoveJsonPropertyPostActionProcessor processor = new();
+
+            bool result = processor.Process(
+                _engineEnvironmentSettings,
+                postAction,
+                new MockCreationEffects(),
+                new MockCreationResult(),
+                targetBasePath);
+
+            Assert.False(result);
+
+            Assert.Equal(originalJsonContent, _engineEnvironmentSettings.Host.FileSystem.ReadAllText(jsonFilePath));
+        }
+
+        [Theory]
+        [MemberData(nameof(RemoveJsonPropertyPostActionTestCase<Mock<IReporter>>.InvalidConfigurationTestCases), MemberType = typeof(ModifyJsonPostActionTestCase<Mock<IReporter>>))]
+        public void FailsWhenMandatoryArgumentsNotConfigured(ModifyJsonPostActionTestCase<Mock<IReporter>> testCase)
+        {
+            string targetBasePath = _engineEnvironmentSettings.GetTempVirtualizedPath();
+
+            CreateJsonFile(targetBasePath, "file.json", testCase.OriginalJsonContent);
+
+            IPostAction postAction = new MockPostAction(default, default, default, default, default!)
+            {
+                ActionId = RemoveJsonPropertyPostActionProcessor.ActionProcessorId,
+                Args = testCase.PostActionArgs
+            };
+
+            Mock<IReporter> mockReporter = new();
+
+            mockReporter.Setup(r => r.WriteLine(It.IsAny<string>()))
+                .Verifiable();
+
+            Reporter.SetError(mockReporter.Object);
+
+            RemoveJsonPropertyPostActionProcessor processor = new();
+
+            bool result = processor.Process(
+                _engineEnvironmentSettings,
+                postAction,
+                new MockCreationEffects(),
+                new MockCreationResult(),
+                targetBasePath);
+
+            Assert.False(result);
+
+            testCase.AssertionCallback(mockReporter);
+        }
+
+        [Theory]
+        [MemberData(nameof(RemoveJsonPropertyPostActionTestCase<(JsonNode, bool)>.SuccessTestCases), MemberType = typeof(ModifyJsonPostActionTestCase<(JsonNode, bool)>))]
+        public void CanSuccessfullyModifyJsonFile(ModifyJsonPostActionTestCase<(JsonNode, bool)> testCase)
+        {
+            string targetBasePath = _engineEnvironmentSettings.GetTempVirtualizedPath();
+
+            string? jsonFileName = testCase.PostActionArgs["jsonFileName"];
+
+            Assert.NotNull(jsonFileName);
+
+            string jsonFilePath = CreateJsonFile(targetBasePath, jsonFileName, testCase.OriginalJsonContent);
+
+            IPostAction postAction = new MockPostAction(default, default, default, default, default!)
+            {
+                ActionId = RemoveJsonPropertyPostActionProcessor.ActionProcessorId,
+                Args = testCase.PostActionArgs
+            };
+
+            RemoveJsonPropertyPostActionProcessor processor = new();
+
+            bool result = processor.Process(
+                _engineEnvironmentSettings,
+                postAction,
+                new MockCreationEffects(),
+                new MockCreationResult(),
+                targetBasePath);
+
+            Assert.True(result);
+
+            JsonNode? modifiedJsonContent = JsonNode.Parse(_engineEnvironmentSettings.Host.FileSystem.ReadAllText(jsonFilePath));
+
+            Assert.NotNull(modifiedJsonContent);
+
+            testCase.AssertionCallback((modifiedJsonContent, false));
+        }
+
+        private string CreateJsonFile(string targetBasePath, string fileName, string jsonContent)
+        {
+            string jsonFileFullPath = Path.Combine(targetBasePath, fileName);
+            _engineEnvironmentSettings.Host.FileSystem.WriteAllText(jsonFileFullPath, jsonContent);
+
+            return jsonFileFullPath;
+        }
+    }
+
+    public record RemoveJsonPropertyPostActionTestCase<TState>(
+        string TestCaseDescription,
+        string OriginalJsonContent,
+        Dictionary<string, string> PostActionArgs,
+        Action<TState> AssertionCallback)
+    {
+        private static readonly RemoveJsonPropertyPostActionTestCase<(JsonNode ResultingJson, bool IsNewJson)>[] _successTestCases =
+        {
+            new(
+                "Can remove simple property",
+                @"{""person"":{""name"":""bob"",""address"":{""city"":""redmond"",""zip"":""98052""}}}",
+                new Dictionary<string, string>
+                {
+                    ["jsonFileName"] = "jsonfile.json",
+                    ["parentPropertyPath"] = "person",
+                    ["jsonPropertyName"] = "name"
+                },
+                tuple =>
+                {
+                    Assert.Null(tuple.ResultingJson["person"]!["name"]);
+                }),
+
+            new(
+                "Can remove complex property",
+                @"{""person"":{""name"":""bob"",""address"":{""city"":""redmond"",""zip"":""98052""}}}",
+                new Dictionary<string, string>
+                {
+                    ["jsonFileName"] = "jsonfile.json",
+                    ["parentPropertyPath"] = "person",
+                    ["jsonPropertyName"] = "address",
+                },
+                tuple =>
+                {
+                    Assert.Null(tuple.ResultingJson["person"]!["address"]);
+                }),
+
+            new(
+                "Can remove property at root",
+                @"{""person"":{""name"":""bob"",""address"":{""city"":""redmond"",""zip"":""98052""}}}",
+                new Dictionary<string, string>
+                {
+                    ["jsonFileName"] = "jsonfile.json",
+                    ["parentPropertyPath"] = null!,
+                    ["jsonPropertyName"] = "person",
+                },
+                tuple =>
+                {
+                    Assert.Null(tuple.ResultingJson["person"]);
+                }),
+        };
+
+        private static readonly RemoveJsonPropertyPostActionTestCase<Mock<IReporter>>[] _invalidConfigurationTestCases =
+        {
+            new(
+                "JsonFileName argument not configured",
+                @"{}",
+                new Dictionary<string, string>
+                {
+                    ["parentPropertyPath"] = "person",
+                    ["jsonPropertyName"] = "lastName",
+                },
+                (Mock<IReporter> errorReporter) =>
+                {
+                    errorReporter.Verify(r => r.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Error_ArgumentNotConfigured, "jsonFileName")), Times.Once);
+                }),
+
+            new(
+                "JsonPropertyName argument not configured",
+                @"{}",
+                new Dictionary<string, string>
+                {
+                    ["jsonFileName"] = "file.json",
+                    ["parentPropertyPath"] = "person",
+                },
+                (Mock<IReporter> errorReporter) =>
+                {
+                    errorReporter.Verify(r => r.WriteLine(string.Format(LocalizableStrings.PostAction_ModifyJson_Error_ArgumentNotConfigured, "newJsonPropertyName")), Times.Once);
+                }),
+        };
+
+        public override string ToString() => TestCaseDescription;
+
+        public static IEnumerable<object[]> SuccessTestCases()
+        {
+            foreach (RemoveJsonPropertyPostActionTestCase<(JsonNode, bool)> testCase in _successTestCases)
+            {
+                yield return new[] { testCase };
+            }
+        }
+
+        public static IEnumerable<object[]> InvalidConfigurationTestCases()
+        {
+            foreach (RemoveJsonPropertyPostActionTestCase<Mock<IReporter>> testCase in _invalidConfigurationTestCases)
+            {
+                yield return new[] { testCase };
+            }
+        }
+    }
+}
