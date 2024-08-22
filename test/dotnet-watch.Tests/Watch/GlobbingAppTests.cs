311c7d9b27a3f1da3147d12b3965b95877201254
Chet Husk <chusk3@gmail.com>
2024-08-21 19:58:29 -0500

    disable one more test that works on local testing

--- test/dotnet-watch.Tests/Watch/GlobbingAppTests.cs
+++ test/dotnet-watch.Tests/Watch/GlobbingAppTests.cs
@@ -53,7 +53,7 @@ namespace Microsoft.DotNet.Watcher.Tests
             await AssertCompiledAppDefinedTypes(expected: 1);
         }
 
-        [Fact]
+        [Fact(Skip = "https://github.com/dotnet/sdk/issues/42921")]
         public async Task DeleteSourceFolder()
         {
             var testAsset = TestAssets.CopyTestAsset(AppName)
