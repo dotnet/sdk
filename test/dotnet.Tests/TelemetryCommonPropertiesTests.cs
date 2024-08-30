4282f755b534bf659998e6fc85162b5280e0c545
Marc Paine <marcpop@microsoft.com>
2024-08-30 16:11:15 -0700

    Add Dev Device ID

--- test/dotnet.Tests/TelemetryCommonPropertiesTests.cs
+++ test/dotnet.Tests/TelemetryCommonPropertiesTests.cs
@@ -34,10 +34,26 @@ namespace Microsoft.DotNet.Tests
         }
 
         [Fact]
+        public void TelemetryCommonPropertiesShouldReturnDevDeviceId()
+        {
+            var unitUnderTest = new TelemetryCommonProperties(getDeviceId: () => "plaintext", userLevelCacheWriter: new NothingCache());
+            unitUnderTest.GetTelemetryCommonProperties()["DevDeviceId"].Should().Be("plaintext");
+        }
+
+        [Fact]
         public void TelemetryCommonPropertiesShouldReturnNewGuidWhenCannotGetMacAddress()
         {
             var unitUnderTest = new TelemetryCommonProperties(getMACAddress: () => null, userLevelCacheWriter: new NothingCache());
             var assignedMachineId = unitUnderTest.GetTelemetryCommonProperties()["Machine ID"];
+
+            Guid.TryParse(assignedMachineId, out var _).Should().BeTrue("it should be a guid");
+        }
+
+        [Fact]
+        public void TelemetryCommonPropertiesShouldReturnNewGuidWhenCannotDevDeviceId()
+        {
+            var unitUnderTest = new TelemetryCommonProperties(getDeviceId: () => null, userLevelCacheWriter: new NothingCache());
+            var assignedMachineId = unitUnderTest.GetTelemetryCommonProperties()["DevDeviceId"];
 
             Guid.TryParse(assignedMachineId, out var _).Should().BeTrue("it should be a guid");
         }
