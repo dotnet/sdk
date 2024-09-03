bf40575909d2c0b2f84dc7ef4735435f98f6defe
Mariam Abdullah <122357303+mariam-abdulla@users.noreply.github.com>
2024-09-03 08:23:43 +0200

    Execute dotnet run behind the hood in dotnet test (#43170)

--- src/Cli/dotnet/commands/dotnet-test/CliConstants.cs
+++ src/Cli/dotnet/commands/dotnet-test/CliConstants.cs
@@ -5,17 +5,17 @@ namespace Microsoft.DotNet.Cli
 {
     internal static class CliConstants
     {
+        public const string DotnetRunCommand = "dotnet run";
         public const string HelpOptionKey = "--help";
-        public const string MSBuildOptionKey = "--msbuild-params";
-        public const string NoBuildOptionKey = "--no-build";
         public const string ServerOptionKey = "--server";
         public const string DotNetTestPipeOptionKey = "--dotnet-test-pipe";
-        public const string DegreeOfParallelismOptionKey = "--degree-of-parallelism";
-        public const string DOPOptionKey = "--dop";
+        public const string ProjectOptionKey = "--project";
+        public const string FrameworkOptionKey = "--framework";
 
         public const string ServerOptionValue = "dotnettestcli";
 
         public const string MSBuildExeName = "MSBuild.dll";
+        public const string ParametersSeparator = "--";
     }
 
     internal static class TestStates
