a336fe74895563a26645744cafed2336bda1a71e
Jerome Laban <jerome@platform.uno>
2024-09-10 09:08:44 -0400

    chore: Adjust for disabled test https://github.com/dotnet/sdk/issues/42850

--- test/dotnet-watch.Tests/HotReload/ApplyDeltaTests.cs
+++ test/dotnet-watch.Tests/HotReload/ApplyDeltaTests.cs
@@ -122,7 +122,7 @@ namespace Microsoft.DotNet.Watcher.Tests
         }
 
         // Test is timing out on .NET Framework: https://github.com/dotnet/sdk/issues/41669
-        [CoreMSBuildOnlyFact]
+        [CoreMSBuildOnlyFact(Skip = "https://github.com/dotnet/sdk/issues/42850")]
         public async Task HandleMissingAssemblyFailure()
         {
             var testAsset = TestAssets.CopyTestAsset("WatchAppMissingAssemblyFailure")
