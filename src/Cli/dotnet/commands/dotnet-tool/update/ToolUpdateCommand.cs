b492f08f7b276fe9ef1b00b726abbaee2ab5724b
Noah Gilson <OTAKUPENGUINOP@GMAIL.COM>
2024-09-18 09:44:58 -0700

    Revert "[release/8.0.4xx] Fix NullReferenceException for `dotnet tool update -g --all`"

--- src/Cli/dotnet/commands/dotnet-tool/update/ToolUpdateCommand.cs
+++ src/Cli/dotnet/commands/dotnet-tool/update/ToolUpdateCommand.cs
@@ -8,7 +8,6 @@ using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.ToolManifest;
 using Microsoft.DotNet.ToolPackage;
 using Microsoft.DotNet.Tools.Tool.Common;
-using Microsoft.Extensions.EnvironmentAbstractions;
 using CreateShellShimRepository = Microsoft.DotNet.Tools.Tool.Install.CreateShellShimRepository;
 
 namespace Microsoft.DotNet.Tools.Tool.Update
@@ -44,17 +43,16 @@ namespace Microsoft.DotNet.Tools.Tool.Update
                         localToolsResolverCache,
                         reporter);
 
-            _global = result.GetValue(ToolInstallCommandParser.GlobalOption);
-            _toolPath = result.GetValue(ToolInstallCommandParser.ToolPathOption);
-            DirectoryPath? location = string.IsNullOrWhiteSpace(_toolPath) ? null : new DirectoryPath(_toolPath);
             _toolUpdateGlobalOrToolPathCommand =
                 toolUpdateGlobalOrToolPathCommand
                 ?? new ToolUpdateGlobalOrToolPathCommand(
                     result,
                     createToolPackageStoreDownloaderUninstaller,
                     createShellShimRepository,
-                    reporter,
-                    ToolPackageFactory.CreateToolPackageStoreQuery(location));
+                    reporter);
+
+            _global = result.GetValue(ToolInstallCommandParser.GlobalOption);
+            _toolPath = result.GetValue(ToolInstallCommandParser.ToolPathOption);
         }
 
 
