61bbc4bc3d7719d849133ac049f6b99d666db703
Daniel Plaisted <daplaist@microsoft.com>
2024-09-26 19:44:57 -0400

    Correctly calculate baseline workload set feature band

---
+++ src/Installer/core-sdk-tasks/GetWorkloadSetFeatureBand.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class GetWorkloadSetFeatureBand : Task
+    {
+        [Required]
+        public string WorkloadSetVersion { get; set; }
+
+        [Output]
+        public string WorkloadSetFeatureBand { get; set; }
+
+        public override bool Execute()
+        {
+            WorkloadSetFeatureBand = Microsoft.DotNet.Workloads.Workload.WorkloadSetVersion.GetFeatureBand(WorkloadSetVersion).ToString();
+            return true;
+        }
+    }
+}
