937cc41d6f077cb2d4ef586aa8d787056e3925e4
Manodasan Wignarajah <mawign@microsoft.com>
2024-10-01 00:53:44 -0700

    Add test

--- test/EndToEnd.Tests/GivenWindowsApp.cs
+++ test/EndToEnd.Tests/GivenWindowsApp.cs
@@ -14,9 +14,9 @@ namespace EndToEnd.Tests
         [InlineData("10.0.20348.0")]
         [InlineData("10.0.22000.0")]
         [InlineData("10.0.22621.0")]
-        // Skipped due to: https://github.com/dotnet/sdk/pull/42090/files#r1680016439
-        //[InlineData("10.0.26100.0")]
-        public void ItCanBuildAndRun(string targetPlatformVersion)
+        [InlineData("10.0.26100.0")]
+        [InlineData("10.0.22621.0", "34")]
+        public void ItCanBuildAndRun(string targetPlatformVersion, string packageVersion = "")
         {
             var testInstance = _testAssetsManager
                 .CopyTestAsset("UseCswinrt", identifier: targetPlatformVersion)
@@ -31,8 +31,20 @@ namespace EndToEnd.Tests
                 .Add(new XElement(ns + "TargetPlatformVersion", targetPlatformVersion));
             project.Root.Element(ns + "PropertyGroup")
                 .Element(ns + "TargetFramework").Value = ToolsetInfo.CurrentTargetFramework;
-            project.Root.Element(ns + "PropertyGroup")
-                .Element(ns + "WindowsSdkPackageVersion").Value = targetPlatformVersion[..^1] + "39"; // Temporary until new projections flow to tests
+
+            if (!string.IsNullOrEmpty(packageVersion))
+            {
+                // Used to test older versions of the package to make sure they can still be referenced.
+                // This currently tests the version before profile support was added to our package.
+                project.Root.Element(ns + "PropertyGroup")
+                    .Element(ns + "WindowsSdkPackageVersion").Value = targetPlatformVersion[..^1] + packageVersion;
+            }
+            else
+            {
+                project.Root.Element(ns + "PropertyGroup")
+                    .Element(ns + "WindowsSdkPackageVersion").Value = targetPlatformVersion[..^1] + "39"; // Temporary until new projections flow to tests
+            }
+
             project.Save(projectPath);
 
             new BuildCommand(testInstance)
