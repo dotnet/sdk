5511df9368d7eb582cfdb89c291c444b5055eb1b
mariam-abdulla <mabdullah@microsoft.com>
2024-10-04 14:45:59 +0200

    Fix WriteSize() and WriteString() methods

--- src/Cli/dotnet/commands/dotnet-test/IPC/Serializers/TestSessionEventSerializer.cs
+++ src/Cli/dotnet/commands/dotnet-test/IPC/Serializers/TestSessionEventSerializer.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 #nullable enable
 
@@ -37,7 +37,7 @@ namespace Microsoft.DotNet.Tools.Test
 
             for (int i = 0; i < fieldCount; i++)
             {
-                int fieldId = ReadShort(stream);
+                ushort fieldId = ReadShort(stream);
                 int fieldSize = ReadInt(stream);
 
                 switch (fieldId)
@@ -47,11 +47,11 @@ namespace Microsoft.DotNet.Tools.Test
                         break;
 
                     case TestSessionEventFieldsId.SessionUid:
-                        sessionUid = ReadString(stream);
+                        sessionUid = ReadStringValue(stream, fieldSize);
                         break;
 
                     case TestSessionEventFieldsId.ExecutionId:
-                        executionId = ReadString(stream);
+                        executionId = ReadStringValue(stream, fieldSize);
                         break;
 
                     default:
