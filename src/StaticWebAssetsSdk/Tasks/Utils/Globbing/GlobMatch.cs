051999c5a2ee32e65c11932eafd08e94b2c2c9bb
Javier Calvarro Nelson <jacalvar@microsoft.com>
2024-10-12 11:16:39 +0200

    Replace globbing

---
+++ src/StaticWebAssetsSdk/Tasks/Utils/Globbing/GlobMatch.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.StaticWebAssets.Tasks;
+
+public struct GlobMatch(bool isMatch, string stem)
+{
+    public bool IsMatch { get; set; } = isMatch;
+    public string Pattern { get; set; } = stem;
+
+    public string GetStem(string path)
+    {
+        return null;
+    }
+}
