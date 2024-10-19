bf8d10bd6d0de082512216f713f2cb48b35d0485
tmat <tomas.matousek@microsoft.com>
2024-10-18 18:02:00 -0700

    Improve error reporting when project graph fails to load

--- test/dotnet-watch.Tests/Watch/ProgramTests.cs
+++ test/dotnet-watch.Tests/Watch/ProgramTests.cs
@@ -236,5 +236,31 @@ namespace Microsoft.DotNet.Watcher.Tests
 
             await App.AssertOutputLine(line => line.Contains("warning : The value of property is '123'", StringComparison.Ordinal));
         }
+
+        [Fact]
+        public async Task ProjectGraphLoadFailure()
+        {
+            var testAsset = TestAssets
+                .CopyTestAsset("WatchAppWithProjectDeps")
+                .WithSource()
+                .WithProjectChanges((path, proj) =>
+                {
+                    if (Path.GetFileName(path) == "App.WithDeps.csproj")
+                    {
+                        proj.Root.Descendants()
+                            .Single(e => e.Name.LocalName == "ItemGroup")
+                            .Add(XElement.Parse("""
+                            <ProjectReference Include="NonExistentDirectory\X.csproj" />
+                            """));
+                    }
+                });
+
+            App.Start(testAsset, [], "AppWithDeps");
+
+            await App.AssertOutputLineStartsWith("dotnet watch ⌚ Hot Reload disabled due to project graph load failure.");
+
+            App.AssertOutputContains(@"dotnet watch ⌚ Failed to load project graph.");
+            App.AssertOutputContains(@"AppWithDeps\NonExistentDirectory\X.csproj");
+        }
     }
 }
