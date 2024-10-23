d5a0fe6a2261880e250df64f97f4d94d67ff3602
Eduardo Villalpando Mello <eduardov@microsoft.com>
2024-10-23 13:49:57 -0700

    [ADD] dotnet sln: Migrate

---
+++ src/Cli/dotnet/commands/dotnet-sln/migrate/SlnMigrateCommand.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.VisualStudio.SolutionPersistence;
+using Microsoft.VisualStudio.SolutionPersistence.Model;
+using Microsoft.VisualStudio.SolutionPersistence.Serializer;
+
+namespace Microsoft.DotNet.Cli
+{
+    internal class SlnMigrateCommand : CommandBase
+    {
+        private readonly string _slnFileFullPath;
+        private readonly IReporter _reporter;
+        public SlnMigrateCommand(
+            ParseResult parseResult,
+            IReporter reporter)
+            : base(parseResult)
+        {
+            _slnFileFullPath = Path.GetFullPath(parseResult.GetValue(SlnCommandParser.SlnArgument));
+            _reporter = reporter ?? Reporter.Output;
+        }
+
+        public override int Execute()
+        {
+            // TODO: Localize errors and outputs
+            if (!File.Exists(_slnFileFullPath) || !Path.GetExtension(_slnFileFullPath).EndsWith("sln"))
+            {
+                throw new Exception("File not found!");
+            }
+            string slnxFileFullPath = Path.ChangeExtension(_slnFileFullPath, "slnx");
+            Task task = ConvertToSlnxAsync(_slnFileFullPath, slnxFileFullPath, CancellationToken.None);
+            _reporter.WriteLine($"Solution file {slnxFileFullPath} generated.");
+            return 0;
+        }
+
+        private static async Task ConvertToSlnxAsync(string filePath, string slnxFilePath, CancellationToken cancellationToken)
+        {
+            // See if the file is a known solution file.
+            ISolutionSerializer? serializer = SolutionSerializers.GetSerializerByMoniker(filePath);
+            if (serializer is null)
+            {
+                return;
+            }
+            SolutionModel solution = await serializer.OpenAsync(filePath, cancellationToken);
+            await SolutionSerializers.SlnXml.SaveAsync(slnxFilePath, solution, cancellationToken);
+        }
+    }
+}
