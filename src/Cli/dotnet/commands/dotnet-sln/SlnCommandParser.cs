c898476bf7b270dc639e5b13409426718fd6d267
Eduardo Villalpando Mello <eduardov@microsoft.com>
2024-10-30 11:56:17 -0700

    [ADD] dotnet solution migrate (#44419)
    Co-authored-by: kasperk81 <83082615+kasperk81@users.noreply.github.com>
    Co-authored-by: Noah Gilson <OTAKUPENGUINOP@GMAIL.COM>

--- src/Cli/dotnet/commands/dotnet-sln/SlnCommandParser.cs
+++ src/Cli/dotnet/commands/dotnet-sln/SlnCommandParser.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.Tools;
 using NuGet.Packaging;
 using LocalizableStrings = Microsoft.DotNet.Tools.Sln.LocalizableStrings;
 
@@ -37,10 +39,33 @@ namespace Microsoft.DotNet.Cli
             command.Subcommands.Add(SlnAddParser.GetCommand());
             command.Subcommands.Add(SlnListParser.GetCommand());
             command.Subcommands.Add(SlnRemoveParser.GetCommand());
+            command.Subcommands.Add(SlnMigrateCommandParser.GetCommand());
 
             command.SetAction((parseResult) => parseResult.HandleMissingCommand());
 
             return command;
+        }
+
+        internal static string GetSlnFileFullPath(string slnFileOrDirectory)
+        {
+            if (File.Exists(slnFileOrDirectory))
+            {
+                return slnFileOrDirectory;
+            }
+            if (Directory.Exists(slnFileOrDirectory))
+            {
+                var files = Directory.GetFiles(slnFileOrDirectory, "*.sln", SearchOption.TopDirectoryOnly);
+                if (files.Length == 0)
+                {
+                    throw new GracefulException(CommonLocalizableStrings.CouldNotFindSolutionIn, slnFileOrDirectory);
+                }
+                if (files.Length > 1)
+                {
+                    throw new GracefulException(CommonLocalizableStrings.MoreThanOneSolutionInDirectory, slnFileOrDirectory);
+                }
+                return files.Single().ToString();
+            }
+            throw new GracefulException(CommonLocalizableStrings.CouldNotFindSolutionOrDirectory, slnFileOrDirectory);
         }
     }
 }
