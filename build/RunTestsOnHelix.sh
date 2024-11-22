0e57aa0866e0dc7404f6df93431f00a04f8b401d
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-11-22 12:22:27 +0100

    Use realpath

--- build/RunTestsOnHelix.sh
+++ build/RunTestsOnHelix.sh
@@ -9,7 +9,7 @@ export MicrosoftNETBuildExtensionsTargets=$HELIX_CORRELATION_PAYLOAD/ex/msbuildE
 export DOTNET_ROOT=$HELIX_CORRELATION_PAYLOAD/d
 export PATH=$DOTNET_ROOT:$PATH
 
-export TestExecutionDirectory=$(mktemp -d "${TMPDIR:-/tmp}"/dotnetSdkTests.XXXXXXXX)
+export TestExecutionDirectory=$(realpath "$(mktemp -d "${TMPDIR:-/tmp}"/dotnetSdkTests.XXXXXXXX)")
 export DOTNET_CLI_HOME=$TestExecutionDirectory/.dotnet
 cp -a $HELIX_CORRELATION_PAYLOAD/t/TestExecutionDirectoryFiles/. $TestExecutionDirectory/
 
