eea2d7fbf006d574d115928313f93dff255c11d8
Eduardo Villalpando Mello <eduardov@microsoft.com>
2024-11-26 14:41:18 -0800

    sln-remove: Support for slnx

--- src/Cli/dotnet/commands/dotnet-sln/remove/Program.cs
+++ src/Cli/dotnet/commands/dotnet-sln/remove/Program.cs
@@ -5,12 +5,11 @@ using System.CommandLine;
 using Microsoft.Build.Construction;
 using Microsoft.Build.Execution;
 using Microsoft.DotNet.Cli;
+using Microsoft.DotNet.Cli.Sln.Internal;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.Tools.Common;
-using Microsoft.Extensions.EnvironmentAbstractions;
 using Microsoft.VisualStudio.SolutionPersistence;
 using Microsoft.VisualStudio.SolutionPersistence.Model;
-using Microsoft.VisualStudio.SolutionPersistence.Serializer.SlnV12;
 
 namespace Microsoft.DotNet.Tools.Sln.Remove
 {
@@ -18,28 +17,6 @@ namespace Microsoft.DotNet.Tools.Sln.Remove
     {
         private readonly string _fileOrDirectory;
         private readonly IReadOnlyCollection<string> _projects;
-
-        private int CountNonFolderDescendants(
-            SolutionModel solution,
-            SolutionFolderModel item,
-            Dictionary<SolutionFolderModel, SolutionItemModel[]> solutionItemsGroupedByParent,
-            Dictionary<SolutionFolderModel, int> cached)
-        {
-            if (cached.ContainsKey(item))
-            {
-                return cached[item];
-            }
-            int count = item.Files?.Count ?? 0;
-            var children = solutionItemsGroupedByParent.TryGetValue(item, out var items) ? items : Array.Empty<SolutionItemModel>();
-            foreach (var child in children)
-            {
-                count += child is SolutionFolderModel folderModel
-                    ? CountNonFolderDescendants(solution, folderModel, solutionItemsGroupedByParent, cached)
-                    : 1;
-            }
-            cached.Add(item, count);
-            return count;
-        }
 
         public RemoveProjectFromSolutionCommand(ParseResult parseResult) : base(parseResult)
         {
@@ -58,30 +35,19 @@ namespace Microsoft.DotNet.Tools.Sln.Remove
                 throw new GracefulException(CommonLocalizableStrings.SpecifyAtLeastOneProjectToRemove);
             }
 
+            IEnumerable<string> fullProjectPaths = _projects.Select(project =>
+            {
+                var fullPath = Path.GetFullPath(project);
+                return Directory.Exists(fullPath) ? MsbuildProject.GetProjectFileFromDirectory(fullPath).FullName : fullPath;
+            });
+
             try
             {
-                var relativeProjectPaths = _projects.Select(p =>
-                {
-                    var fullPath = Path.GetFullPath(p);
-                    return Path.GetRelativePath(
-                        Path.GetDirectoryName(solutionFileFullPath),
-                        Directory.Exists(fullPath)
-                            ? MsbuildProject.GetProjectFileFromDirectory(fullPath).FullName
-                            : fullPath);
-                });
-                RemoveProjectsAsync(solutionFileFullPath, relativeProjectPaths, CancellationToken.None).Wait();
+                RemoveProjectsAsync(solutionFileFullPath, fullProjectPaths, CancellationToken.None).Wait();
                 return 0;
             }
-            catch (Exception ex) when (ex is not GracefulException)
+            catch (Exception ex)
             {
-                if (ex is SolutionException || ex.InnerException is SolutionException)
-                {
-                    throw new GracefulException(CommonLocalizableStrings.InvalidSolutionFormatString, solutionFileFullPath, ex.Message);
-                }
-                if (ex.InnerException is GracefulException)
-                {
-                    throw ex.InnerException;
-                }
                 throw new GracefulException(ex.Message, ex);
             }
         }
@@ -91,44 +57,10 @@ namespace Microsoft.DotNet.Tools.Sln.Remove
             ISolutionSerializer serializer = SlnCommandParser.GetSolutionSerializer(solutionFileFullPath);
             SolutionModel solution = await serializer.OpenAsync(solutionFileFullPath, cancellationToken);
 
-            // set UTF8 BOM encoding for .sln
-            if (serializer is ISolutionSerializer<SlnV12SerializerSettings> v12Serializer)
+            foreach (var project in projectPaths)
             {
-                solution.SerializerExtension = v12Serializer.CreateModelExtension(new()
-                {
-                    Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: true)
-                });
-            }
-
-            foreach (var projectPath in projectPaths)
-            {
-                var project = solution.FindProject(projectPath);
-                if (project != null)
-                {
-                    solution.RemoveProject(project);
-                    Reporter.Output.WriteLine(CommonLocalizableStrings.ProjectRemovedFromTheSolution, projectPath);
-                }
-                else
-                {
-                    Reporter.Output.WriteLine(CommonLocalizableStrings.ProjectNotFoundInTheSolution, projectPath);
-                }
-            }
-
-            Dictionary<SolutionFolderModel, SolutionItemModel[]> solutionItemsGroupedByParent = solution.SolutionItems
-                .Where(i => i.Parent != null)
-                .GroupBy(i => i.Parent)
-                .ToDictionary(g => g.Key, g => g.ToArray());
-
-            Dictionary<SolutionFolderModel, int> nonFolderDescendantsCount = new();
-            foreach (var item in solution.SolutionFolders)
-            {
-                CountNonFolderDescendants(solution, item, solutionItemsGroupedByParent, nonFolderDescendantsCount);
-            }
-
-            var emptyFolders = nonFolderDescendantsCount.Where(i => i.Value == 0).Select(i => i.Key);
-            foreach (var folder in emptyFolders)
-            {
-                solution.RemoveFolder(folder);
+                SolutionProjectModel projectModel = solution.FindProject(project);
+                solution.RemoveProject(projectModel);
             }
 
             await serializer.SaveAsync(solutionFileFullPath, solution, cancellationToken);
