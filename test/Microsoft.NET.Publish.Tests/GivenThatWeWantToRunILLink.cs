508b28b2618134898531858db1b549e1a0b0ef6c
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2024-11-29 21:45:07 +0100

    Revert "forgot to skip one test"
    This reverts commit efab774b18aa375a37adac1fca8c586a14266c97.

--- test/Microsoft.NET.Publish.Tests/GivenThatWeWantToRunILLink.cs
+++ test/Microsoft.NET.Publish.Tests/GivenThatWeWantToRunILLink.cs
@@ -1550,7 +1550,7 @@ namespace Microsoft.NET.Publish.Tests
                 .And.NotHaveStdOutContaining("warning IL2075");
         }
 
-        [RequiresMSBuildVersionTheory("17.0.0.32901", Skip = "https://github.com/dotnet/sdk/issues/45134")]
+        [RequiresMSBuildVersionTheory("17.0.0.32901")]
         [MemberData(nameof(Net5Plus), MemberType = typeof(PublishTestUtils))]
         public void ILLink_can_treat_warnings_as_errors_independently(string targetFramework)
         {
