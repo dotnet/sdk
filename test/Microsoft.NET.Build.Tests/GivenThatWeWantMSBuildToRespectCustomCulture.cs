8c6c9689d626bfef4e85c2f1514ef92b9c7734e0
Jan Krivanek <jankrivanek@microsoft.com>
2024-12-03 09:21:09 +0100

    Separate FullFW and Core test cases

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantMSBuildToRespectCustomCulture.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantMSBuildToRespectCustomCulture.cs
@@ -30,7 +30,8 @@ namespace Microsoft.NET.Build.Tests
         [Theory]
         [InlineData("net7.0")]
         [InlineData("net6.0")]
-        public void SupportRespectAlreadyAssignedItemCulture_IsNotSupported_BuildShouldFail(string targetFramework)
+        [CoreMSBuildOnlyTheory]
+        public void SupportRespectAlreadyAssignedItemCulture_IsNotSupported_BuildShouldWarn(string targetFramework)
         {
             var testAsset = _testAssetsManager
                 .CopyTestAsset("MSBuildCultureResourceGeneration", identifier: targetFramework)
@@ -42,6 +43,27 @@ namespace Microsoft.NET.Build.Tests
             buildCommand.Execute().Should().Pass().And
                 // warning MSB3002: Explicitly set culture "test-1" for item "Resources.test-1.resx" was overwritten with inferred culture "", because 'RespectAlreadyAssignedItemCulture' property was not set.
                 .HaveStdOutContaining("warning MSB3002:");
+        }
+
+        [Theory]
+        [InlineData("net7.0")]
+        [InlineData("net6.0")]
+        [FullMSBuildOnlyTheory]
+        // Is this Failing? Is full FW MSBuild already on 17.13? Then remove this test and remove `[CoreMSBuildOnlyTheory]` attribute on the test above
+        //
+        // Until MSBuild 17.13 is merged into FullFW MSBuild in sdk tests - the test will fail, as
+        //  proper recognition of custom cultures in RAR is not supported and hence the build will fail during copy:
+        //
+        // Microsoft.Common.CurrentVersion.targets(4959,5): error MSB3030: Could not copy the file "obj\Debug\net7.0\test-1\MSBuildCultureResourceGeneration.resources.dll" because it was not found.
+        public void SupportRespectAlreadyAssignedItemCulture_IsNotSupported_BuildShouldFail(string targetFramework)
+        {
+            var testAsset = _testAssetsManager
+                .CopyTestAsset("MSBuildCultureResourceGeneration", identifier: targetFramework)
+                .WithSource()
+                .WithTargetFramework(targetFramework);
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand.Execute().Should().Fail();
         }
     }
 }
