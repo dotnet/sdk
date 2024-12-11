400ef51f9c6b52fa7f5822f84139f1e60a624b0b
Jason Zhai <v-wuzhai@microsoft.com>
2024-12-10 18:24:59 -0800

    Add netcoreapp9.1 tfm to It_fails_to_build_if_targeting_a_higher_framework_than_is_supported test

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildALibrary.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildALibrary.cs
@@ -709,6 +709,7 @@ class Program
         }
 
         [Theory]
+        [InlineData("netcoreapp9.1")]
         [InlineData("netcoreapp10.1")]
         [InlineData("netstandard2.2")]
         public void It_fails_to_build_if_targeting_a_higher_framework_than_is_supported(string targetFramework)
