789c697653811f67cd6a379b11930f3bddd14bc6
Jacques Eloff <joeloff@users.noreply.github.com>
2024-12-13 10:17:03 -0800

    Ensure parity with native finalizer behavior

--- src/Installer/finalizer/Program.cs
+++ src/Installer/finalizer/Program.cs
@@ -7,8 +7,7 @@ using Microsoft.Win32.Msi;
 
 if (args.Length < 3)
 {
-    Console.WriteLine("Invalid arguments. Usage: finalizer <logPath> <sdkVersion> <platform>");
-    return;
+    return (int)Error.INVALID_COMMAND_LINE;
 }
 
 string logPath = args[0];
@@ -22,47 +21,43 @@ Logger.Init(logStream);
 Logger.Log($"{nameof(logPath)}: {logPath}");
 Logger.Log($"{nameof(sdkVersion)}: {sdkVersion}");
 Logger.Log($"{nameof(platform)}: {platform}");
+int exitCode = (int)Error.SUCCESS;
 
 try
 {
     // Step 1: Parse and format SDK feature band version
     string featureBandVersion = new SdkFeatureBand(sdkVersion).ToString();
+    string dependent = $"Microsoft.NET.Sdk,{featureBandVersion},{platform}";
 
     // Step 2: Check if SDK feature band is installed
     bool isInstalled = DetectSdk(featureBandVersion, platform);
     if (isInstalled)
     {
         Logger.Log($"SDK with feature band {featureBandVersion} is already installed.");
-        return;
+        return (int)Error.SUCCESS;
     }
 
     // Step 3: Remove dependent components if necessary
-    bool restartRequired = RemoveDependent($"Microsoft.NET.Sdk,{featureBandVersion},{platform}");
-    if (restartRequired)
+    if (RemoveDependent(dependent))
     {
-        Logger.Log("A restart may be required after removing the dependent component.");
-    }
+        // Pass potential restart exit codes back to the bundle based on executing the
+        // workload related MSIs. The bundle may take additional actions such as prompting the user.
+        exitCode = (int)Error.SUCCESS_REBOOT_REQUIRED;
+    };
 
     // Step 4: Delete workload records
     DeleteWorkloadRecords(featureBandVersion, platform);
 
     // Step 5: Clean up install state file
     RemoveInstallStateFile(featureBandVersion, platform);
-
-    // Final reboot check
-    if (restartRequired || IsRebootPending())
-    {
-        Logger.Log("A system restart is recommended to complete the operation.");
-    }
-    else
-    {
-        Logger.Log("Operation completed successfully. No restart is required.");
-    }
 }
 catch (Exception ex)
 {
     Logger.Log($"Error: {ex}");
+    exitCode = ex.HResult;
 }
+
+return exitCode;
 
 static bool DetectSdk(string featureBandVersion, string platform)
 {
@@ -89,8 +84,7 @@ static bool DetectSdk(string featureBandVersion, string platform)
 
 static bool RemoveDependent(string dependent)
 {
-    // Disable MSI UI
-    _ = MsiSetInternalUI((uint)InstallUILevel.NoChange, IntPtr.Zero);
+    bool restartRequired = false;
 
     // Open the installer dependencies registry key
     // This has to be an exhaustive search as we're not looking for a specific provider key, but for a specific dependent
@@ -108,11 +102,17 @@ static bool RemoveDependent(string dependent)
         Logger.Log($"Processing provider key: {providerKeyName}");
 
         using var hkProviderKey = hkInstallerDependenciesKey.OpenSubKey(providerKeyName, writable: true);
-        if (hkProviderKey is null) continue;
+        if (hkProviderKey is null)
+        {
+            continue;
+        }
 
         // Open the Dependents subkey
         using var hkDependentsKey = hkProviderKey.OpenSubKey("Dependents", writable: true);
-        if (hkDependentsKey is null) continue;
+        if (hkDependentsKey is null)
+        {
+            continue;
+        }
 
         // Check if the dependent exists and continue if it does not
         bool dependentExists = false;
@@ -150,32 +150,56 @@ static bool RemoveDependent(string dependent)
             // No remaining dependents, handle product uninstallation
             try
             {
-                string? productCode = hkProviderKey.GetValue("ProductId")?.ToString();
+                // Default value should be a REG_SZ containing the product code.
+                string? productCode = hkProviderKey.GetValue(null) as string;
+
                 if (productCode is null)
                 {
-                    Logger.Log($"Can't find ProductId in {providerKeyName}");
-                    return false;
+                    Logger.Log($"No product ID found, provider key: {providerKeyName}");
+                    continue;
                 }
 
+                // Let's make sure the product is actually installed. The provider key for an MSI typically
+                // stores the ProductCode, DisplayName, and Version, but by calling into MsiGetProductInfo,
+                // we're doing an implicit detect and getting a property back. This avoids reading additional
+                // registry keys.
+                uint error = WindowsInstaller.GetProductInfo(productCode, "ProductName", out string productName);
+
+                if (error != Error.SUCCESS)
+                {
+                    Logger.Log($"Failed to detect product, ProductCode: {productCode}, result: 0x{error:x8}");
+                    continue;
+                }
+
+                // Need to set the UI level before executing the MSI.
+                _ = WindowsInstaller.SetInternalUI(InstallUILevel.None);
+
                 // Configure the product to be absent (uninstall the product)
-                uint error = MsiConfigureProductEx(productCode, (int)InstallUILevel.Default, InstallState.ABSENT, "");
-                Logger.Log("Product configured to absent successfully.");
+                error = WindowsInstaller.ConfigureProduct(productCode,
+                    WindowsInstaller.INSTALLLEVEL_DEFAULT,
+                    InstallState.ABSENT,
+                    "MSIFASTINSTALL=7 IGNOREDEPENDENCIES=ALL REBOOT=ReallySuppress");
+                Logger.Log($"Uninstall of {productName} ({productCode}) exited with 0x{error:x8}");
+
+                if (error == Error.SUCCESS_REBOOT_INITIATED || error == Error.SUCCESS_REBOOT_REQUIRED)
+                {
+                    restartRequired = true;
+                }
             }
             catch (Exception ex)
             {
-                Logger.Log($"Error handling product configuration: {ex.Message}");
-                return false;
+                Logger.Log($"Failed to process dependentprocess: {ex.Message}");
+                return restartRequired;
             }
         }
-        return true;
     }
 
-    return false;
+    return restartRequired;
 }
 
 static void DeleteWorkloadRecords(string featureBandVersion, string platform)
 {
-    string workloadKey = $@"SOFTWARE\Microsoft\dotnet\InstalledWorkloads\Standalone\{platform}";
+    string? workloadKey = $@"SOFTWARE\Microsoft\dotnet\InstalledWorkloads\Standalone\{platform}";
 
     using (RegistryKey? key = Registry.LocalMachine.OpenSubKey(workloadKey, writable: true))
     {
@@ -187,6 +211,50 @@ static void DeleteWorkloadRecords(string featureBandVersion, string platform)
         else
         {
             Logger.Log("No workload records found to delete.");
+        }
+    }
+
+    DeleteEmptyKeyToRoot(Registry.LocalMachine, workloadKey);
+}
+
+static void DeleteEmptyKeyToRoot(RegistryKey key, string name)
+{
+    string? subKeyName = Path.GetFileName(name.TrimEnd(Path.DirectorySeparatorChar));
+    string? tempName = name;
+
+    while (!string.IsNullOrWhiteSpace(tempName))
+    {
+        using (RegistryKey? k = key.OpenSubKey(tempName))
+        {
+            if (k is not null && k.SubKeyCount == 0 && k.ValueCount == 0)
+            {
+                tempName = Path.GetDirectoryName(tempName);
+
+                if (tempName is not null)
+                {
+                    try
+                    {
+                        using (RegistryKey? parentKey = key.OpenSubKey(tempName, writable: true))
+                        {
+                            if (parentKey is not null)
+                            {
+                                parentKey.DeleteSubKeyTree(subKeyName, throwOnMissingSubKey: false);
+                                Logger.Log($"Deleted empty key: {subKeyName}");
+                                subKeyName = Path.GetFileName(tempName.TrimEnd(Path.DirectorySeparatorChar));
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Log($"Failed to delete key: {tempName}, error: {ex.Message}");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                break;
+            }
         }
     }
 }
@@ -213,39 +281,6 @@ static void RemoveInstallStateFile(string featureBandVersion, string platform)
         Logger.Log("Install state file does not exist.");
     }
 }
-
-static bool IsRebootPending()
-{
-    using (RegistryKey? key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending"))
-    {
-        if (key is not null)
-        {
-            Logger.Log("Reboot is pending due to component-based servicing.");
-            return true;
-        }
-    }
-
-    using (RegistryKey? key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager"))
-    {
-        var value = key?.GetValue("PendingFileRenameOperations");
-        if (value is not null)
-        {
-            Logger.Log("Pending file rename operations indicate a reboot is pending.");
-            return true;
-        }
-    }
-
-    Logger.Log("No reboot pending.");
-    return false;
-}
-
-[DllImport("msi.dll", CharSet = CharSet.Unicode)]
-[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-static extern uint MsiConfigureProductEx(string szProduct, int iInstallLevel, InstallState eInstallState, string szCommandLine);
-
-[DllImport("msi.dll", CharSet = CharSet.Unicode)]
-[DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
-static extern uint MsiSetInternalUI(uint dwUILevel, IntPtr phWnd);
 
 static class Logger
 {
