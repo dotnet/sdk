4ba585bfc965c9b9b0302e316c1223f5a2a53b5d
mariam-abdulla <mabdullah@microsoft.com>
2024-12-16 10:07:49 +0100

    Update namespace

--- src/Cli/dotnet/commands/dotnet-test/TestSettings.cs
+++ src/Cli/dotnet/commands/dotnet-test/TestSettings.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.DotNet.Cli.commands.dotnet_test
+namespace Microsoft.DotNet.Cli
 {
     internal sealed record TestSettings(Runner? Runner);
     internal sealed record Runner(string Name);
