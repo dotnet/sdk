d8e9388f66fb853756e15c4cf1678e7e494667d3
Chet Husk <chusk3@gmail.com>
2025-01-01 14:00:56 -0600

    Add dotnet CLI generate script approval tests to the CLI-level tests

---
+++ test/dotnet-completions.Tests/VerifySettings.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.Cli.Completions.Tests;
+
+using System.Runtime.CompilerServices;
+
+public static class VerifyConfiguration
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        VerifyDiffPlex.Initialize(VerifyTests.DiffPlex.OutputType.Compact);
+        // prevent CI systems from trying to make a snapshot directory, maybe?
+        DerivePathInfo((_, _, _, _) => new("snapshots"));
+        EmptyFiles.FileExtensions.AddTextExtension("ps1");
+        EmptyFiles.FileExtensions.AddTextExtension("nu");
+    }
+}
