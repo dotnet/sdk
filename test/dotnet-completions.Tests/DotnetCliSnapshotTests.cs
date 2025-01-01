0d65128f7e36eb54dd9a087515bd393d4849b7db
Chet Husk <chusk3@gmail.com>
2025-01-01 14:00:56 -0600

    Add dotnet CLI generate script approval tests to the CLI-level tests

---
+++ test/dotnet-completions.Tests/DotnetCliSnapshotTests.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.Cli.Completions.Tests;
+
+using System.CommandLine.StaticCompletions.Shells;
+using VerifyXunit;
+using Xunit.Abstractions;
+
+public class DotnetCliSnapshotTests : SdkTest
+{
+    public DotnetCliSnapshotTests(ITestOutputHelper log) : base(log) { }
+
+    [MemberData(nameof(ShellNames))]
+    [Theory]
+    public async Task VerifyCompletions(string shellName)
+    {
+        var provider = System.CommandLine.StaticCompletions.CompletionsCommand.DefaultShells.Single(x => x.ArgumentName == shellName);
+        var completions = provider.GenerateCompletions(Microsoft.DotNet.Cli.Parser.RootCommand);
+        var settings = new VerifySettings();
+        settings.UseDirectory($"snapshots/{provider.ArgumentName}");
+        await Verifier.Verify(target: completions, extension: provider.Extension, settings: settings);
+    }
+
+    public static IEnumerable<object[]> ShellNames = System.CommandLine.StaticCompletions.CompletionsCommand.DefaultShells.Select<IShellProvider, object[]>(x => [x.ArgumentName]);
+}
