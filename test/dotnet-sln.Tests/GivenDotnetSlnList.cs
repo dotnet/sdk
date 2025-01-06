6e52aeaef128eac87ea7d6bc88688b3b47d1d182
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-01-06 14:32:46 -0800

    Remove SlnFile references

--- test/dotnet-sln.Tests/GivenDotnetSlnList.cs
+++ test/dotnet-sln.Tests/GivenDotnetSlnList.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.DotNet.Cli.Sln.Internal;
 using Microsoft.DotNet.Tools;
 using Microsoft.DotNet.Tools.Common;
 using CommandLocalizableStrings = Microsoft.DotNet.Tools.Sln.LocalizableStrings;
