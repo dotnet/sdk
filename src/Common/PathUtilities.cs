585c51e70dbedc747ec9c7f0502c3096800a4a30
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2025-01-10 16:41:22 +0200

    @tmds version

--- src/Common/PathUtilities.cs
+++ src/Common/PathUtilities.cs
@@ -7,25 +7,12 @@ static class PathUtilities
 {
     public static string CreateTempSubdirectory()
     {
-        string path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-
 #if NETFRAMEWORK
+        string path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
         Directory.CreateDirectory(path);
-#else
-        if (OperatingSystem.IsWindows())
-            Directory.CreateDirectory(path);
-        else
-        {
-            UnixFileMode desiredMode = UnixFileMode.UserRead | UnixFileMode.UserWrite | UnixFileMode.UserExecute;
-
-            do 
-            {
-                path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-                Directory.CreateDirectory(path, desiredMode);
-            } while (File.GetUnixFileMode(path) != desiredMode);
-        }
-#endif
-
         return path;
+#else
+        return Directory.CreateTempSubdirectory().FullName;
+#endif
     }
 }
