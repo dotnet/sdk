5745044911df8aa6df4ef162379e0c5f4ac4e759
Chet Husk <baronfel@users.noreply.github.com>
2025-01-11 14:57:55 -0600

    Remove DerivePathInfo in tests
    If we have corrected paths from the extension methods, maybe we don't need this override?

--- test/dotnet-completions.Tests/VerifySettings.cs
+++ test/dotnet-completions.Tests/VerifySettings.cs
@@ -11,8 +11,6 @@ public static class VerifyConfiguration
     public static void Initialize()
     {
         VerifyDiffPlex.Initialize(VerifyTests.DiffPlex.OutputType.Compact);
-        // prevent CI systems from trying to make a snapshot directory, maybe?
-        DerivePathInfo((_sourceFile, projectDir, type, method) => new(directory: Path.Combine(projectDir, "snapshots"), typeName: type.Name, methodName: method.Name));
         EmptyFiles.FileExtensions.AddTextExtension("ps1");
         EmptyFiles.FileExtensions.AddTextExtension("nu");
     }
