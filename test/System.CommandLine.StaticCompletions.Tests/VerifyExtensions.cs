a3866e4ed49e71d586a37bdcbbde441d9608b3e4
Chet Husk <chusk3@gmail.com>
2025-01-10 23:49:37 -0600

    update verify extensions to forward sourceFile paths

--- test/System.CommandLine.StaticCompletions.Tests/VerifyExtensions.cs
+++ test/System.CommandLine.StaticCompletions.Tests/VerifyExtensions.cs
@@ -2,16 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine.StaticCompletions.Shells;
+using System.Runtime.CompilerServices;
 
 namespace System.CommandLine.StaticCompletions.Tests;
 
 public static class VerifyExtensions
 {
-    public static async Task Verify(CliCommand command, IShellProvider provider)
+    public static async Task Verify(CliCommand command, IShellProvider provider, [CallerFilePath] string sourceFile = "")
     {
         var completions = provider.GenerateCompletions(command);
         var settings = new VerifySettings();
         settings.UseDirectory(Path.Combine("snapshots", provider.ArgumentName));
-        await Verifier.Verify(target: completions, extension: provider.Extension, settings: settings);
+        await Verifier.Verify(target: completions, extension: provider.Extension, settings: settings, sourceFile: sourceFile);
     }
 }
