c7f1b6d7670d3afddf19adac99b38c31c7d5cce0
Forgind <12969783+Forgind@users.noreply.github.com>
2025-01-17 19:31:53 -0800

    Fix 'ends in ::' bug

--- src/Cli/Microsoft.TemplateEngine.Cli/TemplatePackageCoordinator.cs
+++ src/Cli/Microsoft.TemplateEngine.Cli/TemplatePackageCoordinator.cs
@@ -213,7 +213,7 @@ namespace Microsoft.TemplateEngine.Cli
 
                 if (installArg.Contains("::"))
                 {
-                    Reporter.Output.WriteLine(string.Format(LocalizableStrings.Colon_Separator_Deprecated, split[0], split[1]).Yellow());
+                    Reporter.Output.WriteLine(string.Format(LocalizableStrings.Colon_Separator_Deprecated, split[0], split.Length > 0 ? split[1] : string.Empty).Yellow());
                 }
 
                 foreach (string expandedIdentifier in InstallRequestPathResolution.ExpandMaskedPath(identifier, _engineEnvironmentSettings))
