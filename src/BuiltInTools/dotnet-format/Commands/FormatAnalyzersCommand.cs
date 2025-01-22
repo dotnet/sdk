eef0f20888be23637da5a7e636ce6b7be36ead8d
Adam Sitnik <adam.sitnik@gmail.com>
2025-01-22 14:04:22 +0100

    remove dependency to System.CommandLine.Rendering, use Microsoft.Extensions.Logging.Console instead

--- src/BuiltInTools/dotnet-format/Commands/FormatAnalyzersCommand.cs
+++ src/BuiltInTools/dotnet-format/Commands/FormatAnalyzersCommand.cs
@@ -3,7 +3,6 @@
 using System.Collections.Immutable;
 using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.CommandLine.IO;
 using Microsoft.Extensions.Logging;
 using static Microsoft.CodeAnalysis.Tools.FormatCommandCommon;
 
@@ -31,7 +30,7 @@ namespace Microsoft.CodeAnalysis.Tools.Commands
             public override async Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken)
             {
                 var formatOptions = parseResult.ParseVerbosityOption(FormatOptions.Instance);
-                var logger = new SystemConsole().SetupLogging(minimalLogLevel: formatOptions.LogLevel, minimalErrorLevel: LogLevel.Warning);
+                var logger = SetupLogging(minimalLogLevel: formatOptions.LogLevel);
                 formatOptions = parseResult.ParseCommonOptions(formatOptions, logger);
                 formatOptions = parseResult.ParseWorkspaceOptions(formatOptions);
 
