78ed6604225d9015539578120ea9498755c02c9f
Chet Husk <chusk3@gmail.com>
2025-01-26 19:21:11 -0600

    Better help descriptions and guidance for the powershell script provider

--- src/System.CommandLine.StaticCompletions/shells/NuShellShellProvider.cs
+++ src/System.CommandLine.StaticCompletions/shells/NuShellShellProvider.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.CommandLine.StaticCompletions.Resources;
+
 namespace System.CommandLine.StaticCompletions.Shells;
 
 public class NushellShellProvider : IShellProvider
@@ -8,6 +10,11 @@ public class NushellShellProvider : IShellProvider
     public string ArgumentName => "nushell";
 
     public string Extension => "nu";
+
+    public string HelpDescription => Strings.NuShellShellProvider_HelpDescription;
+
+    // override the ToString method to return the argument name so that CLI help is cleaner for 'default' values
+    public override string ToString() => ArgumentName;
 
     private static readonly string _dynamicCompletionScript =
         """
