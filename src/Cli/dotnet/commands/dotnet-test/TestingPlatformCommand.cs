b6ca740793cb73f8d426f872829290e2cc84c21d
Jakub Jareš <me@jakubjares.com>
2025-01-27 14:28:08 +0100

    ishelp

--- src/Cli/dotnet/commands/dotnet-test/TestingPlatformCommand.cs
+++ src/Cli/dotnet/commands/dotnet-test/TestingPlatformCommand.cs
@@ -42,7 +42,7 @@ namespace Microsoft.DotNet.Cli
                 _args = [.. parseResult.UnmatchedTokens];
                 _isHelp = ContainsHelpOption(parseResult.GetArguments());
 
-                InitializeOutput(degreeOfParallelism, _isHelp);
+                InitializeOutput(degreeOfParallelism);
 
                 bool filterModeEnabled = parseResult.HasOption(TestingPlatformOptions.TestModulesFilterOption);
                 if (_isHelp)
