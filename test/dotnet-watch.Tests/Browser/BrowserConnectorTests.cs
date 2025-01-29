95aa7f0e019003310aff4dfef9b6169dc24c6626
Tomas Matousek <tomat@microsoft.com>
2025-01-28 18:50:06 -0800

    Add test cases

--- test/dotnet-watch.Tests/Browser/BrowserConnectorTests.cs
+++ test/dotnet-watch.Tests/Browser/BrowserConnectorTests.cs
@@ -9,11 +9,13 @@ public class BrowserConnectorTests
 {
     [Theory]
     [InlineData(null, "https://localhost:1234", "https://localhost:1234")]
+    [InlineData(null, "https://localhost:1234/", "https://localhost:1234/")]
     [InlineData("", "https://localhost:1234", "https://localhost:1234")]
     [InlineData("   ", "https://localhost:1234", "https://localhost:1234")]
     [InlineData("", "a/b", "a/b")]
     [InlineData("x/y", "a/b", "a/b")]
     [InlineData("a/b?X=1", "https://localhost:1234", "https://localhost:1234/a/b?X=1")]
+    [InlineData("https://localhost:1000/", "https://localhost:1234", "https://localhost:1000/")]
     [InlineData("https://localhost:1000/a/b", "https://localhost:1234", "https://localhost:1000/a/b")]
     [InlineData("https://localhost:1000/x/y?z=u", "https://localhost:1234/a?b=c", "https://localhost:1000/x/y?z=u")]
     public void GetLaunchUrl(string? profileLaunchUrl, string outputLaunchUrl, string expected)
