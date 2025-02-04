c13d121f1f1270ed0e5aa815d95be74fe2b0a233
mariam-abdulla <mabdullah@microsoft.com>
2025-02-04 14:26:28 +0100

    Fix tests

--- test/dotnet-test.Tests/GivenDotnetTestBuildsAndRunsTestBasedOnGlobbingFilter.cs
+++ test/dotnet-test.Tests/GivenDotnetTestBuildsAndRunsTestBasedOnGlobbingFilter.cs
@@ -1,91 +1,84 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Text.RegularExpressions;
 using CommandResult = Microsoft.DotNet.Cli.Utils.CommandResult;
 
 namespace Microsoft.DotNet.Cli.Test.Tests
 {
-	public class GivenDotnetTestBuildsAndRunsTestBasedOnGlobbingFilter : SdkTest
-	{
-		private const string TestApplicationArgsPattern = @".*(Test application arguments).*";
+    public class GivenDotnetTestBuildsAndRunsTestBasedOnGlobbingFilter : SdkTest
+    {
+        private const string TestApplicationArgsPattern = @".*(Test application arguments).*";
 
-		public GivenDotnetTestBuildsAndRunsTestBasedOnGlobbingFilter(ITestOutputHelper log) : base(log)
-		{
-		}
+        public GivenDotnetTestBuildsAndRunsTestBasedOnGlobbingFilter(ITestOutputHelper log) : base(log)
+        {
+        }
 
-		[InlineData(TestingConstants.Debug)]
-		[InlineData(TestingConstants.Release)]
-		[Theory]
-		public void RunTestProjectWithFilterOfDll_ShouldReturnZeroAsExitCode(string configuration)
-		{
-			TestAsset testInstance = _testAssetsManager.CopyTestAsset("TestProjectWithTests", Guid.NewGuid().ToString())
-				.WithSource();
+        [Fact]
+        public void RunTestProjectWithFilterOfDll_ShouldReturnZeroAsExitCode()
+        {
+            TestAsset testInstance = _testAssetsManager.CopyTestAsset("TestProjectWithTests", Guid.NewGuid().ToString())
+                .WithSource();
 
-			new BuildCommand(testInstance)
-				.Execute()
-				.Should().Pass();
+            new BuildCommand(testInstance)
+                .Execute()
+                .Should().Pass();
 
-			var binDirectory = new FileInfo($"{testInstance.Path}{Path.DirectorySeparatorChar}bin").Directory;
-			var binDirectoryLastWriteTime = binDirectory?.LastWriteTime;
+            var binDirectory = new FileInfo($"{testInstance.Path}{Path.DirectorySeparatorChar}bin").Directory;
+            var binDirectoryLastWriteTime = binDirectory?.LastWriteTime;
 
-			CommandResult result = new DotnetTestCommand(Log, disableNewOutput: false)
-									.WithWorkingDirectory(testInstance.Path)
-									.WithEnableTestingPlatform()
-									.Execute(TestingPlatformOptions.TestModulesFilterOption.Name, "**/bin/**/Debug/net8.0/TestProject.dll".Replace('/', Path.DirectorySeparatorChar),
-									TestingPlatformOptions.ConfigurationOption.Name, configuration);
+            CommandResult result = new DotnetTestCommand(Log, disableNewOutput: false)
+                                    .WithWorkingDirectory(testInstance.Path)
+                                    .WithEnableTestingPlatform()
+                                    .Execute(TestingPlatformOptions.TestModulesFilterOption.Name, $"**/bin/**/Debug/{ToolsetInfo.CurrentTargetFramework}/TestProject.dll".Replace('/', Path.DirectorySeparatorChar));
 
-			// Assert that the bin folder hasn't been modified
-			Assert.Equal(binDirectoryLastWriteTime, binDirectory?.LastWriteTime);
+            // Assert that the bin folder hasn't been modified
+            Assert.Equal(binDirectoryLastWriteTime, binDirectory?.LastWriteTime);
+            Assert.Matches(RegexPatternHelper.GenerateProjectRegexPattern("TestProject", TestingConstants.Passed, true, "Debug"), result.StdOut);
 
-			if (!TestContext.IsLocalized())
-			{
-				result.StdOut
-					.Should().Contain("Test run summary: Passed!")
-					.And.Contain("total: 2")
-					.And.Contain("succeeded: 1")
-					.And.Contain("failed: 0")
-					.And.Contain("skipped: 1");
-			}
+            if (!TestContext.IsLocalized())
+            {
+                result.StdOut
+                    .Should().Contain("Test run summary: Passed!")
+                    .And.Contain("total: 2")
+                    .And.Contain("succeeded: 1")
+                    .And.Contain("failed: 0")
+                    .And.Contain("skipped: 1");
+            }
 
-			result.ExitCode.Should().Be(ExitCodes.Success);
-		}
+            result.ExitCode.Should().Be(ExitCodes.Success);
+        }
 
-		[InlineData(TestingConstants.Debug)]
-		[InlineData(TestingConstants.Release)]
-		[Theory]
-		public void RunTestProjectWithFilterOfDllWithRootDirectory_ShouldReturnZeroAsExitCode(string configuration)
-		{
-			TestAsset testInstance = _testAssetsManager.CopyTestAsset("TestProjectWithTests", Guid.NewGuid().ToString())
-				.WithSource();
+        [Fact]
+        public void RunTestProjectWithFilterOfDllWithRootDirectory_ShouldReturnZeroAsExitCode()
+        {
+            TestAsset testInstance = _testAssetsManager.CopyTestAsset("TestProjectWithTests", Guid.NewGuid().ToString())
+                .WithSource();
 
-			new BuildCommand(testInstance)
-				.Execute()
-				.Should().Pass();
+            new BuildCommand(testInstance)
+                .Execute()
+                .Should().Pass();
 
-			CommandResult result = new DotnetTestCommand(Log, disableNewOutput: false)
-									.WithWorkingDirectory(testInstance.Path)
-									.WithEnableTestingPlatform()
-									.WithTraceOutput()
-									.Execute(TestingPlatformOptions.TestModulesFilterOption.Name, "**/bin/**/Debug/net8.0/TestProject.dll".Replace('/', Path.DirectorySeparatorChar),
-									TestingPlatformOptions.TestModulesRootDirectoryOption.Name, testInstance.TestRoot,
-									TestingPlatformOptions.ConfigurationOption.Name, configuration);
+            CommandResult result = new DotnetTestCommand(Log, disableNewOutput: false)
+                                    .WithWorkingDirectory(testInstance.Path)
+                                    .WithEnableTestingPlatform()
+                                    .WithTraceOutput()
+                                    .Execute(TestingPlatformOptions.TestModulesFilterOption.Name, $"**/bin/**/Debug/{ToolsetInfo.CurrentTargetFramework}/TestProject.dll".Replace('/', Path.DirectorySeparatorChar),
+                                    TestingPlatformOptions.TestModulesRootDirectoryOption.Name, testInstance.TestRoot);
 
 
-			var testAppArgs = Regex.Matches(result.StdOut!, TestApplicationArgsPattern);
-			Assert.Contains(RegexPatternHelper.GenerateProjectRegexPattern("TestProject", true, configuration, "exec", addVersionAndArchPattern: false), testAppArgs.FirstOrDefault()?.Value);
+            Assert.Matches(RegexPatternHelper.GenerateProjectRegexPattern("TestProject", TestingConstants.Passed, true, "Debug"), result.StdOut);
 
-			if (!TestContext.IsLocalized())
-			{
-				result.StdOut
-					.Should().Contain("Test run summary: Passed!")
-					.And.Contain("total: 2")
-					.And.Contain("succeeded: 1")
-					.And.Contain("failed: 0")
-					.And.Contain("skipped: 1");
-			}
+            if (!TestContext.IsLocalized())
+            {
+                result.StdOut
+                    .Should().Contain("Test run summary: Passed!")
+                    .And.Contain("total: 2")
+                    .And.Contain("succeeded: 1")
+                    .And.Contain("failed: 0")
+                    .And.Contain("skipped: 1");
+            }
 
-			result.ExitCode.Should().Be(ExitCodes.Success);
-		}
-	}
+            result.ExitCode.Should().Be(ExitCodes.Success);
+        }
+    }
 }
