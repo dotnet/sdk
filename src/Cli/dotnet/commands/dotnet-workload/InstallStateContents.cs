014d73396bf03e985496c222d0e933aceeb55b8d
Forgind <12969783+Forgind@users.noreply.github.com>
2025-02-05 16:13:46 -0800

    EditorBrowsableState.Never

--- src/Cli/dotnet/commands/dotnet-workload/InstallStateContents.cs
+++ src/Cli/dotnet/commands/dotnet-workload/InstallStateContents.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.ComponentModel;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -11,6 +12,7 @@ namespace Microsoft.DotNet.Workloads.Workload
     internal class InstallStateContents
     {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public bool? UseWorkloadSets { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
