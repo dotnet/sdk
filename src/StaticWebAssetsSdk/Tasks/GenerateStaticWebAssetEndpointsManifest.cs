f0d4ea41d36b6bc62bb61c97ddcf63c5446d9308
Marek Fišera <mara@neptuo.com>
2025-02-06 11:30:39 +0100

    Revert changes to GenerateStaticWebAssetEndpointsManifest

--- src/StaticWebAssetsSdk/Tasks/GenerateStaticWebAssetEndpointsManifest.cs
+++ src/StaticWebAssetsSdk/Tasks/GenerateStaticWebAssetEndpointsManifest.cs
@@ -5,7 +5,6 @@
 
 using Microsoft.AspNetCore.StaticWebAssets.Tasks.Utils;
 using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 
 namespace Microsoft.AspNetCore.StaticWebAssets.Tasks;
 
@@ -20,8 +19,7 @@ public class GenerateStaticWebAssetEndpointsManifest : Task
     [Required]
     public string ManifestType { get; set; }
 
-    [Required]
-    public string Source { get; set; }
+    [Required] public string Source { get; set; }
 
     [Required]
     public string ManifestPath { get; set; }
@@ -38,7 +36,38 @@ public class GenerateStaticWebAssetEndpointsManifest : Task
 
         try
         {
-            var manifest = CreateManifest(Log, Assets, Endpoints, ManifestType, Source);
+            // Get the list of the asset that need to be part of the manifest (this is similar to GenerateStaticWebAssetsDevelopmentManifest)
+            var manifestAssets = ComputeManifestAssets(Assets.Select(StaticWebAsset.FromTaskItem), ManifestType)
+                .ToDictionary(a => a.ResolvedAsset.Identity, a => a, OSPath.PathComparer);
+
+            // Filter out the endpoints to those that point to the assets that are part of the manifest
+            var endpoints = StaticWebAssetEndpoint.FromItemGroup(Endpoints);
+            var filteredEndpoints = new List<StaticWebAssetEndpoint>();
+
+            foreach (var endpoint in endpoints)
+            {
+                if (!manifestAssets.TryGetValue(endpoint.AssetFile, out var asset))
+                {
+                    Log.LogMessage(MessageImportance.Low, "Skipping endpoint '{0}' because the asset '{1}' is not part of the manifest", endpoint.Route, endpoint.AssetFile);
+                    continue;
+                }
+
+                filteredEndpoints.Add(endpoint);
+                // Update the endpoint to use the target path of the asset, this will be relative to the wwwroot
+                var path = endpoint.AssetFile;
+
+                endpoint.AssetFile = asset.ResolvedAsset.ComputeTargetPath("", '/', StaticWebAssetTokenResolver.Instance);
+                endpoint.Route = asset.ResolvedAsset.ReplaceTokens(endpoint.Route, StaticWebAssetTokenResolver.Instance);
+
+                Log.LogMessage(MessageImportance.Low, "Including endpoint '{0}' for asset '{1}' with final location '{2}'", endpoint.Route, path, asset.TargetPath);
+            }
+
+            var manifest = new StaticWebAssetEndpointsManifest()
+            {
+                Version = 1,
+                ManifestType = ManifestType,
+                Endpoints = [.. filteredEndpoints]
+            };
 
             this.PersistFileIfChanged(manifest, ManifestPath, StaticWebAssetsJsonSerializerContext.RelaxedEscaping.StaticWebAssetEndpointsManifest);
         }
@@ -51,44 +80,7 @@ public class GenerateStaticWebAssetEndpointsManifest : Task
         return !Log.HasLoggedErrors;
     }
 
-    public static StaticWebAssetEndpointsManifest CreateManifest(TaskLoggingHelper log, ITaskItem[] assets, ITaskItem[] endpointItems, string manifestType, string Source)
-    {
-        // Get the list of the asset that need to be part of the manifest (this is similar to GenerateStaticWebAssetsDevelopmentManifest)
-        var manifestAssets = ComputeManifestAssets(log, assets.Select(StaticWebAsset.FromTaskItem), manifestType, Source)
-            .ToDictionary(a => a.ResolvedAsset.Identity, a => a, OSPath.PathComparer);
-
-        // Filter out the endpoints to those that point to the assets that are part of the manifest
-        var endpoints = StaticWebAssetEndpoint.FromItemGroup(endpointItems);
-        var filteredEndpoints = new List<StaticWebAssetEndpoint>();
-
-        foreach (var endpoint in endpoints)
-        {
-            if (!manifestAssets.TryGetValue(endpoint.AssetFile, out var asset))
-            {
-                log.LogMessage(MessageImportance.Low, "Skipping endpoint '{0}' because the asset '{1}' is not part of the manifest", endpoint.Route, endpoint.AssetFile);
-                continue;
-            }
-
-            filteredEndpoints.Add(endpoint);
-            // Update the endpoint to use the target path of the asset, this will be relative to the wwwroot
-            var path = endpoint.AssetFile;
-
-            endpoint.AssetFile = asset.ResolvedAsset.ComputeTargetPath("", '/', StaticWebAssetTokenResolver.Instance);
-            endpoint.Route = asset.ResolvedAsset.ReplaceTokens(endpoint.Route, StaticWebAssetTokenResolver.Instance);
-
-            log.LogMessage(MessageImportance.Low, "Including endpoint '{0}' for asset '{1}' with final location '{2}'", endpoint.Route, path, asset.TargetPath);
-        }
-
-        var manifest = new StaticWebAssetEndpointsManifest()
-        {
-            Version = 1,
-            ManifestType = manifestType,
-            Endpoints = [.. filteredEndpoints]
-        };
-        return manifest;
-    }
-
-    private static IEnumerable<TargetPathAssetPair> ComputeManifestAssets(TaskLoggingHelper Log, IEnumerable<StaticWebAsset> assets, string kind, string source)
+    private IEnumerable<TargetPathAssetPair> ComputeManifestAssets(IEnumerable<StaticWebAsset> assets, string kind)
     {
         var assetsByTargetPath = assets
             .GroupBy(a => a.ComputeTargetPath("", '/'));
@@ -103,7 +95,7 @@ public class GenerateStaticWebAssetEndpointsManifest : Task
                 continue;
             }
 
-            if (asset.HasSourceId(source) && !StaticWebAssetsManifest.ManifestModes.ShouldIncludeAssetInCurrentProject(asset, StaticWebAssetsManifest.ManifestModes.Root))
+            if (asset.HasSourceId(Source) && !StaticWebAssetsManifest.ManifestModes.ShouldIncludeAssetInCurrentProject(asset, StaticWebAssetsManifest.ManifestModes.Root))
             {
                 Log.LogMessage(MessageImportance.Low, "Skipping candidate asset '{0}' because asset mode is '{1}'",
                     asset.Identity,
