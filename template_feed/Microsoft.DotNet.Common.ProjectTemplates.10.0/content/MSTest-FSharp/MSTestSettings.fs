1b52439abd75bb897d586155932b24683494f5b1
Matt Kotsenas <mattkot@microsoft.com>
2025-02-14 10:43:26 -0800

    Add ExcludeFromCodeCoverage attribute to MSTest templates

--- template_feed/Microsoft.DotNet.Common.ProjectTemplates.10.0/content/MSTest-FSharp/MSTestSettings.fs
+++ template_feed/Microsoft.DotNet.Common.ProjectTemplates.10.0/content/MSTest-FSharp/MSTestSettings.fs
@@ -1,6 +1,8 @@
 ﻿module MSTestSettings
 
+open System.Diagnostics.CodeAnalysis
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
+[<assembly: ExcludeFromCodeCoverage>]
 [<assembly: Parallelize(Scope = ExecutionScope.MethodLevel)>]
 do()
--- template_feed/Microsoft.DotNet.Common.ProjectTemplates.10.0/content/MSTest-VisualBasic/MSTestSettings.vb
+++ template_feed/Microsoft.DotNet.Common.ProjectTemplates.10.0/content/MSTest-VisualBasic/MSTestSettings.vb
@@ -1,3 +1,5 @@
-﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+﻿Imports System.Diagnostics.CodeAnalysis
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
+<Assembly: ExcludeFromCodeCoverage>
 <Assembly: Parallelize(Scope:=ExecutionScope.MethodLevel)>
