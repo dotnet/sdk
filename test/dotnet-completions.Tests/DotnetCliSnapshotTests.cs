8ed0d7742dc4e3cbd9efed78210c21c2073472da
Chet Husk <chusk3@gmail.com>
2025-02-27 16:06:59 -0600

    try to detect helix

--- test/dotnet-completions.Tests/DotnetCliSnapshotTests.cs
+++ test/dotnet-completions.Tests/DotnetCliSnapshotTests.cs
@@ -15,11 +15,10 @@ public class DotnetCliSnapshotTests : SdkTest
     [Theory]
     public async Task VerifyCompletions(string shellName)
     {
-        DumpEnv(Log);
         var provider = System.CommandLine.StaticCompletions.CompletionsCommand.DefaultShells.Single(x => x.ArgumentName == shellName);
         var completions = provider.GenerateCompletions(Microsoft.DotNet.Cli.Parser.RootCommand);
         var settings = new VerifySettings();
-        if (Environment.GetEnvironmentVariable("CI") is string ci && ci.Equals("true", StringComparison.OrdinalIgnoreCase))
+        if (Environment.GetEnvironmentVariable("USER") is string user && user.Equals("helix-runner", StringComparison.OrdinalIgnoreCase))
         {
             Log.WriteLine($"CI environment detected, using snapshots directory in the current working directory {Environment.CurrentDirectory}");
             settings.UseDirectory(Path.Combine(Environment.CurrentDirectory, "snapshots", provider.ArgumentName));
@@ -30,16 +29,6 @@ public class DotnetCliSnapshotTests : SdkTest
             settings.UseDirectory(Path.Combine("snapshots", provider.ArgumentName));
         }
         await Verifier.Verify(target: completions, extension: provider.Extension, settings: settings);
-    }
-
-
-    private static void DumpEnv(ITestOutputHelper log)
-    {
-        log.WriteLine("Environment Variables:");
-        foreach (System.Collections.DictionaryEntry de in Environment.GetEnvironmentVariables())
-        {
-            log.WriteLine($"  {de.Key} = {de.Value}");
-        }
     }
 
     public static IEnumerable<object[]> ShellNames = System.CommandLine.StaticCompletions.CompletionsCommand.DefaultShells.Select<IShellProvider, object[]>(x => [x.ArgumentName]);
