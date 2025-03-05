364ec5c850c2cce7674e1abf09089c42f8d20dc3
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2025-03-04 23:07:04 -0800

    Add destructor tests.

---
+++ test/Microsoft.DotNet.ApiDiff.Tests/Diff.Destructor.Tests.cs
@@ -0,0 +1,70 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.ApiDiff.Tests;
+
+public class DiffDestructorTests : DiffBaseTests
+{
+    [Fact]
+    public Task TestDestructorAdd() => RunTestAsync(
+            beforeCode: """
+                namespace MyNamespace
+                {
+                    public class MyClass
+                    {
+                    }
+                }
+                """,
+            afterCode: """
+                namespace MyNamespace
+                {
+                    public class MyClass
+                    {
+                        ~MyClass()
+                        {
+                        }
+                    }
+                }
+                """,
+            expectedCode: """
+                  namespace MyNamespace
+                  {
+                      public class MyClass
+                      {
+                +         ~MyClass();
+                      }
+                  }
+                """, hideImplicitDefaultConstructors: true);
+
+    [Fact]
+    public Task TestDestructorDelete() => RunTestAsync(
+                beforeCode: """
+                namespace MyNamespace
+                {
+                    public class MyClass
+                    {
+                        ~MyClass()
+                        {
+                        }
+                    }
+                }
+                """,
+                afterCode: """
+                namespace MyNamespace
+                {
+                    public class MyClass
+                    {
+                    }
+                }
+                """,
+                expectedCode: """
+                  namespace MyNamespace
+                  {
+                      public class MyClass
+                      {
+                -         ~MyClass();
+                      }
+                  }
+                """, hideImplicitDefaultConstructors: true);
+
+}
