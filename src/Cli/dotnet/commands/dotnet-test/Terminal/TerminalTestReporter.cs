2696f43e4c38ff85a213369b945eca375b4fce09
mariam-abdulla <mabdullah@microsoft.com>
2025-03-06 13:45:23 +0100

    Undo comment

--- src/Cli/dotnet/commands/dotnet-test/Terminal/TerminalTestReporter.cs
+++ src/Cli/dotnet/commands/dotnet-test/Terminal/TerminalTestReporter.cs
@@ -1,8 +1,9 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Concurrent;
 using System.CommandLine.Help;
+using System.Diagnostics;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using Microsoft.DotNet.Cli;
@@ -124,6 +125,7 @@ internal sealed partial class TerminalTestReporter : IDisposable
     /// </summary>
     public TerminalTestReporter(IConsole console, TerminalTestReporterOptions options)
     {
+        Debugger.Launch();
         _options = options;
 
         Func<bool?> showProgress = _options.ShowProgress;
