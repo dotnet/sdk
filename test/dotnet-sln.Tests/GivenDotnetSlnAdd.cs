e45352fed28feb1732cfcd290f9dd8cabd65349e
Alexander Köplinger <alex.koeplinger@outlook.com>
2025-03-08 15:29:59 +0100

    Reenable vs-solutionpersistence tests

--- test/dotnet-sln.Tests/GivenDotnetSlnAdd.cs
+++ test/dotnet-sln.Tests/GivenDotnetSlnAdd.cs
@@ -244,11 +244,10 @@ Options:
         }
 
         [Theory]
-        // needs https://github.com/microsoft/vs-solutionpersistence/pull/101
-        //[InlineData("sln", true, ".sln")]
-        //[InlineData("sln", false, ".sln")]
-        //[InlineData("solution", true, ".sln")]
-        //[InlineData("solution", false, ".sln")]
+        [InlineData("sln", true, ".sln")]
+        [InlineData("sln", false, ".sln")]
+        [InlineData("solution", true, ".sln")]
+        [InlineData("solution", false, ".sln")]
         [InlineData("sln", true, ".slnx")]
         [InlineData("solution", false, ".slnx")]
         public void WhenNestedProjectIsAddedSolutionFoldersAreCreatedBuild(string solutionCommand, bool fooFirst, string solutionExtension)
