8c73f07843f1cd9d0676b1bba7eed8e7b15b20b0
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-03-10 09:47:56 -0700

    Revert refactoring of comment

--- src/Cli/dotnet/commands/dotnet-workload/restore/WorkloadRestoreCommand.cs
+++ src/Cli/dotnet/commands/dotnet-workload/restore/WorkloadRestoreCommand.cs
@@ -1,5 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this solutionFilePath to you under the MIT license.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
 using Microsoft.Build.Construction;
