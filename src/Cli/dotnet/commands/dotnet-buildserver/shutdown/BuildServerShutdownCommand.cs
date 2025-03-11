e3a096f66279b8012e1cb44985728c69ecdc6eaa
Michael Yanni <miyanni@microsoft.com>
2025-03-11 15:57:18 -0700

    Minor cleanup in dotnet.csproj. Fixed namespace for BuildServer files and made them file-scoped namespaces.

--- src/Cli/dotnet/commands/dotnet-buildserver/shutdown/BuildServerShutdownCommand.cs
+++ src/Cli/dotnet/commands/dotnet-buildserver/shutdown/BuildServerShutdownCommand.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
-using Microsoft.DotNet.BuildServer;
 using Microsoft.DotNet.Cli;
+using Microsoft.DotNet.Cli.BuildServer;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.Cli.Utils.Extensions;
 
