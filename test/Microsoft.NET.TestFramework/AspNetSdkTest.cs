3009bfcf3dbee523ac15510ecbcff7fea64a6da6
Marek Fišera <mara@neptuo.com>
2025-03-11 10:46:31 +0100

    Test with WasmBootConfigFileName

--- test/Microsoft.NET.TestFramework/AspNetSdkTest.cs
+++ test/Microsoft.NET.TestFramework/AspNetSdkTest.cs
@@ -13,6 +13,7 @@ namespace Microsoft.NET.TestFramework
     public abstract class AspNetSdkTest : SdkTest
     {
         public readonly string? DefaultTfm;
+        public const string WasmBootConfigFileName = "dotnet.boot.js";
 
 #if !GENERATE_MSBUILD_LOGS
         public static bool GenerateMSbuildLogs = true;
@@ -55,6 +56,14 @@ namespace Microsoft.NET.TestFramework
                     {
                         targetFrameworks.Value = targetFrameworks.Value.Replace("$(AspNetTestTfm)", overrideTfm ?? DefaultTfm);
                         targetFrameworks.AddAfterSelf(new XElement("StaticWebAssetsFingerprintContent", "false"));
+                    }
+
+                    if (project.Root != null)
+                    {
+                        var itemGroup = new XElement("PropertyGroup");
+                        var fingerprintAssets = new XElement("WasmBootConfigFileName", WasmBootConfigFileName);
+                        itemGroup.Add(fingerprintAssets);
+                        project.Root.Add(itemGroup);
                     }
                 });
 
