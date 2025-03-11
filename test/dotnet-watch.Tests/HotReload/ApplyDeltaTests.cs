6a6bea1fcf587e8c134c27c989eaa904e3381c83
Tomas Matousek <tomat@microsoft.com>
2025-03-10 17:11:41 -0700

    Fix MAUI static asset updates

--- test/dotnet-watch.Tests/HotReload/ApplyDeltaTests.cs
+++ test/dotnet-watch.Tests/HotReload/ApplyDeltaTests.cs
@@ -441,7 +441,8 @@ namespace Microsoft.DotNet.Watch.UnitTests
 
             await App.AssertOutputLineStartsWith("dotnet watch 🔥 Hot reload change handled");
 
-            App.AssertOutputContains($"dotnet watch ⌚ Sending static asset update request to browser: 'app.css'.");
+            // "wwwroot" directory is required for MAUI. Web sites work with or without it.
+            App.AssertOutputContains($"dotnet watch ⌚ Sending static asset update request to browser: 'wwwroot/app.css'.");
             App.AssertOutputContains($"dotnet watch 🔥 Hot reload of static files succeeded.");
             App.AssertOutputContains(MessageDescriptor.NoCSharpChangesToApply);
             App.Process.ClearOutput();
