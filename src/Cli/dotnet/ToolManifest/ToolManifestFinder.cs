95a27f071d1b5aab81986951721b6e0c462b1b2f
Eduardo Villalpando Mello <eduardov@microsoft.com>
2025-03-12 10:06:38 -0700

    Update comment

--- src/Cli/dotnet/ToolManifest/ToolManifestFinder.cs
+++ src/Cli/dotnet/ToolManifest/ToolManifestFinder.cs
@@ -198,7 +198,7 @@ namespace Microsoft.DotNet.ToolManifest
         /*
         The --create-manifest-if-needed will use the following priority to choose the folder where the tool manifest goes:
             1. Walk up the directory tree searching for one that has a.git subfolder
-            2. Walk up the directory tree searching for one that has a .sln/git file in it
+            2. Walk up the directory tree searching for one that has a .sln(x)/git file in it
             3. Use the current working directory
         */
         private DirectoryPath GetDirectoryToCreateToolManifest()
