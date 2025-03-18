f74253b5def3b40430a3efff5bf9e265906c6e20
Marc Paine <marcpop@microsoft.com>
2025-03-18 11:08:35 -0700

    Make the CommandAssertions null checks match the fluent assertions pattern

--- test/Microsoft.NET.TestFramework/Assertions/CommandResultAssertions.cs
+++ test/Microsoft.NET.TestFramework/Assertions/CommandResultAssertions.cs
@@ -42,40 +42,28 @@ namespace Microsoft.NET.TestFramework.Assertions
 
         public AndConstraint<CommandResultAssertions> HaveStdOut(string expectedOutput)
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
-            _commandResult.StdOut.Should().Be(expectedOutput, AppendDiagnosticsTo($"Command did not output with Expected Output. Expected: {expectedOutput}"));
+            _commandResult.StdOut.Should().NotBeNull()
+                .And.Be(expectedOutput, AppendDiagnosticsTo($"Command did not output with Expected Output. Expected: {expectedOutput}"),StringComparison.Ordinal);
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdOutContaining(string pattern)
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
-            _commandResult.StdOut.Should().Contain(pattern, AppendDiagnosticsTo($"The command output did not contain expected result: {pattern}{Environment.NewLine}"));
+            _commandResult.StdOut.Should().NotBeNull()
+                .And.Contain(pattern, AppendDiagnosticsTo($"The command output did not contain expected result: {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdOutContaining(Func<string?, bool> predicate, string description = "")
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
+            _commandResult.StdOut.Should().NotBeNull();
             predicate(_commandResult.StdOut).Should().BeTrue(AppendDiagnosticsTo($"The command output did not contain expected result: {description} {Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> NotHaveStdOutContaining(string pattern, string[]? ignoredPatterns = null)
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
+            _commandResult.StdOut.Should().NotBeNull();
 
             string filteredStdOut = _commandResult.StdOut ?? string.Empty;
             if (ignoredPatterns != null && ignoredPatterns.Length > 0)
@@ -95,72 +83,50 @@ namespace Microsoft.NET.TestFramework.Assertions
 
         public AndConstraint<CommandResultAssertions> HaveStdOutContainingIgnoreSpaces(string pattern)
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
-            string commandResultNoSpaces = _commandResult.StdOut?.Replace(" ", "") ?? string.Empty;
+            _commandResult.StdOut.Should().NotBeNull();
+            string commandResultNoSpaces = _commandResult.StdOut.Replace(" ", "");
             commandResultNoSpaces.Should().Contain(pattern, AppendDiagnosticsTo($"The command output did not contain expected result: {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdOutContainingIgnoreCase(string pattern)
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
-            _commandResult.StdOut.Should().ContainEquivalentOf(pattern, AppendDiagnosticsTo($"The command output did not contain expected result (ignoring case): {pattern}{Environment.NewLine}"));
+            _commandResult.StdOut.Should().NotBeNull()
+                .And.ContainEquivalentOf(pattern, AppendDiagnosticsTo($"The command output did not contain expected result (ignoring case): {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdOutMatching(string pattern, RegexOptions options = RegexOptions.None)
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
+            _commandResult.StdOut.Should().NotBeNull();
             Regex.Match(_commandResult.StdOut ?? string.Empty, pattern, options).Success.Should().BeTrue(AppendDiagnosticsTo($"Matching the command output failed. Pattern: {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> NotHaveStdOutMatching(string pattern, RegexOptions options = RegexOptions.None)
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
+            _commandResult.StdOut.Should().NotBeNull();
             Regex.Match(_commandResult.StdOut ?? string.Empty, pattern, options).Success.Should().BeFalse(AppendDiagnosticsTo($"The command output matched a pattern it should not have. Pattern: {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdErr()
         {
-            if (_commandResult.StdErr is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
             _commandResult.StdErr.Should().NotBeNullOrEmpty(AppendDiagnosticsTo("Command did not output anything to StdErr."));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdErr(string expectedOutput)
         {
-            if (_commandResult.StdErr is null)
-            {
-                throw new InvalidOperationException("StdErr for the command was not captured");
-            }
-            _commandResult.StdErr.Should().Be(expectedOutput, AppendDiagnosticsTo($"Command did not output the expected output to StdErr.{Environment.NewLine}Expected: {expectedOutput}{Environment.NewLine}Actual:   {_commandResult.StdErr}"));
+            _commandResult.StdErr.Should().NotBeNull()
+                .And.Be(expectedOutput, AppendDiagnosticsTo($"Command did not output the expected output to StdErr.{Environment.NewLine}Expected: {expectedOutput}{Environment.NewLine}Actual:   {_commandResult.StdErr}"), StringComparison.Ordinal);
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdErrContaining(string pattern)
         {
-            if (_commandResult.StdErr is null)
-            {
-                throw new InvalidOperationException("StdErr for the command was not captured");
-            }
-            _commandResult.StdErr.Should().Contain(pattern, AppendDiagnosticsTo($"The command error output did not contain expected result: {pattern}{Environment.NewLine}"));
+            _commandResult.StdErr.Should().NotBeNull()
+                .And.Contain(pattern, AppendDiagnosticsTo($"The command error output did not contain expected result: {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
@@ -168,48 +134,34 @@ namespace Microsoft.NET.TestFramework.Assertions
         {
             var lines = _commandResult.StdErr?.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
             var matchingLines = lines?.Where(line => line.Contains(pattern)).Count();
-            matchingLines.Should().NotBe(0, AppendDiagnosticsTo($"The command error output did not contain expected result: {pattern}{Environment.NewLine}"));
-            matchingLines.Should().Be(1, AppendDiagnosticsTo($"The command error output was expected to contain the pattern '{pattern}' once, but found it {matchingLines} times.{Environment.NewLine}"));
+            matchingLines.Should().NotBe(0, AppendDiagnosticsTo($"The command error output did not contain expected result: {pattern}{Environment.NewLine}"))
+                .And.Be(1, AppendDiagnosticsTo($"The command error output was expected to contain the pattern '{pattern}' once, but found it {matchingLines} times.{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
 
         public AndConstraint<CommandResultAssertions> NotHaveStdErrContaining(string pattern)
         {
-            if (_commandResult.StdErr is null)
-            {
-                throw new InvalidOperationException("StdErr for the command was not captured");
-            }
-            _commandResult.StdErr.Should().NotContain(pattern, AppendDiagnosticsTo($"The command error output contained a result it should not have contained: {pattern}{Environment.NewLine}"));
+            _commandResult.StdErr.Should().NotBeNull()
+                .And.NotContain(pattern, AppendDiagnosticsTo($"The command error output contained a result it should not have contained: {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> HaveStdErrMatching(string pattern, RegexOptions options = RegexOptions.None)
         {
-            if (_commandResult.StdErr is null)
-            {
-                throw new InvalidOperationException("StdErr for the command was not captured");
-            }
+            _commandResult.StdErr.Should().NotBeNull();
             Regex.Match(_commandResult.StdErr ?? string.Empty, pattern, options).Success.Should().BeTrue(AppendDiagnosticsTo($"Matching the command error output failed. Pattern: {pattern}{Environment.NewLine}"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> NotHaveStdOut()
         {
-            if (_commandResult.StdOut is null)
-            {
-                throw new InvalidOperationException("StdOut for the command was not captured");
-            }
             _commandResult.StdOut.Should().BeNullOrEmpty(AppendDiagnosticsTo($"Expected command to not output to stdout but it was not:"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
 
         public AndConstraint<CommandResultAssertions> NotHaveStdErr()
         {
-            if (_commandResult.StdErr is null)
-            {
-                throw new InvalidOperationException("StdErr for the command was not captured");
-            }
             _commandResult.StdErr.Should().BeNullOrEmpty(AppendDiagnosticsTo("Expected command to not output to stderr but it was not:"));
             return new AndConstraint<CommandResultAssertions>(this);
         }
