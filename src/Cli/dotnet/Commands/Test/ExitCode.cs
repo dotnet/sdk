fa1d26fe5fb299d1d25519328f338ee2828f9756
Youssef1313 <youssefvictor00@gmail.com>
2025-03-27 18:52:08 +0100

    Doc

--- src/Cli/dotnet/Commands/Test/ExitCode.cs
+++ src/Cli/dotnet/Commands/Test/ExitCode.cs
@@ -1,8 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Reflection.Metadata;
+
 namespace Microsoft.DotNet.Cli;
 
+// IMPORTANT: The exit codes must match MTP:
+// https://github.com/microsoft/testfx/blob/main/src/Platform/Microsoft.Testing.Platform/Helpers/ExitCodes.cs
+// They are also documented in https://learn.microsoft.com/dotnet/core/testing/microsoft-testing-platform-exit-codes
 internal static class ExitCode
 {
     public const int Success = 0;
