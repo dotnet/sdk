be08c4f5a9b766fa76e4cb6565ff80d51559e0a6
Viktor Hofer <viktor.hofer@microsoft.com>
2025-03-31 18:25:52 +0200

    Let this be the final fix

--- src/Tasks/Microsoft.NET.Build.Tasks/CollatePackageDownloads.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/CollatePackageDownloads.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace Microsoft.NET.Build.Tasks
 {
