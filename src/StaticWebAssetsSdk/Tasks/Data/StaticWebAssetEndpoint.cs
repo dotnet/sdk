9e1de784349f281f2141b1c923493424fe1b5ee2
Javier Calvarro Nelson <jacalvar@microsoft.com>
2025-04-01 21:37:41 +0200

    More allocation cleanups

--- src/StaticWebAssetsSdk/Tasks/Data/StaticWebAssetEndpoint.cs
+++ src/StaticWebAssetsSdk/Tasks/Data/StaticWebAssetEndpoint.cs
@@ -193,7 +193,7 @@ public class StaticWebAssetEndpoint : IEquatable<StaticWebAssetEndpoint>, ICompa
         return result;
     }
 
-    public static ITaskItem[] ToTaskItems(IList<StaticWebAssetEndpoint> endpoints)
+    public static ITaskItem[] ToTaskItems(ICollection<StaticWebAssetEndpoint> endpoints)
     {
         if (endpoints == null || endpoints.Count == 0)
         {
@@ -201,9 +201,10 @@ public class StaticWebAssetEndpoint : IEquatable<StaticWebAssetEndpoint>, ICompa
         }
 
         var endpointItems = new ITaskItem[endpoints.Count];
-        for (var i = 0; i < endpoints.Count; i++)
+        var i = 0;
+        foreach (var endpoint in endpoints)
         {
-            endpointItems[i] = endpoints[i].ToTaskItem();
+            endpointItems[i++] = endpoint.ToTaskItem();
         }
 
         return endpointItems;
