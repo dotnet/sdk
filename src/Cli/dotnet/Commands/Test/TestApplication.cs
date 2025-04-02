ef37bdbf95dd76c661947d3ed652c5f70f7a4a07
Youssef Victor <youssefvictor00@gmail.com>
2025-04-02 13:50:29 +0200

    Fix

--- src/Cli/dotnet/Commands/Test/TestApplication.cs
+++ src/Cli/dotnet/Commands/Test/TestApplication.cs
@@ -78,7 +78,7 @@ internal sealed class TestApplication(TestModule module, BuildOptions buildOptio
         }
 
         // If we reach here, that means we have a test project that doesn't produce an executable.
-        throw new InvalidOperationException($"A Microsoft.Testing.Platform test project should produce an executable. The file '{_module.RunProperties.RunCommand}' is dll.");
+        throw new InvalidOperationException($"A Microsoft.Testing.Platform test project should produce an executable. The file '{Module.RunProperties.RunCommand}' is dll.");
     }
 
     private static bool IsArchitectureSpecified(TestOptions testOptions)
