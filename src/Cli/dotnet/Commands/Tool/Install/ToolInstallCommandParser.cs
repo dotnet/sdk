c03312910d577153733374f149f7a5ca76725010
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2025-04-01 18:10:38 -0700

    Add using System.CommandLine.Parsing;

--- src/Cli/dotnet/Commands/Tool/Install/ToolInstallCommandParser.cs
+++ src/Cli/dotnet/Commands/Tool/Install/ToolInstallCommandParser.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
-
+using System.CommandLine.Parsing;
 using Microsoft.DotNet.Cli.Commands.Tool.Common;
 using Microsoft.DotNet.Cli.Commands.Tool.Search;
 using Microsoft.DotNet.Cli.Extensions;
