e935ca85580704455b2ee9e7b40367f9f9dbee7f
Forgind <12969783+Forgind@users.noreply.github.com>
2025-04-01 19:07:01 -0700

    Revert previous change

--- src/Resolvers/Microsoft.NET.Sdk.WorkloadManifestReader/WorkloadResolver.cs
+++ src/Resolvers/Microsoft.NET.Sdk.WorkloadManifestReader/WorkloadResolver.cs
@@ -49,16 +49,7 @@ namespace Microsoft.NET.Sdk.WorkloadManifestReader
                 workloadRootPaths = packRootEnvironmentVariable.Split(Path.PathSeparator).Select(path => (path, false)).Concat(workloadRootPaths).ToArray();
             }
 
-            try
-            {
-                return new WorkloadResolver(manifestProvider, workloadRootPaths, currentRuntimeIdentifiers);
-            }
-            catch (Exception e)
-            {
-                var ex = new Exception(e.Message, e.InnerException);
-                ex.Data.Add("CLI_User_Displayed_Exception", true);
-                throw ex;
-            }
+            return new WorkloadResolver(manifestProvider, workloadRootPaths, currentRuntimeIdentifiers);
         }
 
         public static WorkloadResolver CreateForTests(IWorkloadManifestProvider manifestProvider, string dotNetRoot, bool userLocal = false, string? userProfileDir = null, string[]? currentRuntimeIdentifiers = null)
