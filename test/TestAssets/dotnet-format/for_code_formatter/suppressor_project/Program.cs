988160d2876396e3edac9fb44e8a62f0287ce7ec
Sebastien Lebreton <sebastien@lebreton.fr>
2025-04-16 16:06:24 +0200

    Add unit tests

---
+++ test/TestAssets/dotnet-format/for_code_formatter/suppressor_project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace for_code_formatter
+{
+    class Program : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+namespace UnityEngine
+{
+    public class MonoBehaviour
+    {
+        // This is a placeholder for the Unity MonoBehaviour class.
+        // In a real Unity project, this would be part of the Unity engine.
+    }
+}
