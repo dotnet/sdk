e8efaf9c85fe5af5858a6b7145aa003f420cdb02
Ella Hathaway <ellahathaway@microsoft.com>
2025-04-17 20:42:29 +0000

    Allow non-shipping artifacts

--- eng/GatherDrops.ps1
+++ eng/GatherDrops.ps1
@@ -11,14 +11,15 @@ param(
   [Parameter(Mandatory=$true)]
   [String]$azdevPat,
   [Parameter(Mandatory=$false)]
-  [String]$assetFilter = ".*"
+  [String]$assetFilter = ".*",
+  [Switch]$nonShipping = $false
 )
 $jsonContent = Get-Content -Path $filePath -Raw | ConvertFrom-Json
 foreach ($repo in $jsonContent.repositories) {
   $remoteUri = $repo.remoteUri
   $commitSha = $repo.commitSha
   $path = "$outputPath$($repo.path)"
-  $darcCommand = "$darcPath gather-drop -c $commitSha -r $remoteUri --non-shipping --skip-existing --continue-on-error --use-azure-credential-for-blobs -o $path --github-pat $githubPat --azdev-pat $azdevPat --asset-filter $assetFilter --verbose --ci --include-released"
+  $darcCommand = "$darcPath gather-drop -c $commitSha -r $remoteUri --skip-existing --continue-on-error --use-azure-credential-for-blobs -o $path --github-pat $githubPat --azdev-pat $azdevPat --asset-filter $assetFilter --verbose --ci --include-released $(if ($nonShipping) { '--non-shipping' })"
   Write-Output "Gathering drop for $remoteUri"
   Invoke-Expression $darcCommand
 }
