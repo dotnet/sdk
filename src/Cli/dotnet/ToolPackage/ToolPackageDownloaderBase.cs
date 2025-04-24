49c1f3fbf62a74728b706e427788c5ca6b533e9b
Daniel Plaisted <daplaist@microsoft.com>
2025-04-24 06:55:28 -0400

    Copilot edits: Create ToolPackageDownloaderBase
    Create a new abstract base class called ToolPackageDownloaderBase.  Refactor #ToolPackageDownloader  to derive from ToolPackageDownloaderBase, and move the implementation into the base class.

--- src/Cli/dotnet/ToolPackage/ToolPackageDownloaderBase.cs
+++ src/Cli/dotnet/ToolPackage/ToolPackageDownloaderBase.cs
@@ -1,7 +1,5 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#nullable disable
 
 using System.Reflection;
 using Microsoft.DotNet.Cli.Extensions;
@@ -28,7 +26,7 @@ namespace Microsoft.DotNet.Cli.ToolPackage;
 
 internal abstract class ToolPackageDownloaderBase : IToolPackageDownloader
 {
-    private readonly IToolPackageStore _toolPackageStore;
+    protected readonly IToolPackageStore _toolPackageStore;
 
     // The directory that the tool package is returned 
     protected DirectoryPath _toolReturnPackageDirectory;
@@ -50,7 +48,7 @@ internal abstract class ToolPackageDownloaderBase : IToolPackageDownloader
 
     protected readonly string _runtimeJsonPath;
 
-    private readonly string _currentWorkingDirectory;
+    protected readonly string _currentWorkingDirectory;
 
     protected ToolPackageDownloaderBase(
         IToolPackageStore store,
@@ -77,7 +75,6 @@ internal abstract class ToolPackageDownloaderBase : IToolPackageDownloader
         bool verifySignatures = true,
         RestoreActionConfig restoreActionConfig = null)
     {
-
         ILogger nugetLogger = new NullLogger();
         if (verbosity.IsDetailedOrDiagnostic())
         {
@@ -376,7 +373,7 @@ internal abstract class ToolPackageDownloaderBase : IToolPackageDownloader
     {
         var versionFolderPathResolver = new VersionFolderPathResolver(packagesRootPath);
 
-        string folderToDeleteOnFailure = null;
+        string? folderToDeleteOnFailure = null;
 
         try
         {
@@ -521,16 +518,5 @@ internal abstract class ToolPackageDownloaderBase : IToolPackageDownloader
             additionalSourceFeeds: packageLocation.AdditionalFeeds);
 
         return nugetPackageDownloader.GetBestPackageVersionAsync(packageId, versionRange, packageSourceLocation).GetAwaiter().GetResult();
-    }
-}
-
-internal class ToolPackageDownloader : ToolPackageDownloaderBase
-{
-    public ToolPackageDownloader(
-        IToolPackageStore store,
-        string runtimeJsonPathForTests = null,
-        string currentWorkingDirectory = null
-    ) : base(store, runtimeJsonPathForTests, currentWorkingDirectory)
-    {
     }
 }
