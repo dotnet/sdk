ceec1c48e60492ff781ed383ed87976ee2c295a3
Jared Parsons <jared@paranoidcoding.org>
2025-04-29 18:37:34 +0000

    fix run on dotnet

--- src/Cli/dotnet/Commands/Test/Terminal/TestNodeResultsState.cs
+++ src/Cli/dotnet/Commands/Test/Terminal/TestNodeResultsState.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#nullable disable warnings
+
 using System.Collections.Concurrent;
 using System.Globalization;
 
