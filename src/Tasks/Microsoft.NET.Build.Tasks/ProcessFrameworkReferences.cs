a19b33536e7dbd1364eff3ce12dd2b8f64ae3e83
Jared Parsons <jared@paranoidcoding.org>
2025-04-29 20:57:03 +0000

    fixup

--- src/Tasks/Microsoft.NET.Build.Tasks/ProcessFrameworkReferences.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ProcessFrameworkReferences.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
+#nullable disable warnings
 
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
--- src/Tasks/Microsoft.NET.Build.Tasks/ValidateExecutableReferences.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ValidateExecutableReferences.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
+#nullable disable warnings
 
 using System.Runtime.Versioning;
 using Microsoft.Build.Framework;
