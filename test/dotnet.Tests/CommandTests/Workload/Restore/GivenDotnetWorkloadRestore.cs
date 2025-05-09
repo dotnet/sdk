437c64d061cda1aa54a1540111312426801cc758
Jason Zhai <v-wuzhai@microsoft.com>
2025-05-08 20:11:28 -0700

    Add more logs

--- test/dotnet.Tests/CommandTests/Workload/Restore/GivenDotnetWorkloadRestore.cs
+++ test/dotnet.Tests/CommandTests/Workload/Restore/GivenDotnetWorkloadRestore.cs
@@ -41,6 +41,10 @@ public class GivenDotnetWorkloadRestore : SdkTest
         Directory.CreateDirectory(cliHome);
         CreateUserLocalFileForCurrentSdk(cliHome);
 
+        Log.WriteLine($"[DEBUG] DOTNET_CLI_HOME = {Environment.GetEnvironmentVariable("DOTNET_CLI_HOME")}");
+        Log.WriteLine($"[DEBUG] HOME = {Environment.GetEnvironmentVariable("HOME")}");
+        Log.WriteLine($"[DEBUG] DOTNET_ROOT = {Environment.GetEnvironmentVariable("DOTNET_ROOT")}");
+
         var projectPath =
             _testAssetsManager
                 .CopyTestAsset(TransitiveReferenceNoWorkloadsAssetName)
@@ -48,12 +52,12 @@ public class GivenDotnetWorkloadRestore : SdkTest
                 .Path;
 
         new DotnetWorkloadCommand(Log, "restore")
-        .WithWorkingDirectory(projectPath)
-        .WithEnvironmentVariable("DOTNET_CLI_HOME", cliHome)
-        .Execute()
-        .Should()
-        // if we did try to restore the esproj in this TestAsset we would fail, so passing means we didn't!
-        .Pass();
+            .WithWorkingDirectory(projectPath)
+            .WithEnvironmentVariable("DOTNET_ROOT", cliHome)
+            .Execute()
+            .Should()
+            // if we did try to restore the esproj in this TestAsset we would fail, so passing means we didn't!
+            .Pass();
     }
 
     private void CreateUserLocalFileForCurrentSdk(string cliHome)
@@ -67,8 +71,29 @@ public class GivenDotnetWorkloadRestore : SdkTest
         var version = Version.Parse(sdkVersion.Split('-')[0]);
         var featureBand = $"{version.Major}.{version.Minor}.{(version.Build / 100) * 100}";
 
-        var userlocalPath = Path.Combine(cliHome, ".dotnet", "metadata", "workloads", featureBand);
+        var userlocalPath = Path.Combine(cliHome, "metadata", "workloads", featureBand);
         Directory.CreateDirectory(userlocalPath);
-        File.Create(Path.Combine(userlocalPath, "userlocal")).Dispose();
+
+        var userlocalFile = Path.Combine(userlocalPath, "userlocal");
+        File.Create(userlocalFile).Dispose();
+
+        Log.WriteLine($"[DEBUG] Directory exists({userlocalPath}): {Directory.Exists(userlocalPath)}");
+        Log.WriteLine($"[DEBUG] File exists({userlocalFile}): {File.Exists(userlocalFile)}");
+        Log.WriteLine($"[DEBUG] userlocal path writable: {IsDirectoryWritable(userlocalPath)}");
+    }
+
+    private bool IsDirectoryWritable(string path)
+    {
+        try
+        {
+            var testFile = Path.Combine(path, Path.GetRandomFileName());
+            File.WriteAllText(testFile, "test");
+            File.Delete(testFile);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
