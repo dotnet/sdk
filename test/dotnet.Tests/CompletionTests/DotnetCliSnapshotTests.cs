cc3542860d33db268d2378497103cf90e2718feb
Forgind <12969783+Forgind@users.noreply.github.com>
2025-05-14 11:48:30 -0700

    FF only

--- test/dotnet.Tests/CompletionTests/DotnetCliSnapshotTests.cs
+++ test/dotnet.Tests/CompletionTests/DotnetCliSnapshotTests.cs
@@ -31,5 +31,6 @@ public class DotnetCliSnapshotTests : SdkTest
         await Verify(target: completions, extension: provider.Extension, settings: settings);
     }
 
-    public static IEnumerable<object[]> ShellNames = CompletionsCommand.DefaultShells.Select<IShellProvider, object[]>(x => [x.ArgumentName]).Where(shell => !shell.IsReadOnly.Equals("zsh")); // zsh has been unstable lately
+    public static IEnumerable<object[]> ShellNames = CompletionsCommand.DefaultShells.Select<IShellProvider, object[]>(x => [x.ArgumentName])
+        .Where(shell => !shell.IsReadOnly.Equals("zsh") || !TestContext.Current.ToolsetUnderTest.ShouldUseFullFrameworkMSBuild); // zsh has been unstable lately
 }
