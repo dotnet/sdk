597e613479bc520c1c54e83ccbda9888369c4101
Jacques Eloff <joeloff@users.noreply.github.com>
2025-05-22 10:20:32 -0700

    Onboard templates and toolset MSIs

--- src/Layout/pkg/windows/wix.props/Directory.Wix.targets.pp
+++
@@ -1,8 +0,0 @@
-<!-- Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the MIT license. See License.txt in the project root for full license information. -->
-<Project>
-  <PropertyGroup>
-    <!-- DefineConstants values generate preprocessor variables passed to the WiX compiler. -->
-    <DefineConstants>$(DefineConstants);ProductVersion={msiVersion}</DefineConstants>
-    <DefineConstants>$(DefineConstants);BundleVersion={bundleVersion}</DefineConstants>
-  </PropertyGroup>
-</Project>
