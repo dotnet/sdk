4e5607e78187d3daa752f6629af9b978f7f59297
Chet Husk <chusk3@gmail.com>
2025-05-26 08:45:14 -0500

    add layer download pre-publish, and make it incremental

--- src/Containers/Microsoft.NET.Build.Containers/ManifestListV2.cs
+++ src/Containers/Microsoft.NET.Build.Containers/ManifestListV2.cs
@@ -5,11 +5,16 @@ using System.Text.Json.Serialization;
 
 namespace Microsoft.NET.Build.Containers;
 
-public record struct ManifestListV2(int schemaVersion, string mediaType, PlatformSpecificManifest[] manifests);
+/// <summary>
+/// Marker interface that signals that this contains sub-manifests
+/// </summary>
+public interface IMultiImageManifest: IManifest;
+
+public record struct ManifestListV2(int schemaVersion, string mediaType, PlatformSpecificManifest[] manifests) : IMultiImageManifest;
 
 public record struct PlatformInformation(string architecture, string os, string? variant, string[] features, [property: JsonPropertyName("os.version")][field: JsonPropertyName("os.version")] string? version);
 
 public record struct PlatformSpecificManifest(string mediaType, long size, string digest, PlatformInformation platform);
-public record struct ImageIndexV1(int schemaVersion, string mediaType, PlatformSpecificOciManifest[] manifests);
+public record struct ImageIndexV1(int schemaVersion, string mediaType, PlatformSpecificOciManifest[] manifests) : IMultiImageManifest;
 
 public record struct PlatformSpecificOciManifest(string mediaType, long size, string digest, PlatformInformation platform, Dictionary<string, string> annotations);
