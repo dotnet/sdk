f5fe8f5c13af1dbf8a6cbdcdf297f50aac7951b1
tmat <tomas.matousek@microsoft.com>
2025-06-10 12:16:45 -0700

    Skip BlazorWasm_Restart

--- test/dotnet-watch.Tests/HotReload/ApplyDeltaTests.cs
+++ test/dotnet-watch.Tests/HotReload/ApplyDeltaTests.cs
@@ -502,7 +502,7 @@ namespace Microsoft.DotNet.Watch.UnitTests
             await App.AssertWaitingForChanges();
         }
 
-        [Fact]
+        [Fact(Skip = "https://github.com/dotnet/sdk/issues/49307")]
         public async Task BlazorWasm_Restart()
         {
             var testAsset = TestAssets.CopyTestAsset("WatchBlazorWasm")
