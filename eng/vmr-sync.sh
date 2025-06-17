d2982bdb45b7adbf20a3e5f2e69ae3c7d7d7c1e8
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-06-17 12:27:30 +0200

    [release/9.0.1xx] Bump `darc` (#49444)

--- eng/vmr-sync.sh
+++ eng/vmr-sync.sh
@@ -33,11 +33,6 @@
 ###   --debug
 ###       Optional. Turns on the most verbose logging for the VMR tooling
 ###
-###   --component-template
-###       Optional. Template for VMRs Component.md used for regenerating the file to list the newest versions of
-###       components.
-###       Defaults to src/VirtualMonoRepo/Component.template.md
-###
 ###   --recursive
 ###       Optional. Recursively synchronize all the source build dependencies (declared in Version.Details.xml)
 ###       This is used when performing the full synchronization during sdk's CI and the final VMR sync.
@@ -104,7 +99,6 @@ repository=''
 additional_remotes=''
 recursive=false
 verbosity=verbose
-component_template="$sdk_dir/src/VirtualMonoRepo/Component.template.md"
 tpn_template="$sdk_dir/src/VirtualMonoRepo/THIRD-PARTY-NOTICES.template.txt"
 azdev_pat=''
 
@@ -137,10 +131,6 @@ while [[ $# -gt 0 ]]; do
       ;;
     --remote)
       additional_remotes="$additional_remotes $2"
-      shift
-      ;;
-    --component-template)
-      component_template=$2
       shift
       ;;
     --tpn-template)
@@ -177,11 +167,6 @@ fi
 
 if [[ -z "$tmp_dir" ]]; then
   fail "Missing --tmp-dir argument. Please specify the path to the temporary folder where the repositories will be cloned"
-  exit 1
-fi
-
-if [[ ! -f "$component_template" ]]; then
-  fail "File '$component_template' does not exist. Please specify a valid path to the Component.md template"
   exit 1
 fi
 
@@ -268,7 +253,6 @@ fi
   --$verbosity                               \
   $recursive_arg                             \
   $additional_remotes                        \
-  --component-template "$component_template" \
   --tpn-template "$tpn_template"             \
   --discard-patches                          \
   --generate-credscansuppressions            \
