54d6b648edbbf212391814be3a2707a54251ecac
Daniel Plaisted <daplaist@microsoft.com>
2025-07-06 15:54:24 -0400

    Re-enable other PR queues

--- test/HelixTasks/SDKCustomCreateXUnitWorkItemsWithTestExclusion.cs
+++ test/HelixTasks/SDKCustomCreateXUnitWorkItemsWithTestExclusion.cs
@@ -75,17 +75,10 @@ namespace Microsoft.DotNet.SdkCustomHelix.Sdk
             {
                 return;
             }
-			
-            HashSet<string> workItemsToInclude = new()
-            {
-                "dotnet.Tests.dll.15",
-                "dotnet-watch.Tests.dll.3",
-            };
 
             XUnitWorkItems = (await Task.WhenAll(XUnitProjects.Select(PrepareWorkItem)))
                 .SelectMany(i => i ?? new())
                 .Where(wi => wi != null)
-                //.Where(workItemsToInclude.Contains(wi.ItemSpec))
                 .ToArray();
             return;
         }
