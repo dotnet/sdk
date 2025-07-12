395a0e5fa1798101d56fdab01d0f7f1ba1a67ac9
Chet Husk <chusk3@gmail.com>
2025-07-12 14:36:50 -0500

    push improved MSBuildLogger to a separate project in case we want to spin it off

--- src/Microsoft.Extensions.Logging.MSBuild/MSBuildLoggerProvider.cs
+++ src/Microsoft.Extensions.Logging.MSBuild/MSBuildLoggerProvider.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
 
 using Microsoft.Build.Utilities;
-using Microsoft.Extensions.Logging;
 
-namespace Microsoft.NET.Build.Containers.Logging;
+namespace Microsoft.Extensions.Logging.MSBuild;
 
 /// <summary>
 /// An <see cref="ILoggerProvider"/> that creates <see cref="ILogger"/>s which passes
 /// all the logs to MSBuild's <see cref="TaskLoggingHelper"/>.
 /// </summary>
-internal class MSBuildLoggerProvider : ILoggerProvider
+public class MSBuildLoggerProvider : ILoggerProvider, ISupportExternalScope
 {
     private readonly TaskLoggingHelper _loggingHelper;
+    private List<MSBuildLogger> _loggers = new List<MSBuildLogger>();
+    private IExternalScopeProvider? _scopeProvider;
 
     public MSBuildLoggerProvider(TaskLoggingHelper loggingHelperToWrap)
     {
@@ -22,8 +22,19 @@ internal class MSBuildLoggerProvider : ILoggerProvider
 
     public ILogger CreateLogger(string categoryName)
     {
-        return new MSBuildLogger(categoryName, _loggingHelper);
+        var logger = new MSBuildLogger(categoryName, _loggingHelper, _scopeProvider);
+        _loggers.Add(logger);
+        return logger;
     }
 
     public void Dispose() { }
+
+    public void SetScopeProvider(IExternalScopeProvider scopeProvider)
+    {
+        _scopeProvider = scopeProvider;
+        foreach (var logger in _loggers)
+        {
+            logger.SetScopeProvider(scopeProvider);
+        }
+    }
 }
