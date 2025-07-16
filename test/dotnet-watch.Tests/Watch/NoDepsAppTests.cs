588876b8468745f62fbe88039b2924ea034ba715
Simon Zhao (BEYONDSOFT CONSULTING INC) <v-weidzh@microsoft.com>
2025-07-16 17:07:03 +0800

    Ignore the unstable case

--- test/dotnet-watch.Tests/Watch/NoDepsAppTests.cs
+++ test/dotnet-watch.Tests/Watch/NoDepsAppTests.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Watch.UnitTests
     {
         private const string AppName = "WatchNoDepsApp";
 
-        [Fact]
+        [Fact(Skip = "https://github.com/dotnet/sdk/issues/42921")]
         public async Task RestartProcessOnFileChange()
         {
             var testAsset = TestAssets.CopyTestAsset(AppName)
