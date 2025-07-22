1ec32f4b2543fca99a7060768f9d6b20d9fc5d0d
Eric StJohn <ericstj@microsoft.com>
2025-07-22 09:05:49 -0700

    Update maintenance-packages pruning versions (#49092)

--- src/Tasks/Microsoft.NET.Build.Tasks/FrameworkPackages/FrameworkPackages.netstandard2.1.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/FrameworkPackages/FrameworkPackages.netstandard2.1.cs
@@ -13,7 +13,7 @@ internal partial class FrameworkPackages
     {
         internal static FrameworkPackages Instance { get; } = new(NetStandard21, FrameworkNames.NetStandardLibrary, NETStandard20.Instance)
         {
-            { "System.Buffers", "4.5.1" },
+            { "System.Buffers", "4.6.1" },
             { "System.Collections.Concurrent", "4.3.0" },
             { "System.Collections.Immutable", "1.4.0" },
             { "System.ComponentModel", "4.3.0" },
@@ -22,10 +22,10 @@ internal partial class FrameworkPackages
             { "System.Diagnostics.Contracts", "4.3.0" },
             { "System.Dynamic.Runtime", "4.3.0" },
             { "System.Linq.Queryable", "4.3.0" },
-            { "System.Memory", "4.5.5" },
+            { "System.Memory", "4.6.3" },
             { "System.Net.Requests", "4.3.0" },
             { "System.Net.WebHeaderCollection", "4.3.0" },
-            { "System.Numerics.Vectors", "4.5.0" },
+            { "System.Numerics.Vectors", "4.6.1" },
             { "System.ObjectModel", "4.3.0" },
             { "System.Private.DataContractSerialization", "4.3.0" },
             { "System.Reflection.DispatchProxy", "4.5.1" },
@@ -42,7 +42,7 @@ internal partial class FrameworkPackages
             { "System.Security.Principal", "4.3.0" },
             { "System.Security.Principal.Windows", "4.4.0" },
             { "System.Threading", "4.3.0" },
-            { "System.Threading.Tasks.Extensions", "4.5.4" },
+            { "System.Threading.Tasks.Extensions", "4.6.3" },
             { "System.Threading.Tasks.Parallel", "4.3.0" },
             { "System.Xml.XDocument", "4.3.0" },
             { "System.Xml.XmlSerializer", "4.3.0" },
