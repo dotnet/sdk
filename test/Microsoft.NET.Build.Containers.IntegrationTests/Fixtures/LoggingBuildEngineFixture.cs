b6959085e1aba4a7fd40c3c8413e6cbadfb82118
Chet Husk <chusk3@gmail.com>
2025-08-03 19:25:18 -0500

    Add multi-arch manifest download test

--- test/Microsoft.NET.Build.Containers.IntegrationTests/Fixtures/LoggingBuildEngineFixture.cs
+++ test/Microsoft.NET.Build.Containers.IntegrationTests/Fixtures/LoggingBuildEngineFixture.cs
@@ -6,7 +6,7 @@ using Microsoft.Build.Framework;
 
 namespace Microsoft.NET.Build.Containers.IntegrationTests;
 
-public class LoggingBuildEngineFixture
+public class LoggingBuildEngineFixture: IDisposable
 {
     public List<BuildMessageEventArgs> Messages { get; } = new();
     public List<BuildWarningEventArgs> Warnings { get; } = new();
@@ -19,25 +19,27 @@ public class LoggingBuildEngineFixture
 
     public void SetupBuildEngine(ITestOutputHelper testOutput)
     {
-        if (BuildEngine is null)
+        IBuildEngine buildEngine = A.Fake<IBuildEngine>();
+        A.CallTo(() => buildEngine.LogWarningEvent(A<BuildWarningEventArgs>.Ignored)).Invokes((BuildWarningEventArgs e) =>
         {
-            IBuildEngine buildEngine = A.Fake<IBuildEngine>();
-            A.CallTo(() => buildEngine.LogWarningEvent(A<BuildWarningEventArgs>.Ignored)).Invokes((BuildWarningEventArgs e) =>
-            {
-                Warnings.Add(e);
-                testOutput.WriteLine($"Warning: {e.Message}");
-            });
-            A.CallTo(() => buildEngine.LogErrorEvent(A<BuildErrorEventArgs>.Ignored)).Invokes((BuildErrorEventArgs e) =>
-            {
-                Errors.Add(e);
-                testOutput.WriteLine($"Error: {e.Message}");
-            });
-            A.CallTo(() => buildEngine.LogMessageEvent(A<BuildMessageEventArgs>.Ignored)).Invokes((BuildMessageEventArgs e) =>
-            {
-                Messages.Add(e);
-                testOutput.WriteLine($"Message: {e.Message}");
-            });
-            BuildEngine = buildEngine;
-        }
+            Warnings.Add(e);
+            // testOutput.WriteLine($"Warning: {e.Message}");
+        });
+        A.CallTo(() => buildEngine.LogErrorEvent(A<BuildErrorEventArgs>.Ignored)).Invokes((BuildErrorEventArgs e) =>
+        {
+            Errors.Add(e);
+            // testOutput.WriteLine($"Error: {e.Message}");
+        });
+        A.CallTo(() => buildEngine.LogMessageEvent(A<BuildMessageEventArgs>.Ignored)).Invokes((BuildMessageEventArgs e) =>
+        {
+            Messages.Add(e);
+            // testOutput.WriteLine($"Message: {e.Message}");
+        });
+        BuildEngine = buildEngine;
+    }
+
+    public void Dispose()
+    {
+           
     }
 }
