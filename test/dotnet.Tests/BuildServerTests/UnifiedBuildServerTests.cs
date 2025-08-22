4efbcc12e43646d9392dd425764416020e7073dd
Jan Jones <janjones@microsoft.com>
2025-08-22 13:46:48 +0200

    Capture roslyn logs to help diagnosing test

--- test/dotnet.Tests/BuildServerTests/UnifiedBuildServerTests.cs
+++ test/dotnet.Tests/BuildServerTests/UnifiedBuildServerTests.cs
@@ -22,6 +22,8 @@ public sealed class UnifiedBuildServerTests(ITestOutputHelper output) : SdkTest(
             Console.WriteLine();
             """);
 
+        var roslynLog = Path.Join(testInstance.Path, "roslyn-log.txt");
+
         // Ensure there is no build server running from other tests.
         new DotnetCommand(Log, "build-server", "shutdown", "--unified")
             .WithWorkingDirectory(testInstance.Path)
@@ -33,15 +35,21 @@ public sealed class UnifiedBuildServerTests(ITestOutputHelper output) : SdkTest(
         // Build.
         new DotnetCommand(Log, "build")
             .WithWorkingDirectory(testInstance.Path)
+            .WithEnvironmentVariable("RoslynCommandLineLogFile", roslynLog)
             .Execute()
             .Should().Pass()
             .And.HaveStdOutContaining("app.dll");
 
         // Shutdown the build server.
-        new DotnetCommand(Log, "build-server", "shutdown", "--unified")
+        var result = new DotnetCommand(Log, "build-server", "shutdown", "--unified")
             .WithWorkingDirectory(testInstance.Path)
-            .Execute()
-            .Should().Pass()
+            .Execute();
+
+        Log.WriteLine(roslynLog);
+        string roslynLogText = File.ReadAllText(roslynLog);
+        Log.WriteLine(roslynLogText);
+
+        result.Should().Pass()
             .And.HaveStdOutContaining("VBCSCompiler")
             .And.NotHaveStdErr();
     }
