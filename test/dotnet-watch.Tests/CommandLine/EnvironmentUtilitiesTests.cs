d8d3cd5dbb4aa065db38e20267f54f0e2a82cc0f
Tomas Matousek <tomat@microsoft.com>
2025-08-29 13:05:30 -0700

    Add BrowserRefreshServer abstraction to Client package

---
+++ test/dotnet-watch.Tests/CommandLine/EnvironmentUtilitiesTests.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.DotNet.Watch.UnitTests;
+
+namespace Microsoft.DotNet.HotReload.UnitTests;
+
+public class EnvironmentUtilitiesTests
+{
+    [Fact]
+    public void MultipleValues()
+    {
+        var builder = new Dictionary<string, string>();
+        builder.InsertListItem("X", "a", separator: ';');
+        builder.InsertListItem("X", "b", separator: ';');
+        builder.InsertListItem("X", "a", separator: ';');
+
+        AssertEx.SequenceEqual([KeyValuePair.Create("X", "b;a")], builder);
+    }
+}
