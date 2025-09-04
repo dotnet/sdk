b776b9c05bbd9580d0ed12992d20b7ff0e9c314b
Simon Zhao (BEYONDSOFT CONSULTING INC) <v-weidzh@microsoft.com>
2025-09-04 12:09:12 +0800

    Fix test issues including: package prune tests, pruning in full framework tests, update invalid package name

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToResolveConflicts.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToResolveConflicts.cs
@@ -330,9 +330,10 @@ namespace Microsoft.NET.Build.Tests
         [InlineData("netstandard1.0", false)]
         [InlineData("net451", false)]
         [InlineData("net462")]
-        [InlineData("net481")]
-        [InlineData("net9.0", true, "")]
-        [InlineData("netstandard2.1", true, "")]
+        [InlineData("net481")]                
+        [InlineData("net9.0", false, "")] //These target frameworks shouldn't prune packages unless explicitly enabled
+        [InlineData("netstandard2.1", false, "")]        
+        [InlineData("net10.0", true, "")] //.NET 10 and up should prune packages by default
         public void PrunePackageDataSucceeds(string targetFramework, bool shouldPrune = true, string enablePackagePruning = "True")
         {
             var nugetFramework = NuGetFramework.Parse(targetFramework);
