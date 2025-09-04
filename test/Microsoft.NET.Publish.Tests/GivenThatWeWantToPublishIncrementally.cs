3422187c6a39bc52eb00031efd2a4dccac46b666
--get <chusk3@gmail.com>
2025-09-04 15:00:35 -0500

    binlogs for remaining failing tests

--- test/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishIncrementally.cs
+++ test/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishIncrementally.cs
@@ -62,11 +62,14 @@ namespace Microsoft.NET.Publish.Tests
             };
             var testAsset = _testAssetsManager.CreateTestProject(testProject, testProject.Name);
 
+            var binlogDestPath = Environment.GetEnvironmentVariable("HELIX_WORKITEM_UPLOAD_ROOT") is { } ciOutputRoot ?
+                Path.Combine(ciOutputRoot, "binlog", $"{nameof(It_cleans_between_single_file_publishes)}.binlog") :
+                "./msbuild.binlog";
 
             // Publish as a single file
             var publishCommand = new PublishCommand(testAsset);
             publishCommand
-                .Execute(@"/p:PublishSingleFile=true")
+                .Execute(@"/p:PublishSingleFile=true", $"-bl:{binlogDestPath}")
                 .Should()
                 .Pass();
 
@@ -105,9 +108,13 @@ namespace Microsoft.NET.Publish.Tests
             var testAsset = _testAssetsManager.CreateTestProject(testProject, testProject.Name);
 
             // Publish as a single file
-            var publishCommand = new PublishCommand(testAsset);
+            var binlogDestPath = Environment.GetEnvironmentVariable("HELIX_WORKITEM_UPLOAD_ROOT") is { } ciOutputRoot ?
+                Path.Combine(ciOutputRoot, "binlog", $"{nameof(It_cleans_between_single_file_publishes)}.binlog") :
+                "./msbuild.binlog";
+
+            var publishCommand = new PublishCommand(testAsset).WithWorkingDirectory(testAsset.Path) as PublishCommand;
             publishCommand
-                .Execute(@"/p:PublishSingleFile=true")
+                .Execute(@"/p:PublishSingleFile=true", $"-bl:{binlogDestPath}")
                 .Should()
                 .Pass();
 
