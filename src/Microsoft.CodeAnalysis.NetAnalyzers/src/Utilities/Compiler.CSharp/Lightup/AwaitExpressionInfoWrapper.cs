ef1bb2a4dec3033dbed2577f1b05d0281a613ada
Fred Silberberg <frsilb@microsoft.com>
2025-09-19 11:05:33 -0700

    Update DetectPreviewFeatureAnalyzer for runtime async
    We need to be giving warnings around enabling preview features for runtime async. This updates the analyzer to do this.

---
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Utilities/Compiler.CSharp/Lightup/AwaitExpressionInfoWrapper.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Analyzer.Utilities.Lightup
+{
+    internal static class AwaitExpressionInfoWrapper
+    {
+        private static Func<AwaitExpressionInfo, IMethodSymbol?>? s_RuntimeAwaitMethodAccessor;
+
+        extension(AwaitExpressionInfo info)
+        {
+            public IMethodSymbol? RuntimeAwaitMethod
+            {
+                get
+                {
+                    LazyInitializer.EnsureInitialized(ref s_RuntimeAwaitMethodAccessor, () =>
+                    {
+                        return LightupHelpers.CreatePropertyAccessor<AwaitExpressionInfo, IMethodSymbol?>(
+                            typeof(AwaitExpressionInfo),
+                            "info",
+                            "RuntimeAwaitMethod",
+                            fallbackResult: null);
+                    });
+
+                    return s_RuntimeAwaitMethodAccessor!(info);
+                }
+            }
+        }
+    }
+}
