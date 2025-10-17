0cac607a629d9be323e3cb225cfeac2f76d1fe5e
Tomas Matousek <tomat@microsoft.com>
2025-10-17 09:37:04 -0700

    Nullable annotations

--- src/BuiltInTools/Web.Middleware/BlazorWasmHotReloadMiddleware.cs
+++ src/BuiltInTools/Web.Middleware/BlazorWasmHotReloadMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
--- src/BuiltInTools/Web.Middleware/BrowserScriptMiddleware.cs
+++ src/BuiltInTools/Web.Middleware/BrowserScriptMiddleware.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System.Globalization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
