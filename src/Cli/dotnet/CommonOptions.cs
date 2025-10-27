9016451620fea8f6f6c6d421533de5c70d12aa89
--get <chusk3@gmail.com>
2025-10-27 17:04:27 -0500

    model the usage of --nologo for MSBuild forwarding and infer it from the DOTNET_NOLOGO environment variable

--- src/Cli/dotnet/CommonOptions.cs
+++ src/Cli/dotnet/CommonOptions.cs
@@ -399,6 +399,28 @@ internal static class CommonOptions
 
     public static readonly Option<string[]> TestLoggerOption = new("--logger");
 
+    /// <summary>
+    /// Creates an implementation of the <c>--nologo</c> option.
+    /// This option suppresses the display of the startup banner or logos for commands or products it is applied to.
+    /// The behavior of this option is influenced by the <c>DOTNET_NOLOGO</c> environment variable if it is set:
+    /// <list type="bullet">
+    /// <item>If the <c>--nologo</c> option is not specified on the command line, the environment variable will be checked to determine
+    /// whether to suppress logos. Any truthy value - <c>true</c>, <c>1</c>, <c>yes</c>, <c>on</c> - will suppress logos, while any falsy value - <c>false</c>, <c>0</c>, <c>no</c>, <c>off</c> - will show logos.</item>
+    /// <item>If the option is specified on the command line, it takes precedence over the environment variable.</item>
+    /// </list>
+    /// Finally, if neither the option nor the environment variable is set, the option will default to the provided <paramref name="defaultValue"/>.
+    /// </summary>
+    public static Option<bool> NoLogoOption(bool defaultValue = true, string forwardAs = "-nologo")
+    {
+        return new Option<bool>("--nologo")
+        {
+            Description = Commands.CliCommandStrings.NoLogoOptionDescription,
+            DefaultValueFactory = (ar) => Env.TryGetEnvironmentVariableAsBool("DOTNET_NOLOGO", out bool value) ? value : defaultValue,
+            CustomParser = (ar) => true,
+            Arity = ArgumentArity.Zero
+        }.ForwardIfEnabled(forwardAs);
+    }
+
     public static void ValidateSelfContainedOptions(bool hasSelfContainedOption, bool hasNoSelfContainedOption)
     {
         if (hasSelfContainedOption && hasNoSelfContainedOption)
