75e3b5db8fa932df98bfa6d83257caafa77bc544
Daniel Plaisted <daplaist@microsoft.com>
2025-10-27 21:52:11 -0400

    Add progress target interface and Spectre implementation

---
+++ src/Installer/Microsoft.Dotnet.Installation/IProgressReporter.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Dotnet.Installation;
+
+public interface IProgressTarget
+{
+    public IProgressReporter CreateProgressReporter();
+}
+
+public interface IProgressReporter : IDisposable
+{
+    public IProgressTask AddTask(string description, double maxValue);
+}
+
+public interface IProgressTask
+{
+    double Value { get; set; }
+}
+
+public class NullProgressTarget : IProgressTarget
+{
+    public IProgressReporter CreateProgressReporter() => new NullProgressReporter();
+    class NullProgressReporter : IProgressReporter
+    {
+        public void Dispose()
+        {
+        }
+        public IProgressTask AddTask(string description, double maxValue)
+        {
+            return new NullProgressTask();
+        }
+    }
+    class NullProgressTask : IProgressTask
+    {
+        public double Value { get; set; }
+    }
+}
