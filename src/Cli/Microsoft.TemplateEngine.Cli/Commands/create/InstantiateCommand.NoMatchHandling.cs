95d879fc497c77479eb0f6abc064e8b69cef51fd
Tomas Matousek <tomat@microsoft.com>
2025-11-12 17:17:51 -0800

    Template Engine CLI commands: separate definitions from implementations

--- src/Cli/Microsoft.TemplateEngine.Cli/Commands/create/InstantiateCommand.NoMatchHandling.cs
+++ src/Cli/Microsoft.TemplateEngine.Cli/Commands/create/InstantiateCommand.NoMatchHandling.cs
@@ -153,7 +153,7 @@ namespace Microsoft.TemplateEngine.Cli.Commands
                 reporter.WriteLine(LocalizableStrings.InvalidParameterTemplateHint);
                 var example = Example
                     .For<NewCommand>(args.ParseResult)
-                    .WithArgument(NewCommand.ShortNameArgument, templateGroup.ShortNames[0]);
+                    .WithArgument(CommandDefinition.New.ShortNameArgument, templateGroup.ShortNames[0]);
                 var language = matchInfos.Where(mi => mi.Language != null).FirstOrDefault()?.Language;
                 if (language != null)
                 {
