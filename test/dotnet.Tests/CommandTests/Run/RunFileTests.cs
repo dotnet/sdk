fe2de7c6188ff293377abbc906152eabfaa1f25a
Jan Jones <janjones@microsoft.com>
2025-11-20 20:14:54 +0100

    Fix temp path on macOS

--- test/dotnet.Tests/CommandTests/Run/RunFileTests.cs
+++ test/dotnet.Tests/CommandTests/Run/RunFileTests.cs
@@ -885,7 +885,7 @@ public sealed class RunFileTests(ITestOutputHelper log) : SdkTest(log)
 
         File.WriteAllText(programPath, code);
 
-        var workDir = Path.GetTempPath().TrimEnd(Path.DirectorySeparatorChar);
+        var workDir = TestPathUtility.ResolveTempPrefixLink(Path.GetTempPath()).TrimEnd(Path.DirectorySeparatorChar);
 
         var artifactsDir = VirtualProjectBuildingCommand.GetArtifactsPath(programPath);
         if (Directory.Exists(artifactsDir)) Directory.Delete(artifactsDir, recursive: true);
@@ -934,7 +934,7 @@ public sealed class RunFileTests(ITestOutputHelper log) : SdkTest(log)
 
         File.WriteAllText(programPath, code);
 
-        var workDir = Path.GetTempPath().TrimEnd(Path.DirectorySeparatorChar);
+        var workDir = TestPathUtility.ResolveTempPrefixLink(Path.GetTempPath()).TrimEnd(Path.DirectorySeparatorChar);
 
         var artifactsDir = VirtualProjectBuildingCommand.GetArtifactsPath(programPath);
         if (Directory.Exists(artifactsDir)) Directory.Delete(artifactsDir, recursive: true);
