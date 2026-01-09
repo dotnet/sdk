0e6f777f8d1cac7071997d4346942c3ec91d0acc
Marc Paine <marcpop@microsoft.com>
2026-01-08 17:02:16 -0800

    Fix bad merge

--- src/Cli/dotnet/Commands/Package/Search/PackageSearchCommand.cs
+++ src/Cli/dotnet/Commands/Package/Search/PackageSearchCommand.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.CommandLine;
 using Microsoft.DotNet.Cli.Commands.NuGet;
 using Microsoft.DotNet.Cli.CommandLine;
 using Microsoft.DotNet.Cli.Extensions;
