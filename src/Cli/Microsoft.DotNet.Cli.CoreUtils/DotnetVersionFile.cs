128c0d1593cead2db2cd35b614c378e7b3657762
Tomas Matousek <tomat@microsoft.com>
2026-01-09 09:39:09 -0800

    Fixes

--- src/Cli/Microsoft.DotNet.Cli.CoreUtils/DotnetVersionFile.cs
+++ src/Cli/Microsoft.DotNet.Cli.CoreUtils/DotnetVersionFile.cs
@@ -1,9 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if NET
+
 namespace Microsoft.DotNet.Cli.Utils;
 
-internal class DotnetVersionFile
+public class DotnetVersionFile
 {
     public bool Exists { get; init; }
 
@@ -67,3 +69,5 @@ internal class DotnetVersionFile
         }
     }
 }
+
+#endif
