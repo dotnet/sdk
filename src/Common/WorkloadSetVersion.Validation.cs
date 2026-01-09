ed0b27f1df2bca146391afdb9aabe89440148108
Tomas Matousek <tomat@microsoft.com>
2026-01-09 08:23:57 -0800

    WorkloadSetVersion

--- src/Common/WorkloadSetVersion.Validation.cs
+++ src/Common/WorkloadSetVersion.Validation.cs
@@ -1,11 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.Deployment.DotNet.Releases;
+using Microsoft.NET.Sdk.WorkloadManifestReader;
+
 namespace Microsoft.DotNet.Cli;
 
-internal static class WorkloadSetVersion
+internal static partial class WorkloadSetVersion
 {
-    public static string[] SeparateCoreComponents(string workloadSetVersion, out string[] sections)
+    private static string[] SeparateCoreComponents(string workloadSetVersion, out string[] sections)
     {
         sections = workloadSetVersion.Split(['-', '+'], 2);
         if (sections.Length < 1)
