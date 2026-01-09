f3173f495f7a22fec0d67746bd288db0abb28998
Tomas Matousek <tomat@microsoft.com>
2026-01-08 16:37:01 -0800

    GracefulException

--- src/Cli/Microsoft.DotNet.Cli.CoreUtils/GracefulException.cs
+++ src/Cli/Microsoft.DotNet.Cli.CoreUtils/GracefulException.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.DotNet.Cli.Utils.Extensions;
-
 namespace Microsoft.DotNet.Cli.Utils;
 
 public class GracefulException : Exception
