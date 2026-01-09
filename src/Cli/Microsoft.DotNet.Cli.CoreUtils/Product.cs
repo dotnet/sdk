9f633a2e8e66ad9428f59c07932f001783ffb45a
Tomas Matousek <tomat@microsoft.com>
2026-01-09 09:39:09 -0800

    Fixes

--- src/Cli/Microsoft.DotNet.Cli.CoreUtils/Product.cs
+++ src/Cli/Microsoft.DotNet.Cli.CoreUtils/Product.cs
@@ -1,13 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if NET
+
 using System.Reflection;
 
 namespace Microsoft.DotNet.Cli.Utils;
 
 public static class Product
 {
-    public static string LongName => LocalizableStrings.DotNetSdkInfo;
     public static readonly string Version;
     public static readonly string TargetFrameworkVersion;
 
@@ -34,3 +35,5 @@ public static class Product
         }
     }
 }
+
+#endif
