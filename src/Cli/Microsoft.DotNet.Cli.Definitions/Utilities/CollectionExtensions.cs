679f4d5c969b1f0fe0fc9158764c19399d89fce7
Tomáš Matoušek <tmat@users.noreply.github.com>
2026-01-21 17:59:27 -0800

    Move New command definitions to CommandDefinitions project (#52526)

--- src/Cli/Microsoft.DotNet.Cli.Definitions/Utilities/CollectionExtensions.cs
+++ src/Cli/Microsoft.DotNet.Cli.Definitions/Utilities/CollectionExtensions.cs
@@ -1,9 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.TemplateEngine.Cli;
+namespace Microsoft.DotNet.Cli;
 
-internal static class CollectionExtensions
+public static class CollectionExtensions
 {
     public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
     {
