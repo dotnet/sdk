33a348d3be4f542aca23d5da43f5b3e39639f0b0
Michael Simons <msimons@microsoft.com>
2026-01-30 22:04:15 +0000

    Deduplicate files using links in mac and linux installers

--- test/sdk-tasks.Tests/DeduplicateAssembliesWithLinksTests.cs
+++ test/sdk-tasks.Tests/DeduplicateAssembliesWithLinksTests.cs
@@ -322,24 +322,5 @@ public class DeduplicateAssembliesWithLinksTests(ITestOutputHelper log) : SdkTes
         public uint nFileIndexHigh;
         public uint nFileIndexLow;
     }
-
-    private class MockBuildEngine : IBuildEngine
-    {
-        public bool ContinueOnError => false;
-        public int LineNumberOfTaskNode => 0;
-        public int ColumnNumberOfTaskNode => 0;
-        public string ProjectFileOfTaskNode => string.Empty;
-
-        public bool BuildProjectFile(string projectFileName, string[] targetNames,
-            System.Collections.IDictionary globalProperties, System.Collections.IDictionary targetOutputs)
-        {
-            return true;
-        }
-
-        public void LogCustomEvent(CustomBuildEventArgs e) { }
-        public void LogErrorEvent(BuildErrorEventArgs e) { }
-        public void LogMessageEvent(BuildMessageEventArgs e) { }
-        public void LogWarningEvent(BuildWarningEventArgs e) { }
-    }
 #endif
 }
