1fef0e92a4cb31b1cf675d87fe11bf72f837e8a8
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2026-02-06 11:07:22 -0800

    Fix VMR break
    To help mitigate breaking again I was a bit more aggressive about excluding Windows only stuff.

---
+++ src/Cli/dotnet/Commands/Workload/SignCheck_Unix.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.DotNet.Cli.Commands.Workload;
+
+internal class SignCheck
+{
+    // Does not apply to Unix.
+    public static bool IsDotNetSigned() => false;
+}
