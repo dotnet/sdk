29b5fa38f596db02fb53733c8ffa85297fb2433a
Alexander Köplinger <alex.koeplinger@outlook.com>
2026-02-12 15:48:56 +0100

    Fix merge

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToFloatWarningLevels.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToFloatWarningLevels.cs
@@ -158,12 +158,12 @@ namespace Microsoft.NET.Build.Tests
             buildResult.StdErr.Should().Be(string.Empty);
         }
 
-        [InlineData(ToolsetInfo.CurrentTargetFramework, ToolsetInfo.NextTargetFrameworkVersion)]
+        [InlineData(ToolsetInfo.CurrentTargetFramework)]
         // Fixing this test requires bumping _LatestAnalysisLevel and _PreviewAnalysisLevel
         // Bumping will cause It_maps_analysis_properties_to_globalconfig to fail which requires changes in dotnet/roslyn-analyzers repo.
         // See instructions in the comment in It_maps_analysis_properties_to_globalconfig
         [RequiresMSBuildVersionTheory("16.8")]
-        public void It_defaults_preview_AnalysisLevel_to_the_next_tfm(string currentTFM, string nextTFMVersionNumber)
+        public void It_defaults_preview_AnalysisLevel_to_the_next_tfm(string currentTFM)
         {
             var testProject = new TestProject
             {
