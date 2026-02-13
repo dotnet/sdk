d2e55aeefa15f7fcfb30a71bd388a171d38b2028
Tomas Matousek <tomat@microsoft.com>
2026-02-12 18:47:03 -0800

    TEST: migration

--- test/TestAssets/TestProjects/WatchAspire/WatchAspire.MigrationService/Program.cs
+++ test/TestAssets/TestProjects/WatchAspire/WatchAspire.MigrationService/Program.cs
@@ -1,5 +1,24 @@
 using MigrationService;
 using Microsoft.Extensions.Hosting;
+using System.IO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+var dir = Environment.GetEnvironmentVariable("HELIX_WORKITEM_UPLOAD_ROOT") is { } ciOutputRoot
+    ? Path.Combine(ciOutputRoot, ".hotreload", "AspireMigration")
+    : "AspireMigration.hotreload";
+
+Directory.CreateDirectory(dir);
+
+var d = new List<string>();
+foreach (DictionaryEntry e in Environment.GetEnvironmentVariables())
+{
+    d.Add($"{e.Key}={e.Value}");
+}
+
+File.WriteAllLines(Path.Combine(dir, "env.txt"), d);
 
 var builder = Host.CreateApplicationBuilder(args);
 
