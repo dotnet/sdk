b5a4051f44b3246ce27dd11e2e497d06987b35d0
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2026-02-19 10:58:59 +0100

    Consolidate duplicate VSHostObject implementations (#53028)

--- src/Containers/Microsoft.NET.Build.Containers/Tasks/CreateNewImage.cs
+++ src/Containers/Microsoft.NET.Build.Containers/Tasks/CreateNewImage.cs
@@ -5,6 +5,7 @@ using Microsoft.Build.Framework;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.MSBuild;
 using Microsoft.NET.Build.Containers.Resources;
+using Microsoft.NET.Sdk.Common;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Microsoft.NET.Build.Containers.Tasks;
--- src/Containers/Microsoft.NET.Build.Containers/Tasks/CreateNewImageToolTask.cs
+++ src/Containers/Microsoft.NET.Build.Containers/Tasks/CreateNewImageToolTask.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Microsoft.NET.Build.Containers.Resources;
+using Microsoft.NET.Sdk.Common;
 
 namespace Microsoft.NET.Build.Containers.Tasks;
 
