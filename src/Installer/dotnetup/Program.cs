f1726d19be13fcf82d748f8cd598ad972b393d77
Noah Gilson <noahgilson@microsoft.com>
2026-03-11 10:58:13 -0700

    fix format

--- src/Installer/dotnetup/Program.cs
+++ src/Installer/dotnetup/Program.cs
@@ -66,7 +66,7 @@ internal class DotnetupProgram
             // telemetry to disk and sends it asynchronously.
             try
             {
-                Task.Run(() => DotnetupTelemetry.Instance.Dispose()).Wait(TimeSpan.FromMilliseconds(100));
+                Task.Run(DotnetupTelemetry.Instance.Dispose).Wait(TimeSpan.FromMilliseconds(100));
             }
             catch
             {
