2176c0bfa238a92d8abdf9fd17eb849370b5e0e7
ZakirMemonTangentia <mmemon@tangentia.com>
2026-03-15 21:45:46 -0400

    Use index expression for CA1826 Last() code fix in C# 8+

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.CSharp.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/CSharpDoNotUseEnumerableMethodsOnIndexableCollectionsInsteadUseTheCollectionDirectlyFixer.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Microsoft.CodeAnalysis.CSharp.NetAnalyzers/Microsoft.NetCore.Analyzers/Runtime/CSharpDoNotUseEnumerableMethodsOnIndexableCollectionsInsteadUseTheCollectionDirectlyFixer.cs
@@ -2,16 +2,39 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Composition;
+using Analyzer.Utilities;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
 using Microsoft.NetCore.Analyzers.Runtime;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Microsoft.NetCore.CSharp.Analyzers.Runtime
 {
     [ExportCodeFixProvider(LanguageNames.CSharp), Shared]
     public sealed class CSharpDoNotUseEnumerableMethodsOnIndexableCollectionsInsteadUseTheCollectionDirectlyFixer : DoNotUseEnumerableMethodsOnIndexableCollectionsInsteadUseTheCollectionDirectlyFixer
     {
+        private protected sealed override SyntaxNode CreateLastElementAccessExpression(Document document,SyntaxGenerator generator,SyntaxNode adjustedCollectionSyntaxNoTrailingTrivia,SyntaxNode collectionSyntax)
+        {
+            if (document.Project.ParseOptions is CSharpParseOptions { LanguageVersion: >= LanguageVersion.CSharp8 })
+            {
+                var expression = (ExpressionSyntax)adjustedCollectionSyntaxNoTrailingTrivia;
+
+                var indexExpression = PrefixUnaryExpression(
+                    SyntaxKind.IndexExpression,
+                    LiteralExpression(SyntaxKind.NumericLiteralExpression, Literal(1)));
+
+                return ElementAccessExpression(
+                    expression,
+                    BracketedArgumentList(
+                        SingletonSeparatedList(
+                            Argument(indexExpression))));
+            }
+
+            return base.CreateLastElementAccessExpression(document, generator, adjustedCollectionSyntaxNoTrailingTrivia, collectionSyntax);
+        }
         private protected sealed override SyntaxNode? AdjustSyntaxNode(SyntaxNode? syntaxNode)
         {
             if (syntaxNode?.Parent.IsKind(SyntaxKind.SuppressNullableWarningExpression) == true)
