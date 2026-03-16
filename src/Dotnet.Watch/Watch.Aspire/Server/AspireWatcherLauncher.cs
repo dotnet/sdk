1015ea7749f58121ed4815a3133ccb79b439ae32
tmat <tomas.matousek@microsoft.com>
2026-03-16 12:02:11 -0700

    Cleanup

--- src/Dotnet.Watch/Watch.Aspire/Server/AspireWatcherLauncher.cs
+++ src/Dotnet.Watch/Watch.Aspire/Server/AspireWatcherLauncher.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Immutable;
-using Microsoft.CodeAnalysis.Elfie.Diagnostics;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.DotNet.Watch;
