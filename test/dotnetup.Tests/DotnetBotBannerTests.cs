d2ddb0792e96078845254e8a7a250b9a04d05b02
Noah Gilson <noahgilson@microsoft.com>
2026-03-20 16:13:37 -0700

    remove test which does not test anything

--- test/dotnetup.Tests/DotnetBotBannerTests.cs
+++ test/dotnetup.Tests/DotnetBotBannerTests.cs
@@ -37,49 +37,4 @@ public class DotnetBotBannerTests
         output.Should().Contain("╭");
         output.Should().Contain("╰");
     }
-
-    [Fact]
-    public void Panel_DoesNotContainBotArt()
-    {
-        var panel = DotnetBotBanner.BuildPanel();
-        string output = RenderPanel(panel, 80);
-
-        output.Should().NotContain("▄█▄");
-        output.Should().NotContain("▀█▀");
-        output.Should().NotContain("■");
-    }
-
-    [Fact]
-    public void Panel_IsSizedToContent()
-    {
-        var panel = DotnetBotBanner.BuildPanel();
-        string output = RenderPanel(panel, 100);
-
-        // Panel should auto-size to content (not expand to terminal width).
-        string firstLine = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)[0];
-        firstLine.Length.Should().BeLessThan(100);
-        firstLine.Length.Should().BeGreaterThan(0);
-    }
-
-    [Fact]
-    public void Panel_AllBorderLinesAligned()
-    {
-        var panel = DotnetBotBanner.BuildPanel();
-        string output = RenderPanel(panel, 80);
-
-        string[] lines = output.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-        lines.Should().HaveCountGreaterThan(2);
-
-        char[] leftBorders = ['╭', '│', '╰'];
-        char[] rightBorders = ['╮', '│', '╯'];
-
-        foreach (string line in lines)
-        {
-            line.Should().NotBeEmpty();
-            leftBorders.Should().Contain(line[0],
-                because: "each line should start with a left border character");
-            rightBorders.Should().Contain(line[^1],
-                because: "each line should end with a right border character");
-        }
-    }
 }
