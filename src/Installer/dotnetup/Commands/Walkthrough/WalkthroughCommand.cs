15d4c9e0c1c9e8edf044d86839696223750415d3
Noah Gilson <noahgilson@microsoft.com>
2026-03-20 12:22:20 -0700

    walkthrough command simplification - inherit
    we don't really need these options per se? But I dont mind it and it makes the code cleaner

--- src/Installer/dotnetup/Commands/Walkthrough/WalkthroughCommand.cs
+++ src/Installer/dotnetup/Commands/Walkthrough/WalkthroughCommand.cs
@@ -2,11 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
-using System.Globalization;
-using Microsoft.Dotnet.Installation.Internal;
 using Microsoft.DotNet.Tools.Bootstrapper.Commands.Shared;
-using Spectre.Console;
-using SpectreAnsiConsole = Spectre.Console.AnsiConsole;
 
 namespace Microsoft.DotNet.Tools.Bootstrapper.Commands.Walkthrough;
 
@@ -14,22 +10,14 @@ namespace Microsoft.DotNet.Tools.Bootstrapper.Commands.Walkthrough;
 /// Runs the interactive walkthrough that installs the .NET SDK with defaults
 /// and records the user's path replacement preference to <c>dotnetup.config.json</c>.
 /// </summary>
-internal class WalkthroughCommand(ParseResult result) : CommandBase(result)
+internal class WalkthroughCommand(ParseResult result) : InstallCommand(result)
 {
-    private readonly string? _installPath = result.GetValue(CommonOptions.InstallPathOption);
-    private readonly string? _manifestPath = result.GetValue(CommonOptions.ManifestPathOption);
-    private readonly bool _noProgress = result.GetValue(CommonOptions.NoProgressOption);
-    private readonly Verbosity _verbosity = result.GetValue(CommonOptions.VerbosityOption);
-    private readonly bool _requireMuxerUpdate = result.GetValue(CommonOptions.RequireMuxerUpdateOption);
-
-    private readonly DotnetInstallManager _dotnetInstaller = new();
-    private readonly ChannelVersionResolver _channelVersionResolver = new();
-
     protected override string GetCommandName() => "walkthrough";
 
     protected override int ExecuteCore()
     {
-        // Invoke the full walkthrough workflow passing in the given command options
+        var workflows = new WalkthroughWorkflows(DotnetInstaller, ChannelVersionResolver);
+        workflows.FullIntroductionWalkthrough(this);
         return 0;
     }
 }
