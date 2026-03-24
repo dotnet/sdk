3833de6eda30296e535d97f22b81d4c2486337be
Noah Gilson <noahgilson@microsoft.com>
2026-03-24 13:38:35 -0700

    style fix

--- src/Installer/dotnetup/IDotnetEnvironmentManager.cs
+++ src/Installer/dotnetup/IDotnetEnvironmentManager.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Dotnet.Installation.Internal;
-using Spectre.Console;
 
 namespace Microsoft.DotNet.Tools.Bootstrapper;
 
