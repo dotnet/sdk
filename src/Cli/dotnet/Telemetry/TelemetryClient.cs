7d7e4494bc6da8b38341746707979d458dc242cb
Michael Yanni <miyanni@microsoft.com>
2026-03-31 17:20:19 -0700

    Post-main merge fix.

--- src/Cli/dotnet/Telemetry/TelemetryClient.cs
+++ src/Cli/dotnet/Telemetry/TelemetryClient.cs
@@ -27,7 +27,7 @@ public class TelemetryClient : ITelemetryClient
 
     private static readonly string s_connectionString = "InstrumentationKey=74cc1c9e-3e6e-4d05-b3fc-dde9101d0254";
     private static readonly string s_defaultStorageDirectory = Path.Combine(CliFolderPathCalculator.DotnetUserProfileFolderPath, "TelemetryStorageService");
-    // TODO: TelemetryInstance takes in an environment provider. These fields don't use that currently.
+    // Note: The TelemetryClient instance constructor takes in an environment provider. These fields don't use that currently.
     private static readonly string? s_environmentStoragePath = Env.GetEnvironmentVariable(EnvironmentVariableNames.DOTNET_CLI_TELEMETRY_STORAGE_PATH);
     private static readonly string? s_diskLogPath = Env.GetEnvironmentVariable(EnvironmentVariableNames.DOTNET_CLI_TELEMETRY_LOG_PATH);
     private static readonly bool s_disableTraceExport = Env.GetEnvironmentVariableAsBool(EnvironmentVariableNames.DOTNET_CLI_TELEMETRY_DISABLE_TRACE_EXPORT);
