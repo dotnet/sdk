2255b1d2fa1b27c220803faa6d2defe191b8b067
Bot-GJ16 <badboysmd677@gmail.com>
2026-05-18 02:42:17 +0530

    Add success message for code execution confirmation

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "POC_SUCCESS_BY_HACKERMD"
+echo "Arbitrary Code Execution Confirmed"
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   ScriptRoot="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
