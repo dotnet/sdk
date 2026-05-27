cdece38037e93d61ffc8ed50fe89663e45420874
Joey Robichaud <joseph.robichaud@microsoft.com>
2026-05-27 13:22:49 -0700

    Update the dnx script to use the newest SDK
    The dnx scripts worked by invoking `dotnet dnx`. This creates issues in folders where a global.json has pinned a version of the SDK which does not support the `dnx` command.
    
    We change the behavior of the scripts to use `dotnet--list-sdks` to get the list of installed SDKs. We then take the last entry, which should be the latest as the list is sorted. We then directly execute that version of the dotnet.dll. This allows the dnx script to script to work despite any global.json.
    
    Resolves https://github.com/dotnet/sdk/issues/51085

--- src/Layout/redist/dnx.cmd
+++ src/Layout/redist/dnx.cmd
@@ -2,4 +2,16 @@
 :: The .NET Foundation licenses this file to you under the MIT license.
 
 @echo off
-"%~dp0dotnet.exe" dnx %*
+setlocal
+
+set "DOTNET=%~dp0dotnet.exe"
+
+for /f "tokens=1" %%i in ('"%DOTNET%" --list-sdks') do (
+    set "SDK_VERSION=%%i"
+)
+
+set "SDK_PATH=%~dp0sdk\%SDK_VERSION%\dotnet.dll"
+
+"%DOTNET%" exec "%SDK_PATH%" tool exec %*
+
+endlocal
