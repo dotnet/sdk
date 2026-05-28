46b37cd563278d3dbb19282ce8add5493fed777c
Daniel Plaisted <daplaist@microsoft.com>
2026-05-28 16:06:25 -0400

    Fix style error

--- src/Installer/Microsoft.Dotnet.Installation/Internal/DailyChannelResolver.cs
+++ src/Installer/Microsoft.Dotnet.Installation/Internal/DailyChannelResolver.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net;
-using System.Runtime.InteropServices;
 using Microsoft.Deployment.DotNet.Releases;
 
 namespace Microsoft.Dotnet.Installation.Internal;
