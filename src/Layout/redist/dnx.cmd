c742ee690afcc329d03fa08d0024f3c51e99e96b
Noah Gilson <noahgilson@microsoft.com>
2026-06-04 16:12:37 -0700

    Revert "Merge remote-tracking branch 'upstream/main' into nagilson-use-dotnetup-script-aka-ms-links"
    This reverts commit 3191266cebd8d0af323bd51f944996ea4fec99f9, reversing
    changes made to d151d25cd1b6c34e29f2fa6d427fff06ec97034a.

--- src/Layout/redist/dnx.cmd
+++ src/Layout/redist/dnx.cmd
@@ -2,16 +2,4 @@
 :: The .NET Foundation licenses this file to you under the MIT license.
 
 @echo off
-setlocal enableextensions
-
-set "DOTNET=%~dp0dotnet.exe"
-
-for /f "tokens=1" %%i in ('"%DOTNET%" --list-sdks') do (
-    set "SDK_VERSION=%%i"
-)
-
-set "SDK_PATH=%~dp0sdk\%SDK_VERSION%\dotnet.dll"
-
-"%DOTNET%" exec "%SDK_PATH%" dnx %*
-
-endlocal
+"%~dp0dotnet.exe" dnx %*
