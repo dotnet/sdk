45676bfb1dfafb4370107c87eedd5e234736da3e
Jan Jones <janjones@microsoft.com>
2026-08-03 14:01:35 +0200

    Recognize roslyn telemetry

--- src/Cli/dotnet/Commands/MSBuild/MSBuildLogger.cs
+++ src/Cli/dotnet/Commands/MSBuild/MSBuildLogger.cs
@@ -51,6 +51,11 @@ public sealed class MSBuildLogger : INodeLogger
     internal const string SdkContainerPublishErrorEventName = "sdk/container/publish/error";
 
     /// <summary>
+    /// Emitted by the Roslyn <c>Csc</c>/<c>Vbc</c> build task.
+    /// </summary>
+    internal const string RoslynCompilerCacheEventName = "roslyn/compilercache";
+
+    /// <summary>
     /// Stores aggregated telemetry data by event name and property name.
     /// </summary>
     /// <remarks>
@@ -211,6 +216,9 @@ public sealed class MSBuildLogger : INodeLogger
                 TrackEvent(telemetry, $"msbuild/{TasksDetailsTelemetryEventName}", args.Properties,
                     toBeHashed: []
                 );
+                break;
+            case RoslynCompilerCacheEventName:
+                TrackEvent(telemetry, $"msbuild/{RoslynCompilerCacheEventName}", args.Properties);
                 break;
             // Pass through events that don't need special handling
             case SdkTaskBaseCatchExceptionTelemetryEventName:
