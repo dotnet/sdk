0576be40f422154340c7cf34eba411cd095edbff
Jan Jones <janjones@microsoft.com>
2026-08-06 15:25:31 +0200

    Dispose

--- test/dotnet-format.UnitTests/Analyzers/ThirdPartyAnalyzerFormatterTests.cs
+++ test/dotnet-format.UnitTests/Analyzers/ThirdPartyAnalyzerFormatterTests.cs
@@ -34,7 +34,7 @@ namespace Microsoft.CodeAnalysis.Tools.Tests.Analyzers
 
                 // Load the analyzer_project into a MSBuildWorkspace.
                 var workspacePath = Path.Combine(TestProjectsPathHelper.GetProjectsDirectory(), s_analyzerProjectFilePath);
-                var loadedWorkspace = await MSBuildWorkspaceLoader.LoadAsync(workspacePath, WorkspaceType.Project, binaryLogPath: null, logWorkspaceWarnings: true, logger, targetFramework: null, CancellationToken.None);
+                using var loadedWorkspace = await MSBuildWorkspaceLoader.LoadAsync(workspacePath, WorkspaceType.Project, binaryLogPath: null, logWorkspaceWarnings: true, logger, targetFramework: null, CancellationToken.None);
 
                 TestOutputHelper.WriteLine(logger.GetLog());
 
