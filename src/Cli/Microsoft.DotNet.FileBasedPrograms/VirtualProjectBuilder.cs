2c822283dd7cc3a4efe52f350a26345d66f15d26
Jan Jones <janjones@microsoft.com>
2026-09-08 13:52:51 +0200

    File-based apps: improve legacy artifact support

--- src/Cli/Microsoft.DotNet.FileBasedPrograms/VirtualProjectBuilder.cs
+++ src/Cli/Microsoft.DotNet.FileBasedPrograms/VirtualProjectBuilder.cs
@@ -507,12 +507,9 @@ sealed class VirtualProjectBuilder
             // dotnet CLI has the latest SDK imported but other hosts like MSBuildWorkspace may not.
             if (_useLegacyArtifactsPath is null)
             {
-                var defaultArtifactsPathPropsImported = await project.GetPropertyValueAsync("_DefaultArtifactsPathPropsImported").ConfigureAwait(false);
-                var artifactsPathLocationType = await project.GetPropertyValueAsync("_ArtifactsPathLocationType").ConfigureAwait(false);
+                var defaultArtifactsPathPropsImported = await project.GetPropertyValueAsync("_SupportsFileBasedAppArtifactsPath").ConfigureAwait(false);
 
-                _useLegacyArtifactsPath =
-                    string.Equals(defaultArtifactsPathPropsImported, bool.TrueString, StringComparison.OrdinalIgnoreCase) &&
-                    string.IsNullOrEmpty(artifactsPathLocationType);
+                _useLegacyArtifactsPath = !string.Equals(defaultArtifactsPathPropsImported, bool.TrueString, StringComparison.OrdinalIgnoreCase);
 
                 if (_useLegacyArtifactsPath == true)
                 {
