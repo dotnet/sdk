95de5d427eef28b71e2fb41b428ef976598222c6
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2026-09-08 16:19:05 -0700

    Reduce allocations in template matching tries
    Store the common single-child case inline and materialize the public dictionary only when branching or requested.

--- src/TemplateEngine/Microsoft.TemplateEngine.Core/Matching/Trie.cs
+++ src/TemplateEngine/Microsoft.TemplateEngine.Core/Matching/Trie.cs
@@ -6,12 +6,33 @@ namespace Microsoft.TemplateEngine.Core.Matching
     public class Trie<T>
         where T : TerminalBase
     {
+        private byte _singleNextNodeMatch;
+        private TrieNode<T>? _singleNextNode;
+        private Dictionary<byte, TrieNode<T>>? _nextNodes;
+
         public Trie()
         {
-            NextNodes = new Dictionary<byte, TrieNode<T>>();
         }
 
-        public Dictionary<byte, TrieNode<T>> NextNodes { get; }
+        public Dictionary<byte, TrieNode<T>> NextNodes
+        {
+            get
+            {
+                Dictionary<byte, TrieNode<T>>? nextNodes = _nextNodes;
+                if (nextNodes != null)
+                {
+                    return nextNodes;
+                }
+
+                nextNodes = new Dictionary<byte, TrieNode<T>>();
+                if (_singleNextNode != null)
+                {
+                    nextNodes.Add(_singleNextNodeMatch, _singleNextNode);
+                }
+
+                return Interlocked.CompareExchange(ref _nextNodes, nextNodes, null) ?? nextNodes;
+            }
+        }
 
         public int MaxRemainingLength { get; private set; }
 
@@ -23,23 +44,10 @@ namespace Microsoft.TemplateEngine.Core.Matching
             }
 
             int remainingLength = path.Length - 1;
-            Dictionary<byte, TrieNode<T>>? current = NextNodes;
+            Trie<T> current = this;
             for (int i = 0; i < path.Length; ++i, --remainingLength)
             {
-                if (!current.TryGetValue(path[i], out TrieNode<T> next))
-                {
-                    current[path[i]] = next = new TrieNode<T>(path[i])
-                    {
-                        MaxRemainingLength = remainingLength
-                    };
-                }
-                else
-                {
-                    if (next.MaxRemainingLength < remainingLength)
-                    {
-                        next.MaxRemainingLength = remainingLength;
-                    }
-                }
+                TrieNode<T> next = current.GetOrAddNextNode(path[i], remainingLength);
 
                 if (i == path.Length - 1)
                 {
@@ -58,8 +66,58 @@ namespace Microsoft.TemplateEngine.Core.Matching
                     }
                 }
 
-                current = next.NextNodes;
+                current = next;
             }
+        }
+
+        internal bool TryGetNextNode(byte match, out TrieNode<T> next)
+        {
+            if (_nextNodes != null)
+            {
+                return _nextNodes.TryGetValue(match, out next!);
+            }
+
+            if (_singleNextNode != null && _singleNextNodeMatch == match)
+            {
+                next = _singleNextNode;
+                return true;
+            }
+
+            next = null!;
+            return false;
+        }
+
+        private TrieNode<T> GetOrAddNextNode(byte match, int remainingLength)
+        {
+            TrieNode<T> next;
+            if (_nextNodes != null)
+            {
+                if (!_nextNodes.TryGetValue(match, out next!))
+                {
+                    _nextNodes.Add(match, next = new TrieNode<T>(match));
+                }
+            }
+            else if (_singleNextNode == null)
+            {
+                _singleNextNodeMatch = match;
+                _singleNextNode = next = new TrieNode<T>(match);
+            }
+            else if (_singleNextNodeMatch == match)
+            {
+                next = _singleNextNode;
+            }
+            else
+            {
+                next = new TrieNode<T>(match);
+                NextNodes.Add(match, next);
+            }
+
+            if (next.MaxRemainingLength < remainingLength)
+            {
+                next.MaxRemainingLength = remainingLength;
+            }
+
+            return next;
         }
     }
 }
