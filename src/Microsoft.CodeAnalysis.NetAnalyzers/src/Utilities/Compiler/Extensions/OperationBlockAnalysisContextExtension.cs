c3f5d4d01678edd35f26d3c7ff715b65c2bed9fc
DoctorKrolic <70431552+DoctorKrolic@users.noreply.github.com>
2026-09-15 10:48:06 +0300

    Remove unnecessary compiler defines (#56146)

--- src/Microsoft.CodeAnalysis.NetAnalyzers/src/Utilities/Compiler/Extensions/OperationBlockAnalysisContextExtension.cs
+++ src/Microsoft.CodeAnalysis.NetAnalyzers/src/Utilities/Compiler/Extensions/OperationBlockAnalysisContextExtension.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if HAS_IOPERATION
-
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
@@ -74,5 +72,3 @@ namespace Analyzer.Utilities.Extensions
         }
     }
 }
-
-#endif
